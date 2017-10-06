4f26fbbc96f15d3637f22e0fa887f8112bb2138f
Yudai Udagawa <nimius.rd@gmail.com>
2017-10-06 18:10:39 +0900

    Add file

---
+++ tools/build.js
@@ -0,0 +1,70 @@
+const fs = require('fs')
+const path = require('path')
+const postcss = require('postcss')
+const nested = require('postcss-nested')
+const autoprefixer = require('autoprefixer')
+
+function mkdir(dir) {
+  return new Promise((resolve, reject) => {
+    fs.stat(path.dirname(dir), (err, stats) => {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          fs.mkdir(path.dirname(dir), (err) => {
+            if (err) reject(err)
+          })
+        } else {
+          reject(err)
+        }
+      }
+      resolve();
+    })
+  })
+}
+
+function buildCSS(src, dest) {
+  fs.readFile(src, (err, css) => {
+    if (err) throw err
+    postcss([nested, autoprefixer])
+    .process(css, {
+      from: src,
+      to: dest
+    }).then(result => {
+      mkdir(dest)
+      .then(() => {
+        fs.writeFile(dest, result.css, err => {
+          if (err) throw err
+        })
+      })
+    }).catch(err => {
+      console.log(err)
+    })
+  })
+}
+
+function buildHTML(src, dest) {
+  fs.readFile(src, (err, data) => {
+    mkdir(dest)
+      .then(() => {
+        fs.writeFile(dest, data, err => {
+          if (err) throw err
+        })
+      })
+  })
+}
+
+function build() {
+  buildCSS('src/app.css', 'dest/app.css')
+  buildHTML('src/index.html', 'dest/index.html')
+}
+
+
+if (process.argv[2] === '-w') {
+  const watcher = fs.watch('./src', {recursive: true}, (eventType, filename) => {
+    if (filename) {
+      console.log(`${eventType}: ${filename}`)
+      build()
+    }
+  })
+} else {
+  build()
+}
