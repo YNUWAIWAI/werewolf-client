9259e4d5f3fdb98d335a0421a912ebee963037c8
Yudai Udagawa <nimius.rd@gmail.com>
2017-10-12 23:43:06 +0900

    Add clickHandler

--- src/app.js
+++ src/app.js
@@ -4,6 +4,15 @@ predictions.forEach(elem => {
   elem.addEventListener('click', handleClick)
 });
 
+document.getElementById('yes').addEventListener('click', toggleModal)
+document.getElementById('no').addEventListener('click', toggleModal)
+document.querySelectorAll('.command--option').forEach(elem => {
+  elem.addEventListener('click', e => {
+    console.log(e.target)
+    toggleModal()
+  })
+})
+
 function handleClick(e) {
   const state = ['o', 'x', 'tri', 'unk']
   const currentState = e.target.dataset['state']
@@ -11,3 +20,12 @@ function handleClick(e) {
   const nextState = state[nextIndex]
   e.target.dataset['state'] = nextState
 }
+
+function toggleModal() {
+  const obfucator = document.getElementById('obfucator')
+  const modal = document.getElementById('modal')
+  obfucator.classList.toggle('hidden')
+  modal.classList.toggle('hidden')
+}
+
+
