c31427e3f37e6650061220ff2ffa70b61b68149d
Yudai Udagawa <nimius.rd@gmail.com>
2017-10-13 11:28:36 +0900

    Fix JS

--- src/app.js
+++ src/app.js
@@ -1,31 +1,31 @@
-const predictions = document.querySelectorAll('.prediction > div[ data-state ]')
+const handleClick = e => {
+  const state = [ 'o', 'x', 'tri', 'unk' ]
+  const currentState = e.target.dataset.state
+  const nextIndex = (state.indexOf(currentState) + 1) % state.length
+  const nextState = state[nextIndex]
 
-predictions.forEach(elem => {
+  e.target.dataset.state = nextState
+}
+
+document.querySelectorAll('.prediction > div[ data-state ]').forEach(elem => {
   elem.addEventListener('click', handleClick)
-});
+})
+
+const toggleModal = () => {
+  const obfucator = document.getElementById('obfucator')
+  const modal = document.getElementById('modal')
+
+  obfucator.classList.toggle('hidden')
+  modal.classList.toggle('hidden')
+}
 
 document.getElementById('yes').addEventListener('click', toggleModal)
 document.getElementById('no').addEventListener('click', toggleModal)
 document.querySelectorAll('.command--option').forEach(elem => {
   elem.addEventListener('click', e => {
-    console.log(e.target)
     toggleModal()
   })
 })
 
-function handleClick(e) {
-  const state = ['o', 'x', 'tri', 'unk']
-  const currentState = e.target.dataset['state']
-  const nextIndex = (state.indexOf(currentState) + 1) % state.length
-  const nextState = state[nextIndex]
-  e.target.dataset['state'] = nextState
 }
-
-function toggleModal() {
-  const obfucator = document.getElementById('obfucator')
-  const modal = document.getElementById('modal')
-  obfucator.classList.toggle('hidden')
-  modal.classList.toggle('hidden')
-}
-
 
