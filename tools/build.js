40e20c6d3312ad54ef8d0ae506a491804af6ef57
Yudai Udagawa <nimius.rd@gmail.com>
2017-10-13 19:47:11 +0900

    Use glob

--- tools/build.js
+++ tools/build.js
@@ -3,74 +3,100 @@ const path = require('path')
 const postcss = require('postcss')
 const nested = require('postcss-nested')
 const autoprefixer = require('autoprefixer')
+const glob = require('glob')
 
-function mkdir(dir) {
+const mkdir = dir => {
   return new Promise((resolve, reject) => {
     fs.stat(path.dirname(dir), (err, stats) => {
       if (err) {
         if (err.code === 'ENOENT') {
-          fs.mkdir(path.dirname(dir), (err) => {
-            if (err) reject(err)
+          fs.mkdir(path.dirname(dir), err => {
+            if (err) {
+              reject(err)
+            }
           })
         } else {
           reject(err)
         }
       }
-      resolve();
+      resolve()
     })
   })
 }
 
-function buildCSS(src, dest) {
+const buildCSS = (src, dest) => {
   fs.readFile(src, (err, css) => {
-    if (err) throw err
-    postcss([nested, autoprefixer])
-    .process(css, {
-      from: src,
-      to: dest
-    }).then(result => {
-      mkdir(dest)
-      .then(() => {
-        fs.writeFile(dest, result.css, err => {
-          if (err) throw err
-        })
+    if (err) {
+      throw err
+    }
+    postcss([ nested, autoprefixer ])
+      .process(css, {
+        from: src,
+        to: dest
       })
-    }).catch(err => {
-      console.log(err)
-    })
+      .then(result => {
+        mkdir(dest)
+          .then(() => {
+            fs.writeFile(dest, result.css, err => {
+              if (err) {
+                throw err
+              }
+            })
+          })
+      })
+      .catch(err => {
+        console.log(err)
+      })
   })
 }
 
-function buildHTML(src, dest) {
+const buildHTML = (src, dest) => {
   fs.readFile(src, (err, data) => {
+    if (err) {
+      throw err
+    }
     mkdir(dest)
       .then(() => {
         fs.writeFile(dest, data, err => {
-          if (err) throw err
+          if (err) {
+            throw err
+          }s
         })
       })
   })
 }
 
-function buildJS(src, dest) {
+const buildJS = (src, dest) => {
   fs.readFile(src, (err, data) => {
+    if (err) {
+      throw err
+    }
     mkdir(dest)
       .then(() => {
         fs.writeFile(dest, data, err => {
-          if (err) throw err
+          if (err) {
+            throw err
+          }
         })
       })
   })
 }
 
-function build(destDir) {
+const build = destDir => {
   buildCSS('src/app.css', `${destDir}/app.css`)
   buildHTML('src/index.html', `${destDir}/index.html`)
-  buildJS('src/app.js', `${destDir}/app.js`)
+  glob('src/**/*.js', (err, files) => {
+    if (err) {
+      throw err
+    }
+    files.forEach(file => {
+      buildJS(file, `${destDir}/${path.relative('src', file)}`)
+    })
+  })
 }
 
 if (process.argv[2] === '-w') {
-  const watcher = fs.watch('./src', {recursive: true}, (eventType, filename) => {
+  fs.watch('./src', {recursive: true}, (eventType, filename) => {
     if (filename) {
       console.log(`${eventType}: ${filename}`)
       build('dest')
