cf8bd990e05b3b6d50d7e8ad8cc8319977408cf7
Yudai Udagawa <nimius.rd@gmail.com>
2017-10-13 14:37:45 +0900

    Add timer

--- src/app.js
+++ src/app.js
@@ -27,5 +27,40 @@ document.querySelectorAll('.command--option').forEach(elem => {
   })
 })
 
-}
+const timer = id => {
+  const node = document.getElementById(id)
+  const timerEvent = {
+    timerStart: new Event('timer-start'),
+    timerEnd: new Event('timer-end')
+  }
 
+  function tick(id) {
+    let [,minute,second] = (/残り(\d+)'(\d+)/).exec(node.textContent)
+    minute = Number(minute)
+    second = Number(second)
+
+    if (minute === 0 && second === 0) {
+      node.textContent = '終了'
+      return false
+    }
+
+    second--
+    if (second < 0) {
+      second = 59
+      minute--
+    } else if (second < 10) {
+      second = `0${second}`
+    }
+    node.textContent = `残り${minute}'${second}`
+
+    return true
+  }
+
+  node.dispatchEvent(timerEvent.timerStart)
+  const interval = window.setInterval(() => {
+    if (!tick(id)) {
+      clearInterval(interval)
+      node.dispatchEvent(timerEvent.timerEnd)
+    }
+  }, 1000)
+}
