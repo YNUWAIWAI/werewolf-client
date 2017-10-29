9687f1a1618458fcd052d7759f558b304f907a19
Yudai Udagawa <nimius.rd@gmail.com>
2017-10-29 12:05:53 +0900

    Fix style

--- src/module/prediction.js
+++ src/module/prediction.js
@@ -5,10 +5,12 @@ const initPredictionTable = () => {
   const roles = getAllRoles()
   const agents = getAllAgents()
   const table = {}
+
   agents.forEach(agent => {
     table[agent.id] = {}
     roles.forEach(role => {
       const id = /\/(\w+)$/.exec(role['@id'])[1]
+
       if (agent.agentIsMine && role.roleIsMine) {
         table[agent.id][id] = 'fix'
       } else if (agent.agentIsMine && !role.roleIsMine) {
@@ -23,12 +25,14 @@ const initPredictionTable = () => {
   predictionTable = table
 }
 
-const generatePredictionTable = () => new Promise((resolve, reject) => {
+const generatePredictionTable = () => {
   let dom = ['<div></div>']
   const roles = getAllRoles()
   const agents = getAllAgents()
+
   dom = dom.concat(roles.map(role => {
     const id = /\/(\w+)$/.exec(role['@id'])[1]
+
     return `
     <div data-role="${id}" data-tooltip="${role.name.ja}">
     <img src="${role.image}">
@@ -43,15 +47,19 @@ const generatePredictionTable = () => new Promise((resolve, reject) => {
         <span>${agent.name.ja}</span>
       </div>`]
     const d2 = roles.map(role => {
-      const id = /\/(\w+)$/.exec(role['@id'])[1]
+      const id = (/\/(\w+)$/).exec(role['@id'])[1]
       const state = predictionTable[agent.id][id]
+
       return `<div data-role="${id}" data-player="${agent.id}" data-state="${state}" data-date="1"></div>`
     })
+
     return d1.concat(d2)
   })
+
   dom = dom.concat(agentdom.reduce((a,b) => a.concat(b)))
   document.querySelector('.prediction').style.grid = `repeat(${1 + agents.length}, 1fr) / repeat(${1 + roles.length}, 1fr)`
-  resolve(dom.join(''))
-})
+
+  return dom.join('')
+}
 
 export {generatePredictionTable}
