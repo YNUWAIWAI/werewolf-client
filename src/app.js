f514766837dfce99b8d8e7e35f826608db61153e
Yudai Udagawa <nimius.rd@gmail.com>
2017-11-02 17:29:33 +0900

    Add form

--- src/app.js
+++ src/app.js
@@ -1,4 +1,19 @@
-import {connectWebSocket} from './module/websocket.js'
+import {connectWebSocket, send} from './module/websocket.js'
 const url = document.getElementById('script').dataset.url
 
 document.addEventListener('DOMContentLoaded', connectWebSocket(url))
+document.getElementById('public').addEventListener('click', e => {
+  const text = e.target.form[0].value
+
+  send(text)
+})
+document.getElementById('private').addEventListener('click', e => {
+  const text = e.target.form[0].value
+
+  send(text)
+})
+document.getElementById('limited').addEventListener('click', e => {
+  const text = e.target.form[0].value
+
+  send(text)
+})
