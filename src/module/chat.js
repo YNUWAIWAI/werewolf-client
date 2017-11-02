7d71e7d5ed325e45ec03e3890ebc78b504fb0ee4
Yudai Udagawa <nimius.rd@gmail.com>
2017-11-02 11:12:47 +0900

    Fix chatMessageGenerator

--- src/module/chat.js
+++ src/module/chat.js
@@ -1,21 +1,34 @@
+const channels = {
+  anonymousAudience: 'public',
+  grave: 'grave',
+  hunter: 'limited',
+  master: 'master',
+  onymousAudience: 'public',
+  private: 'private',
+  public: 'public',
+  seer: 'limited',
+  werewolf: 'limited'
+}
+
 const parseChat = text => text
 
 const parseTime = time => time
 
 const generateChatMessage = json =>
-  `<div id="message4" class="chat--item private">
+  `<div id="message4" class="chat--item ${json.chatIsMine ? 'me' : ''} ${channels[json.intensionalDisclosureRange]}">
     <div class="chat--arrow-box">
-      <div class="chat--num">${json.chatId}</div>
-    <div class="chat--text">
-      ${parseChat(json.chatText)}
+      <div class="chat--num">${json.chatId || ''}</div>
+      <div class="chat--text">
+        ${parseChat(json.chatText)}
+      </div>
+      <div class="chat--date">
+        ${parseTime(json.serverTimestamp)}
+      </div>
     </div>
-    <div class="chat--date">
-      ${parseTime(json.serverTimestamp)}
+    <div class="chat--icon">
+      <img src="${json.chatAgent ? json.chatAgent.chatAgentImage : json.chatUserAvatar}">
+      <span>${json.chatAgent ? json.chatAgent.chatAgentName.ja : json.chatUserName}</span>
     </div>
-  </div>
-  <div class="chat--icon">
-    <img src="${json.chatAgent.chatAgentImage}">
-    <span>${json.chatAgent.chatAgentName.ja}</span>
-  </div>`
+  <div>`
 
 export {generateChatMessage}
