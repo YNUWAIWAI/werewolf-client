624a62f06a65bc18668d67f860d2447691c0980a
Yudai Udagawa <nimius.rd@gmail.com>
2017-11-05 10:05:05 +0900

    Add error message when WebSocket is disconnected

--- src/module/websocket.js
+++ src/module/websocket.js
@@ -12,6 +12,14 @@ const connectWebSocket = url => {
   socket.addEventListener('open', event => {
     console.log('WebSocket Connected ', event)
   })
+  socket.addEventListener('close', event => {
+    console.log('WebSocket Disconnected ', event)
+    const obfucator = document.getElementById('obfucator')
+
+    obfucator.style = 'background: rgba(255, 0, 0, 0.85);font-size: 350px;line-height: 0.8;'
+    obfucator.textContent = 'WebSocket is DEAD'
+    obfucator.classList.remove('hidden')
+  })
   socket.addEventListener('error', error => {
     console.log('WebSocket Error ', error)
   })
