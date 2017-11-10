23998eed1a0f777ead2197b12eceecd3c8c3f184
Yudai Udagawa <nimius.rd@gmail.com>
2017-11-10 19:16:18 +0900

    Hide command container

--- src/app.js
+++ src/app.js
@@ -2,3 +2,9 @@ import {connectWebSocket} from './module/websocket.js'
 const url = document.getElementById('script').dataset.url
 
 document.addEventListener('DOMContentLoaded', connectWebSocket(url))
+document.getElementById('hide-button').addEventListener('click', e => {
+  document.getElementById('hide-button').classList.toggle('hide')
+  document.getElementById('hide-button-icon').classList.toggle('hide')
+  document.getElementById('activity-container').classList.toggle('expand')
+  document.getElementById('command').classList.toggle('hide')
+})
