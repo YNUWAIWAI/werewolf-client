befd969b23ef5c92d577e78124a359e1ab00fdf2
Yudai Udagawa <nimius.rd@gmail.com>
2017-12-01 15:32:38 +0900

    Fix build process

--- tools/build.js
+++ tools/build.js
@@ -4,7 +4,8 @@ const postcss = require('postcss')
 const nested = require('postcss-nested')
 const autoprefixer = require('autoprefixer')
 const glob = require('glob')
-const flowRemoveTypes = require('flow-remove-types')
+const webpack = require('webpack')
+const webpackConfig = require('../webpack.config.js')
 
 const mkdir = dir => {
   return new Promise((resolve, reject) => {
@@ -67,20 +68,9 @@ const buildHTML = (src, dest) => {
   })
 }
 
-const buildJS = (src, dest) => {
-  const input = fs.readFileSync(src, 'utf-8')
-  const output = flowRemoveTypes(input, { pretty: true })
-
-  mkdir(dest)
-    .then(() => {
-      fs.writeFileSync(dest, output.toString())
-    })
-}
-
 const build = destDir => {
   const CSSfiles = glob.sync('src/**/*.css')
   const HTMLfiles = glob.sync('src/**/*.html')
-  const JSfiles = glob.sync('src/**/*.js', { ignore: 'src/flow-typed/*.js' })
 
   CSSfiles.forEach(file => {
     buildCSS(file, `${destDir}/stylesheets/${path.relative('src/styles', file)}`)
@@ -89,11 +79,9 @@ const build = destDir => {
   HTMLfiles.forEach(file => {
     buildHTML(file, `${destDir}/../app/views/${path.parse(file).name}.scala.html`)
   })
-
-  JSfiles.forEach(file => {
-    buildJS(file, `${destDir}/javascripts/${path.relative('src/scripts', file)}`)
-  })
 }
+
+const compiler = webpack(webpackConfig)
 
 if (process.argv[2] === '-w') {
   build('../public')
@@ -103,8 +91,27 @@ if (process.argv[2] === '-w') {
       build('../public')
     }
   })
+  compiler.watch({}, (err, stats) => {
+    if (err) {
+      throw err
+    }
+  })
 } else if (process.argv[2] === '-d') {
   build('docs')
 } else {
   build('../public')
+  compiler.run((err, stats) => {
+    if (err) {
+      throw err
+    }
+    const info = stats.toJson()
+
+    if (stats.hasErrors()) {
+      console.error(info.errors)
+    }
+
+    if (stats.hasWarnings()) {
+      console.warn(info.warnings)
+    }
+  })
 }
