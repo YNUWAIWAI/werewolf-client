924c0ee7574531ea280d6de9700f9b61f29185b3
Yudai Udagawa <nimius.rd@gmail.com>
2018-02-10 23:35:36 +0900

    Add Info component

---
+++ src/scripts/components/Info.js
@@ -0,0 +1,13 @@
+import Day from './Day'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Role from './Role'
+
+export default function Info() {
+  return (
+    <div id="info" class="info">
+      <Day />
+      <Role />
+    </div>
+  )
+}
