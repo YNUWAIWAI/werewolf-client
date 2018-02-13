00f2b8324c9d69605bea8a1e8e74b72c3ce48a30
Yudai Udagawa <nimius.rd@gmail.com>
2018-02-13 14:04:51 +0900

    Tweak

--- src/scripts/components/CommandInput.js
+++ src/scripts/components/CommandInput.js
@@ -8,19 +8,14 @@ export default function CommandInput(props) {
     public: '公開用'
   })[props.kind])()
 
-  const PostCounter = () => {
-    if (props.kind !== 'private') {
-      return <span class="command--input--counter" id={`${props.kind}-counter`} data-counter="0">0/10</span>
-    }
-
-    return ''
-  }
-
   return (
     <form name={props.kind} class={`command--input ${props.kind}`}>
       <textarea id={`${props.kind}-textarea`} placeholder={placeholder}></textarea>
       <span id={`${props.kind}-char`} class="command--input--char">0</span>
-      <PostCounter />
+      {
+        props.kind === 'private' ||
+        <span class="command--input--counter" id={`${props.kind}-counter`} data-counter="0">0/10</span>
+      }
       <button id={`${props.kind}-button`} type="button">送信</button>
     </form>
   )
