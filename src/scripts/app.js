436657a2c27a0cdd34dbfa381a03fa6720f3ca0a
Yudai Udagawa <nimius.rd@gmail.com>
2018-02-13 13:22:45 +0900

    Change the order of execution

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,13 +1,3 @@
-import {connectWebSocket} from './module/websocket.js'
-const url = document.getElementById('script').dataset.url
-document.addEventListener('DOMContentLoaded', connectWebSocket(url))
-document.getElementById('hide-button').addEventListener('click', () => {
-  document.getElementById('hide-button').classList.toggle('hide')
-  document.getElementById('hide-button-icon').classList.toggle('hide')
-  document.getElementById('activity-container').classList.toggle('expand')
-  document.getElementById('command').classList.toggle('hide')
-})
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Info from './components/Info'
@@ -24,3 +14,13 @@ ReactDOM.render(
   </React.Fragment>,
   document.getElementById('root')
 )
+
+import {connectWebSocket} from './module/websocket.js'
+const url = document.getElementById('script').dataset.url
+document.addEventListener('DOMContentLoaded', connectWebSocket(url))
+document.getElementById('hide-button').addEventListener('click', () => {
+  document.getElementById('hide-button').classList.toggle('hide')
+  document.getElementById('hide-button-icon').classList.toggle('hide')
+  document.getElementById('activity-container').classList.toggle('expand')
+  document.getElementById('command').classList.toggle('hide')
+})
