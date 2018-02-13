e3574d141295c66b8dc3f108029843a649286aab
Yudai Udagawa <nimius.rd@gmail.com>
2018-02-13 13:18:01 +0900

    Add HideButton component

---
+++ src/scripts/components/HideButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default function HideButton(props) {
+  return (
+    <button id="hide-button" class="hide--button" type="button">
+      <span id="hide-button-icon" class="hide--button--icon"></span>
+    </button>
+  )
+}
