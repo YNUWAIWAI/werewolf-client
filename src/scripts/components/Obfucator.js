b569097ad9cb3a57e73fa489638133d6cb516475
Yudai Udagawa <nimius.rd@gmail.com>
2018-02-13 14:57:56 +0900

    Add Obfucator component and Result component

---
+++ src/scripts/components/Obfucator.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default function Obfucator(props) {
+  return (
+    <div id="obfucator" class="obfucator"></div>
+  )
+}
---
+++ src/scripts/components/Result.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default function Result(props) {
+  return (
+    <div id="result" class="result hidden"></div>
+  )
+}
