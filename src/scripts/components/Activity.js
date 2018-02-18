79d0849d4e73dfff3e40845eecd590a737fe8992
Yudai Udagawa <nimius.rd@gmail.com>
2018-02-18 12:20:38 +0900

    Remove unused module

--- src/scripts/components/Activity.js
+++ src/scripts/components/Activity.js
@@ -1,7 +1,6 @@
 import Chat from './Chat'
 import Prediction from './Prediction'
 import React from 'react'
-import ReactDOM from 'react-dom'
 
 export default function Activity(props) {
   return (
--- src/scripts/components/Info.js
+++ src/scripts/components/Info.js
@@ -1,6 +1,5 @@
 import Day from '../containers/DayContainer'
 import React from 'react'
-import ReactDOM from 'react-dom'
 import Role from '../containers/RoleContainer'
 
 export default function Info(props) {
