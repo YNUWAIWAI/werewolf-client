735b2745fbb8ee0f71661a81a6e13c95818b42d2
Yudai Udagawa <nimius.rd@gmail.com>
2018-02-22 10:05:48 +0900

    Fix Redux devtools

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,15 @@
+import {createStore} from 'redux'
 import App from './containers/App'
+import {Provider} from 'react-redux'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {createStore} from 'redux'
-import {Provider} from 'react-redux'
 import middleware from './middlewares'
 import reducer from './reducers'
+import {composeWithDevTools} from 'redux-devtools-extension'
 
 const store = createStore(
   reducer,
-  middleware
+  composeWithDevTools(middleware)
 )
 
 store.dispatch({
