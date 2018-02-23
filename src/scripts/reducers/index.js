bae7c721b8bd0b5aacc2869676b38f5eea68e4e2
Yudai Udagawa <nimius.rd@gmail.com>
2018-02-23 17:23:47 +0900

    Fix import

--- src/scripts/reducers/index.js
+++ src/scripts/reducers/index.js
@@ -1,4 +1,3 @@
-import c2s from './c2s'
 import chat from './chat'
 import base from './base'
 import mine from './mine'
@@ -7,7 +6,6 @@ import commandInput from './commandInput'
 import obfucator from './obfucator'
 
 const reducer = combineReducers({
-  // c2s,
   chat,
   commandInput,
   base,
