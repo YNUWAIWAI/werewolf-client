24e457124e7e71cd80372b0abe24c899f47bb2bd
Yudai Udagawa <nimius.rd@gmail.com>
2018-02-27 10:17:30 +0900

    PureComponent -> Component

--- src/scripts/components/CommandInput.js
+++ src/scripts/components/CommandInput.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-class CommandInput extends React.PureComponent {
+class CommandInput extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
