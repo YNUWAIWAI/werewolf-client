2f69c3efeb4a244649059601702eb6728e5e7ce8
Yudai Udagawa <nimius.rd@gmail.com>
2018-02-27 23:56:23 +0900

    Add Timer

--- src/scripts/components/Day.js
+++ src/scripts/components/Day.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Timer from '../containers/TimerContainer'
 
 export default function Day(props) {
   return (
     <div id="day" class="day">
       <div class="day--text">
         <span id="day-phase">{props.date}日目 {props.phase}</span>
-        <span id="day-time"></span>
+        <Timer id="day-time" phaseTimeLimit={props.phaseTimeLimit} />
       </div>
     </div>
   )
---
+++ src/scripts/containers/TimerContainer.js
@@ -0,0 +1,14 @@
+import Timer from '../components/Timer'
+import {connect} from 'react-redux'
+import {timer} from '../actions'
+
+const mapStateToProps = state => ({
+  limit: state.timer.phaseTimeLimit === -1 ? -1 : state.timer.phaseTimeLimit * 1000,
+  phase: state.timer.phase,
+  start: new Date(state.timer.phaseStartTime).getTime()
+})
+const TimerContainer = connect(
+  mapStateToProps,
+)(Timer)
+
+export default TimerContainer
