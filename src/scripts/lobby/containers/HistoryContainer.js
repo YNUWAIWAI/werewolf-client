77d1795e9a86cc917668b99a114506dd09882499
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-04 01:29:32 +0900

    Update History

--- src/scripts/lobby/containers/HistoryContainer.js
+++ src/scripts/lobby/containers/HistoryContainer.js
@@ -1,9 +1,14 @@
 import History from '../components/History'
 import {connect} from 'react-redux'
+import {selectVillage} from '../actions'
 
 const mapStateToProps = state => state.history
+const mapDispatchToProps = dispatch => ({
+  selectVillage: id => () => dispatch(selectVillage(id))
+})
 const HistoryContainer = connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(History)
 
 export default HistoryContainer
