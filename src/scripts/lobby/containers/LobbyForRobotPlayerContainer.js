dc8ddd3a6007ef4fe9d011ef5d6c3eac17248039
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-04 00:35:35 +0900

    Update Lobby for Robot Player

--- src/scripts/lobby/containers/LobbyForRobotPlayerContainer.js
+++ src/scripts/lobby/containers/LobbyForRobotPlayerContainer.js
@@ -1,9 +1,14 @@
 import LobbyForRobotPlayer from '../components/LobbyForRobotPlayer'
 import {connect} from 'react-redux'
+import {selectVillage} from '../actions'
 
 const mapStateToProps = state => state.lobbyForRobotPlayer
+const mapDispatchToProps = dispatch => ({
+  selectVillage: id => () => dispatch(selectVillage(id))
+})
 const LobbyForRobotPlayerContainer = connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(LobbyForRobotPlayer)
 
 export default LobbyForRobotPlayerContainer
