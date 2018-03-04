bc0035c7c9bd43332db484249975ab5fc83cb384
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-05 02:39:31 +0900

    Tweak

--- src/scripts/lobby/containers/SettingsContainer.js
+++ src/scripts/lobby/containers/SettingsContainer.js
@@ -1,6 +1,5 @@
 import Settings from '../components/Settings'
 import {connect} from 'react-redux'
-import {selectVillage} from '../actions'
 
 const mapStateToProps = state => state.settings
 const SettingContainer = connect(
