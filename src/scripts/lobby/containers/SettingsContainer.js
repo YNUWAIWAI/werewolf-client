d781fc0cca46281a495a62253442e2d1c456a7e2
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-05 02:36:46 +0900

    Remove unused action

--- src/scripts/lobby/containers/SettingsContainer.js
+++ src/scripts/lobby/containers/SettingsContainer.js
@@ -3,9 +3,6 @@ import {connect} from 'react-redux'
 import {selectVillage} from '../actions'
 
 const mapStateToProps = state => state.settings
-const mapDispatchToProps = dispatch => ({
-  selectVillage: id => () => dispatch(selectVillage(id))
-})
 const SettingContainer = connect(
   mapStateToProps,
   mapDispatchToProps
