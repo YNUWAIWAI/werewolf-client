34365235c57e1b1f3c573323ecf29aa1fe31cc46
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-05 02:39:12 +0900

    Tweak

--- src/scripts/lobby/containers/SettingsContainer.js
+++ src/scripts/lobby/containers/SettingsContainer.js
@@ -4,8 +4,7 @@ import {selectVillage} from '../actions'
 
 const mapStateToProps = state => state.settings
 const SettingContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
+  mapStateToProps
 )(Settings)
 
 export default SettingContainer
