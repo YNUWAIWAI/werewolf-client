ce6bc0ffda9dc8a85d342ed51ff0cc4a55cf4da9
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-05 02:48:51 +0900

    Fix typo

--- src/scripts/lobby/components/VillageList.js
+++ src/scripts/lobby/components/VillageList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import VillageItem from './VlillageItem'
+import VillageItem from './VillageItem'
 
 export default function VillageList(props) {
   const items = props.items.map(item => <VillageItem key={item.name} {... item} handleClick={props.selectVillage(item.id)} isPlayer={props.isPlayer} />)
