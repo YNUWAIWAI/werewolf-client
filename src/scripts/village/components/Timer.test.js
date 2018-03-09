00d7139eeeb4d2a211c250c9354f16ccf53d5529
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-09 13:18:26 +0900

    Update Timer and add test

---
+++ src/scripts/village/components/Timer.test.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Timer from './Timer'
+import {shallow} from 'enzyme'
+
+test('<Timer />', () => {
+  const wrapper = shallow(<Timer />)
+
+  expect(wrapper.find('FormattedTime')).toHaveLength(1)
+  expect(wrapper.state().time).toBe(0)
+})
+test('<Timer /> test tick()', () => {
+  const wrapper = shallow(<Timer />)
+
+  expect(wrapper.find('FormattedTime')).toHaveLength(1)
+  expect(wrapper.state().time).toBe(0)
+  wrapper.setState({
+    time: 1
+  })
+  expect(wrapper.state().time).toBe(1)
+  wrapper.instance().tick()
+  expect(wrapper.state().time).toBe(0)
+  wrapper.instance().tick()
+  expect(wrapper.state().time).toBe(0)
+})
+jest.useFakeTimers()
+test('<Timer /> test setInterval', () => {
+  const wrapper = shallow(<Timer />)
+
+  expect(wrapper.find('FormattedTime')).toHaveLength(1)
+  expect(wrapper.state().time).toBe(0)
+  wrapper.setProps({
+    limit: 2
+  })
+  expect(setInterval).toHaveBeenCalledTimes(1)
+  expect(setInterval).toHaveBeenLastCalledWith(wrapper.instance().tick, 1000)
+  expect(wrapper.state().time).toBe(2)
+})
--- src/scripts/village/containers/TimerContainer.js
+++ src/scripts/village/containers/TimerContainer.js
@@ -2,7 +2,7 @@ import Timer from '../components/Timer'
 import {connect} from 'react-redux'
 
 const mapStateToProps = state => ({
-  limit: state.timer.phaseTimeLimit === -1 ? -1 : state.timer.phaseTimeLimit * 1000,
+  limit: state.timer.phaseTimeLimit === -1 ? -1 : state.timer.phaseTimeLimit,
   phase: state.timer.phase,
   start: new Date(state.timer.phaseStartTime).getTime()
 })
