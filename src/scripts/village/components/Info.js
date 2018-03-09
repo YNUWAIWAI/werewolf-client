8d49ea25e4e843321d7d2d57cc504644c3f570d3
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-09 14:32:33 +0900

    Add Info test

--- src/scripts/village/components/Info.js
+++ src/scripts/village/components/Info.js
@@ -1,8 +1,11 @@
+// @flow
 import Day from '../containers/DayContainer'
 import React from 'react'
 import Role from '../containers/RoleContainer'
 
-export default function Info(props) {
+type Props = {}
+
+export default function Info(props: Props) {
   return (
     <div className="info" id="info">
       <Day />
