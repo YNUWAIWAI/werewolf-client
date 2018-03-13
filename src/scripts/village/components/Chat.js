a82b8903691e7027943afa8aad1e1e1f8f2f32f8
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-13 19:10:31 +0900

    Remove Unused id

--- src/scripts/village/components/Chat.js
+++ src/scripts/village/components/Chat.js
@@ -1,9 +1,14 @@
+// @flow
 import ChatItem from './ChatItem'
 import React from 'react'
+import type ChatItemProps from './ChatItem'
+type Props = {
+  items:
+}
 
 export default function Chat({items}) {
   return (
-    <div className="chat" id="chat">
+    <div className="chat">
       {
         items.map((item, index) =>
           <ChatItem
