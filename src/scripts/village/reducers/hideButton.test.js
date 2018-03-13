5f88db4f46cc993ae1b9813d181b6736853207e3
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-14 01:52:07 +0900

    Add hideButton test

---
+++ src/scripts/village/reducers/hideButton.test.js
@@ -0,0 +1,28 @@
+import * as ActionTypes from '../constants/ActionTypes'
+import reducer from './hideButton'
+
+test('initial state', () => {
+  expect(reducer(undefined, {})).toEqual({
+    hide: false
+  })
+})
+test('CLICK_HIDE_BUTTON hide={true}', () => {
+  const action = {
+    hide: true,
+    type: ActionTypes.CLICK_HIDE_BUTTON
+  }
+
+  expect(reducer(undefined, action)).toEqual({
+    hide: true
+  })
+})
+test('CLICK_HIDE_BUTTON hide={false}', () => {
+  const action = {
+    hide: false,
+    type: ActionTypes.CLICK_HIDE_BUTTON
+  }
+
+  expect(reducer(undefined, action)).toEqual({
+    hide: false
+  })
+})
