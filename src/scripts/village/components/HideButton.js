fe1b84414d1335e630d899c2fe8387bfa082f1ac
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-14 01:07:23 +0900

    Fix HideButton type and test

--- src/scripts/village/components/HideButton.js
+++ src/scripts/village/components/HideButton.js
@@ -2,18 +2,18 @@
 import React from 'react'
 
 type Props = {
-  handleClick: (isHide: boolean) => void,
-  isHide: boolean
+  handleClick: (hide: boolean) => void,
+  hide: boolean
 }
 
 export default function HideButton(props: Props) {
   const handleClick = () => {
-    props.handleClick(props.isHide)
+    props.handleClick(props.hide)
   }
 
   return (
-    <button className={`hide-button ${props.isHide ? 'hide' : ''}`} onClick={handleClick} type="button">
-      <span className={`hide-button--icon ${props.isHide ? 'hide' : ''}`} />
+    <button className={`hide-button ${props.hide ? 'hide' : ''}`} onClick={handleClick} type="button">
+      <span className={`hide-button--icon ${props.hide ? 'hide' : ''}`} />
     </button>
   )
 }
