b9b923b52b496feb5626c41d513a925e7a2db917
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-14 01:07:23 +0900

    Fix HideButton type and test

--- src/scripts/village/components/HideButton.test.js
+++ src/scripts/village/components/HideButton.test.js
@@ -1,29 +1,34 @@
+// @flow
 import HideButton from './HideButton'
 import React from 'react'
 import {shallow} from 'enzyme'
 
-test('<HideButton isHide />', () => {
-  const wrapper = shallow(<HideButton isHide />)
+test('<HideButton hide />', () => {
+  const mockFn = jest.fn()
+  const wrapper = shallow(<HideButton handleClick={mockFn} hide />)
 
   expect(wrapper.find('.hide-button').hasClass('hide')).toBe(true)
   expect(wrapper.find('.hide-button--icon').hasClass('hide')).toBe(true)
+  expect(mockFn).toHaveBeenCalledTimes(0)
 })
-test('<HideButton />', () => {
-  const wrapper = shallow(<HideButton />)
+test('<HideButton hide={false} />', () => {
+  const mockFn = jest.fn()
+  const wrapper = shallow(<HideButton handleClick={mockFn} hide={false} />)
 
   expect(wrapper.find('.hide-button').hasClass('hide')).toBe(false)
   expect(wrapper.find('.hide-button--icon').hasClass('hide')).toBe(false)
+  expect(mockFn).toHaveBeenCalledTimes(0)
 })
-test('<HideButton isHide /> onClick', () => {
-  const mockFn = jest.fn().mockName('handleClick')
-  const wrapper = shallow(<HideButton handleClick={mockFn} isHide />)
+test('<HideButton hide /> onClick', () => {
+  const mockFn = jest.fn()
+  const wrapper = shallow(<HideButton handleClick={mockFn} hide />)
 
   wrapper.simulate('click')
   expect(mockFn).toHaveBeenCalledWith(true)
 })
-test('<HideButton /> onClick', () => {
-  const mockFn = jest.fn().mockName('handleClick')
-  const wrapper = shallow(<HideButton handleClick={mockFn} isHide />)
+test('<HideButton hide={false} /> onClick', () => {
+  const mockFn = jest.fn()
+  const wrapper = shallow(<HideButton handleClick={mockFn} hide={false} />)
 
   wrapper.simulate('click')
   expect(mockFn).toHaveBeenCalledWith(true)
