c40eaa25147ce27315e2ee248d12ef2a7290bd6b
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-14 01:09:15 +0900

    isHide -> hide

--- src/scripts/village/reducers/hideButton.js
+++ src/scripts/village/reducers/hideButton.js
@@ -1,12 +1,12 @@
 import * as ActionTypes from '../constants/ActionTypes'
 
 const initialState = {
-  isHide: false
+  hide: false
 }
 const hideButton = (state = initialState, action) => {
   if (action.type === ActionTypes.CLICK_HIDE_BUTTON) {
     return {
-      isHide: action.isHide
+      hide: action.hide
     }
   }
 
