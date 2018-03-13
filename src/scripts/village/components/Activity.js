b69b3b80714ee887c8a18de634d28e266b360fe0
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-13 23:56:35 +0900

    Add Activity type and test

--- src/scripts/village/components/Activity.js
+++ src/scripts/village/components/Activity.js
@@ -1,10 +1,15 @@
+// @flow
 import Chat from '../containers/ChatContainer'
 import Prediction from '../containers/PredictionContainer'
 import React from 'react'
 
-export default function Activity(props) {
+type Props = {
+  expand: boolean
+}
+
+export default function Activity(props: Props) {
   return (
-    <div className={`activity-container ${props.isExpand ? 'expand' : ''}`} id="activity-container">
+    <div className={`activity-container ${props.expand ? 'expand' : ''}`}>
       <Chat />
       <Prediction />
     </div>
