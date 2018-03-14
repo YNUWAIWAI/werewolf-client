88ebdc8c3e2f550f1e94103acc6a6ff9e54c5d34
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-14 16:54:52 +0900

    Fix Day test and type

--- src/scripts/village/components/Day.test.js
+++ src/scripts/village/components/Day.test.js
@@ -1,19 +1,20 @@
+// @flow
 import Day from './Day'
 import React from 'react'
 import {shallow} from 'enzyme'
 
-test('<Day date={0} phase="昼" phaseTimeLimit={10} />', () => {
-  const wrapper = shallow(<Day date={0} phase="昼" phaseTimeLimit={10} />)
+test('<Day date={0} phase="昼" />', () => {
+  const wrapper = shallow(<Day date={0} phase="昼" />)
 
   expect(wrapper.text()).toMatch('0日目 昼')
 })
-test('<Day date={undefined} phase="昼" phaseTimeLimit={10} />', () => {
-  const wrapper = shallow(<Day phase="昼" phaseTimeLimit={10} />)
+test('<Day date={undefined} phase="昼" />', () => {
+  const wrapper = shallow(<Day date={undefined} phase="昼" />)
 
   expect(wrapper.text()).toMatch('undefined日目 昼')
 })
-test('<Day date={0} phase={undefined} phaseTimeLimit={10} />', () => {
-  const wrapper = shallow(<Day date={0} phaseTimeLimit={10} />)
+test('<Day date={0} phase={undefined} />', () => {
+  const wrapper = shallow(<Day date={0} phase={undefined} />)
 
   expect(wrapper.text()).toMatch('0日目 undefined')
 })
