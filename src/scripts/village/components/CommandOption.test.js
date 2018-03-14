b11f381e08b58771f1f4cad378d395fdb427f7eb
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-14 16:02:14 +0900

    Update CommandOption test

--- src/scripts/village/components/CommandOption.test.js
+++ src/scripts/village/components/CommandOption.test.js
@@ -1,37 +1,46 @@
+// @flow
 import CommandOption from './CommandOption'
 import React from 'react'
 import {shallow} from 'enzyme'
 
 test('<CommandOption id={1} image="image" name="name" />', () => {
-  const wrapper = shallow(<CommandOption id={1} image="image" name="name" />)
+  const mockFn = jest.fn()
+  const wrapper = shallow(<CommandOption handleSelectOption={mockFn} id={1} image="image" name="name" />)
 
   expect(wrapper.is('.command--option[data-player=1]')).toBe(true)
   expect(wrapper.containsMatchingElement(<img src="image" />)).toBe(true)
   expect(wrapper.text()).toBe('name')
+  expect(mockFn).toHaveBeenCalledTimes(0)
 })
 test('<CommandOption id={undefined} image="image" name="name" />', () => {
-  const wrapper = shallow(<CommandOption image="image" name="name" />)
+  const mockFn = jest.fn()
+  const wrapper = shallow(<CommandOption handleSelectOption={mockFn} id={undefined} image="image" name="name" />)
 
   expect(wrapper.is('.command--option[data-player]')).toBe(false)
   expect(wrapper.containsMatchingElement(<img src="image" />)).toBe(true)
   expect(wrapper.text()).toBe('name')
+  expect(mockFn).toHaveBeenCalledTimes(0)
 })
 test('<CommandOption id={1} image={undefined} name="name" />', () => {
-  const wrapper = shallow(<CommandOption id={1} name="name" />)
+  const mockFn = jest.fn()
+  const wrapper = shallow(<CommandOption handleSelectOption={mockFn} id={1} image={undefined} name="name" />)
 
   expect(wrapper.is('.command--option[data-player=1]')).toBe(true)
   expect(wrapper.containsMatchingElement(<img />)).toBe(true)
   expect(wrapper.text()).toBe('name')
+  expect(mockFn).toHaveBeenCalledTimes(0)
 })
 test('<CommandOption id={1} image="image" name={undefined} />', () => {
-  const wrapper = shallow(<CommandOption id={1} image="image" />)
+  const mockFn = jest.fn()
+  const wrapper = shallow(<CommandOption handleSelectOption={mockFn} id={1} image="image" name={undefined} />)
 
   expect(wrapper.is('.command--option[data-player=1]')).toBe(true)
   expect(wrapper.containsMatchingElement(<img src="image" />)).toBe(true)
   expect(wrapper.text()).toBe('')
+  expect(mockFn).toHaveBeenCalledTimes(0)
 })
 test('<CommandOption id={1} image="image" name="name" /> handleSelectOption', () => {
-  const mockFn = jest.fn().mockName('handleSelectOption')
+  const mockFn = jest.fn()
   const wrapper = shallow(<CommandOption handleSelectOption={mockFn} id={1} image="image" name="name" />)
 
   expect(wrapper.is('.command--option[data-player=1]')).toBe(true)
