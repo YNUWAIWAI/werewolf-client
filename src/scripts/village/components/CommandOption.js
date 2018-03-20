473606f0b29dd7484399cd4505ad4ec48233de4f
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-20 20:12:20 +0900

    Fix CommandOpiton component and test

--- src/scripts/village/components/CommandOption.js
+++ src/scripts/village/components/CommandOption.js
@@ -2,19 +2,14 @@
 import React from 'react'
 
 type Props = {
-  handleSelectOption: number => void,
-  id: number,
+  handleSelectOption: void => void,
   image: string,
   name: string
 }
 
 export default function CommandOption(props: Props) {
-  const handleSelectOption = () => {
-    props.handleSelectOption(props.id)
-  }
-
   return (
-    <div className="command--option" data-player={props.id} onClick={handleSelectOption}>
+    <div className="command--option" onClick={props.handleSelectOption}>
       <img src={props.image} />
       <span>
         {props.name}
--- src/scripts/village/components/CommandOption.test.js
+++ src/scripts/village/components/CommandOption.test.js
@@ -3,52 +3,38 @@ import CommandOption from './CommandOption'
 import React from 'react'
 import {shallow} from 'enzyme'
 
-test('<CommandOption id={1} image="image" name="name" />', () => {
+test('<CommandOption image="image" name="name" />', () => {
   const mockFn = jest.fn()
-  const wrapper = shallow(<CommandOption handleSelectOption={mockFn} id={1} image="image" name="name" />)
+  const wrapper = shallow(<CommandOption handleSelectOption={mockFn} image="image" name="name" />)
 
-  expect(wrapper.is('.command--option[data-player=1]')).toBe(true)
   expect(wrapper.containsMatchingElement(<img src="image" />)).toBe(true)
   expect(wrapper.text()).toBe('name')
   expect(mockFn).toHaveBeenCalledTimes(0)
 })
-test('<CommandOption id={undefined} image="image" name="name" />', () => {
+test('<CommandOption image={undefined} name="name" />', () => {
   const mockFn = jest.fn()
-  const wrapper = shallow(<CommandOption handleSelectOption={mockFn} id={undefined} image="image" name="name" />)
+  const wrapper = shallow(<CommandOption handleSelectOption={mockFn} image={undefined} name="name" />)
 
-  expect(wrapper.is('.command--option[data-player]')).toBe(false)
-  expect(wrapper.containsMatchingElement(<img src="image" />)).toBe(true)
-  expect(wrapper.text()).toBe('name')
-  expect(mockFn).toHaveBeenCalledTimes(0)
-})
-test('<CommandOption id={1} image={undefined} name="name" />', () => {
-  const mockFn = jest.fn()
-  const wrapper = shallow(<CommandOption handleSelectOption={mockFn} id={1} image={undefined} name="name" />)
-
-  expect(wrapper.is('.command--option[data-player=1]')).toBe(true)
   expect(wrapper.containsMatchingElement(<img />)).toBe(true)
   expect(wrapper.text()).toBe('name')
   expect(mockFn).toHaveBeenCalledTimes(0)
 })
-test('<CommandOption id={1} image="image" name={undefined} />', () => {
+test('<CommandOption image="image" name={undefined} />', () => {
   const mockFn = jest.fn()
-  const wrapper = shallow(<CommandOption handleSelectOption={mockFn} id={1} image="image" name={undefined} />)
+  const wrapper = shallow(<CommandOption handleSelectOption={mockFn} image="image" name={undefined} />)
 
-  expect(wrapper.is('.command--option[data-player=1]')).toBe(true)
   expect(wrapper.containsMatchingElement(<img src="image" />)).toBe(true)
   expect(wrapper.text()).toBe('')
   expect(mockFn).toHaveBeenCalledTimes(0)
 })
-test('<CommandOption id={1} image="image" name="name" /> handleSelectOption', () => {
+test('<CommandOption image="image" name="name" /> handleSelectOption', () => {
   const mockFn = jest.fn()
-  const wrapper = shallow(<CommandOption handleSelectOption={mockFn} id={1} image="image" name="name" />)
+  const wrapper = shallow(<CommandOption handleSelectOption={mockFn} image="image" name="name" />)
 
-  expect(wrapper.is('.command--option[data-player=1]')).toBe(true)
   expect(wrapper.containsMatchingElement(<img src="image" />)).toBe(true)
   expect(wrapper.text()).toBe('name')
   wrapper.simulate('click')
-  expect(mockFn).toHaveBeenCalledWith(1)
-  expect(wrapper.is('.command--option[data-player=1]')).toBe(true)
+  expect(mockFn).toHaveBeenCalledTimes(1)
   expect(wrapper.containsMatchingElement(<img src="image" />)).toBe(true)
   expect(wrapper.text()).toBe('name')
 })
