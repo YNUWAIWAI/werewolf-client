dc344b7dc45ff23d52adba3eda1d7571e6e0a893
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-20 21:25:48 +0900

    Remove unnecesary tests

--- src/scripts/village/components/CommandOption.test.js
+++ src/scripts/village/components/CommandOption.test.js
@@ -11,22 +11,6 @@ test('<CommandOption image="image" name="name" />', () => {
   expect(wrapper.text()).toBe('name')
   expect(mockFn).toHaveBeenCalledTimes(0)
 })
-test('<CommandOption image={undefined} name="name" />', () => {
-  const mockFn = jest.fn()
-  const wrapper = shallow(<CommandOption handleSelectOption={mockFn} image={undefined} name="name" />)
-
-  expect(wrapper.containsMatchingElement(<img />)).toBe(true)
-  expect(wrapper.text()).toBe('name')
-  expect(mockFn).toHaveBeenCalledTimes(0)
-})
-test('<CommandOption image="image" name={undefined} />', () => {
-  const mockFn = jest.fn()
-  const wrapper = shallow(<CommandOption handleSelectOption={mockFn} image="image" name={undefined} />)
-
-  expect(wrapper.containsMatchingElement(<img src="image" />)).toBe(true)
-  expect(wrapper.text()).toBe('')
-  expect(mockFn).toHaveBeenCalledTimes(0)
-})
 test('<CommandOption image="image" name="name" /> handleSelectOption', () => {
   const mockFn = jest.fn()
   const wrapper = shallow(<CommandOption handleSelectOption={mockFn} image="image" name="name" />)
