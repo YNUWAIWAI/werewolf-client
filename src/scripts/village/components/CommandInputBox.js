5589cdf5490d06baa5b22bb3671c454e8ba4dc43
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-22 01:10:36 +0900

    Fix CommandInputBox

--- src/scripts/village/components/CommandInputBox.js
+++ src/scripts/village/components/CommandInputBox.js
@@ -5,24 +5,20 @@ import CommandInput from './CommandInput'
 export type StateProps = {
   +limited: {
     +available: boolean,
-    +isSendable: boolean,
     +postCount: number,
     +postCountLimit: number
   },
   +private: {
-    +isSendable: boolean,
     +postCount: number,
     +postCountLimit: number
   },
   +public: {
-    +isSendable: boolean,
     +postCount: number,
     +postCountLimit: number
   }
 }
 export type DispatchProps = {
-  +handlePostChat: InputChannel => string => void,
-  +setIsSendable: InputChannel => boolean => void
+  +handlePostChat: InputChannel => string => void
 }
 export type ownProps = {}
 export type Props =
@@ -33,11 +29,11 @@ export type Props =
 export default function CommandInputBox(props: Props) {
   return (
     <Fragment>
-      <CommandInput kind="public" {... props.public} handlePostChat={props.handlePostChat('public')} setIsSendable={props.setIsSendable('public')} />
-      <CommandInput kind="private" {... props.private} handlePostChat={props.handlePostChat('private')} setIsSendable={props.setIsSendable('private')} />
+      <CommandInput kind="public" {... props.public} handlePostChat={props.handlePostChat('public')} />
+      <CommandInput kind="private" {... props.private} handlePostChat={props.handlePostChat('private')} />
       {
         props.limited.available ?
-          <CommandInput kind="limited" {... props.limited} handlePostChat={props.handlePostChat('limited')} setIsSendable={props.setIsSendable('limited')} /> :
+          <CommandInput kind="limited" {... props.limited} handlePostChat={props.handlePostChat('limited')} /> :
           ''
       }
     </Fragment>
