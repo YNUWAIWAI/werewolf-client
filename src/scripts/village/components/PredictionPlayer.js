75d67cbc50dd4668a64c5af1bd5c393cc9d1a04c
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-22 17:02:09 +0900

    Fix type

--- src/scripts/village/components/PredictionPlayer.js
+++ src/scripts/village/components/PredictionPlayer.js
@@ -2,14 +2,14 @@
 import React from 'react'
 
 type Props = {
-  image: string,
-  name: string,
-  status: string
+  +image: string,
+  +name: string,
+  +status: string
 }
 
 export default function PredictionPlayer(props: Props) {
   return (
-    <div className={`prediction--player ${props.status === 'alive' ? '' : 'dead'}`} data-player={props.id}>
+    <div className={`prediction--player ${props.status === 'alive' ? '' : 'dead'}`}>
       <img src={props.image} />
       <span>
         {props.name}
