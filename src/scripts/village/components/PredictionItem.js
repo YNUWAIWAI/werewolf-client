465cfbcc08dffc0949de13930eb05ea71413d881
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-22 16:48:39 +0900

    Add PredictionContainer type Fix Prediction and PredictionItem

--- src/scripts/village/components/PredictionItem.js
+++ src/scripts/village/components/PredictionItem.js
@@ -3,9 +3,7 @@ import React from 'react'
 
 type Props = {
   date: number,
-  handleBoardClick: (nextState: BoardState, playerId: number, roleId: RoleId) => void,
-  playerId: number,
-  roleId: RoleId,
+  handleBoardClick: BoardState => void,
   state: BoardState
 }
 
@@ -13,8 +11,6 @@ export default function PredictionItem(props: Props) {
   const handleBoardClick = () => {
     const state = [ '?', 'Δ', 'O', 'X' ]
     const currentState = props.state
-    const playerId = props.playerId
-    const roleId = props.roleId
 
     if (!state.includes(currentState)) {
       return
@@ -22,14 +18,12 @@ export default function PredictionItem(props: Props) {
     const nextIndex = (state.indexOf(currentState) + 1) % state.length
     const nextState = state[nextIndex]
 
-    props.handleBoardClick(nextState, playerId, roleId)
+    props.handleBoardClick(nextState)
   }
 
   return (
     <div
       data-date={props.date}
-      data-player={props.playerId}
-      data-role={props.roleId}
       data-state={props.state}
       onClick={handleBoardClick}
     />
