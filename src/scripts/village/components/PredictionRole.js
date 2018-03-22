05920fcc7295d3fd93c118b35144a37d303faaa8
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-22 17:16:14 +0900

    Fix type and CSS

--- src/scripts/village/components/PredictionRole.js
+++ src/scripts/village/components/PredictionRole.js
@@ -2,15 +2,14 @@
 import React from 'react'
 
 type Props = {
-  id: string,
-  image: string,
-  name: string,
-  numberOfAgents: number
+  +image: string,
+  +name: string,
+  +numberOfAgents: number
 }
 
 export default function PredictionRole(props: Props) {
   return (
-    <div data-role={props.id} data-tooltip={props.name}>
+    <div data-tooltip={props.name}>
       <img src={props.image} />
       {props.numberOfAgents >= 0 ? `✕${props.numberOfAgents}` : ''}
     </div>
