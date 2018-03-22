cbed979a437881c7ece5a1a0fe57fae77981205c
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-22 13:19:09 +0900

    Add type

--- src/scripts/village/containers/DayContainer.js
+++ src/scripts/village/containers/DayContainer.js
@@ -1,8 +1,10 @@
-import Day from '../components/Day'
+// @flow
+import Day, {type StateProps} from '../components/Day'
 import Phase from '../constants/Phase'
+import type {ReducerState} from '../reducers'
 import {connect} from 'react-redux'
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state: ReducerState): StateProps => ({
   date: state.base.date,
   phase: Phase[state.base.phase]
 })
