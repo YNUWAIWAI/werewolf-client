c278d7ffff376f60ade3253352cfb8804bfd9a4e
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-22 17:02:19 +0900

    Fix tests

--- src/scripts/village/components/PredictionPlayer.test.js
+++ src/scripts/village/components/PredictionPlayer.test.js
@@ -1,52 +1,47 @@
+// @flow
 import PredictionPlayer from './PredictionPlayer'
 import React from 'react'
 import {shallow} from 'enzyme'
 
-test('<PredictionPlayer status="alive" />', () => {
-  const wrapper = shallow(<PredictionPlayer status="alive" />)
+test('<PredictionPlayer image="image" name="name" status="alive" />', () => {
+  const wrapper = shallow(<PredictionPlayer image="image" name="name" status="alive" />)
 
   expect(wrapper.find('.prediction--player').hasClass('dead')).toBe(false)
-})
-test('<PredictionPlayer status="dead" />', () => {
-  const wrapper = shallow(<PredictionPlayer status="dead" />)
-
-  expect(wrapper.find('.prediction--player').hasClass('dead')).toBe(true)
-})
-test('<PredictionPlayer status="death by execution" />', () => {
-  const wrapper = shallow(<PredictionPlayer status="death by execution" />)
-
-  expect(wrapper.find('.prediction--player').hasClass('dead')).toBe(true)
-})
-test('<PredictionPlayer status="death by werewolf attack" />', () => {
-  const wrapper = shallow(<PredictionPlayer status="death by werewolf attack" />)
-
-  expect(wrapper.find('.prediction--player').hasClass('dead')).toBe(true)
-})
-test('<PredictionPlayer status="death by fear" />', () => {
-  const wrapper = shallow(<PredictionPlayer status="death by fear" />)
-
-  expect(wrapper.find('.prediction--player').hasClass('dead')).toBe(true)
-})
-test('<PredictionPlayer status="unnatural death" />', () => {
-  const wrapper = shallow(<PredictionPlayer status="unnatural death" />)
-
-  expect(wrapper.find('.prediction--player').hasClass('dead')).toBe(true)
-})
-test('<PredictionPlayer image="image" name="name" />', () => {
-  const wrapper = shallow(<PredictionPlayer image="image" name="name" />)
-
   expect(wrapper.contains(<img src="image" />)).toBe(true)
   expect(wrapper.text()).toBe('name')
 })
-test('<PredictionPlayer image={undefined} name="name" />', () => {
-  const wrapper = shallow(<PredictionPlayer name="name" />)
+test('<PredictionPlayer image="image" name="name" status="dead" />', () => {
+  const wrapper = shallow(<PredictionPlayer image="image" name="name" status="dead" />)
 
-  expect(wrapper.contains(<img />)).toBe(true)
+  expect(wrapper.find('.prediction--player').hasClass('dead')).toBe(true)
+  expect(wrapper.contains(<img src="image" />)).toBe(true)
   expect(wrapper.text()).toBe('name')
 })
-test('<PredictionPlayer image="image" name={undefined} />', () => {
-  const wrapper = shallow(<PredictionPlayer image="image" />)
+test('<PredictionPlayer image="image" name="name" status="death by execution" />', () => {
+  const wrapper = shallow(<PredictionPlayer image="image" name="name" status="death by execution" />)
 
+  expect(wrapper.find('.prediction--player').hasClass('dead')).toBe(true)
   expect(wrapper.contains(<img src="image" />)).toBe(true)
-  expect(wrapper.text()).toBe('')
+  expect(wrapper.text()).toBe('name')
+})
+test('<PredictionPlayer image="image" name="name" status="death by werewolf attack" />', () => {
+  const wrapper = shallow(<PredictionPlayer image="image" name="name" status="death by werewolf attack" />)
+
+  expect(wrapper.find('.prediction--player').hasClass('dead')).toBe(true)
+  expect(wrapper.contains(<img src="image" />)).toBe(true)
+  expect(wrapper.text()).toBe('name')
+})
+test('<PredictionPlayer image="image" name="name" status="death by fear" />', () => {
+  const wrapper = shallow(<PredictionPlayer image="image" name="name" status="death by fear" />)
+
+  expect(wrapper.find('.prediction--player').hasClass('dead')).toBe(true)
+  expect(wrapper.contains(<img src="image" />)).toBe(true)
+  expect(wrapper.text()).toBe('name')
+})
+test('<PredictionPlayer image="image" name="name" status="unnatural death" />', () => {
+  const wrapper = shallow(<PredictionPlayer image="image" name="name" status="unnatural death" />)
+
+  expect(wrapper.find('.prediction--player').hasClass('dead')).toBe(true)
+  expect(wrapper.contains(<img src="image" />)).toBe(true)
+  expect(wrapper.text()).toBe('name')
 })
