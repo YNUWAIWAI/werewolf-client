d3742eb06b3ff9e12dd3752db5763675667b3bc2
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-22 13:20:24 +0900

    Remove tests

--- src/scripts/village/components/Day.test.js
+++ src/scripts/village/components/Day.test.js
@@ -8,13 +8,3 @@ test('<Day date={0} phase="昼" />', () => {
 
   expect(wrapper.text()).toMatch('0日目 昼')
 })
-test('<Day date={undefined} phase="昼" />', () => {
-  const wrapper = shallow(<Day date={undefined} phase="昼" />)
-
-  expect(wrapper.text()).toMatch('undefined日目 昼')
-})
-test('<Day date={0} phase={undefined} />', () => {
-  const wrapper = shallow(<Day date={0} phase={undefined} />)
-
-  expect(wrapper.text()).toMatch('0日目 undefined')
-})
