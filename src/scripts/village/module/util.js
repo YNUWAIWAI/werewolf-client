96344d5add4cf6234f678be3f0bf272053c2289b
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-24 05:03:55 +0900

    Fix util functions type

--- src/scripts/village/module/util.js
+++ src/scripts/village/module/util.js
@@ -1,9 +1,22 @@
 // @flow
 import {UNPLAYABLE_AGENT} from '../constants/Agent'
 import {UNPLAYABLE_ROLE} from '../constants/Role'
-export const getPlayableAgents = (agents: Agent[]): Agent[] => agents.filter(a => !UNPLAYABLE_AGENT.includes(a['@id']))
-export const getPlayableRoles = (agents: Role[]): Role[] => agents.filter(a => !UNPLAYABLE_ROLE.includes(a['@id']))
-export const getMyAgent = (agents: Agent[]): Agent => getPlayableAgents(agents).filter(a => a.agentIsMine)[0]
-export const getMyRole = (roles: Role[]): Role => getPlayableRoles(roles).filter(r => r.roleIsMine)[0]
-export const trimBaseUri: <T>(string) => T = id => (/\/(\w+)$/).exec(id)[1]
+
+export const getPlayableAgents = <T: {'@id': string}>(agents: T[]): T[] =>
+  agents.filter(a => !UNPLAYABLE_AGENT.includes(a['@id']))
+
+export const getPlayableRoles = <T: {'@id': string}>(roles: T[]): T[] =>
+  roles.filter(a => !UNPLAYABLE_ROLE.includes(a['@id']))
+
+export const getMyAgent = <T: {'@id': string, agentIsMine: boolean}>(agents: T[]): T =>
+  getPlayableAgents(agents)
+    .filter(a => a.agentIsMine)[0]
+
+export const getMyRole = <T: {'@id': string, roleIsMine: boolean}>(roles: T[]): T =>
+  getPlayableRoles(roles)
+    .filter(r => r.roleIsMine)[0]
+
+export const trimBaseUri = <T: *>(id: string): T =>
+  (/\/(\w+)$/).exec(id)[1]
+
 export const xor = (a: boolean, b: boolean): boolean => a !== b
