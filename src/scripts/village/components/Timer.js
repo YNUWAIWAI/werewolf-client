15a87eed6ee96c6559c1103ed7d35d7f47ff8131
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-26 04:18:06 +0900

    Fix TImer

--- src/scripts/village/components/Timer.js
+++ src/scripts/village/components/Timer.js
@@ -2,36 +2,72 @@
 import FormatedTime from './FormattedTime'
 import React from 'react'
 
-type Props = {
-  +limit: number
+export type StateProps = {
+  +limit: number,
+  +phase: Phase | ''
 }
-
-type State = {
-  +intervalID?: IntervalID,
-  +time: number
+export type OwnProps = {}
+export type Props =
+  & StateProps
+  & OwnProps
+export type State = {
+  start: DOMHighResTimeStamp,
+  time: number
 }
 
 export default class Timer extends React.Component<Props, State> {
+  intervalID: IntervalID[]
   constructor(props: Props) {
     super(props)
     this.state = {
+      start: 0,
       time: 0
     }
-    this.tick = this.tick.bind(this)
+    this.intervalID = []
   }
+
   componentWillReceiveProps(nextProps: Props) {
+    if (nextProps.limit === -1) {
+      return
+    }
     this.setState({
-      intervalID: setInterval(this.tick, 1000),
-      time: nextProps.limit
+      start: performance.now()
     })
+    this.intervalID = [
+      ... this.intervalID,
+      setInterval(() => this.tick(), 200)
+    ]
   }
+
+  shouldComponentUpdate(nextProps: Props, nextState: State) {
+    if (nextProps.phase !== this.props.phase) {
+      if (nextProps.limit === -1) {
+        return false
+      }
+
+      return true
+    }
+    if (nextState.time !== this.state.time) {
+      return true
+    }
+
+    return false
+  }
+
   tick() {
-    if (this.state.time > 0) {
-      this.setState(prevState => ({
-        time: prevState.time - 1
-      }))
-    } else {
-      clearInterval(this.state.intervalID)
+    const time = Math.floor((this.state.start + this.props.limit * 1000 - performance.now()) / 1000)
+
+    this.setState({
+      time
+    })
+    if (time < 0) {
+      const [
+        head,
+        ... tail
+      ] = this.intervalID
+
+      clearInterval(head)
+      this.intervalID = tail
     }
   }
 
