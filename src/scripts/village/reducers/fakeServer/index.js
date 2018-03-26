ef9a9de9fc2184c7f5b50ab0fe011316ca874502
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-26 14:49:42 +0900

    Add tests

--- src/scripts/village/reducers/fakeServer/index.js
+++ src/scripts/village/reducers/fakeServer/index.js
@@ -1,10 +1,12 @@
 // @flow
 import firstDayConversationJSON from './firstDayConversation'
+import firstDayConversationSeer2JSON from './firstDayConversationSeer2'
 import myMessageOnChatJSON from './myMsseageOnChat'
 import resultsJSON from './results'
 import theirMessageOnChatJSON from './theirMsseageOnChat'
 
 export const firstDayConversation: Object = firstDayConversationJSON
+export const firstDayConversationSeer2: Object = firstDayConversationSeer2JSON
 export const myMessageOnChat: Object = myMessageOnChatJSON
 export const results: Object = resultsJSON
 export const theirMessageOnChat: Object = theirMessageOnChatJSON
