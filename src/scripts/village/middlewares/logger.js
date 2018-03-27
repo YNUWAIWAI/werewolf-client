8a240e0c3fe5a9f4c47f90247ea038047b1e734b
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-28 07:27:24 +0900

    Fix type

--- src/scripts/village/middlewares/logger.js
+++ src/scripts/village/middlewares/logger.js
@@ -2,9 +2,9 @@
 /* eslint no-console: 0 */
 
 import type {DispatchAPI, Middleware} from 'redux'
+import type {Action} from '.'
 import type {ReducerState} from '../reducers'
 
-type Action = {type: string}
 const logger: Middleware<ReducerState, Action, DispatchAPI<Action>> = store => next => action => {
   console.group(action.type)
   console.info('dispatching', action)
