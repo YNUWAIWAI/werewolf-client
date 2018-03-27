40e51a3148b6d90f37ff97c993a257c1df521e29
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-28 01:37:49 +0900

    Type logger

--- src/scripts/village/middlewares/logger.js
+++ src/scripts/village/middlewares/logger.js
@@ -1,12 +1,17 @@
-const logger = store => next => action => {
+// @flow
+/* eslint no-console: 0 */
+
+import type {DispatchAPI, Middleware} from 'redux'
+import type {ReducerState} from '../reducers'
+
+type Action = {type: string}
+const logger: Middleware<ReducerState, Action, DispatchAPI<Action>> = store => next => action => {
   console.group(action.type)
   console.info('dispatching', action)
-  const result = next(action)
-
   console.log('next state', store.getState())
-  console.groupEnd(action.type)
+  console.groupEnd()
 
-  return result
+  return next(action)
 }
 
 export default logger
