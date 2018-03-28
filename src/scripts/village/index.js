9209f1f00fb748bdb992b7e0cee3bbbeed3eecb0
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-29 00:27:48 +0900

    Type village/index.js

--- src/scripts/village/index.js
+++ src/scripts/village/index.js
@@ -1,11 +1,12 @@
-import {createStore} from 'redux'
+// @flow
 import App from './containers/App'
 import {Provider} from 'react-redux'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {createStore} from 'redux'
 import middleware from './middlewares'
 import reducer from './reducers'
-import {composeWithDevTools} from 'redux-devtools-extension'
 
 const store = createStore(
   reducer,
@@ -16,9 +17,15 @@ store.dispatch({
   type: 'INIT'
 })
 
+const root = document.getElementById('root')
+
+if (!root) {
+  throw Error('Not found root element.')
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  root
 )
