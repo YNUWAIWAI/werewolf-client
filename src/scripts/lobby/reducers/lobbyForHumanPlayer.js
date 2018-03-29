5a1d8fa013388846ee8afa97ac6610d452db9dde
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-29 15:38:07 +0900

    Type lobbyForHumanPlayer

--- src/scripts/lobby/reducers/lobbyForHumanPlayer.js
+++ src/scripts/lobby/reducers/lobbyForHumanPlayer.js
@@ -1,4 +1,13 @@
+// @flow
+/* eslint sort-keys: 0 */
 import * as ActionTypes from '../constants/ActionTypes'
+
+export type State = {
+  isPlayer: boolean,
+  menuItems: MenuItem[],
+  villageItems: Village[]
+}
+type Action = void
 
 const initialState = {
   isPlayer: true,
@@ -22,72 +31,72 @@ const initialState = {
   ],
   villageItems: [
     {
-      "name": "Alice's village",
-      "id": 1,
-      "hostPlayer": {
-        "name": "Alice",
-        "isAnonymous": false
+      'name': 'Alice\'s village',
+      'id': 1,
+      'hostPlayer': {
+        'name': 'Alice',
+        'isAnonymous': false
       },
-      "playerSetting": {
-        "number": 15,
-        "current": 8,
-        "robot": {
-          "min": 7,
-          "current": 3
+      'playerSetting': {
+        'number': 15,
+        'current': 8,
+        'robot': {
+          'min': 7,
+          'current': 3
         },
-        "human": {
-          "max": 8,
-          "current": 5
+        'human': {
+          'max': 8,
+          'current': 5
         }
       },
-      "roleSetting": {
-        "villager": 6,
-        "werewolf": 2,
-        "seer": 1,
-        "medium": 1,
-        "madman": 1,
-        "hunter": 1,
-        "mason": 2,
-        "werehumster": 1
+      'roleSetting': {
+        'villager': 6,
+        'werewolf': 2,
+        'seer': 1,
+        'medium': 1,
+        'madman': 1,
+        'hunter': 1,
+        'mason': 2,
+        'werehumster': 1
       },
-      "avatar": "fixed",
-      "comment": null
+      'avatar': 'fixed',
+      'comment': null
     },
     {
-      "name": "Ruined village",
-      "id": 2,
-      "hostPlayer": {
-        "name": "Anonymous",
-        "isAnonymous": true
+      'name': 'Ruined village',
+      'id': 2,
+      'hostPlayer': {
+        'name': 'Anonymous',
+        'isAnonymous': true
       },
-      "playerSetting": {
-        "number": 5,
-        "current": 4,
-        "robot": {
-          "min": 2,
-          "current": 2
+      'playerSetting': {
+        'number': 5,
+        'current': 4,
+        'robot': {
+          'min': 2,
+          'current': 2
         },
-        "human": {
-          "max": 3,
-          "current": 2
+        'human': {
+          'max': 3,
+          'current': 2
         }
       },
-      "roleSetting": {
-        "villager": 3,
-        "werewolf": 1,
-        "seer": 1,
-        "medium": 0,
-        "madman": 0,
-        "hunter": 0,
-        "mason": 0,
-        "werehumster": 0
+      'roleSetting': {
+        'villager': 3,
+        'werewolf': 1,
+        'seer': 1,
+        'medium': 0,
+        'madman': 0,
+        'hunter': 0,
+        'mason': 0,
+        'werehumster': 0
       },
-      "avatar": "random",
-      "comment": "Open to beginners"
+      'avatar': 'random',
+      'comment': 'Open to beginners'
     }
   ]
 }
 
-const lobbyForHumanPlayer = (state = initialState, action) => state
+const lobbyForHumanPlayer = (state: State = initialState, action: Action) => state
 
 export default lobbyForHumanPlayer
