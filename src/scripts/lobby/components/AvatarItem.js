c53c1501a87c20cc0ccc65620502c8ba55d9a988
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-29 18:45:57 +0900

    Type AvatarItem

--- src/scripts/lobby/components/AvatarItem.js
+++ src/scripts/lobby/components/AvatarItem.js
@@ -1,6 +1,13 @@
+// @flow
 import React from 'react'
 
-export default function AvatarItem(props) {
+type Props = {
+  avatarImage: string,
+  isHost: boolean,
+  name: string
+}
+
+export default function AvatarItem(props: Props) {
   return (
     <div className="avatar-list--item">
       <img className="avatar-list--item--image" src={props.avatarImage} />
