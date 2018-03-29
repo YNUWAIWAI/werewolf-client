2ce058a6031a06295376585f8421ad244f0fd184
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-29 15:38:42 +0900

    Fix Type

--- src/scripts/lobby/reducers/index.js
+++ src/scripts/lobby/reducers/index.js
@@ -3,10 +3,10 @@ import app from './app'
 import buildVillage from './buildVillage'
 import {combineReducers} from 'redux'
 import connectingToRobotPlayer from './connectingToRobotPlayer'
-import history from './history'
-import lobbyForAudience from './lobbyForAudience'
-import lobbyForHumanPlayer from './lobbyForHumanPlayer'
-import lobbyForRobotPlayer from './lobbyForRobotPlayer'
+import history, {type State as HistoryState} from './history'
+import lobbyForAudience, {type State as LobbyForAudienceState} from './lobbyForAudience'
+import lobbyForHumanPlayer, {type State as LobbyForHumanPlayerState} from './lobbyForHumanPlayer'
+import lobbyForRobotPlayer, {type State as LobbyForRobotPlayerState} from './lobbyForRobotPlayer'
 import main, {type State as MainState} from './main'
 import settings, {type State as SettingsState} from './settings'
 import waitingForPlayers, {type State as WaitingForPlayersState} from './waitingForPlayers'
@@ -30,10 +30,10 @@ export type ReducerState = {
   app: */*AppState*/,
   buildVillage: */*BuildVillageState*/,
   connectingToRobotPlayer: */*ConnectingToRobotPlayerState*/,
-  history: */*HistoryState*/,
-  lobbyForAudience: */*LobbyForAudienceState*/,
-  lobbyForHumanPlayer: */*LobbyForHumanPlayerState*/,
-  lobbyForRobotPlayer: */*LobbyForRobotPlayerState*/,
+  history: HistoryState,
+  lobbyForAudience: LobbyForAudienceState,
+  lobbyForHumanPlayer: LobbyForHumanPlayerState,
+  lobbyForRobotPlayer: LobbyForRobotPlayerState,
   main: MainState,
   settings: SettingsState,
   waitingForPlayers: WaitingForPlayersState
