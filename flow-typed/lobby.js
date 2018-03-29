35c3a91d41498789aabfa534a6df496b4067c64c
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-29 15:13:52 +0900

    Type waitingForPlayers

--- flow-typed/lobby.js
+++ flow-typed/lobby.js
@@ -2,3 +2,40 @@
 
 declare type Avatar = 'fixed' | 'random'
 declare type Member = 'A' | 'B' | 'C'
+declare type Village = {
+  name: string,
+  id: number,
+  hostPlayer: HostPlayer,
+  playerSetting: PlayerSetting,
+  roleSetting: RoleSetting,
+  avatar: string,
+  comment: string
+}
+declare type HostPlayer = {
+  name: string,
+  isAnonymous: boolean
+}
+declare type PlayerSetting = {
+  number: number,
+  current: number,
+  robot: Robot,
+  human: Human
+}
+declare type Robot = {
+  min: number,
+  current: number
+}
+declare type Human = {
+  max: number,
+  current: number
+}
+declare type RoleSetting = {
+  villager: number,
+  werewolf: number,
+  seer: number,
+  medium: number,
+  madman: number,
+  hunter: number,
+  mason: number,
+  werehumster: number
+}
