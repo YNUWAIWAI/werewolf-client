2daeb76ee6b87fe63a48ca11b9ea5dabb9ad48f5
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-29 17:45:00 +0900

    Type middlewares/socket

--- src/scripts/lobby/middlewares/socket.js
+++ src/scripts/lobby/middlewares/socket.js
@@ -1,60 +1,38 @@
+// @flow
 import * as types from '../constants/ActionTypes'
-import {
-  ready,
-  socket as socketAction,
-  wait
-} from '../actions'
+import type {DispatchAPI, Middleware} from 'redux'
+import {socket as socketAction} from '../actions'
+import {type Action} from '.'
+import type {ReducerState} from '../reducers'
 
 let socket
 let retryCount = 0
 let isReady = false
 
-const socketMiddleware = (option = {}) => store => next => action => {
-  if (!option.url) {
-    console.error('"option.url" not found.')
-  }
-  option.retry = option.retry || 5
+const socketMiddleware: ({url: string, retry?: number}) => Middleware<ReducerState, Action, DispatchAPI<Action>> = option => store => next => action => {
+  const retry = option.retry || 5
   const connectWebSocket = url => {
-    if (!window.WebSocket || !('WebSocket' in window)) {
-      console.log('WebSocket NOT supported by your Browser!')
-
-      return
-    }
-
     socket = new WebSocket(url)
-
-    socket.addEventListener('open', event => {
-      console.log('WebSocket Connected ', event)
+    socket.onopen = event => {
+      console.info('WebSocket Connected ', event)
       store.dispatch(socketAction.open(event))
-    })
-    socket.addEventListener('close', event => {
+    }
+    socket.onclose = event => {
       console.warn('WebSocket Disconnected ', event)
       socket = null
-      if (isReady) {
-        store.dispatch(wait())
-        isReady = false
-      }
       store.dispatch(socketAction.close(event))
-    })
-    socket.addEventListener('error', error => {
+    }
+    socket.onerror = error => {
       console.error('WebSocket Error ', error)
       socket = null
-      if (isReady) {
-        store.dispatch(wait())
-        isReady = false
-      }
       store.dispatch(socketAction.error(error))
-    })
-    socket.addEventListener('message', event => {
-      if (!isReady) {
-        store.dispatch(ready())
-        isReady = true
-      }
+    }
+    socket.onmessage = event => {
       store.dispatch(socketAction.message(event))
-    })
+    }
   }
 
-  if (!socket && retryCount < option.retry) {
+  if (!socket && retryCount < retry) {
     retryCount += 1
     connectWebSocket(option.url)
   }
@@ -69,7 +47,9 @@ const socketMiddleware = (option = {}) => store => next => action => {
     case types.SOCKET_MESSAGE:
       return next(action)
     case types.SOCKET_SEND:
-      socket.send(JSON.stringify(action.payload))
+      if (socket) {
+        socket.send(JSON.stringify(action.payload))
+      }
 
       return next(action)
     default:
