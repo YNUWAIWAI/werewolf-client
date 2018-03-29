340d69a194c5c56d8ddf4ef1b756807b282d736f
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-30 03:06:42 +0900

    Test Menu

---
+++ src/scripts/lobby/components/Menu.test.js
@@ -0,0 +1,49 @@
+// @flow
+import Menu from './Menu'
+import React from 'react'
+import {shallow} from 'enzyme'
+
+test('<Menu class="class" itemClass="itemClass" /> 0 item', () => {
+  const transitionEventHandler = jest.fn()
+  const transition = jest.fn().mockReturnValue(transitionEventHandler)
+  const items = []
+  const wrapper = shallow(<Menu class="class" itemClass="itemClass" items={items} transition={transition} />)
+
+  expect(wrapper.children()).toHaveLength(0)
+  expect(transitionEventHandler).toHaveBeenCalledTimes(0)
+  expect(transition).toHaveBeenCalledTimes(0)
+})
+test('<Menu class="class" itemClass="itemClass" /> 1 item', () => {
+  const transitionEventHandler = jest.fn()
+  const transition = jest.fn().mockReturnValue(transitionEventHandler)
+  const items = [
+    {
+      text: 'text',
+      type: 'type'
+    }
+  ]
+  const wrapper = shallow(<Menu class="class" itemClass="itemClass" items={items} transition={transition} />)
+
+  expect(wrapper.children()).toHaveLength(1)
+  expect(transitionEventHandler).toHaveBeenCalledTimes(0)
+  expect(transition).toHaveBeenCalledTimes(0)
+})
+test('<Menu class="class" itemClass="itemClass" /> 2 items', () => {
+  const transitionEventHandler = jest.fn()
+  const transition = jest.fn().mockReturnValue(transitionEventHandler)
+  const items = [
+    {
+      text: 'text',
+      type: 'type'
+    },
+    {
+      text: 'text',
+      type: 'type'
+    }
+  ]
+  const wrapper = shallow(<Menu class="class" itemClass="itemClass" items={items} transition={transition} />)
+
+  expect(wrapper.children()).toHaveLength(2)
+  expect(transitionEventHandler).toHaveBeenCalledTimes(0)
+  expect(transition).toHaveBeenCalledTimes(0)
+})
