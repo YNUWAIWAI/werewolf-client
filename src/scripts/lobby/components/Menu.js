8152ab6c5e67ceafd09dfba5ef05f56bcb89cc13
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-30 03:06:32 +0900

    Type Menu

--- src/scripts/lobby/components/Menu.js
+++ src/scripts/lobby/components/Menu.js
@@ -1,7 +1,18 @@
+// @flow
 import MenuItem from './MenuItem'
 import React from 'react'
 
-export default function Menu(props) {
+type Props = {
+  class: string,
+  itemClass: string,
+  items: {
+    text: string,
+    type: string
+  }[],
+  transition: string => void => void,
+}
+
+export default function Menu(props: Props) {
   const items = props.items.map(item =>
     <MenuItem
       class={props.itemClass}
