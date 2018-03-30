337bdd97de1bdd6398b6a00ac983e134cd337db3
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-30 14:21:06 +0900

    Type MemberRole

--- src/scripts/lobby/components/MemberRole.js
+++ src/scripts/lobby/components/MemberRole.js
@@ -1,6 +1,15 @@
+// @flow
 import React from 'react'
 
-export default function MemberRole(props) {
+type Props = {
+  class: string,
+  id: string,
+  image: string,
+  name: string,
+  numberOfAgents: number
+}
+
+export default function MemberRole(props: Props) {
   return (
     <div className={props.class} data-id={props.id} data-tooltip={props.name}>
       <img src={props.image} />
