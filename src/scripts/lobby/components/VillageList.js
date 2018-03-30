797caaec644b62355294ec6aa9dff8cc277cef21
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-30 20:45:01 +0900

    Type VillageList

--- src/scripts/lobby/components/VillageList.js
+++ src/scripts/lobby/components/VillageList.js
@@ -1,7 +1,14 @@
+// @flow
 import React from 'react'
 import VillageItem from './VillageItem'
 
-export default function VillageList(props) {
+type Props = {
+  isPlayer: boolean,
+  items: Village[],
+  selectVillage: number => void => void
+}
+
+export default function VillageList(props: Props) {
   const items = props.items.map(item => <VillageItem key={item.name} {... item} handleClick={props.selectVillage(item.id)} isPlayer={props.isPlayer} />)
 
   return (
