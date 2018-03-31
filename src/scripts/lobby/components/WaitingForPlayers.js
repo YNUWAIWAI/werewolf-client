b37d22e3834256a1a5ee8eee21c9b2a147459c2f
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-31 10:11:29 +0900

    Type WaitingForPlayers

--- src/scripts/lobby/components/WaitingForPlayers.js
+++ src/scripts/lobby/components/WaitingForPlayers.js
@@ -1,23 +1,34 @@
+// @flow
+import AvatarList, {type Props as AvatarListProps} from './AvatarList'
+import Menu, {type Props as MenuProps} from './Menu'
 import AsideContent from './AsideContent'
-import AvatarList from './AvatarList'
 import Header from './Header'
 import MainContent from './MainContent'
-import Menu from './Menu'
 import React from 'react'
 import VillageItem from './VillageItem'
 
-export default function WaitingForPlayers(props) {
+type Props = {
+  isPlayer: boolean,
+  menuItems: $PropertyType<MenuProps, 'items'>,
+  players: $PropertyType<AvatarListProps, 'items'>,
+  transition: string => void => void,
+  village: Village
+}
+
+export default function WaitingForPlayers(props: Props) {
   return (
     <div className="grid">
       <Header text="Waiting for players" />
       <MainContent>
         <VillageItem
           {... props.village}
+          handleClick={() => {}}
           isPlayer={props.isPlayer}
         />
       </MainContent>
       <AsideContent>
         <AvatarList
+          isPlayer={props.isPlayer}
           items={props.players}
         />
         <Menu
