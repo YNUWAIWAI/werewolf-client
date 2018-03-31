dc0ba708fffadb641636b4d1d515d3c7b91f27b6
Yudai Udagawa <nimius.rd@gmail.com>
2018-03-31 11:32:41 +0900

    Test EditableVillageItem

---
+++ src/scripts/lobby/components/EditableVillageItem.test.js
@@ -0,0 +1,54 @@
+// @flow
+import EditableVillageItem from './EditableVillageItem'
+import React from 'react'
+import {shallow} from 'enzyme'
+
+test('<EditableVillageItem villageName="villageName" hostName="hostName" numberOfHumans={8} numberOfPlayers={15} numberOfRobots={7} comment="comment" />', () => {
+  const handleChangeEventHandler = jest.fn()
+  const handleChange = jest.fn().mockReturnValue(handleChangeEventHandler)
+  const wrapper = shallow(
+    <EditableVillageItem
+      comment="comment"
+      handleChange={handleChange}
+      hostName="hostName"
+      numberOfHumans={8}
+      numberOfPlayers={15}
+      numberOfRobots={7}
+      villageName="villageName"
+    />
+  )
+
+  expect(wrapper.children()).toHaveLength(10)
+  expect(wrapper.find('.village--item--village-name--val').props().defaultValue).toBe('villageName')
+  expect(wrapper.find('.village--item--host-name--val').props().defaultValue).toBe('hostName')
+  expect(wrapper.find('.village--item--setup--val1').text()).toBe('<NumberSelect />players')
+  expect(wrapper.find('.village--item--comment--val').props().defaultValue).toBe('comment')
+  expect(handleChange).toHaveBeenCalledTimes(5)
+  expect(handleChangeEventHandler).toHaveBeenCalledTimes(0)
+})
+test('<EditableVillageItem villageName="villageName" hostName="hostName" numberOfHumans={8} numberOfPlayers={15} numberOfRobots={7} comment="comment" /> onChange', () => {
+  const handleChangeEventHandler = jest.fn()
+  const handleChange = jest.fn().mockReturnValue(handleChangeEventHandler)
+  const wrapper = shallow(
+    <EditableVillageItem
+      comment="comment"
+      handleChange={handleChange}
+      hostName="hostName"
+      numberOfHumans={8}
+      numberOfPlayers={15}
+      numberOfRobots={7}
+      villageName="villageName"
+    />
+  )
+
+  expect(handleChangeEventHandler).toHaveBeenCalledTimes(0)
+  wrapper.find('.village--item--village-name--val').simulate('change')
+  expect(handleChangeEventHandler).toHaveBeenCalledTimes(1)
+  wrapper.find('.village--item--host-name--val').simulate('change')
+  expect(handleChangeEventHandler).toHaveBeenCalledTimes(2)
+  wrapper.find('.village--item--setup--val2--select').simulate('change')
+  expect(handleChangeEventHandler).toHaveBeenCalledTimes(3)
+  wrapper.find('.village--item--comment--val').simulate('change')
+  expect(handleChangeEventHandler).toHaveBeenCalledTimes(4)
+  expect(handleChange).toHaveBeenCalledTimes(5)
+})
