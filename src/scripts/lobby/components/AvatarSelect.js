e6e51fa760b45a08d2612e82c0681dbba90174e3
Yudai Udagawa <nimius.rd@gmail.com>
2018-04-01 14:02:48 +0900

    Type BuildVillageContainer

---
+++ src/scripts/lobby/components/AvatarSelect.js
@@ -0,0 +1,33 @@
+// @flow
+import React from 'react'
+
+type Props = {
+  handleChange: Avatar => void,
+}
+
+export default function NumberSelect(props: Props) {
+  const handleChange = (event: SyntheticInputEvent<HTMLSelectElement>) => {
+    const value = event.target.value
+    const avatar: Avatar[] = [ 'fixed', 'random' ]
+    const maybe = avatar.find(v => v === value)
+
+    if (maybe) {
+      props.handleChange(maybe)
+    }
+  }
+
+  return (
+    <select
+      className="village--item--setup--val2--select"
+      name="avatar"
+      onChange={handleChange}
+    >
+      <option value="fixed">
+        {'Fixed Avatar'}
+      </option>
+      <option value="random">
+        {'Random Avatar'}
+      </option>
+    </select>
+  )
+}
