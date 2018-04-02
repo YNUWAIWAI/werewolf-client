b5dbef4bc199d14fd9501ba64264c41fe6b16678
Yudai Udagawa <nimius.rd@gmail.com>
2018-04-03 01:20:31 +0900

    Remove unused import

--- src/scripts/lobby/middlewares/index.js
+++ src/scripts/lobby/middlewares/index.js
@@ -1,5 +1,4 @@
 // @flow
-import * as ActionTypes from '../constants/ActionTypes'
 import * as actions from '../actions'
 import {applyMiddleware} from 'redux'
 import client2server from './client2server'
