3165f5b4f701fe4acf487b960dace319dc1ebdc3
Yudai Udagawa <nimius.rd@gmail.com>
2018-04-04 17:55:32 +0900

    Update lobbyForHumanPlayer

--- src/scripts/lobby/reducers/lobbyForHumanPlayer.js
+++ src/scripts/lobby/reducers/lobbyForHumanPlayer.js
@@ -1,14 +1,16 @@
 // @flow
 /* eslint sort-keys: 0 */
 import * as ActionTypes from '../constants/ActionTypes'
-import type {MenuItem, Village} from 'lobby'
+import type {MenuItem, PayLoad$Lobby, Village} from 'lobby'
+import type {SocketMessage} from '../actions'
 
 export type State = {
   +isPlayer: boolean,
   +menuItems: MenuItem[],
   +villageItems: Village[]
 }
-type Action = void
+type Action =
+  | SocketMessage
 
 const initialState = {
   isPlayer: true,
@@ -98,6 +100,23 @@ const initialState = {
   ]
 }
 
-const lobbyForHumanPlayer = (state: State = initialState, action: Action): State => state
+const lobbyForHumanPlayer = (state: State = initialState, action: Action): State => {
+  switch (action.type) {
+    case ActionTypes.SOCKET_MESSAGE:
+      if (action.payload.type === 'lobby') {
+        const payload: PayLoad$Lobby = action.payload
+
+        return {
+          ... state,
+          villageItems: payload.villages
+        }
+      }
+
+      return state
+
+    default:
+      return state
+  }
+}
 
 export default lobbyForHumanPlayer
