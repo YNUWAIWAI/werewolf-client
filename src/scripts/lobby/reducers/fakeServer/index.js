4596d4144eceb59bb0d65d068c3847a4c6c2c5dc
Yudai Udagawa <nimius.rd@gmail.com>
2018-04-05 13:18:20 +0900

    Add test

--- src/scripts/lobby/reducers/fakeServer/index.js
+++ src/scripts/lobby/reducers/fakeServer/index.js
@@ -1,6 +1,8 @@
 // @flow
 import enterLobbyJSON from './enterLobby'
+import pingJSON from './ping'
 import waitingPageJSON from './waitingPage'
 
 export const enterLobby: Object = enterLobbyJSON
+export const ping: Object = pingJSON
 export const waitingPage: Object = waitingPageJSON
