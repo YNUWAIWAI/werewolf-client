77e787a3d497703c3e9edf8c3c7a2d5b0eb79333
Yudai Udagawa <nimius.rd@gmail.com>
2018-04-09 15:36:57 +0900

    Add Timer story

---
+++ stories/Timer.stories.js
@@ -0,0 +1,15 @@
+// @flow
+import {number, selectV2, withKnobs} from '@storybook/addon-knobs/react'
+import React from 'react'
+import Timer from '../src/scripts/village/components/Timer'
+import {storiesOf} from '@storybook/react'
+
+storiesOf('village/Timer', module)
+  .addDecorator(withKnobs)
+  .add('default', () => {
+    const limit = number('limit', 10)
+    const phase = selectV2('phase', [ 'day conversation', 'day vote', 'night', 'post mortem', 'results' ])
+    const story = <Timer limit={limit} phase={phase} />
+
+    return story
+  })
