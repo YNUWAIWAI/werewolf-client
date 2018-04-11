85939f5a4f0e9d72fbb540e78f95f9411324cc5a
Yudai Udagawa <nimius.rd@gmail.com>
2018-04-11 14:18:04 +0900

    Add CommandInput story

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,26 @@
-// you can use this file to add your custom webpack plugins, loaders and anything you like.
-// This is just the basic way to add additional webpack configurations.
-// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
-
-// IMPORTANT
-// When you add this file, we won't add the default configurations which is similar
-// to "React Create App". This only has babel loader to load JavaScript.
+const path = require('path')
 
 module.exports = {
-  plugins: [
-    // your custom plugins
-  ],
   module: {
     rules: [
-      // add your custom rules.
-    ],
-  },
-};
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: (loader) => [
+                require('postcss-nested')(),
+                require('autoprefixer')()
+              ]
+            }
+          }
+        ],
+        include: path.resolve(__dirname, "../src/styles")
+      }
+    ]
+  }
+}
