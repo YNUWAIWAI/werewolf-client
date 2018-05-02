cae473d27a98dc0197ec1b715f7764f903ddec41
Yudai Udagawa <nimius.rd@gmail.com>
2018-05-03 02:25:40 +0900

    Fix order

--- src/scripts/village/actions/index.js
+++ src/scripts/village/actions/index.js
@@ -25,15 +25,8 @@ export const socket = {
   })
 }
 
-export const toggleObfucator = (visible: boolean): {visible: boolean, type: 'TOGGLE_OBFUCATOR'} => ({
-  type: types.TOGGLE_OBFUCATOR,
-  visible
-})
-
-export const postChat = ({kind, text}: {kind: InputChannel, text: string}): {kind: InputChannel, text: string, type: 'POST_CHAT'} => ({
-  kind,
-  text,
-  type: types.POST_CHAT
+export const closeReuslt = (): {type: 'CLOSE_RESULT'} => ({
+  type: types.CLOSE_RESULT
 })
 
 export const handleBoardClick = (nextState: BoardState, playerId: number, roleId: RoleId): {nextState: BoardState, playerId: number, roleId: RoleId, type: 'CHANGE_PREDICTION_BOARD'} => ({
@@ -43,13 +36,24 @@ export const handleBoardClick = (nextState: BoardState, playerId: number, roleId
   type: types.CHANGE_PREDICTION_BOARD
 })
 
-export const selectOption = (agentId: number): {agentId: number, type: 'SELECT_OPTION'} => ({
-  agentId,
-  type: types.SELECT_OPTION
+export const handleClickHideButton = (hide: boolean): {hide: boolean, type: 'CLICK_HIDE_BUTTON'} => ({
+  hide,
+  type: types.CLICK_HIDE_BUTTON
+})
+
+export const postChat = ({kind, text}: {kind: InputChannel, text: string}): {kind: InputChannel, text: string, type: 'POST_CHAT'} => ({
+  kind,
+  text,
+  type: types.POST_CHAT
 })
 
 export const selectNo = (): {type: 'SELECT_NO'} => ({
   type: types.SELECT_NO
+})
+
+export const selectOption = (agentId: number): {agentId: number, type: 'SELECT_OPTION'} => ({
+  agentId,
+  type: types.SELECT_OPTION
 })
 
 export const selectYes = (agentId: number): {agentId: number, type: 'SELECT_YES'} => ({
@@ -57,13 +61,9 @@ export const selectYes = (agentId: number): {agentId: number, type: 'SELECT_YES'
   type: types.SELECT_YES
 })
 
-export const handleClickHideButton = (hide: boolean): {hide: boolean, type: 'CLICK_HIDE_BUTTON'} => ({
-  hide,
-  type: types.CLICK_HIDE_BUTTON
-})
-
-export const closeReuslt = (): {type: 'CLOSE_RESULT'} => ({
-  type: types.CLOSE_RESULT
+export const toggleObfucator = (visible: boolean): {visible: boolean, type: 'TOGGLE_OBFUCATOR'} => ({
+  type: types.TOGGLE_OBFUCATOR,
+  visible
 })
 
 export type SocketClose = $Call<typeof socket.close, *>
@@ -71,11 +71,11 @@ export type SocketError = $Call<typeof socket.error, *>
 export type SocketMessage = $Call<typeof socket.message, *>
 export type SocketOpen = $Call<typeof socket.open, *>
 export type SocketSend = $Call<typeof socket.send, *>
-export type ToggleObfucator = $Call<typeof toggleObfucator, *>
-export type PostChat = $Call<typeof postChat, *>
+export type ClickHideButton = $Call<typeof handleClickHideButton, *>
+export type CloseReuslt = $Call<typeof closeReuslt>
 export type ChangePredictionBoard = $Call<typeof handleBoardClick, *, *, *>
+export type PostChat = $Call<typeof postChat, *>
 export type SelectOption = $Call<typeof selectOption, *>
 export type SelectNo = $Call<typeof selectNo, *>
 export type SelectYes = $Call<typeof selectYes, *>
-export type ClickHideButton = $Call<typeof handleClickHideButton, *>
-export type CloseReuslt = $Call<typeof closeReuslt>
+export type ToggleObfucator = $Call<typeof toggleObfucator, *>
