36a66cc8ccd34c65ff4240e49030f8768e2870c6
Yudai Udagawa <nimius.rd@gmail.com>
2018-05-10 14:01:50 +0900

    Add reducer entry point

--- src/scripts/village/reducers/index.js
+++ src/scripts/village/reducers/index.js
@@ -5,6 +5,7 @@ import chat, {type State as chatState} from './chat'
 import commandInputBox, {type State as commandInputBoxState} from './commandInputBox'
 import commandSelection, {type State as commandSelectionState} from './commandSelection'
 import hideButton, {type State as hideButtonState} from './hideButton'
+import language, {type State as languageState} from './language'
 import modal, {type State as modalState} from './modal'
 import obfucator, {type State as obfucatorState} from './obfucator'
 import prediction, {type State as predictionState} from './prediction'
@@ -19,6 +20,7 @@ const reducer = combineReducers({
   commandInputBox,
   commandSelection,
   hideButton,
+  language,
   modal,
   obfucator,
   prediction,
@@ -35,6 +37,7 @@ export type ReducerState = {
   commandInputBox: commandInputBoxState,
   commandSelection: commandSelectionState,
   hideButton: hideButtonState,
+  language: languageState,
   modal: modalState,
   obfucator: obfucatorState,
   prediction: predictionState,
