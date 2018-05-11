a471ef05cb86c5158841e42142e6315ceb763f9f
Yudai Udagawa <nimius.rd@gmail.com>
2018-05-11 15:16:16 +0900

    Fix test

--- src/scripts/village/components/PredictionItem.test.js
+++ src/scripts/village/components/PredictionItem.test.js
@@ -3,16 +3,16 @@ import PredictionItem from './PredictionItem'
 import React from 'react'
 import {shallow} from 'enzyme'
 
-test('<PredictionItem date={1} state="?" />', () => {
+test('<PredictionItem date={1} fixed={false} state="?" />', () => {
   const handleBoardClick = jest.fn()
-  const wrapper = shallow(<PredictionItem date={1} handleBoardClick={handleBoardClick} state="?" />)
+  const wrapper = shallow(<PredictionItem date={1} fixed={false} handleBoardClick={handleBoardClick} state="?" />)
 
   expect(wrapper.is('[data-date=1]')).toBe(true)
   expect(wrapper.is('[data-state="?"]')).toBe(true)
 })
-test('<PredictionItem date={1} state="?" /> handleBoardClick', () => {
+test('<PredictionItem date={1} fixed={false} state="?" /> handleBoardClick', () => {
   const handleBoardClick = jest.fn()
-  const wrapper = shallow(<PredictionItem date={1} handleBoardClick={handleBoardClick} state="?" />)
+  const wrapper = shallow(<PredictionItem date={1} fixed={false} handleBoardClick={handleBoardClick} state="?" />)
 
   expect(wrapper.is('[data-date=1]')).toBe(true)
   expect(wrapper.is('[data-state="?"]')).toBe(true)
@@ -20,9 +20,18 @@ test('<PredictionItem date={1} state="?" /> handleBoardClick', () => {
   expect(handleBoardClick).toHaveBeenCalledTimes(1)
   expect(handleBoardClick).toHaveBeenCalledWith('Δ')
 })
-test('<PredictionItem date={1} state="Δ" /> handleBoardClick', () => {
+test('<PredictionItem date={1} fixed state="?" /> handleBoardClick', () => {
   const handleBoardClick = jest.fn()
-  const wrapper = shallow(<PredictionItem date={1} handleBoardClick={handleBoardClick} state="Δ" />)
+  const wrapper = shallow(<PredictionItem date={1} fixed handleBoardClick={handleBoardClick} state="?" />)
+
+  expect(wrapper.is('[data-date=1]')).toBe(true)
+  expect(wrapper.is('[data-state="?"]')).toBe(true)
+  wrapper.simulate('click')
+  expect(handleBoardClick).toHaveBeenCalledTimes(0)
+})
+test('<PredictionItem date={1} fixed={false} state="Δ" /> handleBoardClick', () => {
+  const handleBoardClick = jest.fn()
+  const wrapper = shallow(<PredictionItem date={1} fixed={false} handleBoardClick={handleBoardClick} state="Δ" />)
 
   expect(wrapper.is('[data-date=1]')).toBe(true)
   expect(wrapper.is('[data-state="Δ"]')).toBe(true)
@@ -30,9 +39,18 @@ test('<PredictionItem date={1} state="Δ" /> handleBoardClick', () => {
   expect(handleBoardClick).toHaveBeenCalledTimes(1)
   expect(handleBoardClick).toHaveBeenCalledWith('O')
 })
-test('<PredictionItem date={1} state="O" /> handleBoardClick', () => {
+test('<PredictionItem date={1} fixed state="Δ" /> handleBoardClick', () => {
   const handleBoardClick = jest.fn()
-  const wrapper = shallow(<PredictionItem date={1} handleBoardClick={handleBoardClick} state="O" />)
+  const wrapper = shallow(<PredictionItem date={1} fixed handleBoardClick={handleBoardClick} state="Δ" />)
+
+  expect(wrapper.is('[data-date=1]')).toBe(true)
+  expect(wrapper.is('[data-state="Δ"]')).toBe(true)
+  wrapper.simulate('click')
+  expect(handleBoardClick).toHaveBeenCalledTimes(0)
+})
+test('<PredictionItem date={1} fixed={false} state="O" /> handleBoardClick', () => {
+  const handleBoardClick = jest.fn()
+  const wrapper = shallow(<PredictionItem date={1} fixed={false} handleBoardClick={handleBoardClick} state="O" />)
 
   expect(wrapper.is('[data-date=1]')).toBe(true)
   expect(wrapper.is('[data-state="O"]')).toBe(true)
@@ -40,9 +58,18 @@ test('<PredictionItem date={1} state="O" /> handleBoardClick', () => {
   expect(handleBoardClick).toHaveBeenCalledTimes(1)
   expect(handleBoardClick).toHaveBeenCalledWith('X')
 })
-test('<PredictionItem date={1} state="X" /> handleBoardClick', () => {
+test('<PredictionItem date={1} fixed state="O" /> handleBoardClick', () => {
   const handleBoardClick = jest.fn()
-  const wrapper = shallow(<PredictionItem date={1} handleBoardClick={handleBoardClick} state="X" />)
+  const wrapper = shallow(<PredictionItem date={1} fixed handleBoardClick={handleBoardClick} state="O" />)
+
+  expect(wrapper.is('[data-date=1]')).toBe(true)
+  expect(wrapper.is('[data-state="O"]')).toBe(true)
+  wrapper.simulate('click')
+  expect(handleBoardClick).toHaveBeenCalledTimes(0)
+})
+test('<PredictionItem date={1} fixed={false} state="X" /> handleBoardClick', () => {
+  const handleBoardClick = jest.fn()
+  const wrapper = shallow(<PredictionItem date={1} fixed={false} handleBoardClick={handleBoardClick} state="X" />)
 
   expect(wrapper.is('[data-date=1]')).toBe(true)
   expect(wrapper.is('[data-state="X"]')).toBe(true)
@@ -50,21 +77,30 @@ test('<PredictionItem date={1} state="X" /> handleBoardClick', () => {
   expect(handleBoardClick).toHaveBeenCalledTimes(1)
   expect(handleBoardClick).toHaveBeenCalledWith('?')
 })
-test('<PredictionItem date={1} state="fill" /> handleBoardClick', () => {
+test('<PredictionItem date={1} fixed state="X" /> handleBoardClick', () => {
   const handleBoardClick = jest.fn()
-  const wrapper = shallow(<PredictionItem date={1} handleBoardClick={handleBoardClick} state="fill" />)
+  const wrapper = shallow(<PredictionItem date={1} fixed handleBoardClick={handleBoardClick} state="X" />)
+
+  expect(wrapper.is('[data-date=1]')).toBe(true)
+  expect(wrapper.is('[data-state="X"]')).toBe(true)
+  wrapper.simulate('click')
+  expect(handleBoardClick).toHaveBeenCalledTimes(0)
+})
+test('<PredictionItem date={1} fixed={false} state="fill" /> handleBoardClick', () => {
+  const handleBoardClick = jest.fn()
+  const wrapper = shallow(<PredictionItem date={1} fixed={false} handleBoardClick={handleBoardClick} state="fill" />)
 
   expect(wrapper.is('[data-date=1]')).toBe(true)
   expect(wrapper.is('[data-state="fill"]')).toBe(true)
   wrapper.simulate('click')
   expect(handleBoardClick).toHaveBeenCalledTimes(0)
 })
-test('<PredictionItem date={1} state="fix" /> handleBoardClick', () => {
+test('<PredictionItem date={1} fixed state="fill" /> handleBoardClick', () => {
   const handleBoardClick = jest.fn()
-  const wrapper = shallow(<PredictionItem date={1} handleBoardClick={handleBoardClick} state="fix" />)
+  const wrapper = shallow(<PredictionItem date={1} fixed handleBoardClick={handleBoardClick} state="fill" />)
 
   expect(wrapper.is('[data-date=1]')).toBe(true)
-  expect(wrapper.is('[data-state="fix"]')).toBe(true)
+  expect(wrapper.is('[data-state="fill"]')).toBe(true)
   wrapper.simulate('click')
   expect(handleBoardClick).toHaveBeenCalledTimes(0)
 })
