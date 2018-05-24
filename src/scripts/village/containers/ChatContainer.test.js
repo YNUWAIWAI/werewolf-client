5dfd91be48407c07d6f634b1325db4fbfc4a55da
Yudai Udagawa <nimius.rd@gmail.com>
2018-05-24 11:07:09 +0900

    Add ChatContainer test

---
+++ src/scripts/village/containers/ChatContainer.test.js
@@ -0,0 +1,64 @@
+import ChatContainer from './ChatContainer'
+import React from 'react'
+import {initialState} from '../reducers/chat'
+import {shallow} from 'enzyme'
+
+test('<ChatContainer /> initialState', () => {
+  const getState = () => ({
+    chat: initialState
+  })
+  const subscribe = jest.fn()
+  const store = {
+    getState,
+    subscribe
+  }
+  const wrapper = shallow(<ChatContainer store={store} />)
+
+  expect(wrapper.props().allIds).toEqual([])
+  expect(wrapper.props().byId).toEqual({})
+})
+test('<ChatContainer />', () => {
+  const getState = () => ({
+    chat: {
+      allIds: ['chat0'],
+      byId: {
+        chat0: {
+          id: 12,
+          image: 'https://werewolf.world/image/0.1/Walter.jpg',
+          intensionalDisclosureRange: 'public',
+          isMine: true,
+          name: {
+            en: 'Walter',
+            ja: 'ヴァルター'
+          },
+          phaseStartTime: '2006-10-07T12:06:56.568+09:00',
+          phaseTimeLimit: 600,
+          serverTimestamp: '2006-10-07T12:06:56.568+09:00',
+          text: '>>11\nそれで、あなたは人狼が誰だと思うの？\n\n私はパメラが人狼だと思う。'
+        }
+      }
+    },
+    language: 'ja'
+  })
+  const subscribe = jest.fn()
+  const store = {
+    getState,
+    subscribe
+  }
+  const wrapper = shallow(<ChatContainer store={store} />)
+
+  expect(wrapper.props().allIds).toEqual(['chat0'])
+  expect(wrapper.props().byId).toEqual({
+    chat0: {
+      id: 12,
+      image: 'https://werewolf.world/image/0.1/Walter.jpg',
+      intensionalDisclosureRange: 'public',
+      isMine: true,
+      name: 'ヴァルター',
+      phaseStartTime: '2006-10-07T12:06:56.568+09:00',
+      phaseTimeLimit: 600,
+      serverTimestamp: '2006-10-07T12:06:56.568+09:00',
+      text: '>>11\nそれで、あなたは人狼が誰だと思うの？\n\n私はパメラが人狼だと思う。'
+    }
+  })
+})
