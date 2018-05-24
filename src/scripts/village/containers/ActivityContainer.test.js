56951c2a4d9d186e1a3e51c1b821ada6add966c1
Yudai Udagawa <nimius.rd@gmail.com>
2018-05-24 11:34:27 +0900

    Fix test

--- src/scripts/village/containers/ActivityContainer.test.js
+++ src/scripts/village/containers/ActivityContainer.test.js
@@ -5,11 +5,13 @@ import {initialState} from '../reducers/hideButton'
 import {shallow} from 'enzyme'
 
 test('<ActivityContainer /> initialState', () => {
+  const dispatch = jest.fn()
   const getState = () => ({
     hideButton: initialState
   })
   const subscribe = jest.fn()
   const store = {
+    dispatch,
     getState,
     subscribe
   }
