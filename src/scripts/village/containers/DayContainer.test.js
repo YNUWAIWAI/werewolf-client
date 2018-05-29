e136e4b25d8a792e3e3134149771ca81db02af52
Yudai Udagawa <nimius.rd@gmail.com>
2018-05-29 12:25:24 +0900

    Add DayContainer test

---
+++ src/scripts/village/containers/DayContainer.test.js
@@ -0,0 +1,30 @@
+// @flow
+import {DAY_CONVERSATION} from '../constants/Phase'
+import DayContainer from './DayContainer'
+import React from 'react'
+import {shallow} from 'enzyme'
+
+test('<DayContainer /> initialState', () => {
+  const dispatch = jest.fn()
+  const getState = () => ({
+    base: {
+      date: 1,
+      phase: DAY_CONVERSATION,
+      phaseTimeLimit: 100
+    }
+  })
+  const subscribe = jest.fn()
+  const store = {
+    dispatch,
+    getState,
+    subscribe
+  }
+  const wrapper = shallow(<DayContainer store={store} />)
+
+  expect(wrapper.props().date).toBe(1)
+  expect(wrapper.props().phase).toBe('昼')
+  expect(wrapper.props().timer).toEqual({
+    limit: 100,
+    phase: DAY_CONVERSATION
+  })
+})
