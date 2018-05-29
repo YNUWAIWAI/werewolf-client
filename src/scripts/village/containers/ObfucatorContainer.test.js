f1a0479ddabd5a1298ff678d40156ed5e91c9a6e
Yudai Udagawa <nimius.rd@gmail.com>
2018-05-29 13:11:18 +0900

    Add ObfucatorContainer test

---
+++ src/scripts/village/containers/ObfucatorContainer.test.js
@@ -0,0 +1,22 @@
+// @flow
+import ObfucatorContainer from './ObfucatorContainer'
+import React from 'react'
+import {shallow} from 'enzyme'
+
+test('<ObfucatorContainer />', () => {
+  const dispatch = jest.fn()
+  const getState = () => ({
+    obfucator: {
+      visible: true
+    }
+  })
+  const subscribe = jest.fn()
+  const store = {
+    dispatch,
+    getState,
+    subscribe
+  }
+  const wrapper = shallow(<ObfucatorContainer store={store} />)
+
+  expect(wrapper.props().visible).toBe(true)
+})
