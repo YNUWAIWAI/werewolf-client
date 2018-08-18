794545d529d60156a976c4b014165854abfcc947
Yudai Udagawa <nimius.rd@gmail.com>
2018-08-19 00:42:25 +0900

    Fix story

--- stories/Timer.stories.js
+++ stories/Timer.stories.js
@@ -1,5 +1,6 @@
 // @flow
 import {number, selectV2, withKnobs} from '@storybook/addon-knobs/react'
+import type {Phase} from 'village'
 import React from 'react'
 import Timer from '../src/scripts/village/components/Timer'
 import {storiesOf} from '@storybook/react'
@@ -9,7 +10,7 @@ storiesOf('Timer', module)
   .addDecorator(withKnobs)
   .add('default', withInfo('')(() => {
     const limit = number('limit', 10)
-    const phase = selectV2('phase', [ 'day conversation', 'day vote', 'night', 'post mortem', 'results' ], '')
+    const phase: Phase = selectV2('phase', ['day conversation', 'day vote', 'night', 'post mortem', 'results'], 'day conversation')
     const story = <Timer limit={limit} phase={phase} />
 
     return story
