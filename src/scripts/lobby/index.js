83857da8fe37f3d4e1411811b21f1e405db98e5b
Yudai Udagawa <nimius.rd@gmail.com>
2018-08-18 12:57:11 +0900

    Fix HTML

--- src/scripts/lobby/index.js
+++ src/scripts/lobby/index.js
@@ -4,6 +4,7 @@ import {Provider} from 'react-redux'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {SHOW_MAIN} from './constants/ActionTypes'
+import {changeToken} from './actions'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import {createStore} from 'redux'
 import middleware from './middlewares'
@@ -17,11 +18,37 @@ const store = createStore(
 store.dispatch({
   type: SHOW_MAIN
 })
+const elem = document.getElementById('script')
+
+if (!elem) {
+  throw Error('Not found: script element')
+}
+if (!elem || !elem.dataset || !elem.dataset.humanPlayerToken) {
+  throw Error('Not found: data-human-player-token')
+}
+store.dispatch(changeToken({
+  lobby: 'human player',
+  token: elem.dataset.humanPlayerToken
+}))
+if (!elem || !elem.dataset || !elem.dataset.robotPlayerToken) {
+  throw Error('Not found: data-robot-player-token')
+}
+store.dispatch(changeToken({
+  lobby: 'robot player',
+  token: elem.dataset.robotPlayerToken
+}))
+if (!elem || !elem.dataset || !elem.dataset.onymousAudienceToken) {
+  throw Error('Not found: data-onymous-audience-token')
+}
+store.dispatch(changeToken({
+  lobby: 'onymous audience',
+  token: elem.dataset.onymousAudienceToken
+}))
 
 const root = document.getElementById('root')
 
 if (!root) {
-  throw Error('Not found root element.')
+  throw Error('Not found: root element')
 }
 
 ReactDOM.render(
