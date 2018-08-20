ca692e5a8a9e9be23dd72d494baf9249227a4132
Yudai Udagawa <nimius.rd@gmail.com>
2018-08-20 18:05:11 +0900

    Fix test

--- src/scripts/lobby/components/IdSearchBox.test.js
+++ src/scripts/lobby/components/IdSearchBox.test.js
@@ -6,5 +6,5 @@ import {shallow} from 'enzyme'
 test('<IdSearchBox />', () => {
   const wrapper = shallow(<IdSearchBox numberOfDigit={3} placeholder="3 digits" />)
 
-  expect(wrapper.find('id-search')).toHaveLength(3)
+  expect(wrapper.children()).toHaveLength(3)
 })
