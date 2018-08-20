dab9791fc308ca6a9d684e71feaabbbaefee9aa2
Yudai Udagawa <nimius.rd@gmail.com>
2018-08-21 03:23:49 +0900

    Add test

--- src/scripts/lobby/reducers/advancedSearch.test.js
+++ src/scripts/lobby/reducers/advancedSearch.test.js
@@ -1,6 +1,9 @@
 // @flow
 import * as ActionTypes from '../constants/ActionTypes'
 import reducer, {initialState} from './advancedSearch'
+import Ajv from 'ajv'
+import {VERSION} from '../constants/Version'
+import fetch from 'node-fetch'
 
 test('SHOW_LOBBY_FOR_AUDIENCE', () => {
   expect(
@@ -92,3 +95,46 @@ test('SHOW_LOBBY_FOR_ROBOT_PLAYER', () => {
     }
   )
 })
+describe('SOCKET_MESSAGE', () => {
+  const BASE_URI = `https://werewolf.world/lobby/schema/${VERSION}`
+  const SERVER2CLIENT = `${BASE_URI}/server2client`
+  const ajv = new Ajv()
+  const avatarPayload = {
+    error: null,
+    image: '/assets/images/avatar/default/user.png',
+    name: 'Alice',
+    token: '3F2504E0-4F89-11D3-9A0C-0305E82C3301',
+    type: 'avatar'
+  }
+
+  test('validate the JSON of avatar', async () => {
+    expect.hasAssertions()
+    await fetch(`${SERVER2CLIENT}/avatar.json`)
+      .then(res => res.json())
+      .then(schema => {
+        const validate = ajv.validate(schema, avatarPayload)
+
+        expect(validate).toBe(true)
+      })
+  })
+
+  test('reduce correctly', () => {
+    expect(
+      reducer(
+        initialState,
+        {
+          payload: avatarPayload,
+          type: ActionTypes.SOCKET_MESSAGE
+        }
+      )
+    ).toEqual(
+      {
+        header: '',
+        image: '/assets/images/avatar/default/user.png',
+        menuItems: [],
+        name: 'Alice'
+      }
+    )
+  })
+})
+
--- src/scripts/lobby/reducers/idSearch.test.js
+++ src/scripts/lobby/reducers/idSearch.test.js
@@ -1,6 +1,9 @@
 // @flow
 import * as ActionTypes from '../constants/ActionTypes'
 import reducer, {initialState} from './idSearch'
+import Ajv from 'ajv'
+import {VERSION} from '../constants/Version'
+import fetch from 'node-fetch'
 
 test('SHOW_LOBBY_FOR_AUDIENCE', () => {
   expect(
@@ -113,4 +116,47 @@ test('CHANGE_SEARCH_ID', () => {
       name: ''
     }
   )
+})
+describe('SOCKET_MESSAGE', () => {
+  const BASE_URI = `https://werewolf.world/lobby/schema/${VERSION}`
+  const SERVER2CLIENT = `${BASE_URI}/server2client`
+  const ajv = new Ajv()
+  const avatarPayload = {
+    error: null,
+    image: '/assets/images/avatar/default/user.png',
+    name: 'Alice',
+    token: '3F2504E0-4F89-11D3-9A0C-0305E82C3301',
+    type: 'avatar'
+  }
+
+  test('validate the JSON of avatar', async () => {
+    expect.hasAssertions()
+    await fetch(`${SERVER2CLIENT}/avatar.json`)
+      .then(res => res.json())
+      .then(schema => {
+        const validate = ajv.validate(schema, avatarPayload)
+
+        expect(validate).toBe(true)
+      })
+  })
+
+  test('reduce correctly', () => {
+    expect(
+      reducer(
+        initialState,
+        {
+          payload: avatarPayload,
+          type: ActionTypes.SOCKET_MESSAGE
+        }
+      )
+    ).toEqual(
+      {
+        header: '',
+        id: -1,
+        image: '/assets/images/avatar/default/user.png',
+        menuItems: [],
+        name: 'Alice'
+      }
+    )
+  })
 })
