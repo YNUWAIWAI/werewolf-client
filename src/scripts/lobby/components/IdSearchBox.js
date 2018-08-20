493ee09741937945a8bb0c3727cb63e149d2fd7f
Yudai Udagawa <nimius.rd@gmail.com>
2018-08-20 17:51:17 +0900

    Update IdSearchBox

--- src/scripts/lobby/components/IdSearchBox.js
+++ src/scripts/lobby/components/IdSearchBox.js
@@ -2,13 +2,26 @@
 import React from 'react'
 
 export type Props = {
-  text: string
+  placeholder: string,
+  numberOfDigit: number
 }
 
 export default function IdSearchBox(props: Props) {
   return (
     <div className="id-search">
-      {props.text}
+      <span className="id-search--sharp">
+        {'#'}
+      </span>
+      <input
+        className="id-search--input"
+        maxLength={props.numberOfDigit}
+        minLength={props.numberOfDigit}
+        pattern={`^[0-9]{${props.numberOfDigit}}$`}
+        placeholder={props.placeholder}
+        required
+        type="text"
+      />
+      <span className="id-search--validity" />
     </div>
   )
 }
