1620da6ccb32505e0f308220ca7043fa1f02a523
Yudai Udagawa <nimius.rd@gmail.com>
2018-08-22 16:45:25 +0900

    Add AdvancedSearchBoxRow

--- src/scripts/lobby/components/AdvancedSearchBox.js
+++ src/scripts/lobby/components/AdvancedSearchBox.js
@@ -1,4 +1,5 @@
 // @flow
+import AdvancedSearchBoxRow from './AdvancedSearchBoxRow'
 import React from 'react'
 
 export type Props = {
@@ -10,6 +11,7 @@ export default function AdvancedSearchBox(props: Props) {
     const elem = document.getElementById(event.target.dataset.checkboxId)
 
     if (event.target.validity.valid && event.target.value !== '') {
+      console.log(event.target.validity.valid)
       console.log(event.target.value)
       elem.checked = true
     } else {
@@ -19,125 +21,62 @@ export default function AdvancedSearchBox(props: Props) {
 
   return (
     <div className="advanced-search">
-      <div className="advanced-search--prop">
-        <input
-          id="villageName"
-          name="villageName"
-          type="checkbox"
-        />
-        <label htmlFor="villageName">
-          {'Village Name'}
-        </label>
-      </div>
-      <input
-        className="advanced-search--input"
-        data-checkbox-id="villageName"
-        maxLength="30"
-        minLength="5"
-        onChange={handleChange}
+      <AdvancedSearchBoxRow
+        checked={false}
+        handleChange={handleChange}
+        id="villageName"
+        max={30}
+        min={5}
+        name="Village Name"
         placeholder="5-30 chars"
         type="text"
       />
-      <div className="advanced-search--prop">
-        <input
-          id="hostName"
-          name="hostName"
-          type="checkbox"
-        />
-        <label htmlFor="hostName">
-          {'Host Name'}
-        </label>
-      </div>
-      <input
-        className="advanced-search--input"
-        data-checkbox-id="hostName"
-        maxLength="15"
-        minLength="5"
-        onChange={handleChange}
+      <AdvancedSearchBoxRow
+        checked={false}
+        handleChange={handleChange}
+        id="hostName"
+        max={15}
+        min={5}
+        name="Host Name"
         placeholder="5-15 chars"
         type="text"
       />
-      <div className="advanced-search--prop">
-        <input
-          id="minimum"
-          name="minimum"
-          type="checkbox"
-          value="minimum"
-        />
-        <label htmlFor="minimum">
-          {'Minimum'}
-        </label>
-      </div>
-      <input
-        className="advanced-search--input"
-        data-checkbox-id="minimum"
-        max="15"
-        maxLength="2"
-        min="4"
-        onChange={handleChange}
+      <AdvancedSearchBoxRow
+        checked={false}
+        handleChange={handleChange}
+        id="minimum"
+        max={15}
+        min={5}
+        name="Minimum"
         placeholder="4-15"
         type="number"
       />
-      <div className="advanced-search--prop">
-        <input
-          id="maximum"
-          name="maximum"
-          type="checkbox"
-          value="maximum"
-        />
-        <label htmlFor="maximum">
-          {'Maximum'}
-        </label>
-      </div>
-      <input
-        className="advanced-search--input"
-        data-checkbox-id="maximum"
-        max="15"
-        min="4"
-        onChange={handleChange}
+      <AdvancedSearchBoxRow
+        checked={false}
+        handleChange={handleChange}
+        id="maximum"
+        max={15}
+        min={5}
+        name="Maximum"
         placeholder="4-15"
         type="number"
       />
-      <div className="advanced-search--prop">
-        <input
-          id="avatar"
-          name="avatar"
-          type="checkbox"
-          value="avatar"
-        />
-        <label htmlFor="avatar">
-          {'Avatar'}
-        </label>
-      </div>
-      <input
-        className="advanced-search--input"
-        data-checkbox-id="avatar"
-        max="15"
-        min="4"
-        onChange={handleChange}
-        placeholder="4-15"
-        type="number"
+      <AdvancedSearchBoxRow
+        checked
+        handleChange={handleChange}
+        id="avatar"
+        name="Avatar"
+        type="select"
       />
-      <div className="advanced-search--prop">
-        <input
-          id="comment"
-          name="comment"
-          type="checkbox"
-          value="comment"
-        />
-        <label htmlFor="comment">
-          {'Comment'}
-        </label>
-      </div>
-      <textarea
-        className="advanced-search--input"
-        data-checkbox-id="comment"
-        maxLength="100"
-        minLength="0"
-        onChange={handleChange}
+      <AdvancedSearchBoxRow
+        checked={false}
+        handleChange={handleChange}
+        id="comment"
+        max={100}
+        min={0}
+        name="Comment"
         placeholder="0-100 chars"
-        rows="3"
-        type="text"
+        type="textarea"
       />
     </div>
   )
---
+++ src/scripts/lobby/components/AdvancedSearchBoxRow.js
@@ -0,0 +1,109 @@
+// @flow
+/* eslint react/forbid-component-props: 0 */
+import React, {Fragment} from 'react'
+import Select from 'react-select'
+
+export type Props = {
+  checked: boolean,
+  handleChange: SyntheticInputEvent<HTMLInputElement> => void,
+  id: string,
+  max: number,
+  min: number,
+  name: string,
+  placeholder: string,
+  type: 'number' | 'text' | 'textarea'
+} | {
+  checked: boolean,
+  handleChange: SyntheticInputEvent<HTMLInputElement> => void,
+  id: string,
+  name: string,
+  type: 'select'
+}
+
+export default function AdvancedSearchBoxRow(props: Props) {
+  const InputElem = () => {
+    switch (props.type) {
+      case 'number':
+        return (
+          <input
+            className="advanced-search--input"
+            data-checkbox-id={props.id}
+            max={props.max}
+            min={props.min}
+            onChange={props.handleChange}
+            placeholder={props.placeholder}
+            type={props.type}
+          />
+        )
+      case 'select': {
+        const options = [
+          {
+            label: 'Random',
+            value: 'random'
+          },
+          {
+            label: 'Fixed',
+            value: 'fixed'
+          },
+          {
+            label: 'Unspecified',
+            value: 'unspecified'
+          }
+        ]
+
+        return (
+          <Select
+            className="advanced-search--input"
+            classNamePrefix="react-select"
+            defaultValue={options[0]}
+            name="avatar"
+            onChange={props.handleChange}
+            options={options}
+          />
+        )
+      }
+      case 'text':
+        return (
+          <input
+            className="advanced-search--input"
+            data-checkbox-id={props.id}
+            maxLength={props.max}
+            minLength={props.min}
+            onChange={props.handleChange}
+            placeholder={props.placeholder}
+            type={props.type}
+          />
+        )
+      case 'textarea':
+        return (
+          <textarea
+            className="advanced-search--input"
+            data-checkbox-id={props.id}
+            maxLength={props.max}
+            minLength={props.min}
+            onChange={props.handleChange}
+            placeholder={props.placeholder}
+            rows="3"
+          />
+        )
+      default:
+        return ''
+    }
+  }
+
+  return (
+    <Fragment>
+      <div className="advanced-search--prop">
+        <input
+          checked={props.checked}
+          id={props.id}
+          type="checkbox"
+        />
+        <label htmlFor={props.id}>
+          {props.name}
+        </label>
+      </div>
+      <InputElem />
+    </Fragment>
+  )
+}
