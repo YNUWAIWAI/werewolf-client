071357d32f0403320721e783158a326e9b650dc7
Yudai Udagawa <nimius.rd@gmail.com>
2018-09-05 13:56:52 +0900

    Add svg

---
+++ src/scripts/lobby/components/svg/Safe.js
@@ -0,0 +1,18 @@
+// @flow
+import React from 'react'
+
+export default function Safe() {
+  return (
+    <svg
+      className="safe"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0 0h24v24H0z"
+        fill="none"
+      />
+      <path d="M17 4h3v16h-3zM5 14h3v6H5zm6-5h3v11h-3z" />
+    </svg>
+  )
+}
