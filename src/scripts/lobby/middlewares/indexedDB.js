d751e332893fd107da2e8afa206163f0fa72439c
Yudai Udagawa <nimius.rd@gmail.com>
2018-09-06 15:34:58 +0900

    wip

--- src/scripts/lobby/middlewares/indexedDB.js
+++ src/scripts/lobby/middlewares/indexedDB.js
@@ -5,13 +5,28 @@ import type {Middleware} from 'redux'
 import type {ReducerState} from '../reducers'
 import {socket as socketAction} from '../actions'
 
+const dbName = 'lobby'
 let db
 const indexedDBMiddleware: Middleware<ReducerState, Action> = store => next => action => {
   const connectDB = () => {
-    const request = window.indexedDB.open('lobby')
+    const request = window.indexedDB.open(dbName)
 
     request.onerror = event => {
       console.error(event.target.errorCode)
+    }
+    request.onupgradeneeded = event => {
+      const db_ = event.target.result
+      const objectStore = db_.createObjectStore('lastVisited',  {keyPath: 'token'})
+
+      objectStore.createIndex('villageId', 'villageId', {unique: false})
+      objectStore.transaction.oncomplete = e =>{
+        const lastVisitedObjectStore = db_.transaction('lastVisited', 'readwrite').objectStore('lastVisited')
+
+        lastVisitedObjectStore.add({
+          token: '3F2504E0-4F89-11D3-9A0C-0305E82C3302',
+          villageId: 1
+        })
+      }
     }
     request.onsuccess = event => {
       db = event.target.result
