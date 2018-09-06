b43b32973f82f55bde294b7716ffea15f7588066
Yudai Udagawa <nimius.rd@gmail.com>
2018-09-07 03:27:37 +0900

    wip

--- src/scripts/lobby/middlewares/socket.js
+++ src/scripts/lobby/middlewares/socket.js
@@ -9,7 +9,9 @@ let socket
 const socketMiddleware: ({url: string}) => Middleware<ReducerState, Action> = option => store => next => action => {
   const connectWebSocket = (url => () => new Promise((resolve, reject) => {
     if (socket) {
-      while (socket.readyState === 1) {}
+      while (socket.readyState === 1) {
+        setTimeout(console.log(socket.readyState), 10)
+      }
       resolve(socket)
     }
     socket = new WebSocket(url)
