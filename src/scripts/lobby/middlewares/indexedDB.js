b9bd7acc54ae29b1fe4b7eb204fadcfbb3bfbb55
Yudai Udagawa <nimius.rd@gmail.com>
2018-09-06 15:41:56 +0900

    wip

--- src/scripts/lobby/middlewares/indexedDB.js
+++ src/scripts/lobby/middlewares/indexedDB.js
@@ -14,22 +14,9 @@ const indexedDBMiddleware: Middleware<ReducerState, Action> = store => next => a
     request.onerror = event => {
       console.error(event.target.errorCode)
     }
-    request.onupgradeneeded = event => {
-      const db_ = event.target.result
-      const objectStore = db_.createObjectStore('lastVisited',  {keyPath: 'token'})
-
-      objectStore.createIndex('villageId', 'villageId', {unique: false})
-      objectStore.transaction.oncomplete = e =>{
-        const lastVisitedObjectStore = db_.transaction('lastVisited', 'readwrite').objectStore('lastVisited')
-
-        lastVisitedObjectStore.add({
-          token: '3F2504E0-4F89-11D3-9A0C-0305E82C3302',
-          villageId: 1
-        })
-      }
-    }
     request.onsuccess = event => {
       db = event.target.result
+
       db.onabort = e => {
         console.error('Database opening aborted!', e)
       }
@@ -43,6 +30,18 @@ const indexedDBMiddleware: Middleware<ReducerState, Action> = store => next => a
         console.log('version changed', e)
       }
       console.log('success', db)
+      const objectStore = db.createObjectStore('lastVisited',  {keyPath: 'token'})
+
+      objectStore.createIndex('villageId', 'villageId', {unique: false})
+      objectStore.transaction.oncomplete = e =>{
+        const lastVisitedObjectStore = db.transaction('lastVisited', 'readwrite').objectStore('lastVisited')
+
+        lastVisitedObjectStore.add({
+          token: '3F2504E0-4F89-11D3-9A0C-0305E82C3302',
+          villageId: 1
+        })
+      }
+
     }
   }
 
