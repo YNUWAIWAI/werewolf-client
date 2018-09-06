a75cde81f8a90d6b199629d86793cfe3135b6b76
Yudai Udagawa <nimius.rd@gmail.com>
2018-09-07 02:27:25 +0900

    wip

--- src/scripts/lobby/middlewares/socket.js
+++ src/scripts/lobby/middlewares/socket.js
@@ -5,36 +5,43 @@ import type {Middleware} from 'redux'
 import type {ReducerState} from '../reducers'
 import {socket as socketAction} from '../actions'
 
-let socket
-let retryCount = 0
+const socketMiddleware: ({url: string, retryLimit?: number}) => Middleware<ReducerState, Action> = option => store => next => action => {
+  const retryLimit = option.retryLimit || 5
+  const connectWebSocket = (url => {
+    let socket
+    let retryCount = 0
 
-const socketMiddleware: ({url: string, retry?: number}) => Middleware<ReducerState, Action> = option => store => next => action => {
-  const retry = option.retry || 5
-  const connectWebSocket = url => {
-    socket = new WebSocket(url)
-    socket.onopen = event => {
-      console.info('WebSocket Connected ', event)
-      store.dispatch(socketAction.open(event))
-    }
-    socket.onclose = event => {
-      console.warn('WebSocket Disconnected ', event)
-      socket = null
-      store.dispatch(socketAction.close(event))
-    }
-    socket.onerror = error => {
-      console.error('WebSocket Error ', error)
-      socket = null
-      store.dispatch(socketAction.error(error))
-    }
-    socket.onmessage = event => {
-      store.dispatch(socketAction.message(event))
-    }
-  }
-
-  if (!socket && retryCount < retry) {
-    retryCount += 1
-    connectWebSocket(option.url)
-  }
+    return () => new Promise((resolve, reject) => {
+      if (socket) {
+        resolve(socket)
+      }
+      if (retryCount >= retryLimit) {
+        reject('Exceeded retry limit')
+      }
+      socket = new WebSocket(url)
+      socket.onopen = event => {
+        console.info('WebSocket Connected ', event)
+        store.dispatch(socketAction.open(event))
+        retryCount += 1
+        resolve(socket)
+      }
+      socket.onclose = event => {
+        console.warn('WebSocket Disconnected ', event)
+        socket = null
+        store.dispatch(socketAction.close(event))
+        reject(event)
+      }
+      socket.onerror = error => {
+        console.error('WebSocket Error ', error)
+        socket = null
+        store.dispatch(socketAction.error(error))
+        reject(error)
+      }
+      socket.onmessage = event => {
+        store.dispatch(socketAction.message(event))
+      }
+    })
+  })(option.url)
 
   switch (action.type) {
     case types.SOCKET_OPEN:
@@ -46,9 +53,13 @@ const socketMiddleware: ({url: string, retry?: number}) => Middleware<ReducerSta
     case types.SOCKET_MESSAGE:
       return next(action)
     case types.SOCKET_SEND:
-      if (socket) {
-        socket.send(JSON.stringify(action.payload))
-      }
+      connectWebSocket()
+        .then(socket => {
+          socket.send(JSON.stringify(action.payload))
+        })
+        .catch(reason => {
+          console.error(reason)
+        })
 
       return next(action)
     default:
