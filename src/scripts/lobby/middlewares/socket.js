536b96feb82abaa977bfbe0dc64ee9ff7b451d95
Yudai Udagawa <nimius.rd@gmail.com>
2018-09-08 05:06:36 +0900

    wip

--- src/scripts/lobby/middlewares/socket.js
+++ src/scripts/lobby/middlewares/socket.js
@@ -26,12 +26,12 @@ const connectWebSocket = (() => {
         socket.onclose = event => {
           console.warn(`WebSocket Disconnected code: ${event.code} wasClean: ${String(event.wasClean)} reason: ${event.reason}`)
           store.dispatch(socketAction.close(event))
-          setTimeout(wait, 0)
+          setTimeout(wait, 100)
         }
         socket.onerror = error => {
           console.error('WebSocket Error ', error)
           store.dispatch(socketAction.error(error))
-          setTimeout(wait, 0)
+          setTimeout(wait, 100)
         }
         socket.onmessage = event => {
           store.dispatch(socketAction.message(event))
