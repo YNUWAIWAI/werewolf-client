87eb3a216b7c8356e03d7e44f1635341bc70ac7e
Yudai Udagawa <nimius.rd@gmail.com>
2018-09-22 05:47:13 +0900

    Add Audience.svg

--- src/scripts/village/components/ChatIcon.js
+++ src/scripts/village/components/ChatIcon.js
@@ -1,5 +1,6 @@
 // @flow
-import type {ChatChannel} from 'village'
+import Audience from './svg/Audience'
+import type {Channel} from 'village'
 import Grave from './svg/Grave'
 import Master from './svg/Master'
 import Private from './svg/Private'
@@ -8,44 +9,54 @@ import React from 'react'
 import Werewolf from './svg/Werewolf'
 
 export type Props = {
-  +channel: ChatChannel
+  +channel: Channel,
+  +className: string
 }
 
 export default function ChatIcon(props: Props) {
   switch (props.channel) {
+    case 'anonymousAudience':
+    case 'onymousAudience':
+      return (
+        <div className={props.className}>
+          <Audience />
+        </div>
+      )
     case 'grave':
       return (
-        <div className="chat--icon">
+        <div className={props.className}>
           <Grave />
         </div>
       )
-    case 'limited':
+    case 'hunter':
+    case 'seer':
+    case 'werewolf':
       return (
-        <div className="chat--icon">
+        <div className={props.className}>
           <Werewolf />
         </div>
       )
     case 'master':
       return (
-        <div className="chat--icon">
+        <div className={props.className}>
           <Master />
         </div>
       )
     case 'private':
       return (
-        <div className="chat--icon">
+        <div className={props.className}>
           <Private />
         </div>
       )
     case 'public':
       return (
-        <div className="chat--icon">
+        <div className={props.className}>
           <Public />
         </div>
       )
     default:
       return (
-        <div className="chat--icon" />
+        <div className={props.className} />
       )
   }
 }
--- src/scripts/village/components/ChatItem.js
+++ src/scripts/village/components/ChatItem.js
@@ -22,14 +22,33 @@ export type Props = {
 
 export default function ChatItem(props: Props) {
   return (
-    <div className={`chat--item ${props.isMine ? 'me' : ''} ${getChatChannel(props.intensionalDisclosureRange)}`} id={getChatChannel(props.intensionalDisclosureRange) === 'public' ? `message${String(props.id)}` : undefined}>
+    <div
+      className={`chat--item ${props.isMine ? 'me' : ''} ${getChatChannel(props.intensionalDisclosureRange)}`}
+      id={getChatChannel(props.intensionalDisclosureRange) === 'public' ? `message${String(props.id)}` : undefined}
+    >
       <div className="chat--arrow-box">
-        <ChatIcon channel={getChatChannel(props.intensionalDisclosureRange)} />
-        <ChatNum id={props.id} intensionalDisclosureRange={props.intensionalDisclosureRange} />
-        <ChatText text={props.text} />
-        <ChatDate from={props.phaseStartTime} limit={props.phaseTimeLimit} to={props.serverTimestamp} />
+        <ChatIcon
+          channel={props.intensionalDisclosureRange}
+          className="chat--icon"
+        />
+        <ChatNum
+          id={props.id}
+          intensionalDisclosureRange={props.intensionalDisclosureRange}
+        />
+        <ChatText
+          text={props.text}
+        />
+        <ChatDate
+          from={props.phaseStartTime}
+          limit={props.phaseTimeLimit}
+          to={props.serverTimestamp}
+        />
       </div>
-      <AgentIcon class="chat--agent" image={props.image} name={props.name} />
+      <AgentIcon
+        class="chat--agent"
+        image={props.image}
+        name={props.name}
+      />
     </div>
   )
 }
