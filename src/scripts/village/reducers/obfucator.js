c65de4e3cec9ef3dc98674e9892fe9df619d424b
Yudai Udagawa <nimius.rd@gmail.com>
2018-09-30 02:14:34 +0900

    Fix obfucator

--- src/scripts/village/reducers/obfucator.js
+++ src/scripts/village/reducers/obfucator.js
@@ -1,12 +1,17 @@
 // @flow
 import * as ActionTypes from '../constants/ActionTypes'
-import {type ToggleObfucator} from '../actions'
+import type {HideResult, SelectNo, SelectOption, SelectYes, ToggleObfucator} from '../actions'
 
 export type State = {
   +visible: boolean
 }
 type Action =
+  | HideResult
+  | SelectNo
+  | SelectOption
+  | SelectYes
   | ToggleObfucator
+  | {type: 'SHOW_RESULT'}
 
 export const initialState = {
   visible: true
@@ -14,6 +19,26 @@ export const initialState = {
 
 const obfucator = (state: State = initialState, action: Action): State => {
   switch (action.type) {
+    case ActionTypes.HIDE_RESULT:
+      return {
+        visible: false
+      }
+    case ActionTypes.SELECT_NO:
+      return {
+        visible: false
+      }
+    case ActionTypes.SELECT_OPTION:
+      return {
+        visible: true
+      }
+    case ActionTypes.SELECT_YES:
+      return {
+        visible: false
+      }
+    case ActionTypes.SHOW_RESULT:
+      return {
+        visible: true
+      }
     case ActionTypes.TOGGLE_OBFUCATOR:
       return {
         visible: action.visible
