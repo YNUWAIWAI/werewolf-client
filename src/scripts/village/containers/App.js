2d3d531e0f5d83a13af8509587b328bc1d636d55
Yudai Udagawa <nimius.rd@gmail.com>
2018-10-03 16:10:07 +0900

    Add IntlProvider

--- src/scripts/village/containers/App.js
+++ src/scripts/village/containers/App.js
@@ -3,6 +3,7 @@ import Activity from './ActivityContainer'
 import Command from './CommandContainer'
 import HideButton from './HideButtonContainer'
 import Info from '../components/Info'
+import IntlProvider from './IntlProviderContainer'
 import Modal from './ModalContainer'
 import Obfucator from './ObfucatorContainer'
 import React from 'react'
@@ -10,14 +11,16 @@ import Result from './ResultContainer'
 
 export default function App() {
   return (
-    <div className="app">
-      <Info />
-      <Activity />
-      <Command />
-      <HideButton />
-      <Modal />
-      <Result />
-      <Obfucator />
-    </div>
+    <IntlProvider>
+      <div className="app">
+        <Info />
+        <Activity />
+        <Command />
+        <HideButton />
+        <Modal />
+        <Result />
+        <Obfucator />
+      </div>
+    </IntlProvider>
   )
 }
