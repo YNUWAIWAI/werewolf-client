7a3514abf05b5049c3cf67a5991ab64424100ca9
Yudai Udagawa <nimius.rd@gmail.com>
2018-10-06 19:29:38 +0900

    Test Settings

--- src/scripts/lobby/components/Settings.test.js
+++ src/scripts/lobby/components/Settings.test.js
@@ -4,12 +4,35 @@ import Settings from './Settings'
 import {shallow} from 'enzyme'
 
 test('<Settings />', () => {
-  const items = []
+  const initialValue = {
+    language: 'ja',
+    userEmail: 'example@example.com',
+    userName: 'userName'
+  }
+  const menuItems = []
+  const handleChangeLanguage = jest.fn()
+  const handleChangeUserEmail = jest.fn()
+  const handleChangeUserName = jest.fn()
+  const handleChangeUserPassword = jest.fn()
   const transition = jest.fn()
-  const wrapper = shallow(<Settings menuItems={items} transition={transition} />)
+  const wrapper = shallow(
+    <Settings
+      handleChangeLanguage={handleChangeLanguage}
+      handleChangeUserEmail={handleChangeUserEmail}
+      handleChangeUserName={handleChangeUserName}
+      handleChangeUserPassword={handleChangeUserPassword}
+      initialValue={initialValue}
+      menuItems={menuItems}
+      transition={transition}
+    />
+  )
 
   expect(wrapper.find('Header').exists()).toBe(true)
   expect(wrapper.find('MainContent').exists()).toBe(true)
   expect(wrapper.find('AsideContent').exists()).toBe(true)
+  expect(handleChangeLanguage).toHaveBeenCalledTimes(0)
+  expect(handleChangeUserEmail).toHaveBeenCalledTimes(0)
+  expect(handleChangeUserName).toHaveBeenCalledTimes(0)
+  expect(handleChangeUserPassword).toHaveBeenCalledTimes(0)
   expect(transition).toHaveBeenCalledTimes(0)
 })
