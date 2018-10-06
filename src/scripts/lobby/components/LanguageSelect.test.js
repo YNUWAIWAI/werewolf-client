1e872292ca24d84e2183fb0e19a39567e1dc1115
Yudai Udagawa <nimius.rd@gmail.com>
2018-10-06 19:43:37 +0900

    Test LanguageSelect

---
+++ src/scripts/lobby/components/LanguageSelect.test.js
@@ -0,0 +1,59 @@
+// @flow
+import LanguageSelect from './LanguageSelect'
+import React from 'react'
+import Select from 'react-select'
+import {shallow} from 'enzyme'
+
+describe('<LanguageSelect />', () => {
+  test('render', () => {
+    const handleChangeInner = jest.fn()
+    const handleChange = jest.fn(() => handleChangeInner)
+    const wrapper = shallow(
+      <LanguageSelect
+        className="className"
+        defaultValue="ja"
+        handleChange={handleChange}
+      />
+    )
+
+    expect(wrapper.find(Select)).toHaveLength(1)
+    expect(handleChange).toHaveBeenCalledTimes(0)
+  })
+  test('onChange valid', () => {
+    const handleChangeInner = jest.fn()
+    const handleChange = jest.fn(() => handleChangeInner)
+    const wrapper = shallow(
+      <LanguageSelect
+        className="className"
+        defaultValue="ja"
+        handleChange={handleChange}
+      />
+    )
+
+    wrapper.find(Select).simulate('change', {
+      label: 'English',
+      value: 'en'
+    })
+    expect(handleChange).toHaveBeenCalledTimes(1)
+    expect(handleChange).toHaveBeenCalledWith(true)
+    expect(handleChangeInner).toHaveBeenCalledTimes(1)
+    expect(handleChangeInner).toHaveBeenCalledWith('en')
+  })
+  test('onChange invalid', () => {
+    const handleChangeInner = jest.fn()
+    const handleChange = jest.fn(() => handleChangeInner)
+    const wrapper = shallow(
+      <LanguageSelect
+        className="className"
+        defaultValue="ja"
+        handleChange={handleChange}
+      />
+    )
+
+    wrapper.find(Select).simulate('change', [])
+    expect(handleChange).toHaveBeenCalledTimes(1)
+    expect(handleChange).toHaveBeenCalledWith(false)
+    expect(handleChangeInner).toHaveBeenCalledTimes(1)
+    expect(handleChangeInner).toHaveBeenCalledWith('en')
+  })
+})
