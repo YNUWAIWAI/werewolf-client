66be5990cc0bf983455f77011791eecb5ccf0857
Yudai Udagawa <nimius.rd@gmail.com>
2018-10-06 19:30:39 +0900

    Test settings

--- src/scripts/lobby/reducers/fakeServer/index.js
+++ src/scripts/lobby/reducers/fakeServer/index.js
@@ -3,10 +3,12 @@ import enterHumanPlayerLobbyJSON from './enterHumanPlayerLobby'
 import enterOnymousAudienceLobbyJSON from './enterOnymousAudienceLobby'
 import enterRobotPlayerLobbyJSON from './enterRobotPlayerLobby'
 import pingJSON from './ping'
+import settingsJSON from './settings'
 import waitingPageJSON from './waitingPage'
 
 export const enterHumanPlayerLobby: Object = enterHumanPlayerLobbyJSON
 export const enterOnymousAudienceLobby: Object = enterOnymousAudienceLobbyJSON
 export const enterRobotPlayerLobby: Object = enterRobotPlayerLobbyJSON
 export const ping: Object = pingJSON
+export const settings: Object = settingsJSON
 export const waitingPage: Object = waitingPageJSON
