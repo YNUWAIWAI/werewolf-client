bf7ece822fae8241bef0c2efc5851afd83426e88
Yudai Udagawa <nimius.rd@gmail.com>
2018-10-11 05:13:30 +0900

    Fix ModalContainer

--- src/i18n/village/ja.js
+++ src/i18n/village/ja.js
@@ -3,5 +3,10 @@ export default {
   'CommandInput.placeholder.postMortem': '感想戦',
   'CommandInput.placeholder.private': '非公開用',
   'CommandInput.placeholder.public': '公開用',
-  'CommandInput.send': '送信'
+  'CommandInput.send': '送信',
+  'ModalContainer.dayVote': '投票先はこちらでいいですか？',
+  'ModalContainer.hunterVote': '守護先はこちらでいいですか？',
+  'ModalContainer.seerVote': '占い先はこちらでいいですか？',
+  'ModalContainer.wait': '待ってください',
+  'ModalContainer.werewolfVote': '襲撃先はこちらでいいですか？',
 }
--- src/scripts/village/containers/ModalContainer.js
+++ src/scripts/village/containers/ModalContainer.js
@@ -1,11 +1,13 @@
 // @flow
+import type {Language, Phase, RoleId} from 'village'
 import Modal, {type DispatchProps, type StateProps} from '../components/Modal'
-import type {Phase, RoleId} from 'village'
 import {type SelectNo, type SelectYes, selectNo, selectYes} from '../actions'
 import {DAY_VOTE} from '../constants/Phase'
 import type {Dispatch} from 'redux'
+import {IntlProvider} from 'react-intl'
 import type {ReducerState} from '../reducers'
 import {connect} from 'react-redux'
+import {getMessages} from '../../../i18n/village'
 import {getRoleId} from '../constants/Role'
 import {just} from '../util'
 
@@ -13,45 +15,38 @@ type Action =
   | SelectNo
   | SelectYes
 
-const getText = (phase: Phase, role: RoleId) => {
+const getText = (phase: Phase, role: RoleId, language: Language) => {
+  const {intl} = new IntlProvider(
+    {
+      locale: language,
+      messages: getMessages(language)
+    },
+    {}
+  ).getChildContext()
+
   if (phase === DAY_VOTE) {
-    return {
-      en: 'EN',
-      fr: 'FR',
-      it: 'IT',
-      ja: '投票先はこちらでいいですか？'
-    }
+    return intl.formatMessage({
+      id: 'ModalContainer.dayVote'
+    })
   }
 
   switch (role) {
     case 'werewolf':
-      return {
-        en: 'EN',
-        fr: 'FR',
-        it: 'IT',
-        ja: '襲撃先はこちらでいいですか？'
-      }
+      return intl.formatMessage({
+        id: 'ModalContainer.werewolfVote'
+      })
     case 'seer':
-      return {
-        en: 'EN',
-        fr: 'FR',
-        it: 'IT',
-        ja: '占い先はこちらでいいですか？'
-      }
+      return intl.formatMessage({
+        id: 'ModalContainer.seerVote'
+      })
     case 'hunter':
-      return {
-        en: 'EN',
-        fr: 'FR',
-        it: 'IT',
-        ja: '守護先はこちらでいいですか？'
-      }
+      return intl.formatMessage({
+        id: 'ModalContainer.hunterVote'
+      })
     default:
-      return {
-        en: '',
-        fr: 'FR',
-        it: 'IT',
-        ja: '待ってください'
-      }
+      return intl.formatMessage({
+        id: 'ModalContainer.wait'
+      })
   }
 }
 
@@ -65,7 +60,7 @@ const mapStateToProps = (state: ReducerState): $Exact<StateProps> => {
       id: selectedAgent.id,
       image: selectedAgent.image,
       name: selectedAgent.name[state.language],
-      text: getText(state.base.phase, getRoleId(myRole['@id']))[state.language],
+      text: getText(state.base.phase, getRoleId(myRole['@id']), state.language),
       timer: {
         limit: state.base.phaseTimeLimit,
         phase: state.base.phase
