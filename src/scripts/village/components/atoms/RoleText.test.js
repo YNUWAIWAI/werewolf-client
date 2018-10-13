9677dcaceb0fd0e64e82a6c670e88263b8a72634
Yudai Udagawa <nimius.rd@gmail.com>
2018-10-13 14:44:46 +0900

    Fix test

--- src/scripts/village/components/atoms/RoleText.test.js
+++ src/scripts/village/components/atoms/RoleText.test.js
@@ -1,15 +1,18 @@
 // @flow
 import React from 'react'
 import RoleText from './RoleText'
-import {shallow} from 'enzyme'
+import {getMessages} from '../../../../i18n/village'
+import {initRenderer} from '../../../../../tools/intl-enzyme-test-helper'
+
+const {mountWithIntl} = initRenderer('ja', getMessages('ja'))
 
 test('<RoleText role="人狼" />', () => {
-  const wrapper = shallow(<RoleText role="人狼" />)
+  const wrapper = mountWithIntl(<RoleText role="人狼" />)
 
   expect(wrapper.text()).toBe('あなたは人狼です')
 })
 test('<RoleText role="" />', () => {
-  const wrapper = shallow(<RoleText role="" />)
+  const wrapper = mountWithIntl(<RoleText role="" />)
 
   expect(wrapper.text()).toBe('あなたはです')
 })
