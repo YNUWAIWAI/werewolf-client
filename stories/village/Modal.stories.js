85072ef16fb6f0f40aa2d6209d68601454df3092
Yudai Udagawa <nimius.rd@gmail.com>
2018-10-14 16:22:51 +0900

    Fix story

--- stories/village/Modal.stories.js
+++ stories/village/Modal.stories.js
@@ -1,12 +1,24 @@
 // @flow
+import IntlProvider from '../../src/scripts/village/containers/IntlProviderContainer'
 import Modal from '../../src/scripts/village/components/organisms/Modal'
 import React from 'react'
 import {action} from '@storybook/addon-actions'
+import {createStore} from 'redux'
+import reducer from '../../src/scripts/village/reducers'
 import {storiesOf} from '@storybook/react'
 import {withKnobs} from '@storybook/addon-knobs'
 
+const store = createStore(
+  reducer
+)
+
 storiesOf('village|Modal', module)
   .addDecorator(withKnobs)
+  .addDecorator(story =>
+    <IntlProvider store={store}>
+      {story()}
+    </IntlProvider>
+  )
   .add('default', () => {
     const timer = {
       limit: 10,
