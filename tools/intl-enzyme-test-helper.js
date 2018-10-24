560dc5c7c3bde3d122b43b36b07b52e4afbe6f90
Yudai Udagawa <nimius.rd@gmail.com>
2018-10-25 04:45:46 +0900

    wip

--- tools/intl-enzyme-test-helper.js
+++ tools/intl-enzyme-test-helper.js
@@ -1,12 +1,6 @@
-import {IntlProvider, addLocaleData, intlShape} from 'react-intl'
+import {IntlProvider, intlShape} from 'react-intl'
 import {mount, shallow} from 'enzyme'
 import React from 'react'
-import en from 'react-intl/locale-data/en'
-import fr from 'react-intl/locale-data/fr'
-import it from 'react-intl/locale-data/it'
-import ja from 'react-intl/locale-data/ja'
-
-addLocaleData([... en, ... fr, ... it, ... ja])
 
 export const initRenderer = (locale, messages) => {
   const intlProvider = new IntlProvider(
