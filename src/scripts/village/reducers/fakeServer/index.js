6db9f7a5a7ccdd8b19bc6202c021000d7b35aeee
Yudai Udagawa <nimius.rd@gmail.com>
2018-11-15 12:13:44 +0900

    Fix test

--- src/scripts/village/reducers/fakeServer/index.js
+++ src/scripts/village/reducers/fakeServer/index.js
@@ -1,20 +1,24 @@
 // @flow
 import anonymousAudienceChatJSON from './anonymousAudienceChat'
+import dayJSON from './day'
 import firstDayConversationJSON from './firstDayConversation'
 import firstDayConversationVillagerJSON from './firstDayConversationVillager'
 import firstMorning2JSON from './firstMorning2'
 import firstMorningJSON from './firstMorning'
 import myMessageOnChatJSON from './myMsseageOnChat'
+import nightJSON from './night'
 import onymousAudienceChatJSON from './onymousAudienceChat'
 import resultJSON from './result'
 import theirMessageOnChatJSON from './theirMessageOnChat'
 
 export const anonymousAudienceChat: Object = anonymousAudienceChatJSON
+export const day: Object = dayJSON
 export const firstDayConversation: Object = firstDayConversationJSON
 export const firstDayConversationVillager: Object = firstDayConversationVillagerJSON
 export const firstMorning2: Object = firstMorning2JSON
 export const firstMorning: Object = firstMorningJSON
 export const myMessageOnChat: Object = myMessageOnChatJSON
+export const night: Object = nightJSON
 export const onymousAudienceChat: Object = onymousAudienceChatJSON
 export const result: Object = resultJSON
 export const theirMessageOnChat: Object = theirMessageOnChatJSON
