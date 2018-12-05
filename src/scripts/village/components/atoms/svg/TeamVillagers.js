a13ca3848dbe8e0c62a241d55f14d9ca5a84fce8
Yudai Udagawa <nimius.rd@gmail.com>
2018-12-05 15:59:23 +0900

    Add svg

---
+++ src/scripts/village/components/atoms/svg/TeamVillagers.js
@@ -0,0 +1,77 @@
+// @flow
+import React from 'react'
+
+export default function TeamVillagers() {
+  return (
+    <svg
+      className="teamVillagers"
+      viewBox="0 0 50 50"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g>
+        <rect
+          height="3.01"
+          width="50"
+        />
+        <rect
+          height="3"
+          width="50"
+          y="47"
+        />
+        <rect
+          height="50"
+          width="3"
+        />
+        <rect
+          height="50"
+          width="3"
+          x="47"
+        />
+      </g>
+      <g>
+        <rect
+          height="3"
+          transform="translate(25 60.36) rotate(-135)"
+          width="35"
+          x="7.5"
+          y="23.5"
+        />
+        <rect
+          height="3"
+          transform="translate(-21.65 20.32) rotate(-45)"
+          width="35"
+          x="-3.8"
+          y="34.8"
+        />
+        <rect
+          height="3"
+          transform="translate(-1.43 15.44) rotate(-45)"
+          width="15"
+          x="10.43"
+          y="7.94"
+        />
+        <rect
+          height="3"
+          transform="translate(-10.8 38.07) rotate(-45)"
+          width="15"
+          x="33.06"
+          y="30.57"
+        />
+        <rect
+          height="3"
+          transform="translate(-4.47 22.79) rotate(-45)"
+          width="15"
+          x="17.77"
+          y="15.29"
+        />
+        <rect
+          height="3"
+          transform="translate(-7.68 30.53) rotate(-45)"
+          width="15"
+          x="25.52"
+          y="23.03"
+        />
+      </g>
+    </svg>
+  )
+}
---
+++ src/scripts/village/components/atoms/svg/TeamWerehamster.js
@@ -0,0 +1,53 @@
+// @flow
+import React from 'react'
+
+export default function TeamWerehamster() {
+  return (
+    <svg
+      className="teamWerehamster"
+      viewBox="0 0 50 50"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g>
+        <line
+          x1="9.95"
+          x2="8.61"
+          y1="3.01"
+          y2="3.01"
+        />
+        <polyline
+          points="8.61 3.01 0 3.01 0 0 50 0 50 3.01 9.95 3.01"
+        />
+        <rect
+          height="3"
+          width="50"
+          y="47"
+        />
+        <rect
+          height="50"
+          width="3"
+        />
+        <rect
+          height="50"
+          width="3"
+          x="47"
+        />
+      </g>
+      <g>
+        <polygon
+          points="23.45 47.8 17.63 46.91 24.74 37.86 23.45 47.8"
+        />
+        <path
+          d="M40.25,3.06,25.46,37.4c.62-6.37,2-25.26,2.65-36.06"
+        />
+        <polygon
+          points="26.75 47.8 32.56 46.91 25.46 37.86 26.75 47.8"
+        />
+        <path
+          d="M10,3.06,24.74,37.4c-.62-6.37-2-25.26-2.65-36.06"
+        />
+      </g>
+
+    </svg>
+  )
+}
