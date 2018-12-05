52f38227ec2dc9d09c23dee8e7ac6792ddc90c0b
Yudai Udagawa <nimius.rd@gmail.com>
2018-12-06 03:42:21 +0900

    Add story

---
+++ stories/village/InfoTeam.stories.js
@@ -0,0 +1,52 @@
+// @flow
+import IntlProvider from '../../src/scripts/village/containers/IntlProviderContainer'
+import React from 'react'
+import TeamIcon from '../../src/scripts/village/components/atoms/TeamIcon'
+import {createStore} from 'redux'
+import reducer from '../../src/scripts/village/reducers'
+import {storiesOf} from '@storybook/react'
+import {withKnobs} from '@storybook/addon-knobs'
+
+const store = createStore(
+  reducer
+)
+
+storiesOf('village|Info/InfoTeam', module)
+  .addDecorator(getStory =>
+    <IntlProvider store={store}>
+      {getStory()}
+    </IntlProvider>
+  )
+  .add('villager', () => {
+    const story =
+      <div className="info">
+        <TeamIcon
+          class="info--team"
+          team="villager"
+        />
+      </div>
+
+    return story
+  })
+  .add('werehamster', () => {
+    const story =
+      <div className="info">
+        <TeamIcon
+          class="info--team"
+          team="werehamster"
+        />
+      </div>
+
+    return story
+  })
+  .add('werewolf', () => {
+    const story =
+      <div className="info">
+        <TeamIcon
+          class="info--team"
+          team="werewolf"
+        />
+      </div>
+
+    return story
+  })
