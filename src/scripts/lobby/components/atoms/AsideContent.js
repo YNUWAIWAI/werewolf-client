5e5ee971fc34333926445ea2ace44126bc354eda
Yudai Udagawa <nimius.rd@gmail.com>
2018-12-06 16:13:43 +0900

    Fix AsideContent

--- src/scripts/lobby/components/atoms/AsideContent.js
+++ src/scripts/lobby/components/atoms/AsideContent.js
@@ -2,12 +2,13 @@
 import React from 'react'
 
 type Props = {
-  +children?: React$Node
+  +children?: React$Node,
+  +expand?: boolean
 }
 
 export default function AsideContent(props: Props) {
   return (
-    <div className="aside-content">
+    <div className={`aside-content ${props.expand ? 'expand' : ''}`}>
       {props.children}
     </div>
   )
