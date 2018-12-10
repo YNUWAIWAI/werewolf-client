d47c2e13a321ec8947995deab8fed3c9a910ad6f
Yudai Udagawa <nimius.rd@gmail.com>
2018-12-11 01:17:24 +0900

    Add test

--- src/scripts/village/reducers/fakeServer/index.js
+++ src/scripts/village/reducers/fakeServer/index.js
@@ -4,6 +4,7 @@ import dayJSON from './day'
 import firstMorning1JSON from './firstMorning1'
 import firstMorning2JSON from './firstMorning2'
 import firstMorning3JSON from './firstMorning3'
+import flavorTextJSON from './flavorText'
 import myMessageOnChatJSON from './myMsseageOnChat'
 import nightJSON from './night'
 import onymousAudienceChatJSON from './onymousAudienceChat'
@@ -15,6 +16,7 @@ export const day: Object = dayJSON
 export const firstMorning: Object = firstMorning1JSON
 export const firstMorning2: Object = firstMorning2JSON
 export const firstMorning3: Object = firstMorning3JSON
+export const flavorText: Object = flavorTextJSON
 export const myMessageOnChat: Object = myMessageOnChatJSON
 export const night: Object = nightJSON
 export const onymousAudienceChat: Object = onymousAudienceChatJSON
