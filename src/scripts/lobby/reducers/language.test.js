c02ed9956b127747af2760dad9fa5b5c83cbc3af
Yudai Udagawa <nimius.rd@gmail.com>
2018-12-22 20:25:02 +0900

    Add test

---
+++ src/scripts/lobby/reducers/language.test.js
@@ -0,0 +1,62 @@
+// @flow
+
+import * as ActionTypes from '../constants/ActionTypes'
+import reducer, {initialState} from './language'
+import Ajv from 'ajv'
+import {VERSION} from '../constants/Version'
+import fetch from 'node-fetch'
+
+test('CHANGE_LANGUAGE', () => {
+  expect(
+    reducer(
+      initialState,
+      {
+        language: 'ja',
+        type: ActionTypes.CHANGE_LANGUAGE
+      }
+    )
+  ).toEqual(
+    'ja'
+  )
+})
+describe('socket/MESSAGE', () => {
+  describe('avatar', () => {
+    const BASE_URI = `https://werewolf.world/lobby/schema/${VERSION}`
+    const SERVER2CLIENT = `${BASE_URI}/server2client`
+    const ajv = new Ajv()
+    const avatarPayload = {
+      error: null,
+      image: '/assets/images/avatar/default/user.png',
+      lang: 'ja',
+      name: 'Alice',
+      token: '3F2504E0-4F89-11D3-9A0C-0305E82C3301',
+      type: 'avatar'
+    }
+
+    test('validate the JSON', async () => {
+      expect.hasAssertions()
+      await fetch(`${SERVER2CLIENT}/avatar.json`)
+        .then(res => res.json())
+        .then(schema => {
+          const validate = ajv.validate(schema, avatarPayload)
+
+          expect(validate).toBe(true)
+        })
+    })
+
+    test('reduce correctly', () => {
+      expect(
+        reducer(
+          initialState,
+          {
+            payload: avatarPayload,
+            type: ActionTypes.socket.MESSAGE
+          }
+        )
+      ).toEqual(
+        'ja'
+      )
+    })
+  })
+})
+
