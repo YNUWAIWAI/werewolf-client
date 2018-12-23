c1073e2c05722147bb6eb011ff8495dce2e9c68e
Yudai Udagawa <nimius.rd@gmail.com>
2018-12-23 20:57:00 +0900

    Fix socket.message

--- src/scripts/village/reducers/language.js
+++ src/scripts/village/reducers/language.js
@@ -1,17 +1,23 @@
 // @flow
 import * as ActionTypes from '../constants/ActionTypes'
-import type {ChangeLanguage} from '../actions'
+import type {
+  ChangeLanguage,
+  Played
+} from '../actions'
 import type {Language} from 'village'
 
 export type State = Language
 type Action =
   | ChangeLanguage
+  | Played
 
-export const initialState = 'ja'
+export const initialState = 'en'
 const language = (state: State = initialState, action: Action): State => {
   switch (action.type) {
     case ActionTypes.CHANGE_LANGUAGE:
       return action.language
+    case ActionTypes.PLAYED:
+      return action.payload.lang
     default:
       return state
   }
