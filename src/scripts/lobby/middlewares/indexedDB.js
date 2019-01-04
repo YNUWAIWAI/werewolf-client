f60752869b4aaf5031b9fdbe30370571797ed1f0
Yudai Udagawa <nimius.rd@gmail.com>
2019-01-04 18:57:15 +0900

    wip

--- src/scripts/lobby/middlewares/indexedDB.js
+++ src/scripts/lobby/middlewares/indexedDB.js
@@ -1,134 +1,12 @@
 // @flow
 import * as ActionTypes from '../constants/ActionTypes'
 import {changeLobby, selectVillage} from '../actions'
+import {connectLobbyDB, connectVillageDB} from '../../indexeddb'
 import {type Action} from '.'
 import type {Middleware} from 'redux'
 import type {Payload$WatingPage} from 'lobby'
 import type {ReducerState} from '../reducers'
 
-const connectLobbyDB = (() => {
-  let db
-
-  return () => new Promise((resolve, reject) => {
-    if (db) {
-      resolve(db)
-
-      return
-    }
-    const request = window.indexedDB.open('lobby')
-
-    request.onerror = event => {
-      console.error(event.target.error)
-      reject(event.target.error)
-    }
-    request.onupgradeneeded = event => {
-      db = event.target.result
-      const objectStore = db.createObjectStore(
-        'history',
-        {
-          keyPath: 'type'
-        }
-      )
-
-      objectStore.createIndex(
-        'lobby',
-        'lobby',
-        {
-          unique: false
-        }
-      )
-      objectStore.createIndex(
-        'token',
-        'token',
-        {
-          unique: true
-        }
-      )
-      objectStore.createIndex(
-        'villageId',
-        'villageId',
-        {
-          unique: false
-        }
-      )
-    }
-    request.onsuccess = event => {
-      db = event.target.result
-      db.onabort = e => {
-        console.error('Database opening aborted!', e)
-      }
-      db.onerror = e => {
-        console.error('Error opening database.', e)
-      }
-      db.onclose = e => {
-        console.log(`The database ${db.name} has unexpectedly closed.`)
-      }
-      db.onversionchange = e => {
-        console.log('version changed', e)
-      }
-      console.log('success', db)
-      resolve(db)
-    }
-  })
-})()
-const connectVillageDB = (() => {
-  let db
-
-  return () => new Promise((resolve, reject) => {
-    if (db) {
-      resolve(db)
-
-      return
-    }
-    const request = window.indexedDB.open('village')
-
-    request.onerror = event => {
-      console.error(event.target.error)
-      reject(event.target.error)
-    }
-    request.onupgradeneeded = event => {
-      db = event.target.result
-      const objectStore = db.createObjectStore(
-        'village',
-        {
-          keyPath: 'type'
-        }
-      )
-
-      objectStore.createIndex(
-        'token',
-        'token',
-        {
-          unique: true
-        }
-      )
-      objectStore.createIndex(
-        'villageId',
-        'villageId',
-        {
-          unique: false
-        }
-      )
-    }
-    request.onsuccess = event => {
-      db = event.target.result
-      db.onabort = e => {
-        console.error('Database opening aborted!', e)
-      }
-      db.onerror = e => {
-        console.error('Error opening database.', e)
-      }
-      db.onclose = e => {
-        console.log(`The database ${db.name} has unexpectedly closed.`)
-      }
-      db.onversionchange = e => {
-        console.log('version changed', e)
-      }
-      console.log('success', db)
-      resolve(db)
-    }
-  })
-})()
 const indexedDBMiddleware: Middleware<ReducerState, Action> = store => next => action => {
   switch (action.type) {
     case ActionTypes.indexedDB.INIT: {
@@ -138,7 +16,7 @@ const indexedDBMiddleware: Middleware<ReducerState, Action> = store => next => a
         const request = objectStore.get('lastVisited')
 
         request.onerror = event => {
-          console.error('error')
+          console.error(event)
         }
         request.onsuccess = event => {
           const result = event.target.result
@@ -156,10 +34,10 @@ const indexedDBMiddleware: Middleware<ReducerState, Action> = store => next => a
       connectLobbyDB().then(db => {
         const transaction = db.transaction('history', 'readwrite')
 
-        transaction.oncomplete = event => {
+        transaction.oncomplete = () => {
           console.log('All done!')
         }
-        transaction.onerror = event => {
+        transaction.onerror = () => {
           console.error('transaction error')
         }
 
@@ -181,11 +59,11 @@ const indexedDBMiddleware: Middleware<ReducerState, Action> = store => next => a
           connectVillageDB().then(db => {
             const transaction = db.transaction('village', 'readwrite')
 
-            transaction.oncomplete = event => {
+            transaction.oncomplete = () => {
               console.log('All done!')
               window.location.replace(`${window.location.origin}/village`)
             }
-            transaction.onerror = event => {
+            transaction.onerror = () => {
               console.error('transaction error')
             }
 
@@ -210,10 +88,10 @@ const indexedDBMiddleware: Middleware<ReducerState, Action> = store => next => a
           connectLobbyDB().then(db => {
             const transaction = db.transaction('history', 'readwrite')
 
-            transaction.oncomplete = event => {
+            transaction.oncomplete = () => {
               console.log('All done!')
             }
-            transaction.onerror = event => {
+            transaction.onerror = () => {
               console.error('transaction error')
             }
 
