2e61ce0ff97fda94136e5bafd96df7ac9bd38994
Yudai Udagawa <nimius.rd@gmail.com>
2019-01-10 23:46:56 +0900

    Update type definition

---
+++ @types/lobby/index.d.ts
@@ -0,0 +1,139 @@
+/* eslint no-unused-vars: 0 */
+declare namespace lobby {
+  enum Avatar {
+    fixed = 'fixed',
+    random = 'random',
+    unspecified = 'unspecified'
+  }
+  enum Member {
+    A = 'A',
+    B = 'B',
+    C = 'C'
+  }
+  enum Lobby {
+    human = 'human player',
+    audience = 'onymous audience',
+    robot = 'robot player'
+  }
+  enum PingStatus {
+    danger = 'danger',
+    safe = 'safe',
+    warning = 'warning'
+  }
+  enum Language {
+    fr = 'fr',
+    en = 'en',
+    it = 'it',
+    ja = 'ja'
+  }
+  interface Village {
+    avatar: Avatar;
+    comment: string | null;
+    hostPlayer: HostPlayer;
+    id: number;
+    idForSearching: number;
+    name: string;
+    playerSetting: PlayerSetting;
+    roleSetting: RoleSetting;
+  }
+  interface HostPlayer {
+    isAnonymous: boolean;
+    name: string;
+  }
+  interface PlayerSetting {
+    current: number;
+    human: Human;
+    number: number;
+    robot: Robot;
+  }
+  interface Robot {
+    current: number;
+    min: number;
+  }
+  interface Human {
+    current: number;
+    max: number;
+  }
+  interface RoleSetting {
+    hunter: number;
+    madman: number;
+    mason: number;
+    medium: number;
+    seer: number;
+    villager: number;
+    werehamster: number;
+    werewolf: number;
+  }
+  interface WaitingPlayer {
+    avatarImage: string;
+    isAnonymous: boolean;
+    isHost: boolean;
+    isMe: boolean;
+    name: StringLiteral;
+    token: string;
+  }
+  enum Target {
+    ADVANCED_SEARCH = 'ADVANCED_SEARCH',
+    BUILD_VILLAGE = 'BUILD_VILLAGE',
+    ID_SEARCH = 'ID_SEARCH',
+    LEAVE_WAITING_PAGE = 'LEAVE_WAITING_PAGE',
+    PLAY_GAME = 'PLAY_GAME',
+    REFRESH = 'REFRESH',
+    SEARCH_FOR_A_VILLAGE_IN_SESSION = 'SEARCH_FOR_A_VILLAGE_IN_SESSION',
+    SEARCH_FOR_AN_OLD_VILLAGE = 'SEARCH_FOR_AN_OLD_VILLAGE',
+    SHOW_ADVANCED_SEARCH = 'SHOW_ADVANCED_SEARCH',
+    SHOW_BUILD_VILLAGE = 'SHOW_BUILD_VILLAGE',
+    SHOW_CREDITS = 'SHOW_CREDITS',
+    SHOW_CONNECTING_TO_ROBOT_PLAYER = 'SHOW_CONNECTING_TO_ROBOT_PLAYER',
+    SHOW_HISTORY = 'SHOW_HISTORY',
+    SHOW_HOW_TO_PLAY = 'SHOW_HOW_TO_PLAY',
+    SHOW_ID_SEARCH = 'SHOW_ID_SEARCH',
+    SHOW_LOBBY_FOR_AUDIENCE = 'SHOW_LOBBY_FOR_AUDIENCE',
+    SHOW_LOBBY_FOR_HUMAN_PLAYER = 'SHOW_LOBBY_FOR_HUMAN_PLAYER',
+    SHOW_LOBBY_FOR_ROBOT_PLAYER = 'SHOW_LOBBY_FOR_ROBOT_PLAYER',
+    SHOW_MAIN = 'SHOW_MAIN',
+    SHOW_SETTINGS = 'SHOW_SETTINGS'
+  }
+  interface MenuItem {
+    disabled?: boolean;
+    id: string;
+    isLoading?: boolean;
+    types: Target[];
+  }
+  interface PingResult {
+    ping: string;
+    status: PingStatus;
+    token: string;
+  }
+  interface Payload {
+    type: string;
+  }
+  interface Payload$Avatar extends Payload {
+    image: string;
+    lang: Language;
+    name: string;
+  }
+  interface Payload$GetSettings extends Payload {
+    lang: Language;
+    userEmail: string;
+    userName: string;
+  }
+  interface Payload$Lobby extends Payload {
+    error: string | null;
+    lobby: Lobby;
+    villages: Village[];
+  }
+  interface Payload$Ping extends Payload {
+    id: string;
+    results: PingResult[];
+  }
+  interface Payload$SearchResult extends Payload {
+    error: string | null;
+    villages: Village[];
+  }
+  interface Payload$WatingPage extends Payload {
+    error: string | null;
+    village: Village;
+    players: WaitingPlayer[];
+  }
+}
---
+++ @types/village/index.d.ts
@@ -0,0 +1,473 @@
+/* eslint no-unused-vars: 0, init-declarations: 0 */
+declare namespace village {
+  const enum Directionality {
+    serverToClient = 'server to client',
+    clientToServer = 'client to server'
+  }
+  enum Channel {
+    anonymousAudience = 'anonymousAudience',
+    grave = 'grave',
+    hunter = 'hunter',
+    master = 'master',
+    onymousAudience = 'onymousAudience',
+    private = 'private',
+    public = 'public',
+    seer = 'seer',
+    werewolf = 'werewolf'
+  }
+  enum ChatChannel {
+    grave = 'grave',
+    limited = 'limited',
+    master = 'master',
+    private = 'private',
+    public = 'public'
+  }
+  enum InputChannel {
+    grave = 'grave',
+    limited = 'limited',
+    postMortem = 'post mortem',
+    private = 'private',
+    public = 'public'
+  }
+  enum Phase {
+    morning = 'morning',
+    day = 'day',
+    night = 'night',
+    postMortem = 'post mortem',
+    result = 'result',
+    flavorText = 'flavor text'
+  }
+  enum RoleId {
+    villager = 'villager',
+    seer = 'seer',
+    medium = 'medium',
+    hunter = 'hunter',
+    mason = 'mason',
+    madman = 'madman',
+    werewolf = 'werewolf',
+    werehamster = 'werehamster',
+    master = 'master'
+  }
+  enum BoardState {
+    QUESTION = '?',
+    TRIANGLE = 'Δ',
+    CIRCLE = 'O',
+    CROSS = 'X',
+    FILL = 'fill'
+  }
+  enum Language {
+    en = 'en',
+    fr = 'fr',
+    it = 'it',
+    ja = 'ja'
+  }
+  type LanguageMap = {
+    [lang in Language]: string;
+  }
+
+  enum Result {
+    win = 'win',
+    lose = 'lose'
+  }
+  type AgentId = string
+
+  enum AgentStatus {
+    alive = 'alive',
+    dead = 'dead',
+    deathByExecution = 'death by execution',
+    deathByAttack = 'death by attack',
+    deathByFear = 'death by fear',
+    unnaturalDeath = 'unnatural death'
+  }
+  enum Team {
+    villager = 'villager',
+    werehamster = 'werehamster',
+    werewolf = 'werewolf'
+  }
+  enum Gender {
+    female = 'female',
+    male = 'male'
+  }
+  enum Context {
+    Agent = 'https://werewolf.world/context/0.2/agent.jsonld',
+    Avatar = 'https://werewolf.world/context/0.2/avatar.jsonld',
+    Base = 'https://werewolf.world/context/0.2/base.jsonld',
+    Board = 'https://werewolf.world/context/0.2/board.jsonld',
+    BoardResult = 'https://werewolf.world/context/0.2/boardResult.jsonld',
+    Chat = 'https://werewolf.world/context/0.2/chat.jsonld',
+    Error = 'https://werewolf.world/context/0.2/error.jsonld',
+    FlavorText = 'https://werewolf.world/context/0.2/flavorText.jsonld',
+    Role = 'https://werewolf.world/context/0.2/role.jsonld',
+    Scroll = 'https://werewolf.world/context/0.2/scroll.jsonld',
+    Village = 'https://werewolf.world/context/0.2/village.jsonld',
+    Vote = 'https://werewolf.world/context/0.2/vote.jsonld',
+    VotingResult = 'https://werewolf.world/context/0.2/votingResult.jsonld'
+  }
+  enum BaseContext {
+    Agent = 'https://werewolf.world/context/0.2/agent.jsonld',
+    Avatar = 'https://werewolf.world/context/0.2/avatar.jsonld',
+    Base = 'https://werewolf.world/context/0.2/base.jsonld',
+    Chat = 'https://werewolf.world/context/0.2/chat.jsonld',
+    Error = 'https://werewolf.world/context/0.2/error.jsonld',
+    FlavorText = 'https://werewolf.world/context/0.2/flavorText.jsonld',
+    Role = 'https://werewolf.world/context/0.2/role.jsonld',
+    Scroll = 'https://werewolf.world/context/0.2/scroll.jsonld',
+    Vote = 'https://werewolf.world/context/0.2/vote.jsonld',
+    VotingResult = 'https://werewolf.world/context/0.2/votingResult.jsonld'
+  }
+  enum Message {
+    boardMessage = 'boardMessage',
+    errorMessage = 'errorMessage',
+    flavorTextMessage = 'flavorTextMessage',
+    playerMessage = 'playerMessage',
+    scrollMessage = 'scrollMessage',
+    systemMessage = 'systemMessage',
+    voteMessage = 'voteMessage',
+  }
+  enum NavigationType {
+    SHOW_RESULT = 'SHOW_RESULT',
+    RETURN_TO_LOBBY = 'RETURN_TO_LOBBY'
+  }
+
+  /*
+    'https://werewolf.world/context/0.2/agent.jsonld'
+      dependency:
+        'https://werewolf.world/context/0.2/base.jsonld'
+        'boardMessage'
+        'playerMessage'
+        'systemMessage'
+        'voteMessage'
+        'https://werewolf.world/context/0.2/votingResult.jsonld'
+  */
+  interface Agent {
+    '@context': Context.Agent;
+    '@id': string;
+    isMine?: boolean;
+    name: LanguageMap;
+    'full name'?: LanguageMap;
+    gender?: Gender;
+    image: string;
+    id: number;
+    status?: string; // AgentStatus
+    update?: {
+      date: Time['date'];
+      phase: Time['phase'];
+    };
+    isAChoice?: boolean;
+    result?: Result;
+  }
+
+  /*
+  'https://werewolf.world/context/0.2/avatar.jsonld'
+    dependency:
+      'https://werewolf.world/context/0.2/base.jsonld'
+      'systemMessage'
+  */
+  interface Avatar {
+    '@context'?: Context.Avatar;
+    '@id'?: string;
+    token?: string;
+    name?: string;
+    image?: string;
+  }
+
+  /*
+    'https://werewolf.world/context/0.2/base.jsonld'
+      dependency:
+        'boardMessage'
+        'errorMessage'
+        'flavorTextMessage'
+        'playerMessage'
+        'scrollMessage'
+        'systemMessage'
+        'voteMessage'
+  */
+  interface Base {
+    '@id': string;
+    '@context': BaseContext[];
+    village: {
+      '@context': Context.Village;
+      '@id': string;
+      id: number;
+      lang: Language;
+      name: string;
+      totalNumberOfAgents: number;
+    };
+    token: Avatar['token'];
+    phase: Time['phase'];
+    date: Time['date'];
+    phaseTimeLimit: number;
+    phaseStartTime: string;
+    serverTimestamp: string;
+    clientTimestamp: string;
+    directionality: Directionality;
+    intensionalDisclosureRange: Channel;
+    extensionalDisclosureRange: {
+      '@id': Agent['@id'];
+      id: Agent['id'];
+      image: Agent['image'];
+      name: Agent['name'];
+      role: {
+        '@id': Role['@id'];
+        image: Role['image'];
+        name: Role['name'];
+      };
+      status: Agent['status'];
+    }[];
+    avatar?: {
+      '@context': Avatar['@context'];
+      '@id': Avatar['@id'];
+      token: Avatar['token'];
+      name: Avatar['name'];
+      image: Avatar['image'];
+    };
+    myAgent?: {
+      '@context': Agent['@context'];
+      '@id': Agent['@id'];
+      id: Agent['id'];
+      image: Agent['image'];
+      name: Agent['name'];
+      role: {
+        '@context': Role['@context'];
+        '@id': Role['@id'];
+        image: Role['image'];
+        name: Role['name'];
+      };
+    };
+  }
+
+  /*
+  'https://werewolf.world/context/0.2/board.jsonld'
+    dependency:
+      'boardMessage'
+      'systemMessage'
+  */
+  interface Board {
+    '@context'?: Context.BoardResult;
+    '@id'?: string;
+    polarity?: string; // 'positive' | 'negative'
+    prediction?: string; // BoardState
+  }
+
+  /*
+  'https://werewolf.world/context/0.2/chat.jsonld'
+    dependency;
+      'playerMessage'
+  */
+  interface Chat {
+    isMine: boolean;
+    id?: number;
+    counter?: number;
+    limit?: number;
+    interval?: string;
+    text: {
+      '@value': string;
+      '@language': Language;
+    };
+    characterLimit: number;
+    isOver: boolean;
+  }
+
+  /*
+  'https://werewolf.world/context/0.2/error.jsonld'
+    dependency:
+      'errorMessage'
+  */
+  interface Error {
+    content: {
+      en: string;
+      ja?: string;
+    };
+    severity: 'error' | 'warning';
+    source: string;
+  }
+
+  /*
+  'https://werewolf.world/context/0.2/role.jsonld'
+    dependency:
+      'https://werewolf.world/context/0.2/base.jsonld'
+      'boardMessage'
+      'systemMessage'
+  */
+  interface Role {
+    '@context': Context.Role;
+    '@id': string;
+    name: LanguageMap;
+    image: string;
+    isMine?: boolean;
+    numberOfAgents?: number;
+  }
+
+  /*
+  'https://werewolf.world/context/0.2/scroll.jsonld'
+    dependency:
+      scrollMessage
+  */
+  interface Scroll {
+    nodeId: string;
+    scrollTop: number;
+    scrollHeight: number;
+    offsetHeight: number;
+  }
+
+  /*
+  'https://werewolf.world/context/0.2/time.jsonld'
+    dependency:
+      'https://werewolf.world/context/0.2/agent.jsonld'
+      'https://werewolf.world/context/0.2/base.jsonld'
+      'systemMessage'
+  */
+  interface Time {
+    '@id'?: string;
+    phase: Phase;
+    date: number;
+  }
+
+  /*
+  'https://werewolf.world/context/0.2/votingResult.jsonld'
+    dependency:
+      'systemMessage'
+  */
+  interface VotingResult {
+    votingResultsSummary?: {
+      '@id': string;
+      agentToLynch: {
+        '@id': Agent['@id'];
+        id: Agent['id'];
+        image: Agent['image'];
+        name: Agent['name'];
+      };
+      numberOfVotes: number;
+      rankOfVotes: number;
+    }[];
+    votingResultsDetails?: {
+      '@id': string;
+      sourceAgent: {
+        '@id': Agent['@id'];
+        id: Agent['id'];
+        image: Agent['image'];
+        name: Agent['name'];
+      };
+      targetAgent: {
+        '@id': Agent['@id'];
+        id: Agent['id'];
+        image: Agent['image'];
+        name: Agent['name'];
+      };
+    }[];
+  }
+
+  namespace Payload {
+    interface boardMessage extends Base {
+      agent: {
+        '@context': Agent['@context'];
+        '@id': Agent['@id'];
+        id: Agent['id'];
+        name: Agent['name'];
+        image: Agent['image'];
+      };
+      role: {
+        '@context': Role['@context'];
+        '@id': Role['@id'];
+        name: Role['name'];
+        image: Role['image'];
+      };
+      prediction: Board['prediction'];
+    }
+    interface errorMessage extends Base, Error {}
+    interface flavorTextMessage extends Base {
+      flavorText: playerMessage[];
+    }
+    interface playerMessage extends Base {
+      agent?: {
+        '@context': Agent['@context'];
+        '@id': Agent['@id'];
+        id: Agent['id'];
+        name: Agent['name'];
+        image: Agent['image'];
+      };
+      isMine: Chat['isMine'];
+      id?: Chat['id'];
+      counter?: Chat['counter'];
+      limit?: Chat['limit'];
+      interval?: Chat['interval'];
+      text: Chat['text'];
+      characterLimit: Chat['characterLimit'];
+      isOver: Chat['isOver'];
+    }
+    interface scrollMessage extends Base, Scroll {}
+    interface systemMessage extends Base {
+      votingResultsSummary?: VotingResult['votingResultsSummary'];
+      votingResultsDetails?: VotingResult['votingResultsDetails'];
+      agent?: {
+        '@id': Agent['@id'];
+        isMine: Agent['isMine'];
+        name: Agent['name'];
+        image: Agent['image'];
+        id: Agent['id'];
+        status: Agent['status'];
+        update?: Agent['update'];
+        isAChoice?: Agent['isAChoice'];
+        result?: Agent['result'];
+        avatar?: {
+          token: Avatar['token'];
+          name: Avatar['name'];
+          image: Avatar['image'];
+        };
+        role?: {
+          '@context': Role['@context'];
+          '@id': Role['@id'];
+          name: Role['name'];
+          image: Role['image'];
+        };
+      }[];
+      role?: {
+        '@context': Role['@context'];
+        '@id': Role['@id'];
+        isMine: Role['isMine'];
+        name: Role['name'];
+        image: Role['image'];
+        numberOfAgents: Role['numberOfAgents'];
+        agent?: {
+          '@context': Agent['@context'];
+          '@id': Agent['@id'];
+          name: Agent['name'];
+          image: Agent['image'];
+          id: Agent['id'];
+        };
+        board?: {
+          '@context': Board['@context'];
+          '@id': string;
+          agent: {
+            '@context': Agent['@context'];
+            '@id': Agent['@id'];
+            name: Agent['name'];
+            image: Agent['image'];
+            id: Agent['id'];
+          };
+          polarity: Board['polarity'];
+          phase: Time['phase'];
+          date: Time['date'];
+        }[];
+      }[];
+    }
+    interface voteMessage extends Base {
+      agent?: {
+        '@context': Agent['@context'];
+        '@id': Agent['@id'];
+        name: Agent['name'];
+        image: Agent['image'];
+        id: Agent['id'];
+      };
+    }
+    interface ready {
+      token: string,
+      type: 'ready',
+      villageId: number
+    }
+    interface played {
+      lang: Language,
+      type: 'played'
+    }
+  }
+}
