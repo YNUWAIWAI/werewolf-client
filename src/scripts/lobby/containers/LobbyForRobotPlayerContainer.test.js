45a99a8796282cfad7a691bea64b3d9a0852f32b
Yudai Udagawa <nimius.rd@gmail.com>
2019-01-10 12:25:56 +0900

    Update LobbyForRobotPlayerContainer

--- src/scripts/lobby/containers/LobbyForRobotPlayerContainer.test.js
+++ src/scripts/lobby/containers/LobbyForRobotPlayerContainer.test.js
@@ -1,47 +1,21 @@
 // @flow
-import * as ActionTypes from '../constants/ActionTypes'
+import IntlProviderContainer from './IntlProviderContainer'
 import LobbyForRobotPlayerContainer from './LobbyForRobotPlayerContainer'
+import {Provider} from 'react-redux'
 import React from 'react'
-import {initialState} from '../reducers/lobbyForRobotPlayer'
-import {shallow} from 'enzyme'
+import fakeStore from './fakeStore'
+import {mount} from 'enzyme'
 
 test('<LobbyForRobotPlayerContainer /> initialState', () => {
-  const dispatch = jest.fn()
-  const getState = () => ({
-    lobbyForRobotPlayer: initialState
-  })
-  const subscribe = jest.fn()
-  const store = {
-    dispatch,
-    getState,
-    subscribe
-  }
-  const wrapper = shallow(<LobbyForRobotPlayerContainer store={store} transition={jest.fn()} />)
+  const transition = jest.fn()
+  const store = fakeStore()
+  const wrapper = mount(
+    <Provider store={store} >
+      <IntlProviderContainer>
+        <LobbyForRobotPlayerContainer transition={transition} />
+      </IntlProviderContainer>
+    </Provider>
+  )
 
-  expect(wrapper.props().isPlayer).toBe(true)
-  expect(wrapper.props().menuItems)
-    .toEqual([
-      {
-        id: 'Menu.showBuildVillage',
-        types: [ActionTypes.SHOW_BUILD_VILLAGE]
-      },
-      {
-        id: 'Menu.showIdSearch',
-        types: [ActionTypes.SHOW_ID_SEARCH]
-      },
-      {
-        id: 'Menu.showAdvancedSearch',
-        types: [ActionTypes.SHOW_ADVANCED_SEARCH]
-      },
-      {
-        id: 'Menu.refresh',
-        types: [ActionTypes.REFRESH, ActionTypes.SHOW_LOBBY_FOR_ROBOT_PLAYER]
-      },
-      {
-        id: 'Menu.returnToMainPage',
-        types: [ActionTypes.SHOW_MAIN]
-      }
-    ])
-  expect(wrapper.props().villageItems)
-    .toEqual([])
+  expect(wrapper.html()).toMatchSnapshot()
 })
