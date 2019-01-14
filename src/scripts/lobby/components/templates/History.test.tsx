bf034cd45f689737480f33e1361ae8d9645be0ff
Yudai Udagawa <nimius.rd@gmail.com>
2019-01-14 14:24:03 +0900

    Update History

--- src/scripts/lobby/components/templates/History.test.tsx
+++ src/scripts/lobby/components/templates/History.test.tsx
@@ -1,6 +1,5 @@
-// @flow
+import * as React from 'react'
 import History from './History'
-import React from 'react'
 import {shallow} from 'enzyme'
 
 test('<History />', () => {
--- src/scripts/lobby/components/templates/History.tsx
+++ src/scripts/lobby/components/templates/History.tsx
@@ -1,27 +1,22 @@
-// @flow
-import type {MenuItem, Target, Village} from 'lobby'
+import * as React from 'react'
 import AsideContent from '../atoms/AsideContent'
 import Header from '../atoms/Header'
 import MainContent from '../atoms/MainContent'
 import Menu from '../organisms/Menu'
-import React from 'react'
 import VillageList from '../organisms/VillageList'
 
-export type StateProps = {
-  +isPlayer: boolean,
-  +menuItems: MenuItem[],
-  +villageItems: Village[]
+export interface StateProps {
+  readonly isPlayer: boolean
+  readonly menuItems: lobby.MenuItem[]
+  readonly villageItems: lobby.Village[]
 }
-export type DispatchProps = {
-  +selectVillage: number => void => void
+export interface DispatchProps {
+  readonly selectVillage: (id: number) => () => void
 }
-export type OwnProps = {
-  +transition: Target => void
+export interface OwnProps {
+  readonly transition: (target: lobby.Target) => void
 }
-export type Props =
-  & StateProps
-  & DispatchProps
-  & OwnProps
+export interface Props extends StateProps, DispatchProps, OwnProps {}
 
 export default function History(props: Props) {
   return (
@@ -32,7 +27,6 @@ export default function History(props: Props) {
           isPlayer={props.isPlayer}
           items={props.villageItems}
           selectVillage={props.selectVillage}
-          transition={props.transition}
         />
       </MainContent>
       <AsideContent>
