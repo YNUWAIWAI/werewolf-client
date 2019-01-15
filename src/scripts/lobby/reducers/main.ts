1c4bcc9f2f4168853aff83d64589c808b3883f9f
Yudai Udagawa <nimius.rd@gmail.com>
2019-01-15 17:29:37 +0900

    Update main

--- src/scripts/lobby/reducers/main.ts
+++ src/scripts/lobby/reducers/main.ts
@@ -1,10 +1,9 @@
-// @flow
 import * as ActionTypes from '../constants/ActionTypes'
-import type {MenuItem} from 'lobby'
-import type {SocketMessage} from '../actions'
+import {MenuItemProps as MenuItem} from '../components/organisms/Menu'
+import {SocketMessage} from '../actions'
 
-export type State = {
-  +menuItems: MenuItem[]
+export interface State {
+   readonly menuItems: MenuItem[]
 }
 type Action =
   | SocketMessage
@@ -13,31 +12,31 @@ export const initialState = {
   menuItems: [
     {
       id: 'Menu.lobbyForAudience',
-      types: [ActionTypes.SHOW_LOBBY_FOR_AUDIENCE]
+      types: [ActionTypes.Target.SHOW_LOBBY_FOR_AUDIENCE]
     },
     {
       id: 'Menu.lobbyForRobotPlayer',
-      types: [ActionTypes.SHOW_LOBBY_FOR_ROBOT_PLAYER]
+      types: [ActionTypes.Target.SHOW_LOBBY_FOR_ROBOT_PLAYER]
     },
     {
       id: 'Menu.lobbyForHumanPlayer',
-      types: [ActionTypes.SHOW_LOBBY_FOR_HUMAN_PLAYER]
+      types: [ActionTypes.Target.SHOW_LOBBY_FOR_HUMAN_PLAYER]
     },
     {
       id: 'Menu.history',
-      types: [ActionTypes.SHOW_HISTORY]
+      types: [ActionTypes.Target.SHOW_HISTORY]
     },
     {
       id: 'Menu.settings',
-      types: [ActionTypes.SHOW_SETTINGS]
+      types: [ActionTypes.Target.SHOW_SETTINGS]
     },
     {
       id: 'Menu.howToPlay',
-      types: [ActionTypes.SHOW_HOW_TO_PLAY]
+      types: [ActionTypes.Target.SHOW_HOW_TO_PLAY]
     },
     {
       id: 'Menu.credits',
-      types: [ActionTypes.SHOW_CREDITS]
+      types: [ActionTypes.Target.SHOW_CREDITS]
     }
   ]
 }
