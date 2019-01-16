92e9edbf3f9d8672667ef6ac998a3601f0789706
Yudai Udagawa <nimius.rd@gmail.com>
2019-01-16 18:18:10 +0900

    Update lobby/index

--- src/scripts/lobby/index.tsx
+++ src/scripts/lobby/index.tsx
@@ -1,9 +1,8 @@
-// @flow
-import {SHOW_MAIN, indexedDB, socket} from './constants/ActionTypes'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import {global, indexedDB, socket} from './constants/ActionTypes'
 import App from './containers/App'
 import {Provider} from 'react-redux'
-import React from 'react'
-import ReactDOM from 'react-dom'
 import {changeToken} from './actions'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import config from '../../../config'
@@ -23,7 +22,7 @@ const store =
     )
 
 store.dispatch({
-  type: SHOW_MAIN
+  type: global.SHOW_MAIN
 })
 const elem = document.getElementById('script')
 
@@ -34,21 +33,21 @@ if (!elem || !elem.dataset || !elem.dataset.humanPlayerToken) {
   throw Error('Not found: data-human-player-token')
 }
 store.dispatch(changeToken({
-  lobby: 'human player',
+  lobby: lobby.Lobby.human,
   token: elem.dataset.humanPlayerToken
 }))
 if (!elem || !elem.dataset || !elem.dataset.robotPlayerToken) {
   throw Error('Not found: data-robot-player-token')
 }
 store.dispatch(changeToken({
-  lobby: 'robot player',
+  lobby: lobby.Lobby.robot,
   token: elem.dataset.robotPlayerToken
 }))
 if (!elem || !elem.dataset || !elem.dataset.onymousAudienceToken) {
   throw Error('Not found: data-onymous-audience-token')
 }
 store.dispatch(changeToken({
-  lobby: 'onymous audience',
+  lobby: lobby.Lobby.audience,
   token: elem.dataset.onymousAudienceToken
 }))
 
