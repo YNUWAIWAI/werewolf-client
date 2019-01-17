89777ddb308a897ed9b47c32aa8ca2f0e0c752f0
Yudai Udagawa <nimius.rd@gmail.com>
2019-01-17 17:33:21 +0900

    Move ResultCell

--- src/scripts/village/components/molecules/ResultCell.tsx
+++ src/scripts/village/components/molecules/ResultCell.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import {FormattedMessage} from 'react-intl'
-import TeamIcon from './TeamIcon'
+import TeamIcon from '../atoms/TeamIcon'
 
 export const enum ResultCellType {
   avatarImage = 'avatarImage',
