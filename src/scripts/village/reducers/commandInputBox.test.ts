53dd56b1a6f41eb992ffaae2600a2f30d386d3b8
Yudai Udagawa <nimius.rd@gmail.com>
2019-01-21 00:22:53 +0900

    Update commandInputBox

--- src/scripts/village/reducers/commandInputBox.test.ts
+++ src/scripts/village/reducers/commandInputBox.test.ts
@@ -1,23 +1,16 @@
-// @flow
 import * as ActionTypes from '../constants/ActionTypes'
 import {firstMorning, firstMorning2, myMessageOnChat, theirMessageOnChat} from './fakeServer'
 import reducer, {initialState} from './commandInputBox'
+import {socket} from '../actions'
 
 describe('socket/MESSAGE', () => {
   test('isMine={true}', () => {
     expect(reducer(
       initialState,
-      {
-        payload: myMessageOnChat,
-        type: ActionTypes.socket.MESSAGE
-      }
+      socket.message(myMessageOnChat)
     )).toEqual({
       limited: {
         available: false,
-        postCount: 0,
-        postCountLimit: 10
-      },
-      private: {
         postCount: 0,
         postCountLimit: 10
       },
@@ -30,26 +23,16 @@ describe('socket/MESSAGE', () => {
   test('isMine={false}', () => {
     expect(reducer(
       initialState,
-      {
-        payload: theirMessageOnChat,
-        type: ActionTypes.socket.MESSAGE
-      }
+      socket.message(theirMessageOnChat)
     )).toEqual(initialState)
   })
   test('seer 1', () => {
     expect(reducer(
       initialState,
-      {
-        payload: firstMorning,
-        type: ActionTypes.socket.MESSAGE
-      }
+      socket.message(firstMorning)
     )).toEqual({
       limited: {
         available: false,
-        postCount: 0,
-        postCountLimit: 10
-      },
-      private: {
         postCount: 0,
         postCountLimit: 10
       },
@@ -62,17 +45,10 @@ describe('socket/MESSAGE', () => {
   test('seer 2', () => {
     expect(reducer(
       initialState,
-      {
-        payload: firstMorning2,
-        type: ActionTypes.socket.MESSAGE
-      }
+      socket.message(firstMorning2)
     )).toEqual({
       limited: {
         available: true,
-        postCount: 0,
-        postCountLimit: 10
-      },
-      private: {
         postCount: 0,
         postCountLimit: 10
       },
@@ -81,5 +57,35 @@ describe('socket/MESSAGE', () => {
         postCountLimit: 10
       }
     })
+  })
+})
+test('CHANGE_DATE', () => {
+  expect(reducer(
+    {
+      limited: {
+        available: true,
+        postCount: 10,
+        postCountLimit: 10
+      },
+      public: {
+        postCount: 10,
+        postCountLimit: 10
+      }
+    },
+    {
+      from: village.Phase.morning,
+      to: village.Phase.day,
+      type: ActionTypes.global.CHANGE_PHASE
+    }
+  )).toEqual({
+    limited: {
+      available: true,
+      postCount: 0,
+      postCountLimit: 10
+    },
+    public: {
+      postCount: 0,
+      postCountLimit: 10
+    }
   })
 })
---
+++ src/scripts/village/reducers/commandInputBox.ts
@@ -0,0 +1,112 @@
+import * as ActionTypes from '../constants/ActionTypes'
+import {ChangePhase, SocketMessage} from '../actions'
+import {getInputChannelFromChannel, getMyRole, just, strToRoleId} from '../util'
+import {AVAILABLE_FOR_LIMITED_CHAT} from '../constants/Role'
+
+export interface State {
+  readonly limited: {
+    readonly available: boolean
+    readonly postCount: number
+    readonly postCountLimit: number
+  }
+  readonly public: {
+    readonly postCount: number
+    readonly postCountLimit: number
+  }
+}
+export type Action =
+ | ChangePhase
+ | SocketMessage
+
+export const initialState: State = {
+  limited: {
+    available: false,
+    postCount: 0,
+    postCountLimit: 10
+  },
+  public: {
+    postCount: 0,
+    postCountLimit: 10
+  }
+}
+
+const commandInputBox = (state: State = initialState, action: Action): State => {
+  switch (action.type) {
+    case ActionTypes.global.CHANGE_PHASE: {
+      return {
+        limited: {
+          ... state.limited,
+          postCount: 0
+        },
+        public: {
+          ... state.public,
+          postCount: 0
+        }
+      }
+    }
+    case ActionTypes.socket.MESSAGE:
+      switch (action.payload['@payload']) {
+        case village.Message.playerMessage: {
+          if (!action.payload.isMine) {
+            return state
+          }
+
+          const inputChannel = getInputChannelFromChannel(action.payload.intensionalDisclosureRange)
+
+          switch (inputChannel) {
+            case village.InputChannel.limited:
+              return {
+                ... state,
+                limited: {
+                  ... state.limited,
+                  postCount: just(action.payload.counter),
+                  postCountLimit: just(action.payload.limit)
+                }
+              }
+            case village.InputChannel.public:
+              return {
+                ... state,
+                public: {
+                  ... state.public,
+                  postCount: just(action.payload.counter),
+                  postCountLimit: just(action.payload.limit)
+                }
+              }
+            case village.InputChannel.grave:
+            case village.InputChannel.postMortem:
+            case village.InputChannel.private:
+            default:
+              return state
+          }
+        }
+        case village.Message.systemMessage: {
+          if (!action.payload.role) {
+            return state
+          }
+          const role = getMyRole(action.payload.role)
+
+          if (
+            role.numberOfAgents > 1 &&
+            AVAILABLE_FOR_LIMITED_CHAT.includes(strToRoleId(role.name.en))
+          ) {
+            return {
+              ... state,
+              limited: {
+                ... state.limited,
+                available: true
+              }
+            }
+          }
+
+          return state
+        }
+        default:
+          return state
+      }
+    default:
+      return state
+  }
+}
+
+export default commandInputBox
+
