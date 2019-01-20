cc2fa0c1fc70489041c696e398f37300f4756639
Yudai Udagawa <nimius.rd@gmail.com>
2019-01-21 01:31:20 +0900

    Update language

--- src/scripts/village/reducers/language.ts
+++ src/scripts/village/reducers/language.ts
@@ -1,23 +1,15 @@
-// @flow
 import * as ActionTypes from '../constants/ActionTypes'
-import type {
-  ChangeLanguage,
-  Played
-} from '../actions'
-import type {Language} from 'village'
+import {ChangeLanguage} from '../actions'
 
-export type State = Language
+export type State = village.Language
 type Action =
   | ChangeLanguage
-  | Played
 
-export const initialState = 'en'
+export const initialState: State = village.Language.en
 const language = (state: State = initialState, action: Action): State => {
   switch (action.type) {
-    case ActionTypes.CHANGE_LANGUAGE:
+    case ActionTypes.global.CHANGE_LANGUAGE:
       return action.language
-    case ActionTypes.PLAYED:
-      return action.payload.lang
     default:
       return state
   }
