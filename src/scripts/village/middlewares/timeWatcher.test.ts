96ab3c575e38bb2e66553834664f289ede7673a1
Yudai Udagawa <nimius.rd@gmail.com>
2019-01-22 00:46:00 +0900

    Update timeWatcher

--- src/scripts/village/middlewares/timeWatcher.test.ts
+++ src/scripts/village/middlewares/timeWatcher.test.ts
@@ -1,45 +1,27 @@
 import * as ActionTypes from '../constants/ActionTypes'
 import * as Ajv from 'ajv'
 import {VERSION} from '../constants/Version'
-import {initialState as agents} from '../reducers/agents'
 import {initialState as base} from '../reducers/base'
-import {initialState as chat} from '../reducers/chat'
-import {initialState as commandInputBox} from '../reducers/commandInputBox'
-import {initialState as commandSelection} from '../reducers/commandSelection'
+import fakeServer from '../containers/fakeStore'
 import fetch from 'node-fetch'
-import {initialState as hideButton} from '../reducers/hideButton'
-import {initialState as language} from '../reducers/language'
 import middleware from './timeWatcher'
-import {initialState as modal} from '../reducers/modal'
-import {initialState as obfucator} from '../reducers/obfucator'
-import {initialState as prediction} from '../reducers/prediction'
-import {initialState as result} from '../reducers/result'
-import {initialState as roles} from '../reducers/roles'
 import {socket} from '../actions'
 
 const BASE_URI = `https://werewolf.world/schema/${VERSION}`
 
 describe('socket/MESSAGE', () => {
   describe('phase: night -> morning, date: 0 -> 1', () => {
+    const store = fakeServer({
+      base: {
+        ... base,
+        date: 0,
+        phase: village.Phase.night
+      }
+    })
     const dispatch = jest.fn()
-    const getState = () => ({
-      agents,
-      base,
-      chat,
-      commandInputBox,
-      commandSelection,
-      hideButton,
-      language,
-      modal,
-      obfucator,
-      prediction,
-      result,
-      roles
-    })
-    const nextHandler = middleware({
-      dispatch,
-      getState
-    })
+
+    store.dispatch = dispatch
+    const nextHandler = middleware(store)
     const dispatchAPI = jest.fn()
     const actionHandler = nextHandler(dispatchAPI)
     const payload: village.Payload$systemMessage = {
@@ -117,25 +99,17 @@ describe('socket/MESSAGE', () => {
     })
   })
   describe('phase: night -> morning, date: 0', () => {
+    const store = fakeServer({
+      base: {
+        ... base,
+        date: 0,
+        phase: village.Phase.night
+      }
+    })
     const dispatch = jest.fn()
-    const getState = () => ({
-      agents,
-      base,
-      chat,
-      commandInputBox,
-      commandSelection,
-      hideButton,
-      language,
-      modal,
-      obfucator,
-      prediction,
-      result,
-      roles
-    })
-    const nextHandler = middleware({
-      dispatch,
-      getState
-    })
+
+    store.dispatch = dispatch
+    const nextHandler = middleware(store)
     const dispatchAPI = jest.fn()
     const actionHandler = nextHandler(dispatchAPI)
     const payload: village.Payload$systemMessage = {
@@ -207,26 +181,95 @@ describe('socket/MESSAGE', () => {
       })
     })
   })
-  describe('phase: night, date: 0 -> 1', () => {
+  describe('phase: night -> flavor text, date: 0', () => {
+    const store = fakeServer({
+      base: {
+        ... base,
+        date: 0,
+        phase: village.Phase.night
+      }
+    })
     const dispatch = jest.fn()
-    const getState = () => ({
-      agents,
-      base,
-      chat,
-      commandInputBox,
-      commandSelection,
-      hideButton,
-      language,
-      modal,
-      obfucator,
-      prediction,
-      result,
-      roles
+
+    store.dispatch = dispatch
+    const nextHandler = middleware(store)
+    const dispatchAPI = jest.fn()
+    const actionHandler = nextHandler(dispatchAPI)
+    const payload: village.Payload$flavorTextMessage = {
+      '@context': [
+        village.BaseContext.Base,
+        village.BaseContext.FlavorText
+      ],
+      '@id': 'https://licos.online/state/0.2/village#3/flavorTextMessage',
+      'clientTimestamp': '2006-10-07T12:06:56.568+09:00',
+      'date': 0,
+      'directionality': village.Directionality.serverToClient,
+      'extensionalDisclosureRange': [],
+      'flavorText': [],
+      'intensionalDisclosureRange': village.Channel.public,
+      'phase': village.Phase.flavorText,
+      'phaseStartTime': '2006-10-07T12:06:56.568+09:00',
+      'phaseTimeLimit': 600,
+      'serverTimestamp': '2006-10-07T12:06:56.568+09:00',
+      'token': 'eFVr3O93oLhmnE8OqTMl5VSVGIV',
+      'village': {
+        '@context': village.Context.Village,
+        '@id': 'https://licos.online/state/0.2/village',
+        'id': 3,
+        'lang': village.Language.ja,
+        'name': '横国の森の奥にある時代に取り残された小さな村',
+        'totalNumberOfAgents': 15
+      }
+    }
+
+    test('validate the JSON', async () => {
+      const ajv = new Ajv()
+
+      expect.hasAssertions()
+      await Promise.all([
+        fetch(`${BASE_URI}/systemMessage.json`)
+          .then(res => res.json()),
+        fetch(`${BASE_URI}/agent.json`)
+          .then(res => res.json()),
+        fetch(`${BASE_URI}/avatar.json`)
+          .then(res => res.json()),
+        fetch(`${BASE_URI}/base.json`)
+          .then(res => res.json()),
+        fetch(`${BASE_URI}/boardResult.json`)
+          .then(res => res.json()),
+        fetch(`${BASE_URI}/role.json`)
+          .then(res => res.json()),
+        fetch(`${BASE_URI}/time.json`)
+          .then(res => res.json()),
+        fetch(`${BASE_URI}/votingResult.json`)
+          .then(res => res.json())
+      ])
+        .then(schemas => {
+          const [schema, ... rest] = schemas
+          const validate = ajv
+            .addSchema(rest)
+            .validate(schema, payload)
+
+          expect(validate).toBe(true)
+        })
     })
-    const nextHandler = middleware({
-      dispatch,
-      getState
+    test('dispatch correctly', () => {
+      actionHandler(socket.message(payload))
+      expect(dispatch).toHaveBeenCalledTimes(0)
     })
+  })
+  describe('phase: night, date: 0 -> 1', () => {
+    const store = fakeServer({
+      base: {
+        ... base,
+        date: 0,
+        phase: village.Phase.night
+      }
+    })
+    const dispatch = jest.fn()
+
+    store.dispatch = dispatch
+    const nextHandler = middleware(store)
     const dispatchAPI = jest.fn()
     const actionHandler = nextHandler(dispatchAPI)
     const payload: village.Payload$systemMessage = {
@@ -299,25 +342,17 @@ describe('socket/MESSAGE', () => {
     })
   })
   describe('phase: night, date: 0', () => {
+    const store = fakeServer({
+      base: {
+        ... base,
+        date: 0,
+        phase: village.Phase.night
+      }
+    })
     const dispatch = jest.fn()
-    const getState = () => ({
-      agents,
-      base,
-      chat,
-      commandInputBox,
-      commandSelection,
-      hideButton,
-      language,
-      modal,
-      obfucator,
-      prediction,
-      result,
-      roles
-    })
-    const nextHandler = middleware({
-      dispatch,
-      getState
-    })
+
+    store.dispatch = dispatch
+    const nextHandler = middleware(store)
     const dispatchAPI = jest.fn()
     const actionHandler = nextHandler(dispatchAPI)
     const payload: village.Payload$systemMessage = {
