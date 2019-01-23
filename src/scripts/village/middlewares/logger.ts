63950ee6de67708614e4935179f80a4770f996d0
Yudai Udagawa <nimius.rd@gmail.com>
2019-01-24 04:34:26 +0900

    Add filter

--- src/scripts/village/middlewares/logger.ts
+++ src/scripts/village/middlewares/logger.ts
@@ -1,7 +1,11 @@
 /* eslint no-console: 0 */
+import * as ActionTypes from '../constants/ActionTypes'
 import {Middleware} from '.'
 
 const logger: Middleware = store => next => action => {
+  if (action.type === ActionTypes.global.TICK) {
+    return next(action)
+  }
   console.group(action.type)
   console.info('dispatching', action)
   console.log('next state', store.getState())
