c4bdf6c033e329fa2aa4776ab71510f0175aa8f3
Yudai Udagawa <nimius.rd@gmail.com>
2019-01-24 20:12:16 +0900

    Fix timerWatcher

--- src/scripts/village/middlewares/timeWatcher.ts
+++ src/scripts/village/middlewares/timeWatcher.ts
@@ -6,16 +6,13 @@ const timeWatcher: Middleware = store => next => action => {
   if (action.type === socket.MESSAGE) {
     const base = store.getState().base
 
-    console.log(action.payload.date, base.date)
-    console.log(action.payload.phase, base.phase)
-
     if (action.payload.date !== base.date) {
       store.dispatch(changeDate({
         from: base.date,
         to: action.payload.date
       }))
     }
-    if (action.payload.phase !== village.Phase.flavorText && action.payload.phase !== base.phase) {
+    if (action.payload.phase !== base.phase) {
       store.dispatch(changePhase({
         from: base.phase,
         to: action.payload.phase
