ad92c5ad46cd3461d980a82b076f87f3bfb3fbc2
Yudai Udagawa <nimius.rd@gmail.com>
2019-01-24 19:53:56 +0900

    wip

--- src/scripts/village/middlewares/timeWatcher.ts
+++ src/scripts/village/middlewares/timeWatcher.ts
@@ -6,6 +6,9 @@ const timeWatcher: Middleware = store => next => action => {
   if (action.type === socket.MESSAGE) {
     const base = store.getState().base
 
+    console.log(action.payload.date, base.date)
+    console.log(action.payload.phase, base.phase)
+
     if (action.payload.date !== base.date) {
       store.dispatch(changeDate({
         from: base.date,
