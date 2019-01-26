138ad363a742560aedb64d2fa99129709ddd2f68
Yudai Udagawa <nimius.rd@gmail.com>
2019-01-27 02:55:14 +0900

    Fix test

--- src/scripts/lobby/reducers/ping.test.ts
+++ src/scripts/lobby/reducers/ping.test.ts
@@ -1,3 +1,4 @@
+/* global lobby */
 import * as ActionTypes from '../constants/ActionTypes'
 import {ping, waitingPage} from './fakeServer'
 import reducer, {initialState} from './ping'
@@ -39,42 +40,52 @@ test('socket/MESSAGE ping', () => {
       results: [
         {
           ping: '99.999 s',
+          status: lobby.PingStatus.danger,
           token: '3F2504E0-4F89-11D3-9A0C-0305E82C3301'
         },
         {
-          ping: '99.999 s',
+          ping: '00.999 s',
+          status: lobby.PingStatus.safe,
           token: '3F2504E0-4F89-11D3-9A0C-0305E82C3302'
         },
         {
-          ping: '99.999 s',
+          ping: '02.999 s',
+          status: lobby.PingStatus.warning,
           token: '3F2504E0-4F89-11D3-9A0C-0305E82C3303'
         },
         {
-          ping: '99.999 s',
+          ping: '00.000 s',
+          status: lobby.PingStatus.safe,
           token: '3F2504E0-4F89-11D3-9A0C-0305E82C3304'
         },
         {
           ping: '99.999 s',
+          status: lobby.PingStatus.danger,
           token: '3F2504E0-4F89-11D3-9A0C-0305E82C3305'
         },
         {
           ping: '99.999 s',
+          status: lobby.PingStatus.danger,
           token: '3F2504E0-4F89-11D3-9A0C-0305E82C3306'
         },
         {
           ping: '99.999 s',
+          status: lobby.PingStatus.danger,
           token: '3F2504E0-4F89-11D3-9A0C-0305E82C3307'
         },
         {
           ping: '99.999 s',
+          status: lobby.PingStatus.danger,
           token: '3F2504E0-4F89-11D3-9A0C-0305E82C3308'
         },
         {
           ping: '99.999 s',
+          status: lobby.PingStatus.danger,
           token: '3F2504E0-4F89-11D3-9A0C-0305E82C3309'
         },
         {
           ping: '99.999 s',
+          status: lobby.PingStatus.danger,
           token: '3F2504E0-4F89-11D3-9A0C-0305E82C3310'
         }
       ]
