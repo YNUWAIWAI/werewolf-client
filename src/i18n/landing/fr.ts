7b6182a775958a62309b9e138f82041b00cb1161
Yudai Udagawa <nimius.rd@gmail.com>
2019-02-02 00:30:26 +0900

    Update

--- src/i18n/landing/fr.ts
+++ src/i18n/landing/fr.ts
@@ -3,7 +3,7 @@ export default {
   'login-label-email': 'Email',
   'login-label-password': 'Password',
   'login-submit': 'Login',
-  'signup-confirmation': 'Please agree to our <a href="terms" target="_blank">Terms, Data Policy and Cookies Policy</a> before signing up.',
+  'signup-confirmation': 'Please agree to our <a href="terms" target="_blank" rel="noreferrer noopener">Terms, Data Policy and Cookies Policy</a> before signing up.',
   'signup-error': 'Invalid name, email or password',
   'signup-label-email': 'Email',
   'signup-label-name': 'Name',
