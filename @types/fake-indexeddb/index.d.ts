4ccf9ae88f13de52bb4b8b5b41b4017ea29894e0
Yudai Udagawa <nimius.rd@gmail.com>
2019-02-17 19:33:35 +0900

    Add fake-indexeddb

---
+++ @types/fake-indexeddb/index.d.ts
@@ -0,0 +1,60 @@
+declare module 'fake-indexeddb' {
+  const FDB: any
+
+  export default FDB
+}
+declare module 'fake-indexeddb/lib/FDBCursor' {
+  const FDBCursor: any
+
+  export default FDBCursor
+}
+declare module 'fake-indexeddb/lib/FDBCursorWithValue' {
+  const FDBCursorWithValue: any
+
+  export default FDBCursorWithValue
+}
+declare module 'fake-indexeddb/lib/FDBDatabase' {
+  const FDBDatabase: any
+
+  export default FDBDatabase
+}
+declare module 'fake-indexeddb/lib/FDBFactory' {
+  const FDBFactory: anny
+
+  export default FDBFactory
+}
+declare module 'fake-indexeddb/lib/FDBIndex' {
+  const FDBIndex: any
+
+  export default FDBIndex
+}
+declare module 'fake-indexeddb/lib/FDBKeyRange' {
+  const FDBKeyRange: any
+
+  export default FDBKeyRange
+}
+declare module 'fake-indexeddb/lib/FDBObjectStore' {
+  const FDBObjectStore: any
+
+  export default FDBObjectStore
+}
+declare module 'fake-indexeddb/lib/FDBOpenDBRequest' {
+  const FDBOpenDBRequest: any
+
+  export default FDBOpenDBRequest
+}
+declare module 'fake-indexeddb/lib/FDBRequest' {
+  const FDBRequest: any
+
+  export default FDBRequest
+}
+declare module 'fake-indexeddb/lib/FDBTransaction' {
+  const FDBTransaction: any
+
+  export default FDBTransaction
+}
+declare module 'fake-indexeddb/lib/FDBVersionChangeEvent' {
+  const FDBVersionChangeEvent: any
+
+  export default FDBVersionChangeEvent
+}
