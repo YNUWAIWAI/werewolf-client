e8ee7bb4a6a8fc8e0861dc4e1644d2e19c340f63
Yudai Udagawa <nimius.rd@gmail.com>
2019-02-28 04:59:02 +0900

    Update fakeStore

--- src/scripts/village/containers/fakeStore/index.ts
+++ src/scripts/village/containers/fakeStore/index.ts
@@ -4,6 +4,7 @@ import {ReducerState} from '../../reducers'
 import {initialState as agents} from '../../reducers/agents'
 import {initialState as base} from '../../reducers/base'
 import {initialState as chat} from '../../reducers/chat'
+import {initialState as command} from '../../reducers/command'
 import {initialState as commandGrave} from '../../reducers/commandGrave'
 import {initialState as commandInputBox} from '../../reducers/commandInputBox'
 import {initialState as commandPostMortem} from '../../reducers/commandPostMortem'
@@ -22,6 +23,7 @@ export default (state?: Record<string, unknown>): Store<ReducerState, Action<unk
     agents,
     base,
     chat,
+    command,
     commandGrave,
     commandInputBox,
     commandPostMortem,
