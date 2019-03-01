c3bb115bcae22bb39e92d1d4598542f5103c9807
Yudai Udagawa <nimius.rd@gmail.com>
2019-03-02 05:07:54 +0900

    Update AgentIcon

--- src/scripts/village/components/atoms/AgentIcon.test.tsx
+++ src/scripts/village/components/atoms/AgentIcon.test.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import AgentIcon from './AgentIcon'
+import Question from './svg/Question'
 import {shallow} from 'enzyme'
 
 describe('<AgentIcon />', () => {
@@ -16,6 +17,21 @@ describe('<AgentIcon />', () => {
     expect(wrapper.find('.className').hasClass('undefined')).toBe(false)
     expect(wrapper.find('.className--image').exists()).toBe(true)
     expect(wrapper.find('.className--name').exists()).toBe(true)
+  })
+  test('<Question />', () => {
+    const wrapper = shallow(
+      <AgentIcon
+        className="className"
+        image=""
+        name="name"
+      />
+    )
+
+    expect(wrapper.find('.className').exists()).toBe(true)
+    expect(wrapper.find('.className').hasClass('undefined')).toBe(false)
+    expect(wrapper.find(Question).hasClass('undefined')).toBe(false)
+    expect(wrapper.find('.className--image').exists()).toBe(false)
+    expect(wrapper.find('.className--name').exists()).toBe(false)
   })
   test('additionalClass', () => {
     const wrapper = shallow(
