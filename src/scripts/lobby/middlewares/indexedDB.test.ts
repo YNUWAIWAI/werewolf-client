be0d0d0b9415e1e068924da70c26659021857374
Yudai Udagawa <nimius.rd@gmail.com>
2019-03-06 04:55:23 +0900

    Update type

--- src/scripts/lobby/middlewares/indexedDB.test.ts
+++ src/scripts/lobby/middlewares/indexedDB.test.ts
@@ -22,18 +22,18 @@ import {getCastFromNumberOfPlayers} from '../util'
 import middleware from './indexedDB'
 import {waitingPage} from '../reducers/fakeServer'
 
-(window as any).indexedDB = FDB;
-(global as any).IDBCursor = FDBCursor;
-(global as any).IDBCursorWithValue = FDBCursorWithValue;
-(global as any).IDBDatabase = FDBDatabase;
-(global as any).IDBFactory = FDBFactory;
-(global as any).IDBIndex = FDBIndex;
-(global as any).IDBKeyRange = FDBKeyRange;
-(global as any).IDBObjectStore = FDBObjectStore;
-(global as any).IDBOpenDBRequest = FDBOpenDBRequest;
-(global as any).IDBRequest = FDBRequest;
-(global as any).IDBTransaction = FDBTransaction;
-(global as any).IDBVersionChangeEvent = FDBVersionChangeEvent
+(window as Record<string, unknown>).indexedDB = FDB;
+(global as Record<string, unknown>).IDBCursor = FDBCursor;
+(global as Record<string, unknown>).IDBCursorWithValue = FDBCursorWithValue;
+(global as Record<string, unknown>).IDBDatabase = FDBDatabase;
+(global as Record<string, unknown>).IDBFactory = FDBFactory;
+(global as Record<string, unknown>).IDBIndex = FDBIndex;
+(global as Record<string, unknown>).IDBKeyRange = FDBKeyRange;
+(global as Record<string, unknown>).IDBObjectStore = FDBObjectStore;
+(global as Record<string, unknown>).IDBOpenDBRequest = FDBOpenDBRequest;
+(global as Record<string, unknown>).IDBRequest = FDBRequest;
+(global as Record<string, unknown>).IDBTransaction = FDBTransaction;
+(global as Record<string, unknown>).IDBVersionChangeEvent = FDBVersionChangeEvent
 
 const BASE_URI = `https://werewolf.world/lobby/schema/${VERSION}`
 const CLIENT2SERVER = `${BASE_URI}/client2server`
