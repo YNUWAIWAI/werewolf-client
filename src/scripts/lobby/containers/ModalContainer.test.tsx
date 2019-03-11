6c37da54c5b0702178a3e075325dc84169b71a6a
Yudai Udagawa <nimius.rd@gmail.com>
2019-03-11 19:25:21 +0900

    Fix ModalContainer's test

---
+++ src/scripts/lobby/containers/ModalContainer.test.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react'
+import IntlProviderContainer from './IntlProviderContainer'
+import ModalContainer from './ModalContainer'
+import {Provider} from 'react-redux'
+import fakeStore from './fakeStore'
+import {mount} from 'enzyme'
+
+describe('<ModalContainer />', () => {
+  test('visible', () => {
+    const store = fakeStore({
+      modal: {
+        descriptionId: '',
+        visible: true
+      }
+    })
+    const wrapper = mount(
+      <Provider store={store} >
+        <IntlProviderContainer>
+          <ModalContainer />
+        </IntlProviderContainer>
+      </Provider>
+    )
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+  test('hidden', () => {
+    const store = fakeStore({
+      modal: {
+        descriptionId: '',
+        visible: false
+      }
+    })
+    const wrapper = mount(
+      <Provider store={store} >
+        <IntlProviderContainer>
+          <ModalContainer />
+        </IntlProviderContainer>
+      </Provider>
+    )
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
