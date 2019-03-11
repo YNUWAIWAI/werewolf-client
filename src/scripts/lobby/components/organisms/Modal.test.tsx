71ac68a853e87169f97c8fb5dd51bd5eb504186e
Yudai Udagawa <nimius.rd@gmail.com>
2019-03-11 19:18:54 +0900

    Add Modal's test

---
+++ src/scripts/lobby/components/organisms/Modal.test.tsx
@@ -0,0 +1,112 @@
+/* global lobby */
+import * as React from 'react'
+import Modal from './Modal'
+import {Provider} from 'react-redux'
+import fakeStore from '../../containers/fakeStore'
+import {getMessages} from '../../../../i18n/lobby'
+import {initRenderer} from '../../../../../tools/intl-enzyme-test-helper'
+
+const store = fakeStore()
+const {mountWithIntl} = initRenderer(lobby.Language.ja, getMessages(lobby.Language.ja))
+
+describe('render', () => {
+  test('visible', () => {
+    const handleClickNoMockFn = jest.fn()
+    const handleClickYesMockFn = jest.fn()
+    const wrapper = mountWithIntl(
+      <Provider store={store}>
+        <Modal
+          descriptionId="Modal.Description.dayVote"
+          handleClickNo={handleClickNoMockFn}
+          handleClickYes={handleClickYesMockFn}
+          visible
+        />
+      </Provider>
+    )
+
+    expect(wrapper.find('AgentIcon').exists()).toBe(true)
+    expect(wrapper.find('Description').exists()).toBe(true)
+    expect(wrapper.find('.modal--button')).toHaveLength(2)
+    expect(wrapper.find('.modal--button.yes').exists()).toBe(true)
+    expect(wrapper.find('.modal--button.yes').text()).toBe('はい')
+    expect(wrapper.find('.modal--button.no').exists()).toBe(true)
+    expect(wrapper.find('.modal--button.no').text()).toBe('いいえ')
+    expect(handleClickNoMockFn).toHaveBeenCalledTimes(0)
+    expect(handleClickYesMockFn).toHaveBeenCalledTimes(0)
+  })
+  test('visible={false}', () => {
+    const handleClickNoMockFn = jest.fn()
+    const handleClickYesMockFn = jest.fn()
+    const wrapper = mountWithIntl(
+      <Provider store={store}>
+        <Modal
+          descriptionId="Modal.Description.dayVote"
+          handleClickNo={handleClickNoMockFn}
+          handleClickYes={handleClickYesMockFn}
+          visible={false}
+        />
+      </Provider>
+    )
+
+    expect(wrapper.find('.modal').hasClass('hidden')).toBe(true)
+  })
+})
+test('handleClickNo', () => {
+  const handleClickNoMockFn = jest.fn()
+  const handleClickYesMockFn = jest.fn()
+  const wrapper = mountWithIntl(
+    <Provider store={store}>
+      <Modal
+        descriptionId="Modal.Description.dayVote"
+        handleClickNo={handleClickNoMockFn}
+        handleClickYes={handleClickYesMockFn}
+        visible
+      />
+    </Provider>
+  )
+
+  expect(wrapper.find('.modal--button')).toHaveLength(2)
+  expect(wrapper.find('.modal--button.yes').exists()).toBe(true)
+  expect(wrapper.find('.modal--button.yes').text()).toBe('はい')
+  expect(wrapper.find('.modal--button.no').exists()).toBe(true)
+  expect(wrapper.find('.modal--button.no').text()).toBe('いいえ')
+  wrapper.find('.modal--button.no').simulate('click')
+  expect(handleClickNoMockFn).toHaveBeenCalledTimes(1)
+  expect(handleClickYesMockFn).toHaveBeenCalledTimes(0)
+  expect(handleClickNoMockFn).toHaveBeenCalledWith()
+  expect(wrapper.find('.modal--button')).toHaveLength(2)
+  expect(wrapper.find('.modal--button.yes')).toHaveLength(1)
+  expect(wrapper.find('.modal--button.yes').text()).toBe('はい')
+  expect(wrapper.find('.modal--button.no')).toHaveLength(1)
+  expect(wrapper.find('.modal--button.no').text()).toBe('いいえ')
+})
+test('handleClickYes', () => {
+  const handleClickNoMockFn = jest.fn()
+  const handleClickYesMockFn = jest.fn()
+  const wrapper = mountWithIntl(
+    <Provider store={store}>
+      <Modal
+        descriptionId="Modal.Description.dayVote"
+        handleClickNo={handleClickNoMockFn}
+        handleClickYes={handleClickYesMockFn}
+        visible
+      />
+    </Provider>
+  )
+
+  expect(wrapper.find('.modal--button')).toHaveLength(2)
+  expect(wrapper.find('.modal--button.yes').exists()).toBe(true)
+  expect(wrapper.find('.modal--button.yes').text()).toBe('はい')
+  expect(wrapper.find('.modal--button.no').exists()).toBe(true)
+  expect(wrapper.find('.modal--button.no').text()).toBe('いいえ')
+  wrapper.find('.modal--button.yes').simulate('click')
+  expect(handleClickNoMockFn).toHaveBeenCalledTimes(0)
+  expect(handleClickYesMockFn).toHaveBeenCalledTimes(1)
+  expect(handleClickYesMockFn).toHaveBeenCalledWith(1)
+  expect(wrapper.find('.modal--button')).toHaveLength(2)
+  expect(wrapper.find('.modal--button.yes').exists()).toBe(true)
+  expect(wrapper.find('.modal--button.yes').text()).toBe('はい')
+  expect(wrapper.find('.modal--button.no').exists()).toBe(true)
+  expect(wrapper.find('.modal--button.no').text()).toBe('いいえ')
+})
+
