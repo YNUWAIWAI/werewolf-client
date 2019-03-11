386436f654174ef10985d0970696ea48632a3444
Yudai Udagawa <nimius.rd@gmail.com>
2019-03-11 16:17:42 +0900

    Fix AvatarItem test

--- src/scripts/lobby/components/molecules/AvatarItem.test.tsx
+++ src/scripts/lobby/components/molecules/AvatarItem.test.tsx
@@ -6,141 +6,229 @@ import {initRenderer} from '../../../../../tools/intl-enzyme-test-helper'
 
 const {mountWithIntl} = initRenderer(lobby.Language.ja, getMessages(lobby.Language.ja))
 
-test('<AvatarItem avatarImage="avatarImage" canKickOut isHost isMe name="name" />', () => {
-  const kickOut = jest.fn()
-  const wrapper = mountWithIntl(
-    <AvatarItem
-      avatarImage="avatarImage"
-      canKickOut
-      isAnonymous={false}
-      isHost
-      isMe
-      kickOut={kickOut}
-      name="name"
-      ping="99.999 s"
-      pingStatus={lobby.PingStatus.danger}
-    />
-  )
+describe('render', () => {
+  test('canKickOut={true} isHost={true} isMe={true}', () => {
+    const confirmKickOutPlayer = jest.fn()
+    const wrapper = mountWithIntl(
+      <AvatarItem
+        avatarImage="avatarImage"
+        canKickOut
+        confirmKickOutPlayer={confirmKickOutPlayer}
+        isAnonymous={false}
+        isHost
+        isMe
+        name="name"
+        ping="99.999 s"
+        pingStatus={lobby.PingStatus.danger}
+      />
+    )
 
-  expect(wrapper.find('.avatar-list--item--image').exists()).toBe(true)
-  expect(wrapper.find('.avatar-list--item').hasClass('me')).toBe(true)
-  expect(wrapper.find('.avatar-list--item--name').text()).toBe('name')
-  expect(wrapper.find('.avatar-list--item--host').text()).toBe('Host')
-  expect(wrapper.find('.avatar-list--item--ping').text()).toBe('99.999 s')
-  expect(kickOut).toHaveBeenCalledTimes(0)
+    expect(wrapper.find('.avatar-list--item--image').exists()).toBe(true)
+    expect(wrapper.find('.avatar-list--item').hasClass('me')).toBe(true)
+    expect(wrapper.find('.avatar-list--item--name').text()).toBe('name')
+    expect(wrapper.find('.avatar-list--item--host').text()).toBe('Host')
+    expect(wrapper.find('.avatar-list--item--ping').text()).toBe('99.999 s')
+    expect(confirmKickOutPlayer).toHaveBeenCalledTimes(0)
+  })
+  test('canKickOut={false} isHost={true} isMe={true}', () => {
+    const confirmKickOutPlayer = jest.fn()
+    const wrapper = mountWithIntl(
+      <AvatarItem
+        avatarImage="avatarImage"
+        canKickOut={false}
+        confirmKickOutPlayer={confirmKickOutPlayer}
+        isAnonymous={false}
+        isHost
+        isMe
+        name="name"
+        ping="99.999 s"
+        pingStatus={lobby.PingStatus.danger}
+      />
+    )
 
+    expect(wrapper.find('.avatar-list--item--image').exists()).toBe(true)
+    expect(wrapper.find('.avatar-list--item').hasClass('me')).toBe(true)
+    expect(wrapper.find('.avatar-list--item--name').text()).toBe('name')
+    expect(wrapper.find('.avatar-list--item--host').text()).toBe('Host')
+    expect(wrapper.find('.avatar-list--item--ping').text()).toBe('99.999 s')
+    expect(confirmKickOutPlayer).toHaveBeenCalledTimes(0)
+  })
+  test('canKickOut={true} isHost={false} isMe={true}', () => {
+    const confirmKickOutPlayer = jest.fn()
+    const wrapper = mountWithIntl(
+      <AvatarItem
+        avatarImage="avatarImage"
+        canKickOut
+        confirmKickOutPlayer={confirmKickOutPlayer}
+        isAnonymous={false}
+        isHost={false}
+        isMe
+        name="name"
+        ping="99.999 s"
+        pingStatus={lobby.PingStatus.danger}
+      />
+    )
+
+    expect(wrapper.find('.avatar-list--item--image').exists()).toBe(true)
+    expect(wrapper.find('.avatar-list--item').hasClass('me')).toBe(true)
+    expect(wrapper.find('.avatar-list--item--name').text()).toBe('name')
+    expect(wrapper.find('.avatar-list--item--host').exists()).toBe(false)
+    expect(wrapper.find('.avatar-list--item--ping').text()).toBe('99.999 s')
+    expect(confirmKickOutPlayer).toHaveBeenCalledTimes(0)
+  })
+  test('canKickOut={true} isHost={true} isMe={false}', () => {
+    const confirmKickOutPlayer = jest.fn()
+    const wrapper = mountWithIntl(
+      <AvatarItem
+        avatarImage="avatarImage"
+        canKickOut
+        confirmKickOutPlayer={confirmKickOutPlayer}
+        isAnonymous={false}
+        isHost
+        isMe={false}
+        name="name"
+        ping="99.999 s"
+        pingStatus={lobby.PingStatus.danger}
+      />
+    )
+
+    expect(wrapper.find('.avatar-list--item--image').exists()).toBe(true)
+    expect(wrapper.find('.avatar-list--item').hasClass('me')).toBe(false)
+    expect(wrapper.find('.avatar-list--item--name').text()).toBe('name')
+    expect(wrapper.find('.avatar-list--item--host').text()).toBe('Host')
+    expect(wrapper.find('.avatar-list--item--ping').text()).toBe('99.999 s')
+    expect(confirmKickOutPlayer).toHaveBeenCalledTimes(0)
+  })
+  test('canKickOut={false} isHost={false} isMe={true}', () => {
+    const confirmKickOutPlayer = jest.fn()
+    const wrapper = mountWithIntl(
+      <AvatarItem
+        avatarImage="avatarImage"
+        canKickOut={false}
+        confirmKickOutPlayer={confirmKickOutPlayer}
+        isAnonymous={false}
+        isHost={false}
+        isMe
+        name="name"
+        ping="99.999 s"
+        pingStatus={lobby.PingStatus.danger}
+      />
+    )
+
+    expect(wrapper.find('.avatar-list--item--image').exists()).toBe(true)
+    expect(wrapper.find('.avatar-list--item').hasClass('me')).toBe(true)
+    expect(wrapper.find('.avatar-list--item--name').text()).toBe('name')
+    expect(wrapper.find('.avatar-list--item--host').exists()).toBe(false)
+    expect(wrapper.find('.avatar-list--item--ping').text()).toBe('99.999 s')
+    expect(confirmKickOutPlayer).toHaveBeenCalledTimes(0)
+  })
+  test('canKickOut={true} isHost={false} isMe={false}', () => {
+    const confirmKickOutPlayer = jest.fn()
+    const wrapper = mountWithIntl(
+      <AvatarItem
+        avatarImage="avatarImage"
+        canKickOut
+        confirmKickOutPlayer={confirmKickOutPlayer}
+        isAnonymous={false}
+        isHost={false}
+        isMe={false}
+        name="name"
+        ping="99.999 s"
+        pingStatus={lobby.PingStatus.danger}
+      />
+    )
+
+    expect(wrapper.find('.avatar-list--item--image').exists()).toBe(true)
+    expect(wrapper.find('.avatar-list--item').hasClass('me')).toBe(false)
+    expect(wrapper.find('.avatar-list--item--name').text()).toBe('name')
+    expect(wrapper.find('.avatar-list--item--host').exists()).toBe(false)
+    expect(wrapper.find('.avatar-list--item--ping').text()).toBe('99.999 s')
+    expect(confirmKickOutPlayer).toHaveBeenCalledTimes(0)
+  })
+  test('canKickOut={false} isHost={true} isMe={false}', () => {
+    const confirmKickOutPlayer = jest.fn()
+    const wrapper = mountWithIntl(
+      <AvatarItem
+        avatarImage="avatarImage"
+        canKickOut={false}
+        confirmKickOutPlayer={confirmKickOutPlayer}
+        isAnonymous={false}
+        isHost
+        isMe={false}
+        name="name"
+        ping="99.999 s"
+        pingStatus={lobby.PingStatus.danger}
+      />
+    )
+
+    expect(wrapper.find('.avatar-list--item--image').exists()).toBe(true)
+    expect(wrapper.find('.avatar-list--item').hasClass('me')).toBe(false)
+    expect(wrapper.find('.avatar-list--item--name').text()).toBe('name')
+    expect(wrapper.find('.avatar-list--item--host').exists()).toBe(true)
+    expect(wrapper.find('.avatar-list--item--ping').text()).toBe('99.999 s')
+    expect(confirmKickOutPlayer).toHaveBeenCalledTimes(0)
+  })
+  test('canKickOut={false} isHost={false} isMe={false}', () => {
+    const confirmKickOutPlayer = jest.fn()
+    const wrapper = mountWithIntl(
+      <AvatarItem
+        avatarImage="avatarImage"
+        canKickOut={false}
+        confirmKickOutPlayer={confirmKickOutPlayer}
+        isAnonymous={false}
+        isHost={false}
+        isMe={false}
+        name="name"
+        ping="99.999 s"
+        pingStatus={lobby.PingStatus.danger}
+      />
+    )
+
+    expect(wrapper.find('.avatar-list--item--image').exists()).toBe(true)
+    expect(wrapper.find('.avatar-list--item').hasClass('me')).toBe(false)
+    expect(wrapper.find('.avatar-list--item--name').text()).toBe('name')
+    expect(wrapper.find('.avatar-list--item--host').exists()).toBe(false)
+    expect(wrapper.find('.avatar-list--item--ping').text()).toBe('99.999 s')
+    expect(confirmKickOutPlayer).toHaveBeenCalledTimes(0)
+  })
 })
-test('<AvatarItem avatarImage="avatarImage" canKickOut isHost={false} isMe name="name" />', () => {
-  const kickOut = jest.fn()
-  const wrapper = mountWithIntl(
-    <AvatarItem
-      avatarImage="avatarImage"
-      canKickOut
-      isAnonymous={false}
-      isHost={false}
-      isMe
-      kickOut={kickOut}
-      name="name"
-      ping="99.999 s"
-      pingStatus={lobby.PingStatus.danger}
-    />
-  )
+describe('onClick', () => {
+  test('canKickOut={true}', () => {
+    const confirmKickOutPlayer = jest.fn()
+    const wrapper = mountWithIntl(
+      <AvatarItem
+        avatarImage="avatarImage"
+        canKickOut
+        confirmKickOutPlayer={confirmKickOutPlayer}
+        isAnonymous={false}
+        isHost={false}
+        isMe
+        name="name"
+        ping="99.999 s"
+        pingStatus={lobby.PingStatus.danger}
+      />
+    )
 
-  expect(wrapper.find('.avatar-list--item--image').exists()).toBe(true)
-  expect(wrapper.find('.avatar-list--item').hasClass('me')).toBe(true)
-  expect(wrapper.find('.avatar-list--item--name').text()).toBe('name')
-  expect(wrapper.find('.avatar-list--item--host').exists()).toBe(false)
-  expect(wrapper.find('.avatar-list--item--ping').text()).toBe('99.999 s')
-  expect(kickOut).toHaveBeenCalledTimes(0)
+    wrapper.simulate('click')
+    expect(confirmKickOutPlayer).toHaveBeenCalledTimes(1)
+  })
+  test('canKickOut={false}', () => {
+    const confirmKickOutPlayer = jest.fn()
+    const wrapper = mountWithIntl(
+      <AvatarItem
+        avatarImage="avatarImage"
+        canKickOut={false}
+        confirmKickOutPlayer={confirmKickOutPlayer}
+        isAnonymous={false}
+        isHost={false}
+        isMe
+        name="name"
+        ping="99.999 s"
+        pingStatus={lobby.PingStatus.danger}
+      />
+    )
 
-})
-test('<AvatarItem avatarImage="avatarImage" canKickOut isHost isMe={false} name="name" />', () => {
-  const kickOut = jest.fn()
-  const wrapper = mountWithIntl(
-    <AvatarItem
-      avatarImage="avatarImage"
-      canKickOut
-      isAnonymous={false}
-      isHost
-      isMe={false}
-      kickOut={kickOut}
-      name="name"
-      ping="99.999 s"
-      pingStatus={lobby.PingStatus.danger}
-    />
-  )
-
-  expect(wrapper.find('.avatar-list--item--image').exists()).toBe(true)
-  expect(wrapper.find('.avatar-list--item').hasClass('me')).toBe(false)
-  expect(wrapper.find('.avatar-list--item--name').text()).toBe('name')
-  expect(wrapper.find('.avatar-list--item--host').text()).toBe('Host')
-  expect(wrapper.find('.avatar-list--item--ping').text()).toBe('99.999 s')
-  expect(kickOut).toHaveBeenCalledTimes(0)
-
-})
-test('<AvatarItem avatarImage="avatarImage" canKickOut isHost={false} isMe={false} name="name" />', () => {
-  const kickOut = jest.fn()
-  const wrapper = mountWithIntl(
-    <AvatarItem
-      avatarImage="avatarImage"
-      canKickOut
-      isAnonymous={false}
-      isHost={false}
-      isMe={false}
-      kickOut={kickOut}
-      name="name"
-      ping="99.999 s"
-      pingStatus={lobby.PingStatus.danger}
-    />
-  )
-
-  expect(wrapper.find('.avatar-list--item--image').exists()).toBe(true)
-  expect(wrapper.find('.avatar-list--item').hasClass('me')).toBe(false)
-  expect(wrapper.find('.avatar-list--item--name').text()).toBe('name')
-  expect(wrapper.find('.avatar-list--item--host').exists()).toBe(false)
-  expect(wrapper.find('.avatar-list--item--ping').text()).toBe('99.999 s')
-  expect(kickOut).toHaveBeenCalledTimes(0)
-
-})
-test('<AvatarItem avatarImage="avatarImage" canKickOut isHost={false} isMe name="name" /> onClick', () => {
-  const kickOut = jest.fn()
-  const wrapper = mountWithIntl(
-    <AvatarItem
-      avatarImage="avatarImage"
-      canKickOut
-      isAnonymous={false}
-      isHost={false}
-      isMe
-      kickOut={kickOut}
-      name="name"
-      ping="99.999 s"
-      pingStatus={lobby.PingStatus.danger}
-    />
-  )
-
-  const tmp = window.confirm
-
-  window.confirm = () => true
-  wrapper.simulate('click')
-  expect(kickOut).toHaveBeenCalledTimes(1)
-  window.confirm = tmp
-})
-test('<AvatarItem avatarImage="avatarImage" canKickOut={false} isHost={false} isMe name="name" /> onClick', () => {
-  const kickOut = jest.fn()
-  const wrapper = mountWithIntl(
-    <AvatarItem
-      avatarImage="avatarImage"
-      canKickOut={false}
-      isAnonymous={false}
-      isHost={false}
-      isMe
-      kickOut={kickOut}
-      name="name"
-      ping="99.999 s"
-      pingStatus={lobby.PingStatus.danger}
-    />
-  )
-
-  wrapper.simulate('click')
-  expect(kickOut).toHaveBeenCalledTimes(0)
+    wrapper.simulate('click')
+    expect(confirmKickOutPlayer).toHaveBeenCalledTimes(0)
+  })
 })
