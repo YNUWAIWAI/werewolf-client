b024e9bd5f379b590fd791a30de20b3d197093e2
Yudai Udagawa <nimius.rd@gmail.com>
2019-03-12 23:32:47 +0900

    Fix HistoryContainer

--- src/scripts/lobby/containers/HistoryContainer.test.tsx
+++ src/scripts/lobby/containers/HistoryContainer.test.tsx
@@ -1,19 +1,63 @@
+import * as ActionTypes from '../constants/ActionTypes'
 import * as React from 'react'
+import History from '../components/templates/History'
 import HistoryContainer from './HistoryContainer'
 import IntlProviderContainer from './IntlProviderContainer'
 import {Provider} from 'react-redux'
 import fakeStore from './fakeStore'
 import {mount} from 'enzyme'
 
-test('<HistoryContainer />', () => {
-  const store = fakeStore()
-  const wrapper = mount(
-    <Provider store={store} >
-      <IntlProviderContainer>
-        <HistoryContainer />
-      </IntlProviderContainer>
-    </Provider>
-  )
+describe('<HistoryContainer />', () => {
+  test('render', () => {
+    const store = fakeStore()
+    const wrapper = mount(
+      <Provider store={store} >
+        <IntlProviderContainer>
+          <HistoryContainer />
+        </IntlProviderContainer>
+      </Provider>
+    )
 
-  expect(wrapper.html()).toMatchSnapshot()
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+  test('selectVillage', () => {
+    const store = fakeStore()
+    const dispatch = jest.fn()
+
+    store.dispatch = dispatch
+    const wrapper = mount(
+      <Provider store={store} >
+        <IntlProviderContainer>
+          <HistoryContainer />
+        </IntlProviderContainer>
+      </Provider>
+    )
+    const id = 1
+
+    wrapper.find(History).props().selectVillage(id)()
+    expect(dispatch).toHaveBeenCalledTimes(1)
+    expect(dispatch).toHaveBeenCalledWith({
+      id,
+      type: ActionTypes.global.SELECT_VILLAGE
+    })
+  })
+  test('transition', () => {
+    const store = fakeStore()
+    const dispatch = jest.fn()
+
+    store.dispatch = dispatch
+    const wrapper = mount(
+      <Provider store={store} >
+        <IntlProviderContainer>
+          <HistoryContainer />
+        </IntlProviderContainer>
+      </Provider>
+    )
+
+    wrapper.find(History).props().transition(ActionTypes.Target.SHOW_BUILD_VILLAGE)
+    expect(dispatch).toHaveBeenCalledTimes(1)
+    expect(dispatch).toHaveBeenCalledWith({
+      type: ActionTypes.Target.SHOW_BUILD_VILLAGE
+    })
+  })
 })
