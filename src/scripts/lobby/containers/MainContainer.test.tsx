91ae6f53aff072072868621932aeada92c32ba8d
Yudai Udagawa <nimius.rd@gmail.com>
2019-03-12 22:24:29 +0900

    Fix MainContainer's test

--- src/scripts/lobby/containers/MainContainer.test.tsx
+++ src/scripts/lobby/containers/MainContainer.test.tsx
@@ -1,19 +1,42 @@
+import * as ActionTypes from '../constants/ActionTypes'
 import * as React from 'react'
 import IntlProviderContainer from './IntlProviderContainer'
+import Main from '../components/templates/Main'
 import MainContainer from './MainContainer'
 import {Provider} from 'react-redux'
 import fakeStore from './fakeStore'
 import {mount} from 'enzyme'
 
-test('<MainContainer />', () => {
-  const store = fakeStore()
-  const wrapper = mount(
-    <Provider store={store} >
-      <IntlProviderContainer>
-        <MainContainer />
-      </IntlProviderContainer>
-    </Provider>
-  )
+describe('<MainContainer />', () => {
+  test('render', () => {
+    const store = fakeStore()
+    const wrapper = mount(
+      <Provider store={store} >
+        <IntlProviderContainer>
+          <MainContainer />
+        </IntlProviderContainer>
+      </Provider>
+    )
 
-  expect(wrapper.html()).toMatchSnapshot()
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+  test('transition', () => {
+    const store = fakeStore()
+    const dispatch = jest.fn()
+
+    store.dispatch = dispatch
+    const wrapper = mount(
+      <Provider store={store} >
+        <IntlProviderContainer>
+          <MainContainer />
+        </IntlProviderContainer>
+      </Provider>
+    )
+
+    wrapper.find(Main).props().transition(ActionTypes.Target.SHOW_CREDITS)
+    expect(dispatch).toHaveBeenCalledTimes(1)
+    expect(dispatch).toHaveBeenCalledWith({
+      type: ActionTypes.Target.SHOW_CREDITS
+    })
+  })
 })
