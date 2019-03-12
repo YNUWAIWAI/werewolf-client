ade11d858bf7608237d775df29d1e15f03492898
Yudai Udagawa <nimius.rd@gmail.com>
2019-03-12 15:19:16 +0900

    Fix obfucator's test

--- src/scripts/lobby/reducers/obfucator.test.ts
+++ src/scripts/lobby/reducers/obfucator.test.ts
@@ -1,6 +1,48 @@
+import * as ActionTypes from '../constants/ActionTypes'
 import reducer, {initialState} from './obfucator'
 import {socket} from '../actions'
 
+test('CONFIRM_KICK_OUT_PLAYER', () => {
+  expect(
+    reducer(
+      initialState,
+      {
+        name: 'Alice',
+        token: '3F2504E0-4F89-11D3-9A0C-0305E82C3300',
+        type: ActionTypes.global.CONFIRM_KICK_OUT_PLAYER
+      }
+    )
+  ).toEqual({
+    loading: false,
+    visible: true
+  })
+})
+test('SELECT_NO', () => {
+  expect(
+    reducer(
+      initialState,
+      {
+        type: ActionTypes.global.SELECT_NO
+      }
+    )
+  ).toEqual({
+    loading: false,
+    visible: false
+  })
+})
+test('SELECT_YES', () => {
+  expect(
+    reducer(
+      initialState,
+      {
+        type: ActionTypes.global.SELECT_YES
+      }
+    )
+  ).toEqual({
+    loading: false,
+    visible: false
+  })
+})
 test('socket/CLOSE', () => {
   expect(
     reducer(
