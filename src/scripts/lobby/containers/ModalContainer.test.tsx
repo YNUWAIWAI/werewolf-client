690bdad7db32519bc34f0d5dcb79d443ac98e4e7
Yudai Udagawa <nimius.rd@gmail.com>
2019-03-12 18:49:14 +0900

    Fix ModalContainer's test

--- src/scripts/lobby/containers/ModalContainer.test.tsx
+++ src/scripts/lobby/containers/ModalContainer.test.tsx
@@ -1,34 +1,60 @@
+import * as ActionTypes from '../constants/ActionTypes'
 import * as React from 'react'
 import IntlProviderContainer from './IntlProviderContainer'
+import Modal from '../components/organisms/Modal'
 import ModalContainer from './ModalContainer'
 import {Provider} from 'react-redux'
 import fakeStore from './fakeStore'
 import {mount} from 'enzyme'
 
 describe('<ModalContainer />', () => {
-  test('visible', () => {
-    const store = fakeStore({
-      modal: {
-        descriptionId: '@dummy',
-        type: 'type',
-        values: {
-          name: 'Alice',
-          token: '3F2504E0-4F89-11D3-9A0C-0305E82C3300'
-        },
-        visible: true
-      }
-    })
-    const wrapper = mount(
-      <Provider store={store} >
-        <IntlProviderContainer>
-          <ModalContainer />
-        </IntlProviderContainer>
-      </Provider>
-    )
+  describe('render', () => {
+    test('visible', () => {
+      const store = fakeStore({
+        modal: {
+          descriptionId: '@dummy',
+          type: 'type',
+          values: {
+            name: 'Alice',
+            token: '3F2504E0-4F89-11D3-9A0C-0305E82C3300'
+          },
+          visible: true
+        }
+      })
+      const wrapper = mount(
+        <Provider store={store} >
+          <IntlProviderContainer>
+            <ModalContainer />
+          </IntlProviderContainer>
+        </Provider>
+      )
 
-    expect(wrapper.html()).toMatchSnapshot()
+      expect(wrapper.html()).toMatchSnapshot()
+    })
+    test('hidden', () => {
+      const store = fakeStore({
+        modal: {
+          descriptionId: '@dummy',
+          type: 'type',
+          values: {
+            name: '',
+            token: ''
+          },
+          visible: false
+        }
+      })
+      const wrapper = mount(
+        <Provider store={store} >
+          <IntlProviderContainer>
+            <ModalContainer />
+          </IntlProviderContainer>
+        </Provider>
+      )
+
+      expect(wrapper.html()).toMatchSnapshot()
+    })
   })
-  test('hidden', () => {
+  test('handleClickNo', () => {
     const store = fakeStore({
       modal: {
         descriptionId: '@dummy',
@@ -40,6 +66,9 @@ describe('<ModalContainer />', () => {
         visible: false
       }
     })
+    const dispatch = jest.fn()
+
+    store.dispatch = dispatch
     const wrapper = mount(
       <Provider store={store} >
         <IntlProviderContainer>
@@ -48,6 +77,44 @@ describe('<ModalContainer />', () => {
       </Provider>
     )
 
-    expect(wrapper.html()).toMatchSnapshot()
+    wrapper.find(Modal).props().handleClickNo()
+    expect(dispatch).toHaveBeenCalledTimes(1)
+    expect(dispatch).toHaveBeenCalledWith({
+      type: ActionTypes.global.SELECT_NO
+    })
+  })
+  describe('handleClickYes', () => {
+    test('KICK_OUT_PLAYER', () => {
+      const store = fakeStore({
+        modal: {
+          descriptionId: '@dummy',
+          type: 'type',
+          values: {
+            name: '',
+            token: ''
+          },
+          visible: false
+        }
+      })
+      const dispatch = jest.fn()
+
+      store.dispatch = dispatch
+      const wrapper = mount(
+        <Provider store={store} >
+          <IntlProviderContainer>
+            <ModalContainer />
+          </IntlProviderContainer>
+        </Provider>
+      )
+
+      wrapper.find(Modal).props().handleClickYes(ActionTypes.global.KICK_OUT_PLAYER)
+      expect(dispatch).toHaveBeenCalledTimes(2)
+      expect(dispatch).toHaveBeenCalledWith({
+        type: ActionTypes.global.SELECT_YES
+      })
+      expect(dispatch).toHaveBeenCalledWith({
+        type: ActionTypes.global.KICK_OUT_PLAYER
+      })
+    })
   })
 })
