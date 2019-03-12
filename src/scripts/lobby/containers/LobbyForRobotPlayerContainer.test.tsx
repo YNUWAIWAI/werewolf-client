69c088bfc891584b7ca745f10989d490531e564b
Yudai Udagawa <nimius.rd@gmail.com>
2019-03-12 22:52:54 +0900

    Fix LobbyForRobotPlayer's test

--- src/scripts/lobby/containers/LobbyForRobotPlayerContainer.test.tsx
+++ src/scripts/lobby/containers/LobbyForRobotPlayerContainer.test.tsx
@@ -1,19 +1,42 @@
+import * as ActionTypes from '../constants/ActionTypes'
 import * as React from 'react'
 import IntlProviderContainer from './IntlProviderContainer'
+import LobbyForRobotPlayer from '../components/templates/LobbyForRobotPlayer'
 import LobbyForRobotPlayerContainer from './LobbyForRobotPlayerContainer'
 import {Provider} from 'react-redux'
 import fakeStore from './fakeStore'
 import {mount} from 'enzyme'
 
-test('<LobbyForRobotPlayerContainer />', () => {
-  const store = fakeStore()
-  const wrapper = mount(
-    <Provider store={store} >
-      <IntlProviderContainer>
-        <LobbyForRobotPlayerContainer />
-      </IntlProviderContainer>
-    </Provider>
-  )
+describe('<LobbyForRobotPlayerContainer />', () => {
+  test('render', () => {
+    const store = fakeStore()
+    const wrapper = mount(
+      <Provider store={store} >
+        <IntlProviderContainer>
+          <LobbyForRobotPlayerContainer />
+        </IntlProviderContainer>
+      </Provider>
+    )
 
-  expect(wrapper.html()).toMatchSnapshot()
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+  test('transition', () => {
+    const store = fakeStore()
+    const dispatch = jest.fn()
+
+    store.dispatch = dispatch
+    const wrapper = mount(
+      <Provider store={store} >
+        <IntlProviderContainer>
+          <LobbyForRobotPlayerContainer />
+        </IntlProviderContainer>
+      </Provider>
+    )
+
+    wrapper.find(LobbyForRobotPlayer).props().transition(ActionTypes.Target.SHOW_BUILD_VILLAGE)
+    expect(dispatch).toHaveBeenCalledTimes(1)
+    expect(dispatch).toHaveBeenCalledWith({
+      type: ActionTypes.Target.SHOW_BUILD_VILLAGE
+    })
+  })
 })
