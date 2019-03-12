32da2f05d7118b024baf94479e931635ccd27f89
Yudai Udagawa <nimius.rd@gmail.com>
2019-03-12 14:57:16 +0900

    Fix modal

--- src/scripts/lobby/reducers/modal.ts
+++ src/scripts/lobby/reducers/modal.ts
@@ -1,9 +1,14 @@
+/* global lobby */
 import * as ActionTypes from '../constants/ActionTypes'
 import {ConfirmKickOutPlayer, SelectNo, SelectYes} from '../actions'
 
 export interface State {
   readonly descriptionId: string
   readonly type: string
+  readonly values: {
+    name: string
+    token: lobby.Token
+  }
   readonly visible: boolean
 }
 type Action =
@@ -14,6 +19,10 @@ type Action =
 export const initialState: State = {
   descriptionId: '@dummy',
   type: '',
+  values: {
+    name: 'name',
+    token: ''
+  },
   visible: false
 }
 const modal = (state: State = initialState, action: Action): State => {
@@ -22,6 +31,10 @@ const modal = (state: State = initialState, action: Action): State => {
       return {
         descriptionId: 'Modal.description(kickOutPlayer)',
         type: ActionTypes.global.CONFIRM_KICK_OUT_PLAYER,
+        values: {
+          name: action.name,
+          token: action.token
+        },
         visible: true
       }
     case ActionTypes.global.SELECT_NO:
