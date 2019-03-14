b3d90697e11f49d0042dcb9fc147c07a38236d54
Yudai Udagawa <nimius.rd@gmail.com>
2019-03-14 22:46:57 +0900

    Fix test

--- src/scripts/village/components/organisms/Info.test.tsx
+++ src/scripts/village/components/organisms/Info.test.tsx
@@ -1,9 +1,20 @@
+/* global village */
 import * as React from 'react'
+import {FormattedMessage} from 'react-intl'
 import Info from './Info'
-import {shallow} from 'enzyme'
+import {getMessages} from '../../../../i18n/village'
+import {initRenderer} from '../../../../../tools/intl-enzyme-test-helper'
+
+const {shallowWithIntl} = initRenderer(village.Language.ja, getMessages(village.Language.ja))
 
 test('<Info />', () => {
-  const wrapper = shallow(<Info />)
+  const wrapper = shallowWithIntl(<Info />)
 
   expect(wrapper.children()).toHaveLength(7)
+  expect(wrapper.find(FormattedMessage)).toHaveLength(3)
+  const [agent, role, team] = wrapper.find(FormattedMessage).getElements().map(elem => shallowWithIntl(elem))
+
+  expect(agent.text()).toBe('あなた:')
+  expect(role.text()).toBe('役職:')
+  expect(team.text()).toBe('陣営:')
 })
