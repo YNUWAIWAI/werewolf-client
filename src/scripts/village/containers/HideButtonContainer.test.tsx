64df302b56b1775a920daee061fae65a0a534b7e
Yudai Udagawa <nimius.rd@gmail.com>
2019-03-15 00:05:46 +0900

    Fix HideButtonContainer's test

--- src/scripts/village/containers/HideButtonContainer.test.tsx
+++ src/scripts/village/containers/HideButtonContainer.test.tsx
@@ -1,26 +1,58 @@
+/* global village */
+import * as ActionTypes from '../constants/ActionTypes'
 import * as React from 'react'
+import HideButton from '../components/atoms/HideButton'
 import HideButtonContainer from './HideButtonContainer'
 import IntlProviderContainer from './IntlProviderContainer'
 import {Provider} from 'react-redux'
 import fakeStore from './fakeStore'
 import {mount} from 'enzyme'
 
-test('<HideButtonContainer />', () => {
-  const store = fakeStore(
-    {
-      hideButton: {
-        hide: true
-      }
-    }
-  )
-  const wrapper = mount(
-    <Provider store={store} >
-      <IntlProviderContainer>
-        <HideButtonContainer />
-      </IntlProviderContainer>
-    </Provider>
-  )
+describe('<HideButtonContainer />', () => {
+  test('render', () => {
 
-  expect(wrapper.find('.hide-button--button').hasClass('hide')).toBe(true)
-  expect(wrapper.html()).toMatchSnapshot()
+    const store = fakeStore(
+      {
+        hideButton: {
+          hide: true
+        }
+      }
+    )
+    const wrapper = mount(
+      <Provider store={store} >
+        <IntlProviderContainer>
+          <HideButtonContainer />
+        </IntlProviderContainer>
+      </Provider>
+    )
+
+    expect(wrapper.find('.hide-button--button').hasClass('hide')).toBe(true)
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+  test('handleSelectOption', () => {
+    const store = fakeStore(
+      {
+        language: village.Language.ja
+      }
+    )
+    const dispatch = jest.fn()
+
+    store.dispatch = dispatch
+
+    const wrapper = mount(
+      <Provider store={store} >
+        <IntlProviderContainer>
+          <HideButtonContainer />
+        </IntlProviderContainer>
+      </Provider>
+    )
+    const hide = false
+
+    wrapper.find(HideButton).props().handleClick(hide)
+    expect(dispatch).toHaveBeenCalledTimes(1)
+    expect(dispatch).toHaveBeenCalledWith({
+      hide,
+      type: ActionTypes.global.CLICK_HIDE_BUTTON
+    })
+  })
 })
