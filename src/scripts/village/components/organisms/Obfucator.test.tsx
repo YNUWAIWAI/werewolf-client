43b792aa3e188f81c1fe488b0af6364f5deeaa49
Yudai Udagawa <nimius.rd@gmail.com>
2019-03-17 05:17:28 +0900

    Fix test

--- src/scripts/village/components/organisms/Obfucator.test.tsx
+++ src/scripts/village/components/organisms/Obfucator.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
 import Loader from '../atoms/svg/Loader'
 import Obfucator from './Obfucator'
-import {shallow} from 'enzyme'
+import {mount} from 'enzyme'
 
 describe('<Obfucator />', () => {
   test('loading={true} visible={true}', () => {
-    const wrapper = shallow(
+    const wrapper = mount(
       <Obfucator
         loading
         visible
@@ -16,7 +16,7 @@ describe('<Obfucator />', () => {
     expect(wrapper.find(Loader).exists()).toBe(true)
   })
   test('loading={true} visible={false}', () => {
-    const wrapper = shallow(
+    const wrapper = mount(
       <Obfucator
         loading={false}
         visible
@@ -27,14 +27,14 @@ describe('<Obfucator />', () => {
     expect(wrapper.find(Loader).exists()).toBe(false)
   })
   test('loading={false} visible={false}', () => {
-    const wrapper = shallow(
+    const wrapper = mount(
       <Obfucator
         loading={false}
         visible={false}
       />
     )
 
-    expect(wrapper.isEmptyRender()).toBe(false)
+    expect(wrapper.isEmptyRender()).toBe(true)
     expect(wrapper.find(Loader).exists()).toBe(false)
   })
 })
