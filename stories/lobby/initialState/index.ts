7f62d0635edf46515ba24ab1d6131ee613fd4f9c
Yudai Udagawa <nimius.rd@gmail.com>
2019-03-18 20:56:33 +0900

    Fix story

--- stories/lobby/initialState/index.ts
+++ stories/lobby/initialState/index.ts
@@ -1,3 +1,5 @@
+import _lobbyForHumanPlayer from './lobbyForHumanPlayer'
 import _waitingPage from './waitingPage'
 
+export const lobbyForHumanPlayer = _lobbyForHumanPlayer
 export const waitingPage = _waitingPage
---
+++ stories/lobby/initialState/lobbyForHumanPlayer.ts
@@ -0,0 +1,114 @@
+/* global lobby */
+import * as ActionTypes from '../../../src/scripts/lobby/constants/ActionTypes'
+import LobbyForHumanPlayer from '../../../src/scripts/lobby/containers/LobbyForHumanPlayerContainer'
+import {ReducerState} from '../../../src/scripts/lobby/reducers'
+
+const state: Partial<ReducerState> = {
+  app: {
+    content: LobbyForHumanPlayer
+  },
+  lobbyForHumanPlayer: {
+    image: 'https://werewolf.world/image/0.1/Friedel.jpg',
+    isPlayer: true,
+    menuItems: [
+      {
+        id: 'Menu.showBuildVillage',
+        types: [ActionTypes.Target.SHOW_BUILD_VILLAGE]
+      },
+      {
+        id: 'Menu.showIdSearch',
+        types: [ActionTypes.Target.SHOW_ID_SEARCH]
+      },
+      {
+        id: 'Menu.showAdvancedSearch',
+        types: [ActionTypes.Target.SHOW_ADVANCED_SEARCH]
+      },
+      {
+        id: 'Menu.refresh',
+        isLoading: false,
+        types: [ActionTypes.Target.REFRESH, ActionTypes.Target.SHOW_LOBBY_FOR_HUMAN_PLAYER]
+      },
+      {
+        id: 'Menu.returnToMainPage',
+        types: [ActionTypes.Target.SHOW_MAIN]
+      }
+    ],
+    name: 'Cathy',
+    villageItems: [
+      {
+        avatar: lobby.Avatar.fixed,
+        comment: null,
+        hostPlayer: {
+          isAnonymous: false,
+          isHuman: true,
+          name: 'Alice'
+        },
+        id: 1,
+        idForSearching: 123,
+        name: 'Alice\'s village',
+        playerSetting: {
+          current: 8,
+          human: {
+            current: 5,
+            max: 8
+          },
+          number: 15,
+          robot: {
+            current: 3,
+            min: 7
+          }
+        },
+        roleSetting: {
+          hunter: 1,
+          madman: 1,
+          mason: 2,
+          medium: 1,
+          seer: 1,
+          villager: 6,
+          werehamster: 1,
+          werewolf: 2
+        }
+      },
+      {
+        avatar: lobby.Avatar.random,
+        comment: 'Open to beginners',
+        hostPlayer: {
+          isAnonymous: true,
+          isHuman: false,
+          name: 'Anonymous'
+        },
+        id: 2,
+        idForSearching: 456,
+        name: 'Ruined village',
+        playerSetting: {
+          current: 4,
+          human: {
+            current: 2,
+            max: 3
+          },
+          number: 5,
+          robot: {
+            current: 2,
+            min: 2
+          }
+        },
+        roleSetting: {
+          hunter: 0,
+          madman: 0,
+          mason: 0,
+          medium: 0,
+          seer: 1,
+          villager: 3,
+          werehamster: 0,
+          werewolf: 1
+        }
+      }
+    ]
+  },
+  obfucator: {
+    loading: false,
+    visible: false
+  }
+}
+
+export default state
