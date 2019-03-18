819393c5c758ea4cbaf3b647fe0351f86fdb6f6f
Yudai Udagawa <nimius.rd@gmail.com>
2019-03-18 20:43:04 +0900

    Update VillageList

--- src/scripts/lobby/components/organisms/VillageList.tsx
+++ src/scripts/lobby/components/organisms/VillageList.tsx
@@ -1,25 +1,36 @@
 import * as React from 'react'
+import {CSSTransition, TransitionGroup} from 'react-transition-group'
 import VillageItem from '../molecules/VillageItem'
 
 interface Props {
   readonly isPlayer: boolean
   readonly items: lobby.Village[]
-  readonly selectVillage: (id: number) => () => void
+  readonly selectVillage?: (id: number) => () => void
 }
 
 export default function VillageList(props: Props) {
   const items = props.items.map(item =>
-    <VillageItem
+    <CSSTransition
+      appear
+      classNames="lo--village--item--transition"
       key={item.id}
-      {... item}
-      handleClick={props.selectVillage(item.id)}
-      isPlayer={props.isPlayer}
-    />
+      timeout={{
+        enter: 500,
+        exit: 300
+      }}
+      unmountOnExit
+    >
+      <VillageItem
+        {... item}
+        handleClick={typeof props.selectVillage === 'function' ? props.selectVillage(item.id) : undefined}
+        isPlayer={props.isPlayer}
+      />
+    </CSSTransition>
   )
 
   return (
-    <div className="lo--village">
+    <TransitionGroup className="lo--village">
       {items}
-    </div>
+    </TransitionGroup>
   )
 }
