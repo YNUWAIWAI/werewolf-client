9f3251afa8fb13bd2865fac3a90f421aefb5a278
Yudai Udagawa <nimius.rd@gmail.com>
2019-03-19 18:41:03 +0900

    Fix Chat

--- src/scripts/village/components/organisms/Chat.tsx
+++ src/scripts/village/components/organisms/Chat.tsx
@@ -10,33 +10,67 @@ export interface StateProps {
 }
 export type Props = StateProps
 
-export default function Chat(props: Props) {
-  return (
-    <div className="vi--chat">
-      {
-        props.allIds.map(id => {
-          const item = props.byId[id]
+export default class Chat extends React.Component<Props, {}> {
+  public componentDidMount() {
+    this.scrollToBottom()
+  }
 
-          switch (item.type) {
-            case 'item':
-              return (
-                <ChatItem
-                  key={id}
-                  {... item}
-                />
-              )
-            case 'delimeter':
-              return (
-                <ChatDelimeter
-                  key={id}
-                  {... item}
-                />
-              )
-            default:
-              throw Error('Unexpected item type')
-          }
-        })
-      }
-    </div>
-  )
+  public componentDidUpdate() {
+    const elem = this.chat.current
+
+    if (!elem) {
+      return
+    }
+    const isBottom = elem.scrollHeight <= (elem.scrollTop + elem.clientHeight)
+
+    if (isBottom) {
+      this.scrollToBottom()
+    }
+  }
+
+  private chat = React.createRef<HTMLDivElement>()
+
+  public scrollToBottom() {
+    const elem = this.chat.current
+
+    if (!elem) {
+      return
+    }
+
+    elem.scrollTop = elem.scrollHeight
+  }
+
+  public render() {
+    return (
+      <div
+        className="vi--chat"
+        ref={this.chat}
+      >
+        {
+          this.props.allIds.map(id => {
+            const item = this.props.byId[id]
+
+            switch (item.type) {
+              case 'item':
+                return (
+                  <ChatItem
+                    key={id}
+                    {... item}
+                  />
+                )
+              case 'delimeter':
+                return (
+                  <ChatDelimeter
+                    key={id}
+                    {... item}
+                  />
+                )
+              default:
+                throw Error('Unexpected item type')
+            }
+          })
+        }
+      </div>
+    )
+  }
 }
