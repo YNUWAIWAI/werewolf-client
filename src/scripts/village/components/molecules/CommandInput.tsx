d572e0dd06569e483269c1b7550413c302798cde
Yudai Udagawa <nimius.rd@gmail.com>
2019-03-21 03:35:49 +0900

    Add suggest on textarea

--- src/scripts/village/components/molecules/CommandInput.tsx
+++ src/scripts/village/components/molecules/CommandInput.tsx
@@ -3,14 +3,15 @@ import * as React from 'react'
 import {getChannelFromInputChennel, spaceSeparatedToCamelCase} from '../../util'
 import ChatIcon from '../atoms/ChatIcon'
 import {FormattedMessage} from 'react-intl'
+import getCaretCoordinates = require('textarea-caret')
 
-const countText = (text: string): number => Array.of(... text).length
-const isValidTextLength = (text: string, upperLimit: number, lowerLimit: number = 1): boolean => {
-  const textCount = countText(text)
-
-  return textCount <= upperLimit && textCount >= lowerLimit
+const enum Key {
+  ArrowDown = 'ArrowDown',
+  ArrowLeft = 'ArrowLeft',
+  ArrowRight = 'ArrowRight',
+  ArrowUp = 'ArrowUp',
+  Enter = 'Enter'
 }
-const isSendable = (postCount: number, postCountLimit: number): boolean => postCount < postCountLimit
 
 type Props = {
   readonly characterLimit: number
@@ -25,11 +26,28 @@ type Props = {
 }
 
 interface State {
+  caretPosition: number
+  isSuggest: boolean
   sendable: boolean
+  suggestLeft: number
+  suggestSelected: number
+  suggestTop: number
   text: string
   textCount: number
   validTextLength: boolean
 }
+
+const data = [
+  'A',
+  'B'
+]
+const countText = (text: string): number => Array.of(... text).length
+const isValidTextLength = (text: string, upperLimit: number, lowerLimit: number = 1): boolean => {
+  const textCount = countText(text)
+
+  return textCount <= upperLimit && textCount >= lowerLimit
+}
+const isSendable = (postCount: number, postCountLimit: number): boolean => postCount < postCountLimit
 
 export default class CommandInput extends React.Component<Props, State> {
   public constructor(props: Props) {
@@ -37,12 +55,19 @@ export default class CommandInput extends React.Component<Props, State> {
     const text = ''
 
     this.state = {
+      caretPosition: 0,
+      isSuggest: false,
       sendable: this.isSendable(),
+      suggestLeft: 0,
+      suggestSelected: 0,
+      suggestTop: 0,
       text,
       textCount: countText(text),
       validTextLength: isValidTextLength(text, props.characterLimit)
     }
   }
+
+  private textareaRef = React.createRef()
 
   public isSendable() {
     switch (this.props.inputChannel) {
@@ -58,6 +83,21 @@ export default class CommandInput extends React.Component<Props, State> {
     }
   }
 
+  public updateIsSuggest(elem: HTMLTextAreaElement) {
+    const text = elem.value
+    const pos = elem.selectionEnd - 1
+
+    if (text[pos] === '@' && text[pos - 1] !== '@') {
+      this.setState({
+        isSuggest: true
+      })
+    } else {
+      this.setState({
+        isSuggest: false
+      })
+    }
+  }
+
   public updateText(text: string) {
     this.setState({
       sendable: this.isSendable(),
@@ -67,8 +107,32 @@ export default class CommandInput extends React.Component<Props, State> {
     })
   }
 
+  public updateCaretPosition(elem: HTMLTextAreaElement) {
+    const {left, top} = getCaretCoordinates(elem, elem.selectionEnd)
+
+    this.setState({
+      caretPosition: elem.selectionEnd - 1,
+      suggestLeft: left,
+      suggestTop: top - elem.scrollTop
+    })
+  }
+
+  public handleSuggestClick(suggest: string) {
+    const text = this.state.text
+    const pos = this.state.caretPosition
+    const newText = text.slice(0, pos) + suggest + text.slice(pos + 1)
+
+    this.updateText(newText)
+    this.setState({
+      isSuggest: false,
+      suggestSelected: 0
+    })
+  }
+
   public handleTextChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
     this.updateText(event.target.value)
+    this.updateCaretPosition(event.target)
+    this.updateIsSuggest(event.target)
   }
 
   public handlePostChat() {
@@ -79,14 +143,60 @@ export default class CommandInput extends React.Component<Props, State> {
   }
 
   public handleKeyDown(event: React.KeyboardEvent<HTMLTextAreaElement>) {
-    if ((event.ctrlKey || event.metaKey) && event.key === 'Enter') {
+    if ((event.ctrlKey || event.metaKey) && event.key === Key.Enter) {
       this.handlePostChat()
+    }
+    if (this.state.isSuggest) {
+      if (event.key === Key.ArrowLeft || event.key === Key.ArrowRight) {
+        this.setState({
+          isSuggest: false
+        })
+      } else if (event.key === Key.ArrowDown) {
+        event.preventDefault()
+        this.setState(prevState => ({
+          suggestSelected: (prevState.suggestSelected + 1) % data.length
+        }))
+      } else if (event.key === Key.ArrowUp) {
+        event.preventDefault()
+        this.setState(prevState => ({
+          suggestSelected: (prevState.suggestSelected - 1 + data.length) % data.length
+        }))
+      } else if (event.key === Key.Enter) {
+        event.preventDefault()
+        this.handleSuggestClick(data[this.state.suggestSelected])
+      }
     }
   }
 
   public render() {
+    const SuggestStyle = {
+      left: this.state.suggestLeft,
+      top: this.state.suggestTop
+    }
+    const Suggest = () =>
+      <div
+        className="vi--command--input--suggest--container"
+      >
+        <div
+          className="vi--command--input--suggest--list"
+          style={SuggestStyle}
+        >
+          {
+            data.map((item, index) =>
+              <div
+                className={`vi--command--input--suggest--item ${index === this.state.suggestSelected ? 'selected' : ''}`}
+                key={item}
+                onClick={() => this.handleSuggestClick(item)}
+              >
+                {item}
+              </div>
+            )
+          }
+        </div>
+      </div>
+
     return (
-      <form className={`vi--command--input ${spaceSeparatedToCamelCase(this.props.inputChannel)}`}>
+      <form className="vi--command--input">
         <FormattedMessage id={`CommandInput.placeholder.${spaceSeparatedToCamelCase(this.props.inputChannel)}`}>
           {
             text => {
@@ -96,6 +206,7 @@ export default class CommandInput extends React.Component<Props, State> {
 
               return (
                 <textarea
+                  className={`vi--command--input--textarea ${spaceSeparatedToCamelCase(this.props.inputChannel)}`}
                   onChange={event => this.handleTextChange(event)}
                   onKeyDown={event => this.handleKeyDown(event)}
                   placeholder={text}
@@ -105,6 +216,11 @@ export default class CommandInput extends React.Component<Props, State> {
             }
           }
         </FormattedMessage>
+        {
+          this.state.isSuggest ?
+            <Suggest /> :
+            null
+        }
         <span className={`vi--command--input--char ${this.state.validTextLength ? '' : 'error'}`}>
           {this.state.textCount}
         </span>
