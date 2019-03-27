8113f3e6e1304e23d1896669cc18291754c9b931
Yudai Udagawa <nimius.rd@gmail.com>
2019-03-28 05:47:47 +0900

    Fix Props

--- src/scripts/village/components/organisms/CommandGrave.tsx
+++ src/scripts/village/components/organisms/CommandGrave.tsx
@@ -3,10 +3,13 @@ import * as React from 'react'
 import CommandNavigation, {Props as CommandNavigationProps} from '../molecules/CommandNavigation'
 import CommandInput from '../molecules/CommandInput'
 import {Navigation} from '../../constants/ActionTypes'
+import {State} from '../../reducers/suggest'
 
 export interface StateProps {
   readonly characterLimit: number
+  readonly language: village.Language
   readonly navigation: CommandNavigationProps['items']
+  readonly suggestData: State['data']
 }
 export interface DispatchProps {
   readonly handleNavigationClick: (type: Navigation) => void
@@ -21,6 +24,8 @@ export default function CommandGrave(props: Props) {
         characterLimit={props.characterLimit}
         handlePostChat={props.handlePostChat(village.InputChannel.grave)}
         inputChannel={village.InputChannel.grave}
+        language={props.language}
+        suggestData={props.suggestData}
       />
       <CommandNavigation
         handleClick={props.handleNavigationClick}
--- src/scripts/village/components/organisms/CommandInputBox.tsx
+++ src/scripts/village/components/organisms/CommandInputBox.tsx
@@ -1,9 +1,11 @@
 /* global village */
 import * as React from 'react'
 import CommandInput from '../molecules/CommandInput'
+import {State} from '../../reducers/suggest'
 
 export interface StateProps {
   readonly characterLimit: number
+  readonly language: village.Language
   readonly limited: {
     readonly available: boolean
     readonly postCount: number
@@ -12,6 +14,7 @@ export interface StateProps {
   readonly public: {
     readonly postCount: number
   }
+  readonly suggestData: State['data']
 }
 export interface DispatchProps {
   readonly handlePostChat: (channel: village.InputChannel) => (text: string) => void
@@ -25,13 +28,17 @@ export default function CommandInputBox(props: Props) {
         characterLimit={props.characterLimit}
         handlePostChat={props.handlePostChat(village.InputChannel.public)}
         inputChannel={village.InputChannel.public}
+        language={props.language}
         postCount={props.public.postCount}
         postCountLimit={props.postCountLimit}
+        suggestData={props.suggestData}
       />
       <CommandInput
         characterLimit={props.characterLimit}
         handlePostChat={props.handlePostChat(village.InputChannel.private)}
         inputChannel={village.InputChannel.private}
+        language={props.language}
+        suggestData={props.suggestData}
       />
       {
         props.limited.available ?
@@ -39,8 +46,10 @@ export default function CommandInputBox(props: Props) {
             characterLimit={props.characterLimit}
             handlePostChat={props.handlePostChat(village.InputChannel.limited)}
             inputChannel={village.InputChannel.limited}
+            language={props.language}
             postCount={props.limited.postCount}
             postCountLimit={props.postCountLimit}
+            suggestData={props.suggestData}
           /> :
           null
       }
