0ab8db6e903d97fac570a0e1fa46a6c857c58c50
Yudai Udagawa <nimius.rd@gmail.com>
2019-03-28 05:49:34 +0900

    Fix story

--- stories/village/initialState/morningWithLimitedChat.ts
+++ stories/village/initialState/morningWithLimitedChat.ts
@@ -961,6 +961,115 @@ const state: ReducerState = {
     visible: false,
     winners: []
   },
+  suggest: {
+    data: [
+      {
+        id: 'Alvin',
+        name: {
+          'en': 'Alvin',
+          'ja': 'アルビン'
+        }
+      },
+      {
+        id: 'Catalina',
+        name: {
+          'en': 'Catalina',
+          'ja': 'カタリナ'
+        }
+      },
+      {
+        id: 'Dieter',
+        name: {
+          'en': 'Dieter',
+          'ja': 'ディーター'
+        }
+      },
+      {
+        id: 'Gert',
+        name: {
+          'en': 'Gert',
+          'ja': 'ゲルト'
+        }
+      },
+      {
+        id: 'Jacob',
+        name: {
+          'en': 'Jacob',
+          'ja': 'ヤコブ'
+        }
+      },
+      {
+        id: 'Joachim',
+        name: {
+          'en': 'Joachim',
+          'ja': 'ヨアヒム'
+        }
+      },
+      {
+        id: 'Lisa',
+        name: {
+          'en': 'Lisa',
+          'ja': 'リーザ'
+        }
+      },
+      {
+        id: 'Moritz',
+        name: {
+          'en': 'Moritz',
+          'ja': 'モーリッツ'
+        }
+      },
+      {
+        id: 'Nicholas',
+        name: {
+          'en': 'Nicholas',
+          'ja': 'ニコラス'
+        }
+      },
+      {
+        id: 'Otto',
+        name: {
+          'en': 'Otto',
+          'ja': 'オットー'
+        }
+      },
+      {
+        id: 'Pamela',
+        name: {
+          'en': 'Pamela',
+          'ja': 'パメラ'
+        }
+      },
+      {
+        id: 'Peter',
+        name: {
+          'en': 'Peter',
+          'ja': 'ペーター'
+        }
+      },
+      {
+        id: 'Simson',
+        name: {
+          'en': 'Simson',
+          'ja': 'ジムゾン'
+        }
+      },
+      {
+        id: 'Thomas',
+        name: {
+          'en': 'Thomas',
+          'ja': 'トーマス'
+        }
+      },
+      {
+        id: 'Walter',
+        name: {
+          'en': 'Walter',
+          'ja': 'ヴァルター'
+        }
+      }
+    ]
+  },
   timer: {
     phaseStartTime: new Date('2006-10-07T12:06:56.568+09:00').getTime(),
     phaseTimeLimit: 600000,
