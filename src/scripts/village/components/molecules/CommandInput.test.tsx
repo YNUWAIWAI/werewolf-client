f30620b17853c80fe467d99f625af93ee5e26f52
Yudai Udagawa <nimius.rd@gmail.com>
2019-04-05 02:48:50 +0900

    Fix test

--- src/scripts/village/components/molecules/CommandInput.test.tsx
+++ src/scripts/village/components/molecules/CommandInput.test.tsx
@@ -1,122 +1,136 @@
 /* global village */
 import * as React from 'react'
 import CommandInput from './CommandInput'
+import CommandInputSuggest from '../atoms/CommandInputSuggest'
 import {getMessages} from '../../../../i18n/village'
 import {initRenderer} from '../../../../../tools/intl-enzyme-test-helper'
 
 const {mountWithIntl} = initRenderer(village.Language.ja, getMessages(village.Language.ja))
 
 describe('<CommandInput />', () => {
-  test('kind="grave"', () => {
-    const handlePostChat = jest.fn()
-    const wrapper = mountWithIntl(
-      <CommandInput
-        characterLimit={140}
-        handlePostChat={handlePostChat}
-        inputChannel={village.InputChannel.grave}
-        language={village.Language.ja}
-        suggesttedData={[]}
-      />
-    )
+  describe('render', () => {
+    test('inputChannel="grave"', () => {
+      const handlePostChat = jest.fn()
+      const wrapper = mountWithIntl(
+        <CommandInput
+          characterLimit={140}
+          handlePostChat={handlePostChat}
+          inputChannel={village.InputChannel.grave}
+          language={village.Language.ja}
+          postCount={-1}
+          postCountLimit={-1}
+          suggesttedData={[]}
+        />
+      )
 
-    expect(wrapper.find('.vi--command--input--textarea').hasClass('grave')).toBe(true)
-    expect(wrapper.find('textarea').is('[placeholder="墓場"]')).toBe(true)
-    expect(wrapper.find('textarea').is('[value=""]')).toBe(true)
-    expect(wrapper.find('.vi--command--input--char').hasClass('error')).toBe(true)
-    expect(wrapper.find('.vi--command--input--char').text()).toBe('0')
-    expect(wrapper.find('.vi--command--input--counter').exists()).toBe(false)
-    expect(wrapper.find('button').is('[disabled=true]')).toBe(true)
-    expect(handlePostChat).toHaveBeenCalledTimes(0)
-  })
-  test('kind="limited"', () => {
-    const handlePostChat = jest.fn()
-    const wrapper = mountWithIntl(
-      <CommandInput
-        characterLimit={140}
-        handlePostChat={handlePostChat}
-        inputChannel={village.InputChannel.limited}
-        language={village.Language.ja}
-        postCount={0}
-        postCountLimit={10}
-        suggesttedData={[]}
-      />
-    )
+      expect(wrapper.find('.vi--command--input--textarea').hasClass('grave')).toBe(true)
+      expect(wrapper.find('.vi--command--input--textarea').is('[placeholder="墓場"]')).toBe(true)
+      expect(wrapper.find('.vi--command--input--textarea').is('[value=""]')).toBe(true)
+      expect(wrapper.find('.vi--command--input--char').hasClass('error')).toBe(true)
+      expect(wrapper.find('.vi--command--input--char').text()).toBe('0')
+      expect(wrapper.find('.vi--command--input--counter').exists()).toBe(false)
+      expect(wrapper.find('button').is('[disabled=true]')).toBe(true)
+      expect(wrapper.find(CommandInputSuggest).isEmptyRender()).toBe(true)
+      expect(handlePostChat).toHaveBeenCalledTimes(0)
+    })
+    test('inputChannel="limited"', () => {
+      const handlePostChat = jest.fn()
+      const wrapper = mountWithIntl(
+        <CommandInput
+          characterLimit={140}
+          handlePostChat={handlePostChat}
+          inputChannel={village.InputChannel.limited}
+          language={village.Language.ja}
+          postCount={0}
+          postCountLimit={10}
+          suggesttedData={[]}
+        />
+      )
 
-    expect(wrapper.find('.vi--command--input--textarea').hasClass('limited')).toBe(true)
-    expect(wrapper.find('textarea').is('[placeholder="人狼用"]')).toBe(true)
-    expect(wrapper.find('textarea').is('[value=""]')).toBe(true)
-    expect(wrapper.find('.vi--command--input--char').hasClass('error')).toBe(true)
-    expect(wrapper.find('.vi--command--input--char').text()).toBe('0')
-    expect(wrapper.find('.vi--command--input--counter').exists()).toBe(true)
-    expect(wrapper.find('.vi--command--input--counter').text()).toBe('0/10')
-    expect(wrapper.find('button').is('[disabled=true]')).toBe(true)
-    expect(handlePostChat).toHaveBeenCalledTimes(0)
-  })
-  test('kind="private"', () => {
-    const handlePostChat = jest.fn()
-    const wrapper = mountWithIntl(
-      <CommandInput
-        characterLimit={140}
-        handlePostChat={handlePostChat}
-        inputChannel={village.InputChannel.private}
-        language={village.Language.ja}
-        suggesttedData={[]}
-      />
-    )
+      expect(wrapper.find('.vi--command--input--textarea').hasClass('limited')).toBe(true)
+      expect(wrapper.find('.vi--command--input--textarea').is('[placeholder="人狼用"]')).toBe(true)
+      expect(wrapper.find('.vi--command--input--textarea').is('[value=""]')).toBe(true)
+      expect(wrapper.find('.vi--command--input--char').hasClass('error')).toBe(true)
+      expect(wrapper.find('.vi--command--input--char').text()).toBe('0')
+      expect(wrapper.find('.vi--command--input--counter').exists()).toBe(true)
+      expect(wrapper.find('.vi--command--input--counter').text()).toBe('0/10')
+      expect(wrapper.find('button').is('[disabled=true]')).toBe(true)
+      expect(wrapper.find(CommandInputSuggest).isEmptyRender()).toBe(true)
+      expect(handlePostChat).toHaveBeenCalledTimes(0)
+    })
+    test('inputChannel="private"', () => {
+      const handlePostChat = jest.fn()
+      const wrapper = mountWithIntl(
+        <CommandInput
+          characterLimit={140}
+          handlePostChat={handlePostChat}
+          inputChannel={village.InputChannel.private}
+          language={village.Language.ja}
+          postCount={-1}
+          postCountLimit={-1}
+          suggesttedData={[]}
+        />
+      )
 
-    expect(wrapper.find('.vi--command--input--textarea').hasClass('private')).toBe(true)
-    expect(wrapper.find('textarea').is('[placeholder="非公開用"]')).toBe(true)
-    expect(wrapper.find('textarea').is('[value=""]')).toBe(true)
-    expect(wrapper.find('.vi--command--input--char').hasClass('error')).toBe(true)
-    expect(wrapper.find('.vi--command--input--char').text()).toBe('0')
-    expect(wrapper.find('.vi--command--input--counter').exists()).toBe(false)
-    expect(wrapper.find('button').is('[disabled=true]')).toBe(true)
-    expect(handlePostChat).toHaveBeenCalledTimes(0)
-  })
-  test('kind="post mortem"', () => {
-    const handlePostChat = jest.fn()
-    const wrapper = mountWithIntl(
-      <CommandInput
-        characterLimit={140}
-        handlePostChat={handlePostChat}
-        inputChannel={village.InputChannel.postMortem}
-        language={village.Language.ja}
-        suggesttedData={[]}
-      />
-    )
+      expect(wrapper.find('.vi--command--input--textarea').hasClass('private')).toBe(true)
+      expect(wrapper.find('.vi--command--input--textarea').is('[placeholder="非公開用"]')).toBe(true)
+      expect(wrapper.find('.vi--command--input--textarea').is('[value=""]')).toBe(true)
+      expect(wrapper.find('.vi--command--input--char').hasClass('error')).toBe(true)
+      expect(wrapper.find('.vi--command--input--char').text()).toBe('0')
+      expect(wrapper.find('.vi--command--input--counter').exists()).toBe(false)
+      expect(wrapper.find('button').is('[disabled=true]')).toBe(true)
+      expect(wrapper.find(CommandInputSuggest).isEmptyRender()).toBe(true)
+      expect(handlePostChat).toHaveBeenCalledTimes(0)
+    })
+    test('inputChannel="post mortem"', () => {
+      const handlePostChat = jest.fn()
+      const wrapper = mountWithIntl(
+        <CommandInput
+          characterLimit={140}
+          handlePostChat={handlePostChat}
+          inputChannel={village.InputChannel.postMortem}
+          language={village.Language.ja}
+          postCount={-1}
+          postCountLimit={-1}
+          suggesttedData={[]}
+        />
+      )
 
-    expect(wrapper.find('.vi--command--input--textarea').hasClass('postMortem')).toBe(true)
-    expect(wrapper.find('textarea').is('[placeholder="感想戦"]')).toBe(true)
-    expect(wrapper.find('textarea').is('[value=""]')).toBe(true)
-    expect(wrapper.find('.vi--command--input--char').hasClass('error')).toBe(true)
-    expect(wrapper.find('.vi--command--input--char').text()).toBe('0')
-    expect(wrapper.find('.vi--command--input--counter').exists()).toBe(false)
-    expect(wrapper.find('button').is('[disabled=true]')).toBe(true)
-    expect(handlePostChat).toHaveBeenCalledTimes(0)
-  })
-  test('kind="public"', () => {
-    const handlePostChat = jest.fn()
-    const wrapper = mountWithIntl(
-      <CommandInput
-        characterLimit={140}
-        handlePostChat={handlePostChat}
-        inputChannel={village.InputChannel.public}
-        language={village.Language.ja}
-        postCount={0}
-        postCountLimit={10}
-        suggesttedData={[]}
-      />
-    )
+      expect(wrapper.find('.vi--command--input--textarea').hasClass('postMortem')).toBe(true)
+      expect(wrapper.find('.vi--command--input--textarea').is('[placeholder="感想戦"]')).toBe(true)
+      expect(wrapper.find('.vi--command--input--textarea').is('[value=""]')).toBe(true)
+      expect(wrapper.find('.vi--command--input--char').hasClass('error')).toBe(true)
+      expect(wrapper.find('.vi--command--input--char').text()).toBe('0')
+      expect(wrapper.find('.vi--command--input--counter').exists()).toBe(false)
+      expect(wrapper.find('button').is('[disabled=true]')).toBe(true)
+      expect(wrapper.find(CommandInputSuggest).isEmptyRender()).toBe(true)
+      expect(handlePostChat).toHaveBeenCalledTimes(0)
+    })
+    test('inputChannel="public"', () => {
+      const handlePostChat = jest.fn()
+      const wrapper = mountWithIntl(
+        <CommandInput
+          characterLimit={140}
+          handlePostChat={handlePostChat}
+          inputChannel={village.InputChannel.public}
+          language={village.Language.ja}
+          postCount={0}
+          postCountLimit={10}
+          suggesttedData={[]}
+        />
+      )
 
-    expect(wrapper.find('.vi--command--input--textarea').hasClass('public')).toBe(true)
-    expect(wrapper.find('textarea').is('[placeholder="公開用"]')).toBe(true)
-    expect(wrapper.find('textarea').is('[value=""]')).toBe(true)
-    expect(wrapper.find('.vi--command--input--char').hasClass('error')).toBe(true)
-    expect(wrapper.find('.vi--command--input--char').text()).toBe('0')
-    expect(wrapper.find('.vi--command--input--counter').exists()).toBe(true)
-    expect(wrapper.find('.vi--command--input--counter').text()).toBe('0/10')
-    expect(wrapper.find('button').is('[disabled=true]')).toBe(true)
-    expect(handlePostChat).toHaveBeenCalledTimes(0)
+      expect(wrapper.find('.vi--command--input--textarea').hasClass('public')).toBe(true)
+      expect(wrapper.find('.vi--command--input--textarea').is('[placeholder="公開用"]')).toBe(true)
+      expect(wrapper.find('.vi--command--input--textarea').is('[value=""]')).toBe(true)
+      expect(wrapper.find('.vi--command--input--char').hasClass('error')).toBe(true)
+      expect(wrapper.find('.vi--command--input--char').text()).toBe('0')
+      expect(wrapper.find('.vi--command--input--counter').exists()).toBe(true)
+      expect(wrapper.find('.vi--command--input--counter').text()).toBe('0/10')
+      expect(wrapper.find('button').is('[disabled=true]')).toBe(true)
+      expect(wrapper.find(CommandInputSuggest).isEmptyRender()).toBe(true)
+      expect(handlePostChat).toHaveBeenCalledTimes(0)
+    })
   })
   describe('handleTextChange', () => {
     test('kind="public" postCount={0} postCountLimit={10} textarea: 0 -> charLimit -> charLimit + 1', () => {
