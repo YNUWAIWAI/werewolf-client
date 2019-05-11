440f9e97921bd313753ced165872785ee74d2094
Yudai Udagawa <nimius.rd@gmail.com>
2019-05-11 22:03:34 +0900

    wip

--- src/scripts/landing/components/LoginField.tsx
+++ src/scripts/landing/components/LoginField.tsx
@@ -2,92 +2,118 @@ import * as React from 'react'
 import {FormattedMessage} from 'react-intl'
 
 interface Props {}
+interface State {
+  email: string
+  password: string
+}
 
-export default function LoginField(props: Props) {
-  return (
-    <>
-      <div className="field">
-        <dl>
-          <dt>
-            <FormattedMessage
-              id="login-label-email"
-            >
-              {
-                text => (
-                  <label
-                    className="label"
-                    htmlFor="login-email"
-                    id="login-label-email"
-                  >
-                    {text}
-                  </label>
-                )
-              }
+export default class LoginField extends React.Component<Props, State> {
+  public constructor(props: Props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
 
-            </FormattedMessage>
-          </dt>
-          <dd>
-            <input
-              autoComplete="off"
-              autoFocus
-              className="input"
-              id="login-email"
-              name="email"
-              required
-              type="text"
-              value=""
-            />
-          </dd>
-        </dl>
-      </div>
-      <div className="field" id="login-field">
-        <dl>
-          <dt>
-            <FormattedMessage
-              id="login-label-password"
-            >
-              {
-                text => (
-                  <label
-                    className="label"
-                    htmlFor="login-password"
-                    id="login-label-password"
-                  >
-                    {text}
-                  </label>
-                )
-              }
+  public handleEmailChange(text: string) {
+    this.setState({
+      email: text
+    })
+  }
 
-            </FormattedMessage>
-          </dt>
-          <dd>
-            <input
-              autoComplete="off"
-              autoFocus
-              className="input"
-              id="login-password"
-              name="password"
-              required
-              type="text"
-              value=""
-            />
-          </dd>
-        </dl>
-      </div>
-      <FormattedMessage
-        id="login-submit"
-      >
-        {
-          text => (
-            <button
-              className="login-submit"
-              type="submit"
-            >
-              {text}
-            </button>
-          )
-        }
-      </FormattedMessage>
-    </>
-  )
+  public handlePasswordChange(text: string) {
+    this.setState({
+      password: text
+    })
+  }
+
+  public render() {
+    return (
+      <>
+        <div className="field">
+          <dl>
+            <dt>
+              <FormattedMessage
+                id="login-label-email"
+              >
+                {
+                  text => (
+                    <label
+                      className="label"
+                      htmlFor="login-email"
+                      id="login-label-email"
+                    >
+                      {text}
+                    </label>
+                  )
+                }
+
+              </FormattedMessage>
+            </dt>
+            <dd>
+              <input
+                autoComplete="off"
+                autoFocus
+                className="input"
+                id="login-email"
+                name="email"
+                required
+                type="text"
+                value={this.state.email}
+              />
+            </dd>
+          </dl>
+        </div>
+        <div className="field" id="login-field">
+          <dl>
+            <dt>
+              <FormattedMessage
+                id="login-label-password"
+              >
+                {
+                  text => (
+                    <label
+                      className="label"
+                      htmlFor="login-password"
+                      id="login-label-password"
+                    >
+                      {text}
+                    </label>
+                  )
+                }
+
+              </FormattedMessage>
+            </dt>
+            <dd>
+              <input
+                autoComplete="off"
+                autoFocus
+                className="input"
+                id="login-password"
+                name="password"
+                required
+                type="text"
+                value={this.state.password}
+              />
+            </dd>
+          </dl>
+        </div>
+        <FormattedMessage
+          id="login-submit"
+        >
+          {
+            text => (
+              <button
+                className="login-submit"
+                type="submit"
+              >
+                {text}
+              </button>
+            )
+          }
+        </FormattedMessage>
+      </>
+    )
+  }
 }
