499f1a1275193ad2b573691eacf012e0ef4e36b4
Yudai Udagawa <nimius.rd@gmail.com>
2019-05-12 01:54:09 +0900

    wip

--- src/scripts/landing/index.tsx
+++ src/scripts/landing/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import * as ReactDom from 'react-dom'
+import App from './App'
+import {Language} from '../../i18n/landing'
 
 const lang = (() => {
   if ((/^eng?/).test(navigator.language)) {
@@ -41,36 +43,43 @@ const lang = (() => {
 
 // ids.forEach(insertText)
 
-if (lead) {
-  ReactDom.render(
-    <IntlProvider
-      locale={lang}
-      messages={messages}
-    >
-      <Lead />
-    </IntlProvider>,
-    lead
-  )
+const elem = document.getElementById('script')
+const root = document.getElementById('root')
+
+if (!elem) {
+  throw Error('Not found: script element')
 }
-if (loginError) {
-  ReactDom.render(
-    <IntlProvider
-      locale={lang}
-      messages={messages}
-    >
-      <LoginError />
-    </IntlProvider>,
-    loginError
-  )
+if (!elem.dataset.csrfToken) {
+  throw Error('Not found: data-csrf-token')
 }
-if (loginFiled) {
-  ReactDom.render(
-    <IntlProvider
-      locale={lang}
-      messages={messages}
-    >
-      <LoginFiled />
-    </IntlProvider>,
-    loginFiled
-  )
+if (!elem.dataset.loginAction) {
+  throw Error('Not found: data-login-action')
 }
+if (!elem.dataset.loginError) {
+  throw Error('Not found: data-login-error')
+}
+if (!elem.dataset.loginAction) {
+  throw Error('Not found: data-login-action')
+}
+if (!elem.dataset.signupError) {
+  throw Error('Not found: data-signup-error')
+}
+if (!elem.dataset.signupAction) {
+  throw Error('Not found: data-signup-action')
+}
+if (!root) {
+  throw Error('Not found: root element')
+}
+console.log(elem.dataset.loginError)
+console.log(elem.dataset.signupError)
+ReactDom.render(
+  <App
+    csrfToken={elem.dataset.csrfToken}
+    lang={lang}
+    loginAction={elem.dataset.loginAction}
+    loginError={elem.dataset.loginError === 'true'}
+    signupAction={elem.dataset.signupAction}
+    signupError={elem.dataset.signupError === 'true'}
+  />,
+  root
+)
