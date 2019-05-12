5ff85a7a04b9e2cf44536bae06a0ddad09f2556c
Yudai Udagawa <nimius.rd@gmail.com>
2019-05-13 05:41:58 +0900

    Test SubmitButton

---
+++ src/scripts/landing/components/atoms/SubmitButton.test.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import {Language, getMessages} from '../../../../i18n/landing'
+import SubmitButton from './SubmitButton'
+import {initRenderer} from '../../../../../tools/intl-enzyme-test-helper'
+
+const {mountWithIntl} = initRenderer(Language.en, getMessages(Language.en))
+
+test('render', () => {
+  const wrapper = mountWithIntl(
+    <SubmitButton
+      type="login"
+    />
+  )
+
+  expect(wrapper.text()).toBe('Login')
+})
