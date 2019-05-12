9b0e6c5cc48b7714b49c8c2d56fb9ab683678199
Yudai Udagawa <nimius.rd@gmail.com>
2019-05-13 05:02:56 +0900

    Test Csrf

---
+++ src/scripts/landing/components/atoms/Csrf.test.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Csrf from './Csrf'
+import {shallow} from 'enzyme'
+
+test('render', () => {
+  const wrapper = shallow(
+    <Csrf
+      token="token"
+    />
+  )
+
+  expect(wrapper.exists()).toBe(true)
+})
