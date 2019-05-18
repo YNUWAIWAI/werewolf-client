15d2718e052f8b48d198fc42fdac8ee53159b9bf
Yudai Udagawa <nimius.rd@gmail.com>
2019-05-19 03:26:56 +0900

    Fix test

--- src/scripts/landing/components/templates/App.test.tsx
+++ src/scripts/landing/components/templates/App.test.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react'
 import App from './App'
+import FormNavigation from '../atoms/FormNavigation'
 import {Language} from '../../../../i18n/landing'
+import Lead from '../organism/Lead'
+import Login from '../organism/Login'
+import Signup from '../organism/Signup'
 import {shallow} from 'enzyme'
 
 test('render', () => {
@@ -15,5 +19,29 @@ test('render', () => {
     />
   )
 
-  expect(wrapper.exists()).toBe(true)
+  expect(wrapper.find(Lead).exists()).toBe(true)
+  expect(wrapper.find(FormNavigation).exists()).toBe(true)
+  expect(wrapper.find(Signup).exists()).toBe(false)
+  expect(wrapper.find(Login).exists()).toBe(true)
+})
+test('setIsSignup', () => {
+  const wrapper = shallow(
+    <App
+      csrfToken="token"
+      lang={Language.en}
+      loginAction="/login"
+      loginError={false}
+      signupAction="/signup"
+      signupError={false}
+    />
+  )
+
+  const {handleClick, isSignup} = wrapper.find(FormNavigation).props()
+
+  handleClick(!isSignup)
+
+  expect(wrapper.find(Lead).exists()).toBe(true)
+  expect(wrapper.find(FormNavigation).exists()).toBe(true)
+  expect(wrapper.find(Signup).exists()).toBe(true)
+  expect(wrapper.find(Login).exists()).toBe(false)
 })
