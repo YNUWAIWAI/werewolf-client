2f517b2e012ab11e3ee45c5e6157fefc0c45cedd
Yudai Udagawa <nimius.rd@gmail.com>
2019-05-19 03:43:38 +0900

    Fix test

--- src/scripts/landing/components/atoms/FormNavigation.test.tsx
+++ src/scripts/landing/components/atoms/FormNavigation.test.tsx
@@ -16,6 +16,7 @@ describe('render', () => {
     )
 
     expect(wrapper.text()).toBe('Login')
+    expect(handleClick).toHaveBeenCalledTimes(0)
   })
   test('isSignup={false}', () => {
     const handleClick = jest.fn()
@@ -27,5 +28,19 @@ describe('render', () => {
     )
 
     expect(wrapper.text()).toBe('Signup')
+    expect(handleClick).toHaveBeenCalledTimes(0)
   })
+})
+test('onClick', () => {
+  const handleClick = jest.fn()
+  const wrapper = mountWithIntl(
+    <FormNavigation
+      handleClick={handleClick}
+      isSignup={false}
+    />
+  )
+
+  wrapper.find('.la--form-navigation').simulate('click')
+  expect(handleClick).toHaveBeenCalledTimes(1)
+  expect(handleClick).toHaveBeenCalledWith(true)
 })
