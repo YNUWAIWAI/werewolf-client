c2f9a69836978e81af1106b0151bc8369ccbe7df
Yudai Udagawa <nimius.rd@gmail.com>
2019-05-19 10:20:17 +0900

    Add test

---
+++ integration/landing/App.test.ts
@@ -0,0 +1,34 @@
+import {sleep} from '../util'
+
+describe('App', () => {
+  test('default', async () => {
+    await page.goto('http://localhost:9009/iframe.html?id=landing-app--default')
+    await sleep(500)
+    const image = await page.screenshot()
+
+    expect(image).toMatchImageSnapshot({
+      failureThreshold: 18,
+      failureThresholdType: 'pixel'
+    })
+  })
+  test('loginError', async () => {
+    await page.goto('http://localhost:9009/iframe.html?id=landing-app--loginerror')
+    await sleep(500)
+    const image = await page.screenshot()
+
+    expect(image).toMatchImageSnapshot({
+      failureThreshold: 18,
+      failureThresholdType: 'pixel'
+    })
+  })
+  test('signupError', async () => {
+    await page.goto('http://localhost:9009/iframe.html?id=landing-app--signuperror')
+    await sleep(500)
+    const image = await page.screenshot()
+
+    expect(image).toMatchImageSnapshot({
+      failureThreshold: 18,
+      failureThresholdType: 'pixel'
+    })
+  })
+})
