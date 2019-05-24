f37ada3e948a4ef36b67b89d41240ea59a640c56
Yudai Udagawa <nimius.rd@gmail.com>
2019-05-25 00:26:56 +0900

    Add trailer

--- src/scripts/landing/components/organism/Lead.tsx
+++ src/scripts/landing/components/organism/Lead.tsx
@@ -15,14 +15,12 @@ export default function Lead(props: Props) {
           {'* Flavor text *'}
         </p>
       </div>
-      <div className="la--image">
-        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-          <circle cx="50" cy="50" r="50" />
-          <text fill="white" x="12.5" y="75">
-            {'絵'}
-          </text>
-        </svg>
-      </div>
+      <iframe
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        className="la--trailer"
+        src="https://www.youtube.com/embed/PSeoJpc4J1s"
+      />
     </div>
   )
 }
