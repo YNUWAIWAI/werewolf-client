e6ea3dfb8fe2886746e2af5061c9d250ab301aa7
Yudai Udagawa <nimius.rd@gmail.com>
2019-05-26 03:57:49 +0900

    Fix test

--- integration/landing/App.test.ts
+++ integration/landing/App.test.ts
@@ -2,34 +2,22 @@ describe('App', () => {
   test('default', async () => {
     await page.goto('http://localhost:9009/iframe.html?id=landing-app--default')
     await page.evaluateHandle('document.fonts.ready')
-    await page.waitFor(1000)
     const image = await page.screenshot()
 
-    expect(image).toMatchImageSnapshot({
-      failureThreshold: 18,
-      failureThresholdType: 'pixel'
-    })
+    expect(image).toMatchImageSnapshot()
   })
   test('loginError', async () => {
     await page.goto('http://localhost:9009/iframe.html?id=landing-app--loginerror')
     await page.evaluateHandle('document.fonts.ready')
-    await page.waitFor(1000)
     const image = await page.screenshot()
 
-    expect(image).toMatchImageSnapshot({
-      failureThreshold: 18,
-      failureThresholdType: 'pixel'
-    })
+    expect(image).toMatchImageSnapshot()
   })
   test('signupError', async () => {
     await page.goto('http://localhost:9009/iframe.html?id=landing-app--signuperror')
     await page.evaluateHandle('document.fonts.ready')
-    await page.waitFor(1000)
     const image = await page.screenshot()
 
-    expect(image).toMatchImageSnapshot({
-      failureThreshold: 18,
-      failureThresholdType: 'pixel'
-    })
+    expect(image).toMatchImageSnapshot()
   })
 })
--- integration/setupTests.js
+++ integration/setupTests.js
@@ -1,7 +1,11 @@
-const {toMatchImageSnapshot} = require('jest-image-snapshot')
+const {configureToMatchImageSnapshot} = require('jest-image-snapshot')
+
+const toMatchImageSnapshot = configureToMatchImageSnapshot({
+  failureThreshold: 1,
+  failureThresholdType: 'percent'
+})
 
 expect.extend({
   toMatchImageSnapshot
 })
-
 jest.setTimeout(30000)
