970da7d3c245e4c62769dc1d8da698c49a4d4abf
Yudai Udagawa <nimius.rd@gmail.com>
2019-05-29 17:45:46 +0900

    Update InfoAgentContainer

--- src/scripts/village/containers/InfoAgentContainer.ts
+++ src/scripts/village/containers/InfoAgentContainer.ts
@@ -1,13 +1,14 @@
 import AgentIcon, {Props as StateProps} from '../components/atoms/AgentIcon'
+import {getInitial, getText} from '../util'
 import {ReducerState} from '../reducers'
 import {connect} from 'react-redux'
-import {getText} from '../util'
 
 const mapStateToProps = (state: ReducerState): StateProps => {
   if (!state.mine.agent) {
     return {
       className: 'vi--info--agent',
       image: '',
+      initial: '',
       name: ''
     }
   }
@@ -15,6 +16,7 @@ const mapStateToProps = (state: ReducerState): StateProps => {
   return {
     className: 'vi--info--agent',
     image: state.mine.agent.image,
+    initial: getInitial(state.mine.agent.name.en),
     name: getText({
       language: state.language,
       languageMap: state.mine.agent.name
