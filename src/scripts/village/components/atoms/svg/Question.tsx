8fed5ec1fbea0498ad84a6e590fc8552f487213b
Yudai Udagawa <nimius.rd@gmail.com>
2019-05-30 05:30:12 +0900

    Update Question

--- src/scripts/village/components/atoms/svg/Question.tsx
+++ src/scripts/village/components/atoms/svg/Question.tsx
@@ -2,8 +2,17 @@ import * as React from 'react'
 
 export default function Question() {
   return (
-    <svg viewBox="0 0 180 180" xmlns="http://www.w3.org/2000/svg">
-      <path d="M60,67c0-13 1-19 8-26c7-9 18-10 28-8c10,2 22,12 22,26c0,14-11,19-15,22c-7,2-10,6-11,11v20m0,12v16" fill="none" stroke="#000" strokeWidth="16" />
+    <svg
+      className="question"
+      viewBox="0 0 180 180"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M60,67c0-13 1-19 8-26c7-9 18-10 28-8c10,2 22,12 22,26c0,14-11,19-15,22c-7,2-10,6-11,11v20m0,12v16"
+        fill="none"
+        stroke="#000"
+        strokeWidth="16"
+      />
     </svg>
   )
 }
