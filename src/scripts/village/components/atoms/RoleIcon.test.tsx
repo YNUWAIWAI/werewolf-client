52f36f6d0e43782967ef0490625be7cc513bbbc4
Yudai Udagawa <nimius.rd@gmail.com>
2019-05-30 05:30:00 +0900

    Update RoleIcon

--- src/scripts/village/components/atoms/RoleIcon.test.tsx
+++ src/scripts/village/components/atoms/RoleIcon.test.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import Question from './svg/Question'
 import RoleIcon from './RoleIcon'
 import {shallow} from 'enzyme'
 
--- src/scripts/village/components/atoms/RoleIcon.tsx
+++ src/scripts/village/components/atoms/RoleIcon.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import Question from './svg/Question'
 
 export interface Props {
   readonly additionalClass?: string
