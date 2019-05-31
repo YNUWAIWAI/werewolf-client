21ab25703555e1232b63bf523542b4d1695365d9
Yudai Udagawa <nimius.rd@gmail.com>
2019-05-31 22:20:25 +0900

    Update ResultCellRoleImage

--- src/scripts/village/components/atoms/ResultCellRoleImage.test.tsx
+++ src/scripts/village/components/atoms/ResultCellRoleImage.test.tsx
@@ -26,4 +26,48 @@ describe('<ResultCellRoleImage />', () => {
 
     expect(warpper.html()).toMatchSnapshot()
   })
+  test('deathByAttack', () => {
+    const warpper = shallow(
+      <ResultCellRoleImage
+        image="https://werewolf.world/image/0.3/role_icons/50x50withTI/villager_50x50.png"
+        name="Villager"
+        status={village.AgentStatus.deathByAttack}
+      />
+    )
+
+    expect(warpper.html()).toMatchSnapshot()
+  })
+  test('deathByExecution', () => {
+    const warpper = shallow(
+      <ResultCellRoleImage
+        image="https://werewolf.world/image/0.3/role_icons/50x50withTI/villager_50x50.png"
+        name="Villager"
+        status={village.AgentStatus.deathByExecution}
+      />
+    )
+
+    expect(warpper.html()).toMatchSnapshot()
+  })
+  test('deathByFear', () => {
+    const warpper = shallow(
+      <ResultCellRoleImage
+        image="https://werewolf.world/image/0.3/role_icons/50x50withTI/villager_50x50.png"
+        name="Villager"
+        status={village.AgentStatus.deathByFear}
+      />
+    )
+
+    expect(warpper.html()).toMatchSnapshot()
+  })
+  test('unnaturalDeath', () => {
+    const warpper = shallow(
+      <ResultCellRoleImage
+        image="https://werewolf.world/image/0.3/role_icons/50x50withTI/villager_50x50.png"
+        name="Villager"
+        status={village.AgentStatus.unnaturalDeath}
+      />
+    )
+
+    expect(warpper.html()).toMatchSnapshot()
+  })
 })
