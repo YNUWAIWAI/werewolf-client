18dc0272201db23b45a78097b53a95ab6882debe
Yudai Udagawa <nimius.rd@gmail.com>
2019-05-31 19:38:29 +0900

    Test ResultCellStatus

---
+++ src/scripts/village/components/atoms/ResultCellStatus.test.tsx
@@ -0,0 +1,64 @@
+/* global village */
+import * as React from 'react'
+import ResultCellStatus from './ResultCellStatus'
+import {getMessages} from '../../../../i18n/village'
+import {initRenderer} from '../../../../../tools/intl-enzyme-test-helper'
+
+const {mountWithIntl} = initRenderer(village.Language.en, getMessages(village.Language.en))
+
+describe('<ResultCellStatus />', () => {
+  test('alive', () => {
+    const warpper = mountWithIntl(
+      <ResultCellStatus
+        status={village.AgentStatus.alive}
+      />
+    )
+
+    expect(warpper.html()).toMatchSnapshot()
+  })
+  test('dead', () => {
+    const warpper = mountWithIntl(
+      <ResultCellStatus
+        status={village.AgentStatus.dead}
+      />
+    )
+
+    expect(warpper.html()).toMatchSnapshot()
+  })
+  test('deathByAttack', () => {
+    const warpper = mountWithIntl(
+      <ResultCellStatus
+        status={village.AgentStatus.deathByAttack}
+      />
+    )
+
+    expect(warpper.html()).toMatchSnapshot()
+  })
+  test('deathByExecution', () => {
+    const warpper = mountWithIntl(
+      <ResultCellStatus
+        status={village.AgentStatus.deathByExecution}
+      />
+    )
+
+    expect(warpper.html()).toMatchSnapshot()
+  })
+  test('deathByFear', () => {
+    const warpper = mountWithIntl(
+      <ResultCellStatus
+        status={village.AgentStatus.deathByFear}
+      />
+    )
+
+    expect(warpper.html()).toMatchSnapshot()
+  })
+  test('unnaturalDeath', () => {
+    const warpper = mountWithIntl(
+      <ResultCellStatus
+        status={village.AgentStatus.unnaturalDeath}
+      />
+    )
+
+    expect(warpper.html()).toMatchSnapshot()
+  })
+})
