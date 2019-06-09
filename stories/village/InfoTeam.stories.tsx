57e8633271d62a38cd89689f3327c55f60974af5
Yudai Udagawa <nimius.rd@gmail.com>
2019-06-09 21:58:56 +0900

    Update InfoTeam story

--- stories/village/InfoTeam.stories.tsx
+++ stories/village/InfoTeam.stories.tsx
@@ -4,51 +4,72 @@ import IntlProvider from '../../src/scripts/village/containers/IntlProviderConta
 import {Provider} from 'react-redux'
 import TeamIcon from '../../src/scripts/village/components/atoms/TeamIcon'
 import {createStore} from 'redux'
+import language from '../language'
+import {radios} from '@storybook/addon-knobs'
 import reducer from '../../src/scripts/village/reducers'
 import {storiesOf} from '@storybook/react'
 
-const store = createStore(
-  reducer
-)
-
 storiesOf('village|Info/InfoTeam', module)
-  .addDecorator(story => (
-    <Provider store={store}>
-      <IntlProvider>
-        {story()}
-      </IntlProvider>
-    </Provider>
-  ))
   .add('villager', () => {
+    const store = createStore(
+      reducer,
+      {
+        language: radios(language.label, language.options, language.defaultValue)
+      }
+    )
     const story =
-      <div className="vi--info">
-        <TeamIcon
-          class="vi--info--team"
-          team={village.Team.villager}
-        />
-      </div>
+      <Provider store={store}>
+        <IntlProvider>
+          <div className="vi--info">
+            <TeamIcon
+              class="vi--info--team"
+              team={village.Team.villager}
+            />
+          </div>
+        </IntlProvider>
+      </Provider>
 
     return story
   })
   .add('werehamster', () => {
+    const store = createStore(
+      reducer,
+      {
+        language: radios(language.label, language.options, language.defaultValue)
+      }
+    )
     const story =
-      <div className="vi--info">
-        <TeamIcon
-          class="vi--info--team"
-          team={village.Team.werehamster}
-        />
-      </div>
+      <Provider store={store}>
+        <IntlProvider>
+          <div className="vi--info">
+            <TeamIcon
+              class="vi--info--team"
+              team={village.Team.werehamster}
+            />
+          </div>
+        </IntlProvider>
+      </Provider>
 
     return story
   })
   .add('werewolf', () => {
+    const store = createStore(
+      reducer,
+      {
+        language: radios(language.label, language.options, language.defaultValue)
+      }
+    )
     const story =
-      <div className="vi--info">
-        <TeamIcon
-          class="vi--info--team"
-          team={village.Team.werewolf}
-        />
-      </div>
+      <Provider store={store}>
+        <IntlProvider>
+          <div className="vi--info">
+            <TeamIcon
+              class="vi--info--team"
+              team={village.Team.werewolf}
+            />
+          </div>
+        </IntlProvider>
+      </Provider>
 
     return story
   })
