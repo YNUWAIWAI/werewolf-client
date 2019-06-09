0df5e29127dd8ba265291a72ed3559d25c4a3a57
Yudai Udagawa <nimius.rd@gmail.com>
2019-06-10 00:52:01 +0900

    Update Modal story

--- stories/village/Modal.stories.tsx
+++ stories/village/Modal.stories.tsx
@@ -4,33 +4,34 @@ import Modal from '../../src/scripts/village/components/organisms/Modal'
 import {Provider} from 'react-redux'
 import {action} from '@storybook/addon-actions'
 import {createStore} from 'redux'
+import language from '../language'
+import {radios} from '@storybook/addon-knobs'
 import reducer from '../../src/scripts/village/reducers'
 import {storiesOf} from '@storybook/react'
 
-const store = createStore(
-  reducer
-)
-
 storiesOf('village|Modal', module)
-  .addDecorator(story => (
-    <Provider store={store}>
-      <IntlProvider>
-        {story()}
-      </IntlProvider>
-    </Provider>
-  ))
   .add('default', () => {
+    const store = createStore(
+      reducer,
+      {
+        language: radios(language.label, language.options, language.defaultValue)
+      }
+    )
     const story =
-      <Modal
-        descriptionId="Modal.Description.dayVote"
-        handleClickNo={action('handleClickNo')}
-        handleClickYes={action('handleClickYes')}
-        id={1}
-        image="https://werewolf.world/image/0.3/agent_icons/50x50/a_50x50.png"
-        initial="A"
-        name="Adil"
-        visible
-      />
+      <Provider store={store}>
+        <IntlProvider>
+          <Modal
+            descriptionId="Modal.Description.dayVote"
+            handleClickNo={action('handleClickNo')}
+            handleClickYes={action('handleClickYes')}
+            id={1}
+            image="https://werewolf.world/image/0.3/agent_icons/50x50/a_50x50.png"
+            initial="A"
+            name="Adil"
+            visible
+          />
+        </IntlProvider>
+      </Provider>
 
     return story
   })
