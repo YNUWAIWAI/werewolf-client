42461902b8960c64ba813b939e69ee3badcda0d2
Yudai Udagawa <nimius.rd@gmail.com>
2019-06-10 02:27:15 +0900

    Add landing/language.ts

---
+++ stories/landing/language.ts
@@ -0,0 +1,14 @@
+import {Language} from '../../src/i18n/landing'
+
+const label = 'Language'
+const options = {
+  en: Language.en,
+  ja: Language.ja
+}
+const defaultValue = options.en
+
+export default {
+  defaultValue,
+  label,
+  options
+}
