107523de8484c61e8aeae20f41eecfdc23cc5653
Yudai Udagawa <nimius.rd@gmail.com>
2019-06-10 01:33:03 +0900

    Update Timer story

--- stories/village/Timer.stories.tsx
+++ stories/village/Timer.stories.tsx
@@ -3,23 +3,25 @@ import IntlProvider from '../../src/scripts/village/containers/IntlProviderConta
 import {Provider} from 'react-redux'
 import Timer from '../../src/scripts/village/containers/TimerContainer'
 import {createStore} from 'redux'
+import language from '../language'
+import {radios} from '@storybook/addon-knobs'
 import reducer from '../../src/scripts/village/reducers'
 import {storiesOf} from '@storybook/react'
 
-const store = createStore(
-  reducer
-)
-
 storiesOf('village|Timer', module)
-  .addDecorator(story => (
-    <Provider store={store}>
-      <IntlProvider>
-        {story()}
-      </IntlProvider>
-    </Provider>
-  ))
   .add('default', () => {
-    const story = <Timer />
+    const store = createStore(
+      reducer,
+      {
+        language: radios(language.label, language.options, language.defaultValue)
+      }
+    )
+    const story =
+      <Provider store={store}>
+        <IntlProvider>
+          <Timer />
+        </IntlProvider>
+      </Provider>
 
     return story
   })
