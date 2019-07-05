b9ed67661cc7c19eb979071442738db907176b7c
Yudai Udagawa <nimius.rd@gmail.com>
2019-07-05 15:27:10 +0900

    Add isSilent

--- src/scripts/village/components/atoms/PredictionPlayer.tsx
+++ src/scripts/village/components/atoms/PredictionPlayer.tsx
@@ -6,12 +6,15 @@ interface Props {
   readonly initial: string
   readonly name: string
   readonly status: string
+  readonly isSilent: boolean
 }
 
 export default function PredictionPlayer(props: Props) {
+  const isSilent = props.isSilent ? 'silent' : ''
+
   return (
     <AgentIcon
-      additionalClass={props.status === 'alive' ? '' : 'dead'}
+      additionalClass={props.status === 'alive' ? isSilent : 'dead'}
       className="vi--prediction--player"
       image={props.image}
       initial={props.initial}
