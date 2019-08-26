77620d874efd4635871d4efa93e5decc2f89126d
Yudai Udagawa <nimius.rd@gmail.com>
2019-08-27 05:03:12 +0900

    Fix test

--- src/scripts/village/components/organisms/CommandSelection.test.tsx
+++ src/scripts/village/components/organisms/CommandSelection.test.tsx
@@ -24,91 +24,91 @@ describe('<CommandSelection />', () => {
   test('15 agents fixed={false}', () => {
     const agents = [
       {
-        id: 1,
+        id: '1',
         image: 'https://werewolf.world/image/0.3/agent_icons/50x50/a_50x50.png',
         initial: 'A',
         name: 'Adil'
       },
       {
-        id: 2,
+        id: '2',
         image: 'https://werewolf.world/image/0.3/agent_icons/50x50/b_50x50.png',
         initial: 'B',
         name: 'Borya'
       },
       {
-        id: 3,
+        id: '3',
         image: 'https://werewolf.world/image/0.3/agent_icons/50x50/c_50x50.png',
         initial: 'C',
         name: 'Chacha'
       },
       {
-        id: 4,
+        id: '4',
         image: 'https://werewolf.world/image/0.3/agent_icons/50x50/d_50x50.png',
         initial: 'D',
         name: 'Devdatta'
       },
       {
-        id: 5,
+        id: '5',
         image: 'https://werewolf.world/image/0.3/agent_icons/50x50/e_50x50.png',
         initial: 'E',
         name: 'Ekrem'
       },
       {
-        id: 6,
+        id: '6',
         image: 'https://werewolf.world/image/0.3/agent_icons/50x50/f_50x50.png',
         initial: 'F',
         name: 'Fernando'
       },
       {
-        id: 7,
+        id: '7',
         image: 'https://werewolf.world/image/0.3/agent_icons/50x50/g_50x50.png',
         initial: 'G',
         name: 'Gavriil'
       },
       {
-        id: 8,
+        id: '8',
         image: 'https://werewolf.world/image/0.3/agent_icons/50x50/h_50x50.png',
         initial: 'H',
         name: 'Henrik'
       },
       {
-        id: 9,
+        id: '9',
         image: 'https://werewolf.world/image/0.3/agent_icons/50x50/i_50x50.png',
         initial: 'I',
         name: 'Ileanna'
       },
       {
-        id: 10,
+        id: '10',
         image: 'https://werewolf.world/image/0.3/agent_icons/50x50/j_50x50.png',
         initial: 'J',
         name: 'Jasmin'
       },
       {
-        id: 11,
+        id: '11',
         image: 'https://werewolf.world/image/0.3/agent_icons/50x50/k_50x50.png',
         initial: 'K',
         name: 'Kaiji'
       },
       {
-        id: 12,
+        id: '12',
         image: 'https://werewolf.world/image/0.3/agent_icons/50x50/l_50x50.png',
         initial: 'L',
         name: 'Louise'
       },
       {
-        id: 13,
+        id: '13',
         image: 'https://werewolf.world/image/0.3/agent_icons/50x50/m_50x50.png',
         initial: 'M',
         name: 'Marthe'
       },
       {
-        id: 14,
+        id: '14',
         image: 'https://werewolf.world/image/0.3/agent_icons/50x50/n_50x50.png',
         initial: 'N',
         name: 'Nanyamka'
       },
       {
-        id: 15,
+        id: '15',
         image: 'https://werewolf.world/image/0.3/agent_icons/50x50/o_50x50.png',
         initial: 'O',
         name: 'Oliwia'
@@ -132,7 +132,7 @@ describe('<CommandSelection />', () => {
   test('1 agents fixed', () => {
     const agents = [
       {
-        id: 1,
+        id: '1',
         image: 'https://werewolf.world/image/0.3/agent_icons/50x50/a_50x50.png',
         initial: 'A',
         name: 'Adil'
