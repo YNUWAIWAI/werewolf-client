4f098850f44f497c75010b915f981fd731de9d1c
Yudai Udagawa <nimius.rd@gmail.com>
2019-10-06 09:35:35 +0900

    Fix jest.config

--- integration/jest.config.js
+++ integration/jest.config.js
@@ -1,14 +1,7 @@
 const {defaults: tsjPreset} = require('ts-jest/presets')
-const {pathsToModuleNameMapper} = require('ts-jest/utils')
 const {compilerOptions} = require('../tsconfig')
 
 module.exports = {
-  moduleNameMapper: pathsToModuleNameMapper(
-    compilerOptions.paths,
-    {
-      prefix: '<rootDir>/'
-    }
-  ),
   preset: 'jest-puppeteer',
   setupFilesAfterEnv: [
     './setupTests.js'
