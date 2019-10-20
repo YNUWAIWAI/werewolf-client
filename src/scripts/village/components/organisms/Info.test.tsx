e043ebc0798063f37dc4cc1503fd00ac8c734a06
Yudai Udagawa <nimius.rd@gmail.com>
2019-10-20 15:39:26 +0900

    Fix Info test

--- src/scripts/village/components/organisms/Info.test.tsx
+++ src/scripts/village/components/organisms/Info.test.tsx
@@ -4,17 +4,18 @@ import {FormattedMessage} from 'react-intl'
 import Info from './Info'
 import {getMessages} from '../../i18n'
 import {initRenderer} from '../../tools'
+import {shallow} from 'enzyme'
 
-const {shallowWithIntl} = initRenderer(village.Language.ja, getMessages(village.Language.ja))
+const {mountWithIntl} = initRenderer(village.Language.en, getMessages(village.Language.en))
 
 test('<Info />', () => {
-  const wrapper = shallowWithIntl(<Info />)
+  const wrapper = shallow(<Info />)
 
   expect(wrapper.children()).toHaveLength(7)
   expect(wrapper.find(FormattedMessage)).toHaveLength(3)
-  const [agent, role, team] = wrapper.find(FormattedMessage).getElements().map(elem => shallowWithIntl(elem))
+  const [agent, role, team] = wrapper.find(FormattedMessage).getElements().map(elem => mountWithIntl(elem))
 
-  expect(agent.text()).toBe('あなた:')
-  expect(role.text()).toBe('役職:')
-  expect(team.text()).toBe('陣営:')
+  expect(agent.text()).toBe('You:')
+  expect(role.text()).toBe('Role:')
+  expect(team.text()).toBe('Team:')
 })
