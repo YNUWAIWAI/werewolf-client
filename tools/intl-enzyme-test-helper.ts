9f7c11df2abefe8dd8790508c600ff528e782966
Yudai Udagawa <nimius.rd@gmail.com>
2019-10-20 14:30:55 +0900

    Update helper function

--- tools/intl-enzyme-test-helper.ts
+++ tools/intl-enzyme-test-helper.ts
@@ -1,51 +1,33 @@
 import * as React from 'react'
 import * as lobby from '../types/lobby'
 import * as village from '../types/village'
-import {IntlProvider, intlShape} from 'react-intl'
-import {MountRendererProps, ShallowRendererProps, mount, shallow} from 'enzyme'
+import {mount, shallow} from 'enzyme'
+import {IntlProvider} from 'react-intl'
 import {Language} from '../src/i18n/landing'
 
-export const initRenderer = (locale: lobby.Language | village.Language | Language, messages: {[key: string]: string}) => {
-  const intlProvider = new IntlProvider(
-    {
-      locale,
-      messages
-    },
-    {}
-  )
-  const {intl} = intlProvider.getChildContext()
-  const nodeWithIntlProp = (node: React.ReactElement) => React.cloneElement(
+const defaultLocale = 'en'
+
+export const initRenderer = (locale: lobby.Language | village.Language | Language, messages: {[key: string]: string}) => ({
+  mountWithIntl: (node: React.ReactElement) => mount(
     node,
     {
-      intl
+      wrappingComponent: IntlProvider,
+      wrappingComponentProps: {
+        defaultLocale,
+        locale,
+        messages
+      }
+    }
+  ),
+  shallowWithIntl: (node: React.ReactElement) => shallow(
+    node,
+    {
+      wrappingComponent: IntlProvider,
+      wrappingComponentProps: {
+        defaultLocale,
+        locale,
+        messages
+      }
     }
   )
-
-  return {
-    mountWithIntl: <C extends React.Component>(node: React.ReactElement<any>, {context, childContextTypes, ... additionalOptions}: MountRendererProps = {}) => mount<C>(
-      nodeWithIntlProp(node),
-      {
-        childContextTypes: {
-          intl: intlShape,
-          ... childContextTypes
-        },
-        context: {
-          ... context,
-          intl
-        },
-        ... additionalOptions
-      }
-    ),
-    shallowWithIntl: <C extends React.Component>(node: React.ReactElement<any>, {context, ... additionalOptions}: ShallowRendererProps = {}) => shallow<C>(
-      nodeWithIntlProp(node),
-      {
-        context: {
-          ... context,
-          intl
-        },
-        ... additionalOptions
-      }
-    )
-  }
-}
-
+})
