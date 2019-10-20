3c1a752bb4421f84bada3b1abea278cd8564c53c
Yudai Udagawa <nimius.rd@gmail.com>
2019-10-20 22:28:36 +0900

    Update test

--- src/scripts/lobby/components/organisms/AdvancedSearchBox.test.tsx
+++ src/scripts/lobby/components/organisms/AdvancedSearchBox.test.tsx
@@ -5,7 +5,7 @@ import Select from 'react-select'
 import {getMessages} from '../../i18n'
 import {initRenderer} from '../../tools'
 
-const {mountWithIntl} = initRenderer(lobby.Language.ja, getMessages(lobby.Language.ja))
+const {mountWithIntl} = initRenderer(lobby.Language.en, getMessages(lobby.Language.en))
 
 describe('<AdvancedSearchBox />', () => {
   test('render', () => {
