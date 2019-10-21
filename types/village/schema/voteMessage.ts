0e1f654903b7371c6c3ed8cf9c39f6eb5846b959
Yudai Udagawa <nimius.rd@gmail.com>
2019-10-22 02:07:20 +0900

    Add types of VoteMessage

---
+++ types/village/schema/voteMessage.ts
@@ -0,0 +1,28 @@
+/*
+  'https://werewolf.world/village/schema/0.3/voteMessage.json'
+*/
+import {Base} from './base'
+import {Character} from './character'
+
+export interface VoteMessage {
+  '@context': Base['@context']
+  '@id': Base['@id']
+  character?: {
+    '@context': Character['@context']
+    '@id': Character['@id']
+    id: Character['id']
+    image: Character['image']
+    name: Character['name']
+  }
+  clientTimestamp: Base['clientTimestamp']
+  date: Base['date']
+  directionality: Base['directionality']
+  extensionalDisclosureRange: Base['extensionalDisclosureRange']
+  intensionalDisclosureRange: Base['intensionalDisclosureRange']
+  phase: Base['phase']
+  phaseStartTime: Base['phaseStartTime']
+  phaseTimeLimit: Base['phaseTimeLimit']
+  serverTimestamp: Base['serverTimestamp']
+  token: Base['token']
+  village: Base['village']
+}
