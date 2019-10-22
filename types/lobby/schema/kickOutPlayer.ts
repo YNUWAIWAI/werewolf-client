ec51473098358338d616ea093e5f06b63f4f640a
Yudai Udagawa <nimius.rd@gmail.com>
2019-10-22 14:50:18 +0900

    Add types of KickoutPlayer

---
+++ types/lobby/schema/kickOutPlayer.ts
@@ -0,0 +1,16 @@
+/*
+  'https://werewolf.world/lobby/schema/0.3/kickOutPlayer.json'
+*/
+import {
+  PayloadType,
+  Token
+} from '..'
+
+
+export interface KickOutPlayer {
+  players: {
+    token: Token
+  }[]
+  token: Token
+  type: PayloadType.kickOutPlayer
+}
