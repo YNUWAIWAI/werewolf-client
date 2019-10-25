c96135a214abec3c0047d8ea27a8e24c6f32766c
Yudai Udagawa <nimius.rd@gmail.com>
2019-10-26 00:39:57 +0900

    Update CommandGraceContainer

--- src/scripts/village/containers/CommandGraveContainer.ts
+++ src/scripts/village/containers/CommandGraveContainer.ts
@@ -10,7 +10,7 @@ type Action =
 
 const mapStateToProps = (state: ReducerState): StateProps => ({
   ... state.commandGrave,
-  maxLengthOfUnicodeCodePoints: state.base.village.chatSettings.characterLimit,
+  maxLengthOfUnicodeCodePoints: state.base.village.chatSettings.maxLengthOfUnicodeCodePoints,
   language: state.language,
   suggesttedData: state.suggest.data
 })
