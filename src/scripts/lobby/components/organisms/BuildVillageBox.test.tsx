b3f760b650bb1568aa4a7583f127841b70c27369
Yudai Udagawa <nimius.rd@gmail.com>
2019-10-25 21:53:11 +0900

    Update BuildVillageBox

--- src/scripts/lobby/components/organisms/BuildVillageBox.test.tsx
+++ src/scripts/lobby/components/organisms/BuildVillageBox.test.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react'
-import {lobby} from '../../types'
 import BuildVillageBox, {Props} from './BuildVillageBox'
 import Select from 'react-select'
 import {getMessages} from '../../i18n'
 import {initRenderer} from '../../tools'
+import {lobby} from '../../types'
 
 const {mountWithIntl} = initRenderer(lobby.Language.en, getMessages(lobby.Language.en))
 
