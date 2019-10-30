5b92cf5b80d683885db25f12810c9c4a6f0a2d87
Yudai Udagawa <nimius.rd@gmail.com>
2019-10-31 02:13:24 +0900

    ChatDate -> ChatDay

--- src/scripts/village/components/atoms/ChatDay.test.tsx
+++ src/scripts/village/components/atoms/ChatDay.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import ChatDate from './ChatDay'
+import ChatDay from './ChatDay'
 import {getMessages} from '../../i18n'
 import {initRenderer} from '../../tools'
 import {village} from '../../types'
@@ -8,17 +8,35 @@ const {mountWithIntl} = initRenderer(village.Language.en, getMessages(village.La
 
 describe('<ChatDay />', () => {
   test('from="2018-01-01T00:00:00.000Z" limit={50} to="2018-01-01T00:00:10.000Z"', () => {
-    const wrapper = mountWithIntl(<ChatDate from="2018-01-01T00:00:00.000Z" limit={50} to="2018-01-01T00:00:10.000Z" />)
+    const wrapper = mountWithIntl(
+      <ChatDay
+        from="2018-01-01T00:00:00.000Z"
+        limit={50}
+        to="2018-01-01T00:00:10.000Z"
+      />
+    )
 
     expect(wrapper.text()).toMatch('01/01/2018, 09:00:10 (00:40 left)')
   })
   test('from="2018-01-01T00:00:00.000Z" limit={50} to="2018-01-01T00:00:50.000Z"', () => {
-    const wrapper = mountWithIntl(<ChatDate from="2018-01-01T00:00:00.000Z" limit={50} to="2018-01-01T00:00:50.000Z" />)
+    const wrapper = mountWithIntl(
+      <ChatDay
+        from="2018-01-01T00:00:00.000Z"
+        limit={50}
+        to="2018-01-01T00:00:50.000Z"
+      />
+    )
 
     expect(wrapper.text()).toMatch('01/01/2018, 09:00:50 (00:00 left)')
   })
   test('from="2018-01-01T00:00:00.000Z" limit={50} to="2018-01-01T00:01:00.000Z"', () => {
-    const wrapper = mountWithIntl(<ChatDate from="2018-01-01T00:00:00.000Z" limit={50} to="2018-01-01T00:01:00.000Z" />)
+    const wrapper = mountWithIntl(
+      <ChatDay
+        from="2018-01-01T00:00:00.000Z"
+        limit={50}
+        to="2018-01-01T00:01:00.000Z"
+      />
+    )
 
     expect(wrapper.text()).toMatch('01/01/2018, 09:01:00 (00:00 left)')
   })
