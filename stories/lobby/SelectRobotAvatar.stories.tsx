ff21380f441dd67c32abd6005daefddbbdc91933
Yudai Udagawa <nimius.rd@gmail.com>
2019-11-09 14:08:28 +0900

    Update story

--- stories/lobby/SelectRobotAvatar.stories.tsx
+++ stories/lobby/SelectRobotAvatar.stories.tsx
@@ -18,7 +18,7 @@ storiesOf('lobby|SelectRobotAvatar', module)
         language: radios(language.label, language.options, language.defaultValue),
         selectRobotAvatar: {
           avatar: {
-            allIds: ['a1', 'a2', 'a3'],
+            allIds: ['a1', 'a2', 'a3', 'a4', 'a5'],
             byId: {
               'a1': {
                 accessToken: '3F2504E0-4F89-11D3-9A0C-0305E82C3300',
@@ -26,6 +26,7 @@ storiesOf('lobby|SelectRobotAvatar', module)
                 automation: lobby.Automation.full,
                 checked: false,
                 name: 'avatar1',
+                status: lobby.AvatarStatus.awaitingAuthorization,
                 testStatus: lobby.TestStatus.notPassed
               },
               'a2': {
@@ -34,6 +35,7 @@ storiesOf('lobby|SelectRobotAvatar', module)
                 automation: lobby.Automation.semi,
                 checked: false,
                 name: 'avatar2',
+                status: lobby.AvatarStatus.awaitingCommunicationTest,
                 testStatus: lobby.TestStatus.passed
               },
               'a3': {
@@ -42,6 +44,25 @@ storiesOf('lobby|SelectRobotAvatar', module)
                 automation: lobby.Automation.semi,
                 checked: false,
                 name: 'avatar3',
+                status: lobby.AvatarStatus.connected,
+                testStatus: lobby.TestStatus.passed
+              },
+              'a4': {
+                accessToken: '3F2504E0-4F89-11D3-9A0C-0305E82C3303',
+                authorized: lobby.Authorized.waitForAcceptance,
+                automation: lobby.Automation.semi,
+                checked: false,
+                name: 'avatar4',
+                status: lobby.AvatarStatus.runningInTheBackground,
+                testStatus: lobby.TestStatus.passed
+              },
+              'a5': {
+                accessToken: '3F2504E0-4F89-11D3-9A0C-0305E82C3304',
+                authorized: lobby.Authorized.waitForAcceptance,
+                automation: lobby.Automation.semi,
+                checked: false,
+                name: 'avatar5',
+                status: lobby.AvatarStatus.runningInTheForeground,
                 testStatus: lobby.TestStatus.passed
               }
             }
