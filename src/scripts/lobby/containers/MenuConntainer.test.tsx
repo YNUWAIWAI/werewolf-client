c56b3a546d9289bf04326e6e3dba74f8aa8ba3bd
Yudai Udagawa <nimius.rd@gmail.com>
2019-11-10 02:05:31 +0900

    Update test

--- src/scripts/lobby/containers/MenuConntainer.test.tsx
+++ src/scripts/lobby/containers/MenuConntainer.test.tsx
@@ -1,5 +1,7 @@
+import * as ActionTypes from '../constants/ActionTypes'
 import * as React from 'react'
 import IntlProviderContainer from './IntlProviderContainer'
+import Menu from '../components/organisms/Menu'
 import MenuContainer from './MenuContainer'
 import {Provider} from 'react-redux'
 import fakeStore from './fakeStore'
@@ -21,5 +23,28 @@ describe('<MenuContainer />', () => {
     )
 
     expect(wrapper.html()).toMatchSnapshot()
+  })
+  test('transition', () => {
+    const store = fakeStore()
+    const dispatch = jest.fn()
+
+    store.dispatch = dispatch
+    const wrapper = mount(
+      <Provider store={store} >
+        <IntlProviderContainer>
+          <MenuContainer
+            className="className"
+            itemClassName="itemClassName"
+            items={[]}
+          />
+        </IntlProviderContainer>
+      </Provider>
+    )
+
+    wrapper.find(Menu).props().transition(ActionTypes.Target.ADVANCED_SEARCH)
+    expect(dispatch).toHaveBeenCalledTimes(1)
+    expect(dispatch).toHaveBeenCalledWith({
+      type: ActionTypes.Target.ADVANCED_SEARCH
+    })
   })
 })
