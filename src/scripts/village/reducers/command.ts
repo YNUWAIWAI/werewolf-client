9329dc1a2de9b63b594aee51f566007918740c6d
Yudai Udagawa <nimius.rd@gmail.com>
2019-12-03 01:55:52 +0900

    Update command

--- src/scripts/village/reducers/command.ts
+++ src/scripts/village/reducers/command.ts
@@ -1,5 +1,5 @@
 import * as ActionTypes from '../constants/ActionTypes'
-import {SocketMessage} from '../actions'
+import {Message$SystemMessage} from '../actions'
 import {village} from '../types'
 
 export const enum Content {
@@ -13,7 +13,7 @@ export interface State {
   readonly isDead: boolean
 }
 type Action =
-  | SocketMessage
+  | Message$SystemMessage
 
 export const initialState = {
   content: Content.CommandSelection,
@@ -21,85 +21,82 @@ export const initialState = {
 }
 const command = (state: State = initialState, action: Action): State => {
   switch (action.type) {
-    case ActionTypes.Socket.MESSAGE:
-      if (action.payload['@payload'] === village.Message.systemMessage) {
-        const payload = action.payload
-        const isDead = (() => {
-          if (typeof payload.character === 'undefined') {
-            return state.isDead
-          }
+    case ActionTypes.Message.SYSTEM_MESSAGE: {
+      const payload = action.payload
+      const isDead = (() => {
+        if (typeof payload.character === 'undefined') {
+          return state.isDead
+        }
 
-          const maybe = payload.character.find(c => c.isMine)
+        const maybe = payload.character.find(c => c.isMine)
 
-          if (!maybe) {
-            return false
-          }
+        if (!maybe) {
+          return false
+        }
 
-          return maybe.status !== village.CharacterStatus.alive
-        })()
+        return maybe.status !== village.CharacterStatus.alive
+      })()
 
-        switch (action.payload.phase) {
-          case village.Phase.noon:
-          case village.Phase.night:
-            if (isDead) {
-              return {
-                content: Content.CommandGrave,
-                isDead
-              }
+      switch (action.payload.phase) {
+        case village.Phase.noon:
+        case village.Phase.night:
+          if (isDead) {
+            return {
+              content: Content.CommandGrave,
+              isDead
             }
+          }
 
+          return {
+            content: Content.CommandSelection,
+            isDead
+          }
+        case village.Phase.flavorText:
+          if (isDead) {
+            return {
+              content: Content.CommandGrave,
+              isDead
+            }
+          }
+
+          if (payload.day === 0) {
             return {
               content: Content.CommandSelection,
               isDead
             }
-          case village.Phase.flavorText:
-            if (isDead) {
-              return {
-                content: Content.CommandGrave,
-                isDead
-              }
-            }
-
-            if (payload.day === 0) {
-              return {
-                content: Content.CommandSelection,
-                isDead
-              }
-            } else if (payload.day === -1) {
-              return {
-                content: Content.CommandPostMortem,
-                isDead
-              }
-            }
-
-            return {
-              content: Content.CommandInputBox,
-              isDead
-            }
-          case village.Phase.morning:
-            if (isDead) {
-              return {
-                content: Content.CommandGrave,
-                isDead
-              }
-            }
-
-            return {
-              content: Content.CommandInputBox,
-              isDead
-            }
-          case village.Phase.postMortem:
-          case village.Phase.result:
+          } else if (payload.day === -1) {
             return {
               content: Content.CommandPostMortem,
               isDead
             }
-          default:
-            throw Error(`Unknown: ${action.payload.phase}`)
-        }
-      }
+          }
 
-      return state
+          return {
+            content: Content.CommandInputBox,
+            isDead
+          }
+        case village.Phase.morning:
+          if (isDead) {
+            return {
+              content: Content.CommandGrave,
+              isDead
+            }
+          }
+
+          return {
+            content: Content.CommandInputBox,
+            isDead
+          }
+        case village.Phase.postMortem:
+        case village.Phase.result:
+          return {
+            content: Content.CommandPostMortem,
+            isDead
+          }
+        default:
+          throw Error(`Unknown: ${action.payload.phase}`)
+      }
+    }
     default:
       return state
   }
