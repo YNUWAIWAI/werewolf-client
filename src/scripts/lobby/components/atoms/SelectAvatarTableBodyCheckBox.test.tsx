dffc3a55f02ecff8a593ed59e303e4155152fcef
Yudai Udagawa <nimius.rd@gmail.com>
2019-12-26 13:44:41 +0900

    Add test in SelectAvatarTableBodyCheckBox

---
+++ src/scripts/lobby/components/atoms/SelectAvatarTableBodyCheckBox.test.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import SelectAvatarTableBodyCheckBox from './SelectAvatarTableBodyCheckBox'
+import {getMessages} from '../../i18n'
+import {initRenderer} from '../../tools'
+import {lobby} from '../../types'
+
+const {mountWithIntl} = initRenderer(lobby.Language.en, getMessages(lobby.Language.en))
+
+test('render', () => {
+  const handleClick = jest.fn()
+  const wrapper = mountWithIntl(
+    <SelectAvatarTableBodyCheckBox
+      additionalClassName={[]}
+      checked
+      handleClick={handleClick}
+    />
+  )
+
+  expect(wrapper.html()).toMatchSnapshot()
+})
+test('handleClick', () => {
+  const handleClick = jest.fn()
+  const wrapper = mountWithIntl(
+    <SelectAvatarTableBodyCheckBox
+      additionalClassName={[]}
+      checked
+      handleClick={handleClick}
+    />
+  )
+
+  wrapper.find('.lo--select-avatar--table--body--item.check-box').simulate('click')
+  expect(handleClick).toHaveBeenCalledTimes(1)
+})
