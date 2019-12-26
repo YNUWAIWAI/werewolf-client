e0c7c301629742613cd653127e3f5b8e27d9bc34
Yudai Udagawa <nimius.rd@gmail.com>
2019-12-26 14:33:18 +0900

    Add test in SelectAvatarTableBodyTest

---
+++ src/scripts/lobby/components/atoms/SelectAvatarTableBodyTest.test.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import SelectAvatarTableBodyTest from './SelectAvatarTableBodyTest'
+import {getMessages} from '../../i18n'
+import {initRenderer} from '../../tools'
+import {lobby} from '../../types'
+
+const {mountWithIntl} = initRenderer(lobby.Language.en, getMessages(lobby.Language.en))
+
+test('render', () => {
+  const handleSelect = jest.fn()
+  const wrapper = mountWithIntl(
+    <SelectAvatarTableBodyTest
+      additionalClassName={[]}
+      handleSelect={handleSelect}
+      test={lobby.TestStatus.notPassed}
+    />
+  )
+
+  expect(wrapper.html()).toMatchSnapshot()
+})
+test('handleSelect', () => {
+  const handleSelect = jest.fn()
+  const wrapper = mountWithIntl(
+    <SelectAvatarTableBodyTest
+      additionalClassName={[]}
+      handleSelect={handleSelect}
+      test={lobby.TestStatus.notPassed}
+    />
+  )
+
+  wrapper.find('.lo--select-avatar--table--body--item.test').simulate('click')
+  expect(handleSelect).toHaveBeenCalledTimes(1)
+})
