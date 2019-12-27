5fcfd9f9e72fc13436c5ac1b7115cdb157c2390e
Yudai Udagawa <nimius.rd@gmail.com>
2019-12-27 20:30:18 +0900

    Add test in SelectHumanAvatarTableHeader

---
+++ src/scripts/lobby/components/molecules/SelectHumanAvatarTableHeader.test.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import SelectHumanAvatarTableHeader from './SelectHumanAvatarTableHeader'
+import {getMessages} from '../../i18n'
+import {initRenderer} from '../../tools'
+import {lobby} from '../../types'
+
+const {mountWithIntl} = initRenderer(lobby.Language.en, getMessages(lobby.Language.en))
+
+test('render', () => {
+  const wrapper = mountWithIntl(
+    <SelectHumanAvatarTableHeader />
+  )
+
+  expect(wrapper.html()).toMatchSnapshot()
+})
