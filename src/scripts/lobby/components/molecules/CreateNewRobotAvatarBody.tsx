6c4087dcc34aedc46932362ae8a3ef3b4ee9c740
Yudai Udagawa <nimius.rd@gmail.com>
2019-12-30 04:00:00 +0900

    Add CreateNewRobotAvatarBody

---
+++ src/scripts/lobby/components/molecules/CreateNewRobotAvatarBody.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react'
+import AutomationSelect from '../atoms/AutomationSelect'
+import {FormattedMessage} from 'react-intl'
+import Menu from '../../containers/MenuContainer'
+import {MenuItemProps as MenuItem} from '../organisms/Menu'
+import TextInput from '../atoms/TextInput'
+import {lobby} from '../../types'
+
+interface Props {
+  command: MenuItem[]
+}
+
+export default function CreateNewRobotAvatarBody(props: Props) {
+  return (
+    <div
+      className="lo--create-new-avatar--body"
+    >
+      <FormattedMessage
+        id="CreateNewAvatar.label"
+      >
+        {
+          text => (
+            <label
+              className="lo--create-new-avatar--label"
+              htmlFor="newAvatarName"
+            >
+              {text}
+            </label>
+          )
+        }
+      </FormattedMessage>
+      <TextInput
+        className="lo--create-new-avatar--input"
+        handleChange={() => () => {}}
+        id="newAvatarName"
+        initialValue=""
+        max={15}
+        min={5}
+        placeholder=""
+        required
+      />
+      <AutomationSelect
+        className="lo--create-new-avatar--select"
+        defaultValue={lobby.Automation.full}
+        handleChange={() => () => {}}
+      />
+      <Menu
+        className="lo--create-new-avatar--command"
+        itemClassName="lo--create-new-avatar--command--item"
+        items={props.command}
+      />
+    </div>
+  )
+}
