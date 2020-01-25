4a8fb550126ae5acc103df800a78e04b24f1f747
Yudai Udagawa <nimius.rd@gmail.com>
2020-01-25 09:03:09 +0900

    Update schemaPath

--- src/scripts/lobby/constants/SchemaPath.ts
+++ src/scripts/lobby/constants/SchemaPath.ts
@@ -10,6 +10,10 @@ const client2server = {
   changeUserEmail: `${CLIENT2SERVER}/changeUserEmail.json`,
   changeUserName: `${CLIENT2SERVER}/changeUserName.json`,
   changeUserPassword: `${CLIENT2SERVER}/changeUserPassword.json`,
+  createHumanPlayer: `${CLIENT2SERVER}/createHumanPlayer.json`,
+  createOnymousAudience: `${CLIENT2SERVER}/createOnymousAudience.json`,
+  createRobotPlayer: `${CLIENT2SERVER}/createRobotPlayer.json`,
+  deleteAvatar: `${CLIENT2SERVER}/deleteAvatar.json`,
   enterLobby: `${CLIENT2SERVER}/enterLobby.json`,
   getAvatar: `${CLIENT2SERVER}/getAvatar.json`,
   getSettings: `${CLIENT2SERVER}/getSettings.json`,
@@ -20,17 +24,22 @@ const client2server = {
   pong: `${CLIENT2SERVER}/pong.json`,
   ready: `${CLIENT2SERVER}/ready.json`,
   renewAvatarToken: `${CLIENT2SERVER}/renewAvatarToken.json`,
-  selectVillage: `${CLIENT2SERVER}/selectVillage.json`
+  runRobotPlayerInTheBackground: `${CLIENT2SERVER}/runRobotPlayerInTheBackground.json`,
+  selectVillage: `${CLIENT2SERVER}/selectVillage.json`,
+  stopRobotPlayer: `${CLIENT2SERVER}/stopRobotPlayer.json`,
+  updateAvatar: `${CLIENT2SERVER}/updateAvatar.json`
 }
 const SERVER2CLIENT = `${LOBBY_BASE}/server2client`
 const server2client = {
   authorizationRequest: `${SERVER2CLIENT}/authorizationRequest.json`,
   authorizationRequestAcceptedResponse: `${SERVER2CLIENT}/authorizationRequestAcceptedResponse.json`,
   avatar: `${SERVER2CLIENT}/avatar.json`,
+  humanPlayerSelectionPage: `${SERVER2CLIENT}/humanPlayerSelectionPage.json`,
   lobby: `${SERVER2CLIENT}/lobby.json`,
-  newAvatarToken: `${SERVER2CLIENT}/newAvatarToken.json`,
+  onymousAudienceSelectionPage: `${SERVER2CLIENT}/onymousAudienceSelectionPage.json`,
   ping: `${SERVER2CLIENT}/ping.json`,
   played: `${SERVER2CLIENT}/played.json`,
+  robotPlayerSelectionPage: `${SERVER2CLIENT}/robotPlayerSelectionPage.json`,
   searchResult: `${SERVER2CLIENT}/searchResult.json`,
   settings: `${SERVER2CLIENT}/settings.json`,
   waitingPage: `${SERVER2CLIENT}/waitingPage.json`
