c164022d3bfcb8f949d651ca4da4c9899ddacd53
Yudai Udagawa <nimius.rd@gmail.com>
2020-02-01 00:35:55 +0900

    Update story

--- stories/lobby/initialState/selectHumanAvatar.ts
+++ stories/lobby/initialState/selectHumanAvatar.ts
@@ -1,5 +1,7 @@
 import * as ActionTypes from '../../../src/scripts/lobby/constants/ActionTypes'
+import {ImagePath} from '../../../src/scripts/lobby/constants/ImagePath'
 import {ReducerState} from '../../../src/scripts/lobby/reducers'
+import {lobby} from '../types'
 
 export const selectHumanAvatar: Partial<ReducerState> = {
   obfucator: {
@@ -15,12 +17,16 @@ export const selectHumanAvatar: Partial<ReducerState> = {
       byId: {
         '3F2504E0-4F89-11D3-9A0C-0305E82C3300': {
           checked: false,
+          image: ImagePath.Character.a,
           isHover: false,
+          language: lobby.Language.en,
           name: 'avatar1'
         },
         '3F2504E0-4F89-11D3-9A0C-0305E82C3301': {
           checked: false,
+          image: ImagePath.Character.b,
           isHover: false,
+          language: lobby.Language.en,
           name: 'avatar2'
         }
       }
