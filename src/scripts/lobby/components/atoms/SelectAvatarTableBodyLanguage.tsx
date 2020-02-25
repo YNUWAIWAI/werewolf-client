678c3673cd3e1e07b548b05fd17312f79a6b43fe
Yudai Udagawa <nimius.rd@gmail.com>
2020-02-26 02:49:21 +0900

    Add SelectAvatarTableBodyLanguage

---
+++ src/scripts/lobby/components/atoms/SelectAvatarTableBodyLanguage.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react'
+import LanguageSelect from './LanguageSelect'
+import {lobby} from '../../types'
+
+interface Props {
+  readonly additionalClassName: string[]
+  readonly handleChange: (valid: boolean) => (value: lobby.Language) => void
+  readonly handleSelect: () => void
+  readonly language: lobby.Language
+}
+
+export default function SelectAvatarTableBodyLanguage(props: Props) {
+  const handleSelect = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (event.target === event.currentTarget) {
+      props.handleSelect()
+    }
+  }
+
+  return (
+    <div
+      className={`lo--select-avatar--table--body--item language ${props.additionalClassName.join(' ')}`}
+      onClick={handleSelect}
+    >
+      <LanguageSelect
+        className="input"
+        defaultValue={props.language}
+        handleChange={props.handleChange}
+      />
+    </div>
+  )
+}
