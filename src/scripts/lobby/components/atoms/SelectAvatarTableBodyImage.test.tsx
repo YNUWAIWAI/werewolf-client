66295b96ec6fd14039435bf454c411c3bc8df607
Yudai Udagawa <nimius.rd@gmail.com>
2020-02-28 22:14:42 +0900

    Add test

---
+++ src/scripts/lobby/components/atoms/SelectAvatarTableBodyImage.test.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react'
+import SelectAvatarTableBodyImage from './SelectAvatarTableBodyImage'
+import {getMessages} from '../../i18n'
+import {initRenderer} from '../../tools'
+import {lobby} from '../../types'
+
+const {mountWithIntl} = initRenderer(lobby.Language.en, getMessages(lobby.Language.en))
+
+test('render', () => {
+  const handleClick = jest.fn()
+  const handleSelect = jest.fn()
+  const wrapper = mountWithIntl(
+    <SelectAvatarTableBodyImage
+      additionalClassName={[]}
+      handleClick={handleClick}
+      handleSelect={handleSelect}
+      image=""
+    />
+  )
+
+  expect(wrapper.html()).toMatchSnapshot()
+})
+test('handleClick', () => {
+  const handleClick = jest.fn()
+  const handleSelect = jest.fn()
+  const wrapper = mountWithIntl(
+    <SelectAvatarTableBodyImage
+      additionalClassName={[]}
+      handleClick={handleClick}
+      handleSelect={handleSelect}
+      image=""
+    />
+  )
+
+  wrapper.find('.lo--select-avatar--table--body--item. > img').simulate('click')
+  expect(handleClick).toHaveBeenCalledTimes(1)
+})
+test('handleSelect', () => {
+  const handleClick = jest.fn()
+  const handleSelect = jest.fn()
+  const wrapper = mountWithIntl(
+    <SelectAvatarTableBodyImage
+      additionalClassName={[]}
+      handleClick={handleClick}
+      handleSelect={handleSelect}
+      image=""
+    />
+  )
+
+  wrapper.find('.lo--select-avatar--table--body--item.image').simulate('click')
+  expect(handleSelect).toHaveBeenCalledTimes(1)
+})
