9b20b442ce0fce7d919b925a7be4fd8ee8b3ab0a
Yudai Udagawa <nimius.rd@gmail.com>
2020-03-03 02:39:24 +0900

    Update test

--- src/scripts/lobby/reducers/modal.test.ts
+++ src/scripts/lobby/reducers/modal.test.ts
@@ -1,5 +1,6 @@
 import * as ActionTypes from '../constants/ActionTypes'
 import reducer, {initialState} from './modal'
+import {ImagePath} from '../constants/ImagePath'
 import {lobby} from '../types'
 
 test('CONFIRM_KICK_OUT_PLAYER', () => {
@@ -47,5 +48,48 @@ test('SELECT_YES', () => {
   ).toStrictEqual({
     type: lobby.ModalType.confirmation,
     visible: false
+  })
+})
+test('selectAvatarImage/CLOSE_MODAL', () => {
+  expect(
+    reducer(
+      initialState,
+      {
+        type: ActionTypes.SelectAvatarImage.CLOSE_MODAL
+      }
+    )
+  ).toStrictEqual({
+    type: lobby.ModalType.avatarImageSelect,
+    visible: false
+  })
+})
+test('selectHumanAvatar/SHOW_AVATAR_IMAGE_SELECT', () => {
+  expect(
+    reducer(
+      initialState,
+      {
+        image: ImagePath.Character.a,
+        token: '3F2504E0-4F89-11D3-9A0C-0305E82C3300',
+        type: ActionTypes.SelectHumanAvatar.SHOW_AVATAR_IMAGE_SELECT
+      }
+    )
+  ).toStrictEqual({
+    type: lobby.ModalType.avatarImageSelect,
+    visible: true
+  })
+})
+test('selectRobotAvatar/SHOW_AVATAR_IMAGE_SELECT', () => {
+  expect(
+    reducer(
+      initialState,
+      {
+        image: ImagePath.Character.a,
+        token: '3F2504E0-4F89-11D3-9A0C-0305E82C3300',
+        type: ActionTypes.SelectRobotAvatar.SHOW_AVATAR_IMAGE_SELECT
+      }
+    )
+  ).toStrictEqual({
+    type: lobby.ModalType.avatarImageSelect,
+    visible: true
   })
 })
