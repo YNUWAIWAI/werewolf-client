531081de1b19b26174955b72c495a68f38c89401
Yudai Udagawa <nimius.rd@gmail.com>
2020-03-03 08:11:48 +0900

    Update

--- src/scripts/lobby/reducers/obfucator.ts
+++ src/scripts/lobby/reducers/obfucator.ts
@@ -31,17 +31,17 @@ export const initialState: State = {
 const obfucator = (state: State = initialState, action: Action): State => {
   switch (action.type) {
     case ActionTypes.App.CONFIRM_KICK_OUT_PLAYER:
-    case ActionTypes.Confirmation.SELECT_NO:
-    case ActionTypes.Confirmation.SELECT_YES:
-      return {
-        loading: false,
-        visible: false
-      }
     case ActionTypes.SelectHumanAvatar.SHOW_AVATAR_IMAGE_SELECT:
     case ActionTypes.SelectRobotAvatar.SHOW_AVATAR_IMAGE_SELECT:
       return {
         loading: false,
         visible: true
+      }
+    case ActionTypes.Confirmation.SELECT_NO:
+    case ActionTypes.Confirmation.SELECT_YES:
+      return {
+        loading: false,
+        visible: false
       }
     case ActionTypes.SelectAvatarImage.CLOSE_MODAL:
       return {
