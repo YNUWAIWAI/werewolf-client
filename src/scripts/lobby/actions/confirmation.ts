4eff9b2876d243747dfe290a5e6cbd4e9474de5c
Yudai Udagawa <nimius.rd@gmail.com>
2020-03-03 07:33:25 +0900

    Update ActionTypes

--- src/scripts/lobby/actions/confirmation.ts
+++ src/scripts/lobby/actions/confirmation.ts
@@ -3,18 +3,18 @@ import * as ActionTypes from '../constants/ActionTypes'
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export declare namespace Confirmation {
   type SelectNo = {
-    type: ActionTypes.App.SELECT_NO
+    type: ActionTypes.Confirmation.SELECT_NO
   }
   type SelectYes = {
-    type: ActionTypes.App.SELECT_YES
+    type: ActionTypes.Confirmation.SELECT_YES
   }
 }
 
 const selectNo = (): Confirmation.SelectNo => ({
-  type: ActionTypes.App.SELECT_NO
+  type: ActionTypes.Confirmation.SELECT_NO
 })
 const selectYes = (): Confirmation.SelectYes => ({
-  type: ActionTypes.App.SELECT_YES
+  type: ActionTypes.Confirmation.SELECT_YES
 })
 
 export const confirmation = {
