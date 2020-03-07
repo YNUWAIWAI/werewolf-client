a91052e99c89a627b2e1c1d831f1d8db7631fd54
Yudai Udagawa <nimius.rd@gmail.com>
2020-03-08 03:25:57 +0900

    Add test

---
+++ src/scripts/lobby/components/atoms/MuteButton.test.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import Mute from './svg/Mute'
+import MuteButton from './MuteButton'
+import Unmute from './svg/Unmute'
+import {shallow} from 'enzyme'
+
+describe('render', () => {
+  test('muted', () => {
+    const handleClick = jest.fn()
+    const wrapper = shallow(
+      <MuteButton
+        handleClick={handleClick}
+        muted
+      />
+    )
+
+    expect(wrapper.find(Mute).exists()).toBe(true)
+  })
+  test('unmuted', () => {
+    const handleClick = jest.fn()
+    const wrapper = shallow(
+      <MuteButton
+        handleClick={handleClick}
+        muted={false}
+      />
+    )
+
+    expect(wrapper.find(Unmute).exists()).toBe(true)
+  })
+})
