30ed0956d7e7e04cedf28ac678da69421b0222bb
Yudai Udagawa <nimius.rd@gmail.com>
2020-03-17 22:32:41 +0900

    Update story

--- stories/lobby/initialState/waitingPage.ts
+++ stories/lobby/initialState/waitingPage.ts
@@ -8,6 +8,87 @@ export const waitingPage: Partial<ReducerState> = {
     loading: false,
     visible: false
   },
+  ping: {
+    id: '3F2504E0-4F89-11D3-9A0C-0305E82C3300',
+    myToken: '3F2504E0-4F89-11D3-9A0C-0305E82C3302',
+    results: [
+      {
+        ping: '99.999',
+        status: lobby.PingStatus.danger,
+        token: '3F2504E0-4F89-11D3-9A0C-0305E82C3301'
+      },
+      {
+        ping: '00.999',
+        status: lobby.PingStatus.safe,
+        token: '3F2504E0-4F89-11D3-9A0C-0305E82C3302'
+      },
+      {
+        ping: '02.999',
+        status: lobby.PingStatus.warning,
+        token: '3F2504E0-4F89-11D3-9A0C-0305E82C3303'
+      },
+      {
+        ping: '00.000',
+        status: lobby.PingStatus.safe,
+        token: '3F2504E0-4F89-11D3-9A0C-0305E82C3304'
+      },
+      {
+        ping: '99.999',
+        status: lobby.PingStatus.danger,
+        token: '3F2504E0-4F89-11D3-9A0C-0305E82C3305'
+      },
+      {
+        ping: '99.999',
+        status: lobby.PingStatus.danger,
+        token: '3F2504E0-4F89-11D3-9A0C-0305E82C3306'
+      },
+      {
+        ping: '99.999',
+        status: lobby.PingStatus.danger,
+        token: '3F2504E0-4F89-11D3-9A0C-0305E82C3307'
+      },
+      {
+        ping: '99.999',
+        status: lobby.PingStatus.danger,
+        token: '3F2504E0-4F89-11D3-9A0C-0305E82C3308'
+      },
+      {
+        ping: '99.999',
+        status: lobby.PingStatus.danger,
+        token: '3F2504E0-4F89-11D3-9A0C-0305E82C3309'
+      },
+      {
+        ping: '99.999',
+        status: lobby.PingStatus.danger,
+        token: '3F2504E0-4F89-11D3-9A0C-0305E82C3310'
+      },
+      {
+        ping: '99.999',
+        status: lobby.PingStatus.danger,
+        token: '3F2504E0-4F89-11D3-9A0C-0305E82C3311'
+      },
+      {
+        ping: '99.999',
+        status: lobby.PingStatus.danger,
+        token: '3F2504E0-4F89-11D3-9A0C-0305E82C3312'
+      },
+      {
+        ping: '99.999',
+        status: lobby.PingStatus.danger,
+        token: '3F2504E0-4F89-11D3-9A0C-0305E82C3313'
+      },
+      {
+        ping: '99.999',
+        status: lobby.PingStatus.danger,
+        token: '3F2504E0-4F89-11D3-9A0C-0305E82C3314'
+      },
+      {
+        ping: '99.999',
+        status: lobby.PingStatus.danger,
+        token: '3F2504E0-4F89-11D3-9A0C-0305E82C3315'
+      }
+    ]
+  },
   waitingForPlayers: {
     isPlayer: true,
     kickOutToken: '',
