3435ed6eb25a076dbb68c663bcfb43de25f6d17a
Yudai Udagawa <nimius.rd@gmail.com>
2020-04-21 04:09:18 +0900

    Add actions

---
+++ src/scripts/lobby/actions/createNewRobotAvatar.ts
@@ -0,0 +1,37 @@
+import * as ActionTypes from '../constants/ActionTypes'
+import {lobby} from '../types'
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export declare namespace CreateNewRobotAvatar {
+  type ChangeAvatarLangauge = {
+    language: lobby.Language
+    type: ActionTypes.CreateNewRobotAvatar.CHANGE_AVATAR_LANGUAGE
+  }
+  type ChangeAvatarName = {
+    name: string
+    type: ActionTypes.CreateNewRobotAvatar.CHANGE_AVATAR_NAME
+  }
+  type ShowAvatarImageSelect = {
+    image: string
+    type: ActionTypes.CreateNewRobotAvatar.SHOW_AVATAR_IMAGE_SELECT
+  }
+}
+
+const changeAvatarLanguage = (language: lobby.Language): CreateNewRobotAvatar.ChangeAvatarLangauge => ({
+  language,
+  type: ActionTypes.CreateNewRobotAvatar.CHANGE_AVATAR_LANGUAGE
+})
+const changeAvatarName = (name: string): CreateNewRobotAvatar.ChangeAvatarName => ({
+  name,
+  type: ActionTypes.CreateNewRobotAvatar.CHANGE_AVATAR_NAME
+})
+const showAvatarImageSelect = (image: string): CreateNewRobotAvatar.ShowAvatarImageSelect => ({
+  image,
+  type: ActionTypes.CreateNewRobotAvatar.SHOW_AVATAR_IMAGE_SELECT
+})
+
+export const createNewHumanAvatar = {
+  changeAvatarLanguage,
+  changeAvatarName,
+  showAvatarImageSelect
+}
