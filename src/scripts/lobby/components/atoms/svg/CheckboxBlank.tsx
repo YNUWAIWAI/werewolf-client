faf169888f2b157133dfa81309ab323b2ce18ee2
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-18 01:13:27 +0900

    Update svg

--- src/scripts/lobby/components/atoms/svg/CheckboxBlank.tsx
+++ src/scripts/lobby/components/atoms/svg/CheckboxBlank.tsx
@@ -1,13 +1,9 @@
 import * as React from 'react'
 
-interface Props {
-  readonly className?: string
-}
-
-export default function CheckboxBlank(props: Props) {
+export default function CheckboxBlank() {
   return (
     <svg
-      className={`${props.className || ''} chackbox blank`}
+      className="checkbox blank"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
