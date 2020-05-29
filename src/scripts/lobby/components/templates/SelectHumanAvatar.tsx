d4375f6f0642129636822ab80bb09c08b9bbff09
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-30 03:48:24 +0900

    Update SelectHumanAvatar

--- src/scripts/lobby/components/templates/SelectHumanAvatar.tsx
+++ src/scripts/lobby/components/templates/SelectHumanAvatar.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
-import AsideContent from '../atoms/AsideContent'
-import Header from '../atoms/Header'
-import MainContent from '../atoms/MainContent'
+import {AsideContent} from '../atoms/AsideContent'
+import {Header} from '../atoms/Header'
+import {MainContent} from '../atoms/MainContent'
 import Menu from '../../containers/MenuContainer'
 import {MenuItemProps as MenuItem} from '../organisms/Menu'
-import SelectHumanAvatarBox from '../organisms/SelectHumanAvatarBox'
+import {SelectHumanAvatarBox} from '../organisms/SelectHumanAvatarBox'
 
 export interface StateProps {
   readonly command: MenuItem[]
@@ -12,22 +12,21 @@ export interface StateProps {
 }
 export type Props = StateProps
 
-export default function SelectHumanAvatar(props: Props) {
-  return (
-    <div className="lo--grid">
-      <Header id="Header.selectHumanAvatar" />
-      <MainContent>
-        <SelectHumanAvatarBox
-          command={props.command}
-        />
-      </MainContent>
-      <AsideContent>
-        <Menu
-          className="lo--compact-menu"
-          itemClassName="lo--compact-menu--item"
-          items={props.menuItems}
-        />
-      </AsideContent>
-    </div>
-  )
-}
+export const SelectHumanAvatar: React.FC<Props> = props => (
+  <div className="lo--grid">
+    <Header id="Header.selectHumanAvatar" />
+    <MainContent>
+      <SelectHumanAvatarBox
+        command={props.command}
+      />
+    </MainContent>
+    <AsideContent>
+      <Menu
+        className="lo--compact-menu"
+        itemClassName="lo--compact-menu--item"
+        items={props.menuItems}
+      />
+    </AsideContent>
+  </div>
+)
+SelectHumanAvatar.displayName = 'SelectHumanAvatar'
