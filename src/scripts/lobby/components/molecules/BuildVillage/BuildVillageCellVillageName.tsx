eb5ebed0c4826f635ae7acfbd13ded05a5f3982d
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-30 02:37:03 +0900

    Update BuildVillageCellVillageName

--- src/scripts/lobby/components/molecules/BuildVillage/BuildVillageCellVillageName.tsx
+++ src/scripts/lobby/components/molecules/BuildVillage/BuildVillageCellVillageName.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import {FormattedMessage} from 'react-intl'
-import TextInput from '../../atoms/TextInput'
+import {TextInput} from '../../atoms/TextInput'
 
 interface Props {
   readonly handleValueChange: (valid: boolean) => (value: string) => void
@@ -15,41 +15,40 @@ const values = {
   min: 5
 }
 
-export default function BuildVillageCellVillageName(props: Props) {
-  return (
-    <>
-      <div className="lo--village--item--prop village-name">
+export const BuildVillageCellVillageName: React.FC<Props> = props => (
+  <>
+    <div className="lo--village--item--prop village-name">
+      <FormattedMessage
+        id="BuildVillage.label(villageName)"
+      />
+    </div>
+    {
+      props.isFiexdAvatar ?
         <FormattedMessage
-          id="BuildVillage.label(villageName)"
-        />
-      </div>
-      {
-        props.isFiexdAvatar ?
+          id="BuildVillage.placeholder"
+          values={values}
+        >
+          {
+            text => (
+              <TextInput
+                className={`lo--village--item--val village-name ${props.valid ? '' : 'invalid'}`}
+                handleChange={props.handleValueChange}
+                initialValue={props.value}
+                max={values.max}
+                min={values.min}
+                navigatable={props.navigatable}
+                placeholder={typeof text === 'string' ? text : ''}
+                required
+              />
+            )
+          }
+        </FormattedMessage> :
+        <div className="lo--village--item--val village-name">
           <FormattedMessage
-            id="BuildVillage.placeholder"
-            values={values}
-          >
-            {
-              text => (
-                <TextInput
-                  className={`lo--village--item--val village-name ${props.valid ? '' : 'invalid'}`}
-                  handleChange={props.handleValueChange}
-                  initialValue={props.value}
-                  max={values.max}
-                  min={values.min}
-                  navigatable={props.navigatable}
-                  placeholder={typeof text === 'string' ? text : ''}
-                  required
-                />
-              )
-            }
-          </FormattedMessage> :
-          <div className="lo--village--item--val village-name">
-            <FormattedMessage
-              id={`BuildVillage.villageName.anonymous(${props.value})`}
-            />
-          </div>
-      }
-    </>
-  )
-}
+            id={`BuildVillage.villageName.anonymous(${props.value})`}
+          />
+        </div>
+    }
+  </>
+)
+BuildVillageCellVillageName.displayName = 'BuildVillageCellVillageName'
