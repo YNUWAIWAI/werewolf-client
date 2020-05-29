70489480ff42204824b3d5e928a77d6d2856aab4
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-30 07:48:17 +0900

    Update ResultCellSummary

--- src/scripts/village/components/atoms/ResultCellSummary.tsx
+++ src/scripts/village/components/atoms/ResultCellSummary.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import {FormattedMessage} from 'react-intl'
-import TeamIcon from '../atoms/TeamIcon'
+import {TeamIcon} from '../atoms/TeamIcon'
 import {village} from '../../types'
 
 interface Props {
@@ -18,30 +18,29 @@ const getId = (myTeam: village.Team | '', winnerTeam: village.Team) => {
   return 'Result.summary.description(player, lose)'
 }
 
-export default function ResultCellSummary(props: Props) {
-  return (
-    <div
-      className="vi--result--cell--summary"
-    >
-      <FormattedMessage
-        id={getId(props.myTeam, props.winnerTeam)}
-        values={
-          {
-            myTeam:
-              <TeamIcon
-                class="vi--result--cell--summary--team"
-                key="myTeam"
-                team={props.myTeam || ''}
-              />,
-            winnerTeam:
-              <TeamIcon
-                class="vi--result--cell--summary--team"
-                key="winnerTeam"
-                team={props.winnerTeam}
-              />
-          }
+export const ResultCellSummary: React.FC<Props> = props => (
+  <div
+    className="vi--result--cell--summary"
+  >
+    <FormattedMessage
+      id={getId(props.myTeam, props.winnerTeam)}
+      values={
+        {
+          myTeam:
+            <TeamIcon
+              class="vi--result--cell--summary--team"
+              key="myTeam"
+              team={props.myTeam || ''}
+            />,
+          winnerTeam:
+            <TeamIcon
+              class="vi--result--cell--summary--team"
+              key="winnerTeam"
+              team={props.winnerTeam}
+            />
         }
-      />
-    </div>
-  )
-}
+      }
+    />
+  </div>
+)
+ResultCellSummary.displayName = 'ResultCellSummary'
