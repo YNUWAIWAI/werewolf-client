3d817eed9ba3bdd0a703e7d742602ec271d4515d
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-29 15:38:16 +0900

    Add SettingsBoxBucketUser

---
+++ src/scripts/lobby/components/molecules/SettingsBox/SettingsBoxBucketUser.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import {FormattedMessage} from 'react-intl'
+import {SettingsBoxBucketUserEmail} from '../../atoms/SettingsBoxBucket/SettingsBoxBucketUserEmail'
+import {SettingsBoxBucketUserName} from '../../atoms/SettingsBoxBucket/SettingsBoxBucketUserName'
+import {SettingsBoxBucketUserPassword} from '../../atoms/SettingsBoxBucket/SettingsBoxBucketUserPassword'
+
+export interface Props {
+  readonly handleChangeUserEmail: (value: string) => void
+  readonly handleChangeUserName: (value: string) => void
+  readonly handleChangeUserPassword: (value: string) => void
+  readonly initialUserEmail: string
+  readonly initialUserName: string
+  readonly navigatable: boolean
+}
+
+export const SettingsBoxBucketUser: React.FC<Props> = props => (
+  <div className="lo--settings--bucket">
+    <div className="lo--settings--bucket--head">
+      <FormattedMessage
+        id="Settings.head(user)"
+      />
+    </div>
+    <SettingsBoxBucketUserName
+      handleSubmit={props.handleChangeUserName}
+      initialValue={props.initialUserName}
+      navigatable={props.navigatable}
+    />
+    <SettingsBoxBucketUserEmail
+      handleSubmit={props.handleChangeUserEmail}
+      initialValue={props.initialUserEmail}
+      navigatable={props.navigatable}
+    />
+    <SettingsBoxBucketUserPassword
+      handleSubmit={props.handleChangeUserPassword}
+      navigatable={props.navigatable}
+    />
+  </div>
+)
