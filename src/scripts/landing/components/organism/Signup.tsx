8997893f136370ace0258ca23e330ff8f7ce1bf7
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-29 17:35:20 +0900

    Update Signup

--- src/scripts/landing/components/organism/Signup.tsx
+++ src/scripts/landing/components/organism/Signup.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react'
-import Confirmation from '../atoms/Confirmation'
-import Csrf from '../atoms/Csrf'
-import ErrorMessage from '../atoms/ErrorMessage'
-import SubmitButton from '../atoms/SubmitButton'
-import TextField from '../molecules/TextField'
+import {Confirmation} from '../atoms/Confirmation'
+import {Csrf} from '../atoms/Csrf'
+import {ErrorMessage} from '../atoms/ErrorMessage'
+import {SubmitButton} from '../atoms/SubmitButton'
+import {TextField} from '../molecules/TextField'
 
 interface Props {
   action: string
@@ -11,39 +11,38 @@ interface Props {
   error: boolean
 }
 
-export default function Signup(props: Props) {
-  return (
-    <form
-      action={props.action}
-      className="la--form"
-      method="POST"
-    >
-      <ErrorMessage
-        type="signup"
-        visible={props.error}
-      />
-      <TextField
-        autoFocus
-        name="name"
-        type="signup"
-      />
-      <TextField
-        autoFocus={false}
-        name="email"
-        type="signup"
-      />
-      <TextField
-        autoFocus={false}
-        name="password"
-        type="signup"
-      />
-      <Confirmation />
-      <SubmitButton
-        type="signup"
-      />
-      <Csrf
-        token={props.csrfToken}
-      />
-    </form>
-  )
-}
+export const Signup: React.FC<Props> = props => (
+  <form
+    action={props.action}
+    className="la--form"
+    method="POST"
+  >
+    <ErrorMessage
+      type="signup"
+      visible={props.error}
+    />
+    <TextField
+      autoFocus
+      name="name"
+      type="signup"
+    />
+    <TextField
+      autoFocus={false}
+      name="email"
+      type="signup"
+    />
+    <TextField
+      autoFocus={false}
+      name="password"
+      type="signup"
+    />
+    <Confirmation />
+    <SubmitButton
+      type="signup"
+    />
+    <Csrf
+      token={props.csrfToken}
+    />
+  </form>
+)
+Signup.displayName = 'Signup'
