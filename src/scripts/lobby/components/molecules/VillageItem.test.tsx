acb66cddc0439f82c0e2fcf51e85654c7d9cbd54
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-30 01:57:29 +0900

    Update VillageItem

--- src/scripts/lobby/components/molecules/VillageItem.test.tsx
+++ src/scripts/lobby/components/molecules/VillageItem.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import VillageItem from './VillageItem'
+import {VillageItem} from './VillageItem'
 import {getMessages} from '../../i18n'
 import {initRenderer} from '../../tools'
 import {lobby} from '../../types'
--- src/scripts/lobby/components/molecules/VillageItem.tsx
+++ src/scripts/lobby/components/molecules/VillageItem.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import {FormattedMessage} from 'react-intl'
-import Member from './Member'
+import {Member} from './Member'
 import {lobby} from '../../types'
 
 interface Props extends Readonly<lobby.Village> {
@@ -8,67 +8,66 @@ interface Props extends Readonly<lobby.Village> {
   readonly isPlayer: boolean
 }
 
-export default function VillageItem(props: Props) {
-  return (
-    <div
-      className={`lo--village--item ${typeof props.handleClick === 'function' ? 'clickable' : ''}`}
-      onClick={props.handleClick}
-    >
-      <div className="lo--village--item--prop village-name">
-        <FormattedMessage
-          id="VillageItem.label(villageName)"
-        />
-      </div>
-      <div className="lo--village--item--val village-name">
-        {props.name}
-      </div>
-      <div className="lo--village--item--val id-for-searching">
-        {Number.isInteger(props.idForSearching) ? `#${props.idForSearching}` : '####'}
-      </div>
-      <div className="lo--village--item--prop host-name">
-        <FormattedMessage
-          id="VillageItem.label(hostName)"
-        />
-      </div>
-      {
-        props.avatar === 'fixed' ?
-          <div className="lo--village--item--val host-name">
-            {props.hostPlayer.name}
-          </div> :
-          <div className="lo--village--item--val host-name">
-            <FormattedMessage
-              id="VillageItem.hostName.anonymous"
-            />
-          </div>
-      }
-      <div className="lo--village--item--prop setup">
-        <FormattedMessage
-          id="VillageItem.label(setup)"
-        />
-      </div>
-      <div className="lo--village--item--val setup1">
-        <FormattedMessage
-          id={`VillageItem.val(numOfPlayers, ${props.isPlayer ? 'player' : 'audience'})`}
-          values={{
-            current: props.playerSetting.current,
-            number: props.playerSetting.number
-          }}
-        />
-      </div>
-      <div className="lo--village--item--val setup2">
-        <FormattedMessage
-          id={`VillageItem.val(avatar, ${props.avatar})`}
-        />
-      </div>
-      <Member human={props.playerSetting.human} robot={props.playerSetting.robot} role={props.roleSetting} />
-      <div className="lo--village--item--prop comment">
-        <FormattedMessage
-          id="VillageItem.label(comment)"
-        />
-      </div>
-      <div className="lo--village--item--val comment">
-        {props.comment}
-      </div>
+export const VillageItem: React.FC<Props> = props => (
+  <div
+    className={`lo--village--item ${typeof props.handleClick === 'function' ? 'clickable' : ''}`}
+    onClick={props.handleClick}
+  >
+    <div className="lo--village--item--prop village-name">
+      <FormattedMessage
+        id="VillageItem.label(villageName)"
+      />
     </div>
-  )
-}
+    <div className="lo--village--item--val village-name">
+      {props.name}
+    </div>
+    <div className="lo--village--item--val id-for-searching">
+      {Number.isInteger(props.idForSearching) ? `#${props.idForSearching}` : '####'}
+    </div>
+    <div className="lo--village--item--prop host-name">
+      <FormattedMessage
+        id="VillageItem.label(hostName)"
+      />
+    </div>
+    {
+      props.avatar === 'fixed' ?
+        <div className="lo--village--item--val host-name">
+          {props.hostPlayer.name}
+        </div> :
+        <div className="lo--village--item--val host-name">
+          <FormattedMessage
+            id="VillageItem.hostName.anonymous"
+          />
+        </div>
+    }
+    <div className="lo--village--item--prop setup">
+      <FormattedMessage
+        id="VillageItem.label(setup)"
+      />
+    </div>
+    <div className="lo--village--item--val setup1">
+      <FormattedMessage
+        id={`VillageItem.val(numOfPlayers, ${props.isPlayer ? 'player' : 'audience'})`}
+        values={{
+          current: props.playerSetting.current,
+          number: props.playerSetting.number
+        }}
+      />
+    </div>
+    <div className="lo--village--item--val setup2">
+      <FormattedMessage
+        id={`VillageItem.val(avatar, ${props.avatar})`}
+      />
+    </div>
+    <Member human={props.playerSetting.human} robot={props.playerSetting.robot} role={props.roleSetting} />
+    <div className="lo--village--item--prop comment">
+      <FormattedMessage
+        id="VillageItem.label(comment)"
+      />
+    </div>
+    <div className="lo--village--item--val comment">
+      {props.comment}
+    </div>
+  </div>
+)
+VillageItem.displayName = 'VillageItem'
