04a68c33325297cc5c53ac5d961c14bb11ac7d95
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-30 03:33:33 +0900

    Update LobbyForHumanPlayer

--- src/scripts/lobby/components/templates/LobbyForHumanPlayer.test.tsx
+++ src/scripts/lobby/components/templates/LobbyForHumanPlayer.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
-import AsideContent from '../atoms/AsideContent'
-import Avatar from '../atoms/Avatar'
-import Header from '../atoms/Header'
-import LobbyForHumanPlayer from './LobbyForHumanPlayer'
-import MainContent from '../atoms/MainContent'
+import {AsideContent} from '../atoms/AsideContent'
+import {Avatar} from '../atoms/Avatar'
+import {Header} from '../atoms/Header'
+import {LobbyForHumanPlayer} from './LobbyForHumanPlayer'
+import {MainContent} from '../atoms/MainContent'
 import Menu from '../../containers/MenuContainer'
-import VillageList from '../organisms/VillageList'
+import {VillageList} from '../organisms/VillageList'
 import {shallow} from 'enzyme'
 
 test('<LobbyForHumanPlayer />', () => {
