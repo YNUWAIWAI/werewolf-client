85693d3f4b1697122cea245570903b76f400167a
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-30 00:50:51 +0900

    Update SelectAvatarTableBodyTest

--- src/scripts/lobby/components/atoms/SelectAvatarTable/SelectAvatarTableBodyTest.tsx
+++ src/scripts/lobby/components/atoms/SelectAvatarTable/SelectAvatarTableBodyTest.tsx
@@ -8,15 +8,14 @@ interface Props {
   readonly test: lobby.TestStatus
 }
 
-export default function SelectAvatarTableBodyTest(props: Props) {
-  return (
-    <div
-      className={`lo--select-avatar--table--body--item test ${props.additionalClassName.join(' ')}`}
-      onClick={props.handleSelect}
-    >
-      <FormattedMessage
-        id={`SelectAvatarTableBodyTest.test(${props.test})`}
-      />
-    </div>
-  )
-}
+export const SelectAvatarTableBodyTest: React.FC<Props> = props => (
+  <div
+    className={`lo--select-avatar--table--body--item test ${props.additionalClassName.join(' ')}`}
+    onClick={props.handleSelect}
+  >
+    <FormattedMessage
+      id={`SelectAvatarTableBodyTest.test(${props.test})`}
+    />
+  </div>
+)
+SelectAvatarTableBodyTest.displayName = 'SelectAvatarTableBodyTest'
