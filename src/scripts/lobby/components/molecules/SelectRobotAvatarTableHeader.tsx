c02222a6dc84a4ab6f9002b63b04b644133c0556
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-30 01:56:06 +0900

    Update SelectRobotAvatarTableHeader

--- src/scripts/lobby/components/molecules/SelectRobotAvatarTableHeader.tsx
+++ src/scripts/lobby/components/molecules/SelectRobotAvatarTableHeader.tsx
@@ -1,18 +1,17 @@
 import * as React from 'react'
-import SelectAvatarTableHeaderItem from '../atoms/SelectAvatarTable/SelectAvatarTableHeaderItem'
+import {SelectAvatarTableHeaderItem} from '../atoms/SelectAvatarTable/SelectAvatarTableHeaderItem'
 
-export default function SelectRobotAvatarTableHeader() {
-  return (
-    <>
-      <SelectAvatarTableHeaderItem type="check-box" />
-      <SelectAvatarTableHeaderItem type="avatar-name" />
-      <SelectAvatarTableHeaderItem type="language" />
-      <SelectAvatarTableHeaderItem type="image" />
-      <SelectAvatarTableHeaderItem type="status" />
-      <SelectAvatarTableHeaderItem type="authorized" />
-      <SelectAvatarTableHeaderItem type="test" />
-      <SelectAvatarTableHeaderItem type="automation" />
-      <SelectAvatarTableHeaderItem type="access-token" />
-    </>
-  )
-}
+export const SelectRobotAvatarTableHeader: React.FC<{}> = () => (
+  <>
+    <SelectAvatarTableHeaderItem type="check-box" />
+    <SelectAvatarTableHeaderItem type="avatar-name" />
+    <SelectAvatarTableHeaderItem type="language" />
+    <SelectAvatarTableHeaderItem type="image" />
+    <SelectAvatarTableHeaderItem type="status" />
+    <SelectAvatarTableHeaderItem type="authorized" />
+    <SelectAvatarTableHeaderItem type="test" />
+    <SelectAvatarTableHeaderItem type="automation" />
+    <SelectAvatarTableHeaderItem type="access-token" />
+  </>
+)
+SelectRobotAvatarTableHeader.displayName = 'SelectRobotAvatarTableHeader'
