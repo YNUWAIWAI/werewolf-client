a63b895f2a234cbd3eb92c728c5046fcd086caa9
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-30 02:07:00 +0900

    Update AdvancedSearchCellComment

--- src/scripts/lobby/components/molecules/AdvancedSearch/AdvancedSearchCellComment.tsx
+++ src/scripts/lobby/components/molecules/AdvancedSearch/AdvancedSearchCellComment.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import AdvancedSearchProp from '../../atoms/AdvancedSearchProp'
+import {AdvancedSearchProp} from '../../atoms/AdvancedSearchProp'
 import {FormattedMessage} from 'react-intl'
-import TextareaInput from '../../atoms/TextareaInput'
+import {TextareaInput} from '../../atoms/TextareaInput'
 
 interface Props {
   readonly checked: boolean
@@ -16,36 +16,35 @@ const values = {
   min: 0
 }
 
-export default function AdvancedSearchCellComment(props: Props) {
-  return (
-    <>
-      <AdvancedSearchProp
-        checked={props.checked}
-        handleChange={props.handleCheckboxChange}
-        name="comment"
-        navigatable={props.navigatable}
-        valid={props.valid}
-      />
-      <FormattedMessage
-        id="AdvancedSearch.placeholder(comment)"
-        values={values}
-      >
-        {
-          text => (
-            <TextareaInput
-              className="lo--advanced-search--input"
-              handleChange={props.handleValueChange}
-              initialValue=""
-              max={values.max}
-              min={values.min}
-              navigatable={props.navigatable}
-              placeholder={typeof text === 'string' ? text : ''}
-              required={false}
-              rows={3}
-            />
-          )
-        }
-      </FormattedMessage>
-    </>
-  )
-}
+export const AdvancedSearchCellComment: React.FC<Props> = props => (
+  <>
+    <AdvancedSearchProp
+      checked={props.checked}
+      handleChange={props.handleCheckboxChange}
+      name="comment"
+      navigatable={props.navigatable}
+      valid={props.valid}
+    />
+    <FormattedMessage
+      id="AdvancedSearch.placeholder(comment)"
+      values={values}
+    >
+      {
+        text => (
+          <TextareaInput
+            className="lo--advanced-search--input"
+            handleChange={props.handleValueChange}
+            initialValue=""
+            max={values.max}
+            min={values.min}
+            navigatable={props.navigatable}
+            placeholder={typeof text === 'string' ? text : ''}
+            required={false}
+            rows={3}
+          />
+        )
+      }
+    </FormattedMessage>
+  </>
+)
+AdvancedSearchCellComment.displayName = 'AdvancedSearchCellComment'
