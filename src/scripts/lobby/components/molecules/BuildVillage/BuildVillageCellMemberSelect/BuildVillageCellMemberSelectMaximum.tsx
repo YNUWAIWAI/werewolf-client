e546a641f7b3ecb9168bcf1beeeac3fe85400ce8
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-30 02:37:52 +0900

    Update BuildVillageCellMemberSelectMaximum

--- src/scripts/lobby/components/molecules/BuildVillage/BuildVillageCellMemberSelect/BuildVillageCellMemberSelectMaximum.tsx
+++ src/scripts/lobby/components/molecules/BuildVillage/BuildVillageCellMemberSelect/BuildVillageCellMemberSelectMaximum.tsx
@@ -5,15 +5,14 @@ interface Props {
   readonly numberOfHumans: number
 }
 
-export default function BuildVillageCellMemberSelectMaximum(props: Props) {
-  return (
-    <div className="lo--village--item--member-select--human">
-      <FormattedMessage
-        id="MemberSelect.max"
-        values={{
-          num: props.numberOfHumans
-        }}
-      />
-    </div>
-  )
-}
+export const BuildVillageCellMemberSelectMaximum: React.FC<Props> = props => (
+  <div className="lo--village--item--member-select--human">
+    <FormattedMessage
+      id="MemberSelect.max"
+      values={{
+        num: props.numberOfHumans
+      }}
+    />
+  </div>
+)
+BuildVillageCellMemberSelectMaximum.displayName = 'BuildVillageCellMemberSelectMaximum'
