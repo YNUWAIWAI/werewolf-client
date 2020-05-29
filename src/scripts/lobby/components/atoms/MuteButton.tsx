41870234ca55efd4a5b16f2203d6d37e7a6df1cc
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-29 17:56:36 +0900

    Update MuteButton

--- src/scripts/lobby/components/atoms/MuteButton.tsx
+++ src/scripts/lobby/components/atoms/MuteButton.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import Mute from './svg/Mute'
-import Unmute from './svg/Unmute'
+import {Mute} from './svg/Mute'
+import {Unmute} from './svg/Unmute'
 
 export interface StateProps {
   readonly muted: boolean
@@ -10,17 +10,16 @@ export interface DispatchProps {
 }
 export type Props = StateProps & DispatchProps
 
-export default function MuteButton(props: Props) {
-  return (
-    <div
-      className="lo--mute-button"
-      onClick={() => props.handleClick(!props.muted)}
-    >
-      {
-        props.muted ?
-          <Mute /> :
-          <Unmute />
-      }
-    </div>
-  )
-}
+export const MuteButton: React.FC<Props> = props => (
+  <div
+    className="lo--mute-button"
+    onClick={() => props.handleClick(!props.muted)}
+  >
+    {
+      props.muted ?
+        <Mute /> :
+        <Unmute />
+    }
+  </div>
+)
+MuteButton.displayName = 'MuteButton'
