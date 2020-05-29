2b40b508a7d92ab9ec158bff855793e3c15141b8
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-30 02:39:35 +0900

    Update BuildVillageCellMemberSelectRole

--- src/scripts/lobby/components/molecules/BuildVillage/BuildVillageCellMemberSelect/BuildVillageCellMemberSelectRole.tsx
+++ src/scripts/lobby/components/molecules/BuildVillage/BuildVillageCellMemberSelect/BuildVillageCellMemberSelectRole.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import MemberRoleList from '../../../molecules/MemberRoleList'
+import {MemberRoleList} from '../../../molecules/MemberRoleList'
 import {lobby} from '../../../../types'
 
 type RoleSetting = Readonly<lobby.Payload$BuildVillage['roleSetting']>
@@ -10,32 +10,31 @@ interface Props {
   readonly role: RoleSetting
 }
 
-export default function BuildVillageCellMemberSelectRole(props: Props) {
-  return (
-    <label
-      className={`lo--village--item--member-select--role ${props.label}`}
-      htmlFor={`member${props.label}`}
+export const BuildVillageCellMemberSelectRole: React.FC<Props> = props => (
+  <label
+    className={`lo--village--item--member-select--role ${props.label}`}
+    htmlFor={`member${props.label}`}
+  >
+    <div
+      className="lo--village--item--member-select--role--radio"
     >
-      <div
-        className="lo--village--item--member-select--role--radio"
-      >
-        <input
-          defaultChecked={props.defaultChecked}
-          id={`member${props.label}`}
-          name="member"
-          onChange={props.handleMemberChange}
-          type="radio"
-        />
-      </div>
-      <div
-        className="lo--village--item--member-select--role--text"
-      >
-        {props.label}
-      </div>
-      <MemberRoleList
-        className="lo--village--item--member-select--role--list"
-        role={props.role}
+      <input
+        defaultChecked={props.defaultChecked}
+        id={`member${props.label}`}
+        name="member"
+        onChange={props.handleMemberChange}
+        type="radio"
       />
-    </label>
-  )
-}
+    </div>
+    <div
+      className="lo--village--item--member-select--role--text"
+    >
+      {props.label}
+    </div>
+    <MemberRoleList
+      className="lo--village--item--member-select--role--list"
+      role={props.role}
+    />
+  </label>
+)
+BuildVillageCellMemberSelectRole.displayName = 'BuildVillageCellMemberSelectRole'
