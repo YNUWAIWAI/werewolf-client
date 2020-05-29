1f08f516180e9ece7d2a8cac918d35386199d72c
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-30 02:40:37 +0900

    Update CreateNewAvatarSupportBoardColumnHeader

--- src/scripts/lobby/components/molecules/CreateNewAvatar/SupportBoard/CreateNewAvatarSupportBoardColumnHeader.tsx
+++ src/scripts/lobby/components/molecules/CreateNewAvatar/SupportBoard/CreateNewAvatarSupportBoardColumnHeader.tsx
@@ -6,31 +6,30 @@ interface Props {
   readonly className: string
 }
 
-export default function CreateNewAvatarSupportBoardColumnHeader(props: Props) {
-  return (
-    <>
-      <div
-        className={props.className}
-      >
-        <FormattedMessage
-          id="CreateNewAvatarSupportBoardColumnHeader.numberOfPlayers"
-        />
-      </div>
-      <div
-        className={props.className}
-      >
-        {lobby.Member.A}
-      </div>
-      <div
-        className={props.className}
-      >
-        {lobby.Member.B}
-      </div>
-      <div
-        className={props.className}
-      >
-        {lobby.Member.C}
-      </div>
-    </>
-  )
-}
+export const CreateNewAvatarSupportBoardColumnHeader: React.FC<Props> = props => (
+  <>
+    <div
+      className={props.className}
+    >
+      <FormattedMessage
+        id="CreateNewAvatarSupportBoardColumnHeader.numberOfPlayers"
+      />
+    </div>
+    <div
+      className={props.className}
+    >
+      {lobby.Member.A}
+    </div>
+    <div
+      className={props.className}
+    >
+      {lobby.Member.B}
+    </div>
+    <div
+      className={props.className}
+    >
+      {lobby.Member.C}
+    </div>
+  </>
+)
+CreateNewAvatarSupportBoardColumnHeader.displayName = 'CreateNewAvatarSupportBoardColumnHeader'
