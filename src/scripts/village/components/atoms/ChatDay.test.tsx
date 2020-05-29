a833a12d4489d21968ef3ee4c65042e4d1cdf116
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-30 06:30:52 +0900

    Update ChatDay

--- src/scripts/village/components/atoms/ChatDay.test.tsx
+++ src/scripts/village/components/atoms/ChatDay.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import ChatDay from './ChatDay'
+import {ChatDay} from './ChatDay'
 import {getMessages} from '../../i18n'
 import {initRenderer} from '../../tools'
 import {village} from '../../types'
