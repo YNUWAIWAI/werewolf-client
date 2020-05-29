3bc120ae8b12f5784ccac6faeea2db9e05c2084e
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-30 01:07:04 +0900

    Update Safe

--- src/scripts/lobby/components/atoms/svg/Safe.tsx
+++ src/scripts/lobby/components/atoms/svg/Safe.tsx
@@ -1,17 +1,16 @@
 import * as React from 'react'
 
-export default function Safe() {
-  return (
-    <svg
-      className="safe"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M0 0h24v24H0z"
-        fill="none"
-      />
-      <path d="M17 4h3v16h-3zM5 14h3v6H5zm6-5h3v11h-3z" />
-    </svg>
-  )
-}
+export const Safe: React.FC<{}> = () => (
+  <svg
+    className="safe"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M0 0h24v24H0z"
+      fill="none"
+    />
+    <path d="M17 4h3v16h-3zM5 14h3v6H5zm6-5h3v11h-3z" />
+  </svg>
+)
+Safe.displayName = 'Safe'
