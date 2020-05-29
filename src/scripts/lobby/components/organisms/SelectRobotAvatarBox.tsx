d9d6e381c2bb81d1a82ecbd1a556cedace54e511
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-30 04:01:15 +0900

    Update SelectRobotAvatarBox

--- src/scripts/lobby/components/organisms/SelectRobotAvatarBox.tsx
+++ src/scripts/lobby/components/organisms/SelectRobotAvatarBox.tsx
@@ -2,29 +2,27 @@ import * as React from 'react'
 import {FormattedMessage} from 'react-intl'
 import Menu from '../../containers/MenuContainer'
 import {MenuItemProps as MenuItem} from './Menu'
-import SelectRobotAvatarTable from '../organisms/SelectRobotAvatarTable'
+import {SelectRobotAvatarTable} from '../organisms/SelectRobotAvatarTable'
 
 interface Props {
   readonly command: MenuItem[]
 }
 
-export const SelectRobotAvatarBox: React.FC<Props> = props => {
-  return (
-    <>
-      <div
-        className="lo--select-avatar--header"
-      >
-        <FormattedMessage
-          id="SelectRobotAvatarBox.header"
-        />
-      </div>
-      <SelectRobotAvatarTable />
-      <Menu
-        className="lo--select-avatar--command"
-        itemClassName="lo--select-avatar--command--item"
-        items={props.command}
+export const SelectRobotAvatarBox: React.FC<Props> = props => (
+  <>
+    <div
+      className="lo--select-avatar--header"
+    >
+      <FormattedMessage
+        id="SelectRobotAvatarBox.header"
       />
-    </>
-  )
-}
+    </div>
+    <SelectRobotAvatarTable />
+    <Menu
+      className="lo--select-avatar--command"
+      itemClassName="lo--select-avatar--command--item"
+      items={props.command}
+    />
+  </>
+)
 SelectRobotAvatarBox.displayName = 'SelectRobotAvatarBox'
