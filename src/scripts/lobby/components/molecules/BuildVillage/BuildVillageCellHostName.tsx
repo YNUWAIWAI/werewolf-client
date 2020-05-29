2304f44b02d6743d6401db25af4618dabd202d4b
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-30 02:34:29 +0900

    Update BuildVillageCellHostName

--- src/scripts/lobby/components/molecules/BuildVillage/BuildVillageCellHostName.tsx
+++ src/scripts/lobby/components/molecules/BuildVillage/BuildVillageCellHostName.tsx
@@ -6,25 +6,24 @@ interface Props {
   readonly value: string
 }
 
-export default function BuildVillageCellHostName(props: Props) {
-  return (
-    <>
-      <div className="lo--village--item--prop host-name">
-        <FormattedMessage
-          id="BuildVillage.label(hostName)"
-        />
-      </div>
-      {
-        props.isFiexdAvatar ?
-          <div className="lo--village--item--val host-name">
-            {props.value}
-          </div> :
-          <div className="lo--village--item--val host-name">
-            <FormattedMessage
-              id="BuildVillage.hostName.anonymous"
-            />
-          </div>
-      }
-    </>
-  )
-}
+export const BuildVillageCellHostName: React.FC<Props> = props => (
+  <>
+    <div className="lo--village--item--prop host-name">
+      <FormattedMessage
+        id="BuildVillage.label(hostName)"
+      />
+    </div>
+    {
+      props.isFiexdAvatar ?
+        <div className="lo--village--item--val host-name">
+          {props.value}
+        </div> :
+        <div className="lo--village--item--val host-name">
+          <FormattedMessage
+            id="BuildVillage.hostName.anonymous"
+          />
+        </div>
+    }
+  </>
+)
+BuildVillageCellHostName.displayName = 'BuildVillageCellHostName'
