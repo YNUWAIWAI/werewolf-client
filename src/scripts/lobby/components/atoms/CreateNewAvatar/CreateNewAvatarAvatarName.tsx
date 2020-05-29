7ef09a01eeb3ee7ff597f8a13a4ea782e8e75cc3
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-29 18:07:25 +0900

    Update CreateNewAvatarAvatarName

--- src/scripts/lobby/components/atoms/CreateNewAvatar/CreateNewAvatarAvatarName.tsx
+++ src/scripts/lobby/components/atoms/CreateNewAvatar/CreateNewAvatarAvatarName.tsx
@@ -1,34 +1,33 @@
 import * as React from 'react'
 import {FormattedMessage} from 'react-intl'
-import TextInput from '../TextInput'
+import {TextInput} from '../TextInput'
 
 interface Props {
   readonly handleChange: (valid: boolean) => (value: string) => void
   readonly navigatable: boolean
 }
 
-export default function CreateNewAvatarAvatarName(props: Props) {
-  return (
-    <>
-      <label
-        className="lo--create-new-avatar--label name"
-        htmlFor="newAvatarName"
-      >
-        <FormattedMessage
-          id="CreateNewAvatar.label(avatarName)"
-        />
-      </label>
-      <TextInput
-        className="lo--create-new-avatar--input name"
-        handleChange={props.handleChange}
-        id="newAvatarName"
-        initialValue=""
-        max={15}
-        min={5}
-        navigatable={props.navigatable}
-        placeholder=""
-        required
+export const CreateNewAvatarAvatarName: React.FC<Props> = props => (
+  <>
+    <label
+      className="lo--create-new-avatar--label name"
+      htmlFor="newAvatarName"
+    >
+      <FormattedMessage
+        id="CreateNewAvatar.label(avatarName)"
       />
-    </>
-  )
-}
+    </label>
+    <TextInput
+      className="lo--create-new-avatar--input name"
+      handleChange={props.handleChange}
+      id="newAvatarName"
+      initialValue=""
+      max={15}
+      min={5}
+      navigatable={props.navigatable}
+      placeholder=""
+      required
+    />
+  </>
+)
+CreateNewAvatarAvatarName.displayName = 'CreateNewAvatarAvatarName'
