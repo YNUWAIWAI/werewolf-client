c04397b9b8e1f6a2a302917c3769996bfbc5435a
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-31 05:47:03 +0900

    Update CreateNewRobotAvatar

--- src/scripts/lobby/components/templates/CreateNewRobotAvatar.tsx
+++ src/scripts/lobby/components/templates/CreateNewRobotAvatar.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
 import {AsideContent} from '../atoms/AsideContent'
 import {CreateNewAvatarHeader} from '../molecules/CreateNewAvatarHeader'
-import CreateNewRobotAvatarSupportBoard from '../../containers/CreateNewRobotAvatarSupportBoardContainer'
-import CreateNewRobotAvatarTable from '../../containers/CreateNewRobotAvatarTableContainer'
+import {CreateNewRobotAvatarSupportBoardContainer} from '../../containers/CreateNewRobotAvatarSupportBoardContainer'
+import {CreateNewRobotAvatarTableContainer} from '../../containers/CreateNewRobotAvatarTableContainer'
 import {Header} from '../atoms/Header'
 import {MainContent} from '../atoms/MainContent'
-import Menu from '../../containers/MenuContainer'
+import {MenuContainer} from '../../containers/MenuContainer'
 import {MenuItemProps as MenuItem} from '../organisms/Menu'
 
 export interface StateProps {
@@ -19,18 +19,18 @@ export const CreateNewRobotAvatar: React.FC<Props> = props => (
     <Header id="Header.selectRobotAvatar" />
     <MainContent>
       <CreateNewAvatarHeader />
-      <CreateNewRobotAvatarTable />
-      <CreateNewRobotAvatarSupportBoard
+      <CreateNewRobotAvatarTableContainer />
+      <CreateNewRobotAvatarSupportBoardContainer
         className="lo--create-new-avatar--support-board"
       />
-      <Menu
+      <MenuContainer
         className="lo--create-new-avatar--command"
         itemClassName="lo--create-new-avatar--command--item"
         items={props.command}
       />
     </MainContent>
     <AsideContent>
-      <Menu
+      <MenuContainer
         className="lo--compact-menu"
         itemClassName="lo--compact-menu--item"
         items={props.menuItems}
