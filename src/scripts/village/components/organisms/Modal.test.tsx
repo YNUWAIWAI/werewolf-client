b6b1d06ecfe913d16f6e3f3b4da5001b3a412650
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-31 03:56:56 +0900

    Update Modal

--- src/scripts/village/components/organisms/Modal.test.tsx
+++ src/scripts/village/components/organisms/Modal.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import Modal from './Modal'
+import {Modal} from './Modal'
 import {Provider} from 'react-redux'
 import fakeStore from '../../containers/fakeStore'
 import {getMessages} from '../../i18n'
