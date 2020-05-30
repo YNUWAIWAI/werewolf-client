741ac319930a6493d41b76a2c6acd84f431ada36
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-31 06:03:45 +0900

    Update SelectHumanAvatarTable

--- src/scripts/lobby/components/organisms/SelectHumanAvatarTable.test.tsx
+++ src/scripts/lobby/components/organisms/SelectHumanAvatarTable.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import {SelectHumanAvatarTable} from './SelectHumanAvatarTable'
-import SelectHumanAvatarTableBody from '../../containers/SelectHumanAvatarTableBodyContainer'
+import {SelectHumanAvatarTableBodyContainer} from '../../containers/SelectHumanAvatarTableBodyContainer'
 import {SelectHumanAvatarTableHeader} from '../molecules/SelectHumanAvatarTableHeader'
 import {shallow} from 'enzyme'
 
@@ -9,6 +9,6 @@ test('render', () => {
     <SelectHumanAvatarTable />
   )
 
-  expect(wrapper.find(SelectHumanAvatarTableBody)).toHaveLength(1)
+  expect(wrapper.find(SelectHumanAvatarTableBodyContainer)).toHaveLength(1)
   expect(wrapper.find(SelectHumanAvatarTableHeader)).toHaveLength(1)
 })
--- src/scripts/lobby/components/organisms/SelectHumanAvatarTable.tsx
+++ src/scripts/lobby/components/organisms/SelectHumanAvatarTable.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
-import SelectHumanAvatarTableBody from '../../containers/SelectHumanAvatarTableBodyContainer'
+import {SelectHumanAvatarTableBodyContainer} from '../../containers/SelectHumanAvatarTableBodyContainer'
 import {SelectHumanAvatarTableHeader} from '../molecules/SelectHumanAvatarTableHeader'
 
 export const SelectHumanAvatarTable: React.FC<{}> = () => (
   <div className="lo--select-avatar--table human">
     <SelectHumanAvatarTableHeader />
-    <SelectHumanAvatarTableBody />
+    <SelectHumanAvatarTableBodyContainer />
   </div>
 )
 SelectHumanAvatarTable.displayName = 'SelectHumanAvatarTable'
