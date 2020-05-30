1d99561609eea13223ec6e52bba538f0f0f60163
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-31 05:20:24 +0900

    Update App

--- src/scripts/village/App.tsx
+++ src/scripts/village/App.tsx
@@ -1,32 +1,30 @@
 import * as React from 'react'
-import Chat from './containers/ChatContainer'
-import Command from './containers/CommandContainer'
-import HideButton from './containers/HideButtonContainer'
-import Info from './components/organisms/Info'
-import IntlProvider from './containers/IntlProviderContainer'
-import Modal from './containers/ModalContainer'
-import Obfucator from './containers/ObfucatorContainer'
-import Prediction from './containers/PredictionContainer'
-import Result from './containers/ResultContainer'
-import Theme from './containers/ThemeContainer'
+import {ChatContainer as Chat} from './containers/ChatContainer'
+import {CommandContainer as Command} from './containers/CommandContainer'
+import {HideButtonContainer as HideButton} from './containers/HideButtonContainer'
+import {Info} from './components/organisms/Info'
+import {IntlProviderContainer as IntlProvider} from './containers/IntlProviderContainer'
+import {ModalContainer as Modal} from './containers/ModalContainer'
+import {ObfucatorContainer as Obfucator} from './containers/ObfucatorContainer'
+import {PredictionContainer as Prediction} from './containers/PredictionContainer'
+import {ResultContainer as Result} from './containers/ResultContainer'
+import {ThemeContainer as Theme} from './containers/ThemeContainer'
 
-export const App: React.FC<{}> = () => {
-  return (
-    <IntlProvider>
-      <Theme>
-        <div className="vi--grid">
-          <Info />
-          <Chat />
-          <Prediction />
-          <Command />
-          <HideButton />
-          <Modal />
-          <Result />
-          <Obfucator />
-        </div>
-        <div className="vi--background" />
-      </Theme>
-    </IntlProvider>
-  )
-}
+export const App: React.FC<{}> = () => (
+  <IntlProvider>
+    <Theme>
+      <div className="vi--grid">
+        <Info />
+        <Chat />
+        <Prediction />
+        <Command />
+        <HideButton />
+        <Modal />
+        <Result />
+        <Obfucator />
+      </div>
+      <div className="vi--background" />
+    </Theme>
+  </IntlProvider>
+)
 App.displayName = 'App'
