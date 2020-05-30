3ec8f986b3f8eeb6544d98a7ab4430c425057261
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-31 05:44:08 +0900

    Update CreateNewHumanAvatar

--- src/scripts/lobby/components/templates/CreateNewHumanAvatar.tsx
+++ src/scripts/lobby/components/templates/CreateNewHumanAvatar.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
 import {AsideContent} from '../atoms/AsideContent'
 import {CreateNewAvatarHeader} from '../molecules/CreateNewAvatarHeader'
-import CreateNewHumanAvatarTable from '../../containers/CreateNewHumanAvatarTableContainer'
+import {CreateNewHumanAvatarTableContainer} from '../../containers/CreateNewHumanAvatarTableContainer'
 import {Header} from '../atoms/Header'
 import {MainContent} from '../atoms/MainContent'
-import Menu from '../../containers/MenuContainer'
+import {MenuContainer} from '../../containers/MenuContainer'
 import {MenuItemProps as MenuItem} from '../organisms/Menu'
 
 export interface StateProps {
@@ -18,15 +18,15 @@ export const CreateNewHumanAvatar: React.FC<Props> = props => (
     <Header id="Header.selectHumanAvatar" />
     <MainContent>
       <CreateNewAvatarHeader />
-      <CreateNewHumanAvatarTable />
-      <Menu
+      <CreateNewHumanAvatarTableContainer />
+      <MenuContainer
         className="lo--create-new-avatar--command"
         itemClassName="lo--create-new-avatar--command--item"
         items={props.command}
       />
     </MainContent>
     <AsideContent>
-      <Menu
+      <MenuContainer
         className="lo--compact-menu"
         itemClassName="lo--compact-menu--item"
         items={props.menuItems}
