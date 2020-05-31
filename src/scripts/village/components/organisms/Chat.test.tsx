5decae7b2db4add446ab7718574bb557315b986e
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-31 22:30:28 +0900

    Update Chat

--- src/scripts/village/components/organisms/Chat.test.tsx
+++ src/scripts/village/components/organisms/Chat.test.tsx
@@ -4,185 +4,33 @@ import {
   Props
 } from './Chat'
 import {ImagePath} from '../../constants/ImagePath'
-import {shallow} from 'enzyme'
+import {getMessages} from '../../i18n'
+import {initRenderer} from '../../tools'
 import {village} from '../../types'
 
-describe('<Chat />', () => {
-  describe('render', () => {
-    test('0 item', () => {
-      const allIds: Props['allIds'] = []
-      const byId: Props['byId'] = {}
-      const handleStarInner = jest.fn()
-      const handleStar = jest.fn(() => handleStarInner)
-      const wrapper = shallow(
-        <Chat
-          allIds={allIds}
-          byId={byId}
-          expand={false}
-          handleStar={handleStar}
-        />
-      )
+const {mountWithIntl} = initRenderer(village.Language.en, getMessages(village.Language.en))
 
-      expect(wrapper.find('ChatItem')).toHaveLength(0)
-      expect(handleStarInner).toHaveBeenCalledTimes(0)
-      expect(handleStar).toHaveBeenCalledTimes(0)
-    })
-    test('1 item', () => {
-      const allIds: Props['allIds'] = ['chat0']
-      const byId: Props['byId'] = {
-        'chat0': {
-          id: 1,
-          image: ImagePath.Character.a,
-          initial: 'A',
-          intensionalDisclosureRange: village.Channel.anonymousAudience,
-          isMarked: false,
-          isMine: true,
-          name: 'Adil',
-          phaseStartTime: '2018-01-01T00:00:00.000Z',
-          phaseTimeLimit: 50,
-          serverTimestamp: '2018-01-01T00:00:10.000Z',
-          text: 'text',
-          type: village.ChatItemType.item
-        }
-      }
-      const handleStarInner = jest.fn()
-      const handleStar = jest.fn(() => handleStarInner)
-      const wrapper = shallow(
-        <Chat
-          allIds={allIds}
-          byId={byId}
-          expand={false}
-          handleStar={handleStar}
-        />
-      )
+describe('render', () => {
+  test('0 item', () => {
+    const allIds: Props['allIds'] = []
+    const byId: Props['byId'] = {}
+    const handleStarInner = jest.fn()
+    const handleStar = jest.fn(() => handleStarInner)
+    const wrapper = mountWithIntl(
+      <Chat
+        allIds={allIds}
+        byId={byId}
+        expand={false}
+        handleStar={handleStar}
+        navigatable
+      />
+    )
 
-      expect(wrapper.find('ChatItem')).toHaveLength(1)
-      expect(handleStarInner).toHaveBeenCalledTimes(0)
-      expect(handleStar).toHaveBeenCalledTimes(1)
-      expect(handleStar).toHaveBeenCalledWith('chat0')
-    })
-    test('2 items', () => {
-      const allIds: Props['allIds'] = ['chat1', 'chat0']
-      const byId: Props['byId'] = {
-        'chat0': {
-          id: 1,
-          image: ImagePath.Character.a,
-          initial: 'A',
-          intensionalDisclosureRange: village.Channel.anonymousAudience,
-          isMarked: false,
-          isMine: true,
-          name: 'Adil',
-          phaseStartTime: '2018-01-01T00:00:00.000Z',
-          phaseTimeLimit: 50,
-          serverTimestamp: '2018-01-01T00:00:10.000Z',
-          text: 'text',
-          type: village.ChatItemType.item
-        },
-        'chat1': {
-          id: 2,
-          image: ImagePath.Character.a,
-          initial: 'A',
-          intensionalDisclosureRange: village.Channel.anonymousAudience,
-          isMarked: false,
-          isMine: true,
-          name: 'Adil',
-          phaseStartTime: '2018-01-01T00:00:00.000Z',
-          phaseTimeLimit: 50,
-          serverTimestamp: '2018-01-01T00:00:10.000Z',
-          text: 'text',
-          type: village.ChatItemType.item
-        }
-      }
-      const handleStarInner = jest.fn()
-      const handleStar = jest.fn(() => handleStarInner)
-      const wrapper = shallow(
-        <Chat
-          allIds={allIds}
-          byId={byId}
-          expand={false}
-          handleStar={handleStar}
-        />
-      )
-
-      expect(wrapper.find('ChatItem')).toHaveLength(2)
-      expect(wrapper.find('ChatItem').map(n => n.prop('id'))).toStrictEqual([2, 1])
-      expect(handleStarInner).toHaveBeenCalledTimes(0)
-      expect(handleStar).toHaveBeenCalledTimes(2)
-      expect(handleStar).toHaveBeenCalledWith('chat0')
-      expect(handleStar).toHaveBeenCalledWith('chat1')
-    })
-    test('3 items with 1 delimeter', () => {
-      const allIds: Props['allIds'] = ['delimeter0', 'chat2', 'chat1', 'chat0']
-      const byId: Props['byId'] = {
-        'chat0': {
-          id: 1,
-          image: ImagePath.Character.a,
-          initial: 'A',
-          intensionalDisclosureRange: village.Channel.anonymousAudience,
-          isMarked: false,
-          isMine: true,
-          name: 'Adil',
-          phaseStartTime: '2018-01-01T00:00:00.000Z',
-          phaseTimeLimit: 50,
-          serverTimestamp: '2018-01-01T00:00:10.000Z',
-          text: 'text',
-          type: village.ChatItemType.item
-        },
-        'chat1': {
-          id: 2,
-          image: ImagePath.Character.a,
-          initial: 'A',
-          intensionalDisclosureRange: village.Channel.anonymousAudience,
-          isMarked: false,
-          isMine: true,
-          name: 'Adil',
-          phaseStartTime: '2018-01-01T00:00:00.000Z',
-          phaseTimeLimit: 50,
-          serverTimestamp: '2018-01-01T00:00:10.000Z',
-          text: 'text',
-          type: village.ChatItemType.item
-        },
-        'chat2': {
-          id: 3,
-          image: ImagePath.Character.a,
-          initial: 'A',
-          intensionalDisclosureRange: village.Channel.anonymousAudience,
-          isMarked: false,
-          isMine: true,
-          name: 'Adil',
-          phaseStartTime: '2018-01-01T00:00:00.000Z',
-          phaseTimeLimit: 50,
-          serverTimestamp: '2018-01-01T00:00:10.000Z',
-          text: 'text',
-          type: village.ChatItemType.item
-        },
-        'delimeter0': {
-          day: 1,
-          type: village.ChatItemType.delimeter
-        }
-      }
-      const handleStarInner = jest.fn()
-      const handleStar = jest.fn(() => handleStarInner)
-      const wrapper = shallow(
-        <Chat
-          allIds={allIds}
-          byId={byId}
-          expand={false}
-          handleStar={handleStar}
-        />
-      )
-
-      expect(wrapper.find('ChatItem')).toHaveLength(3)
-      expect(wrapper.find('ChatDelimeter')).toHaveLength(1)
-      expect(wrapper.find('ChatItem').map(n => n.prop('id'))).toStrictEqual([3, 2, 1])
-      expect(handleStarInner).toHaveBeenCalledTimes(0)
-      expect(handleStar).toHaveBeenCalledTimes(3)
-      expect(handleStar).toHaveBeenCalledWith('chat0')
-      expect(handleStar).toHaveBeenCalledWith('chat1')
-      expect(handleStar).toHaveBeenCalledWith('chat2')
-    })
+    expect(wrapper.html()).toMatchSnapshot()
+    expect(handleStarInner).toHaveBeenCalledTimes(0)
+    expect(handleStar).toHaveBeenCalledTimes(0)
   })
-  test('handleScroll', () => {
+  test('1 item', () => {
     const allIds: Props['allIds'] = ['chat0']
     const byId: Props['byId'] = {
       'chat0': {
@@ -202,18 +50,181 @@ describe('<Chat />', () => {
     }
     const handleStarInner = jest.fn()
     const handleStar = jest.fn(() => handleStarInner)
-    const wrapper = shallow(
+    const wrapper = mountWithIntl(
       <Chat
         allIds={allIds}
         byId={byId}
         expand={false}
         handleStar={handleStar}
+        navigatable
       />
     )
 
-    wrapper.simulate('scroll')
-    expect(wrapper.state()).toStrictEqual({
-      atBottom: true
-    })
+    expect(wrapper.html()).toMatchSnapshot()
+    expect(handleStarInner).toHaveBeenCalledTimes(0)
+    expect(handleStar).toHaveBeenCalledTimes(1)
+    expect(handleStar).toHaveBeenCalledWith('chat0')
   })
+  test('2 items', () => {
+    const allIds: Props['allIds'] = ['chat1', 'chat0']
+    const byId: Props['byId'] = {
+      'chat0': {
+        id: 1,
+        image: ImagePath.Character.a,
+        initial: 'A',
+        intensionalDisclosureRange: village.Channel.anonymousAudience,
+        isMarked: false,
+        isMine: true,
+        name: 'Adil',
+        phaseStartTime: '2018-01-01T00:00:00.000Z',
+        phaseTimeLimit: 50,
+        serverTimestamp: '2018-01-01T00:00:10.000Z',
+        text: 'text',
+        type: village.ChatItemType.item
+      },
+      'chat1': {
+        id: 2,
+        image: ImagePath.Character.a,
+        initial: 'A',
+        intensionalDisclosureRange: village.Channel.anonymousAudience,
+        isMarked: false,
+        isMine: true,
+        name: 'Adil',
+        phaseStartTime: '2018-01-01T00:00:00.000Z',
+        phaseTimeLimit: 50,
+        serverTimestamp: '2018-01-01T00:00:10.000Z',
+        text: 'text',
+        type: village.ChatItemType.item
+      }
+    }
+    const handleStarInner = jest.fn()
+    const handleStar = jest.fn(() => handleStarInner)
+    const wrapper = mountWithIntl(
+      <Chat
+        allIds={allIds}
+        byId={byId}
+        expand={false}
+        handleStar={handleStar}
+        navigatable
+      />
+    )
+
+    expect(wrapper.find('ChatItem')).toHaveLength(2)
+    expect(wrapper.find('ChatItem').map(n => n.prop('id'))).toStrictEqual([2, 1])
+    expect(wrapper.html()).toMatchSnapshot()
+    expect(handleStarInner).toHaveBeenCalledTimes(0)
+    expect(handleStar).toHaveBeenCalledTimes(2)
+    expect(handleStar).toHaveBeenCalledWith('chat0')
+    expect(handleStar).toHaveBeenCalledWith('chat1')
+  })
+  test('3 items with 1 delimeter', () => {
+    const allIds: Props['allIds'] = ['delimeter0', 'chat2', 'chat1', 'chat0']
+    const byId: Props['byId'] = {
+      'chat0': {
+        id: 1,
+        image: ImagePath.Character.a,
+        initial: 'A',
+        intensionalDisclosureRange: village.Channel.anonymousAudience,
+        isMarked: false,
+        isMine: true,
+        name: 'Adil',
+        phaseStartTime: '2018-01-01T00:00:00.000Z',
+        phaseTimeLimit: 50,
+        serverTimestamp: '2018-01-01T00:00:10.000Z',
+        text: 'text',
+        type: village.ChatItemType.item
+      },
+      'chat1': {
+        id: 2,
+        image: ImagePath.Character.a,
+        initial: 'A',
+        intensionalDisclosureRange: village.Channel.anonymousAudience,
+        isMarked: false,
+        isMine: true,
+        name: 'Adil',
+        phaseStartTime: '2018-01-01T00:00:00.000Z',
+        phaseTimeLimit: 50,
+        serverTimestamp: '2018-01-01T00:00:10.000Z',
+        text: 'text',
+        type: village.ChatItemType.item
+      },
+      'chat2': {
+        id: 3,
+        image: ImagePath.Character.a,
+        initial: 'A',
+        intensionalDisclosureRange: village.Channel.anonymousAudience,
+        isMarked: false,
+        isMine: true,
+        name: 'Adil',
+        phaseStartTime: '2018-01-01T00:00:00.000Z',
+        phaseTimeLimit: 50,
+        serverTimestamp: '2018-01-01T00:00:10.000Z',
+        text: 'text',
+        type: village.ChatItemType.item
+      },
+      'delimeter0': {
+        day: 1,
+        type: village.ChatItemType.delimeter
+      }
+    }
+    const handleStarInner = jest.fn()
+    const handleStar = jest.fn(() => handleStarInner)
+    const wrapper = mountWithIntl(
+      <Chat
+        allIds={allIds}
+        byId={byId}
+        expand={false}
+        handleStar={handleStar}
+        navigatable
+      />
+    )
+
+    expect(wrapper.find('ChatItem')).toHaveLength(3)
+    expect(wrapper.find('ChatDelimeter')).toHaveLength(1)
+    expect(wrapper.find('ChatItem').map(n => n.prop('id'))).toStrictEqual([3, 2, 1])
+    expect(wrapper.html()).toMatchSnapshot()
+    expect(handleStarInner).toHaveBeenCalledTimes(0)
+    expect(handleStar).toHaveBeenCalledTimes(3)
+    expect(handleStar).toHaveBeenCalledWith('chat0')
+    expect(handleStar).toHaveBeenCalledWith('chat1')
+    expect(handleStar).toHaveBeenCalledWith('chat2')
+  })
+})
+test('handleScroll', () => {
+  const allIds: Props['allIds'] = ['chat0']
+  const byId: Props['byId'] = {
+    'chat0': {
+      id: 1,
+      image: ImagePath.Character.a,
+      initial: 'A',
+      intensionalDisclosureRange: village.Channel.anonymousAudience,
+      isMarked: false,
+      isMine: true,
+      name: 'Adil',
+      phaseStartTime: '2018-01-01T00:00:00.000Z',
+      phaseTimeLimit: 50,
+      serverTimestamp: '2018-01-01T00:00:10.000Z',
+      text: 'text',
+      type: village.ChatItemType.item
+    }
+  }
+  const handleStarInner = jest.fn()
+  const handleStar = jest.fn(() => handleStarInner)
+  const wrapper = mountWithIntl(
+    <Chat
+      allIds={allIds}
+      byId={byId}
+      expand={false}
+      handleStar={handleStar}
+      navigatable
+    />
+  )
+  const spy = jest.spyOn(
+    wrapper.find('.vi--chat').getDOMNode(),
+    'scrollTop',
+    'set'
+  )
+
+  wrapper.simulate('scroll')
+  expect(spy).toHaveBeenCalled()
 })
