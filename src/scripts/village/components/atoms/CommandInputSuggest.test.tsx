381f8938586c6f6da38e858cac71eea7911a62dd
Yudai Udagawa <nimius.rd@gmail.com>
2020-05-31 18:31:27 +0900

    Update test

--- src/scripts/village/components/atoms/CommandInputSuggest.test.tsx
+++ src/scripts/village/components/atoms/CommandInputSuggest.test.tsx
@@ -3,223 +3,8 @@ import {CommandInputSuggest} from './CommandInputSuggest'
 import {mount} from 'enzyme'
 import {village} from '../../types'
 
-describe('<CommandInputSuggest />', () => {
-  describe('render', () => {
-    test('data.length > 0', () => {
-      const data = [
-        {
-          id: 'Alvin',
-          name: {
-            'en': 'Alvin',
-            'ja': 'アルビン'
-          }
-        },
-        {
-          id: 'Catalina',
-          name: {
-            'en': 'Catalina',
-            'ja': 'カタリナ'
-          }
-        }
-      ]
-      const handleSuggestClick = jest.fn()
-      const wrapper = mount(
-        <CommandInputSuggest
-          data={data}
-          handleSuggestClick={handleSuggestClick}
-          language={village.Language.en}
-          left={0}
-          selected={0}
-          suggestable
-          top={0}
-        />
-      )
-
-      expect(wrapper.isEmptyRender()).toBe(false)
-      expect(handleSuggestClick).toHaveBeenCalledTimes(0)
-    })
-    test('data.length <= 0', () => {
-      const handleSuggestClick = jest.fn()
-      const wrapper = mount(
-        <CommandInputSuggest
-          data={[]}
-          handleSuggestClick={handleSuggestClick}
-          language={village.Language.en}
-          left={0}
-          selected={0}
-          suggestable
-          top={0}
-        />
-      )
-
-      expect(wrapper.isEmptyRender()).toBe(true)
-      expect(handleSuggestClick).toHaveBeenCalledTimes(0)
-    })
-    test('suggestable={false}', () => {
-      const data = [
-        {
-          id: 'Alvin',
-          name: {
-            'en': 'Alvin',
-            'ja': 'アルビン'
-          }
-        },
-        {
-          id: 'Catalina',
-          name: {
-            'en': 'Catalina',
-            'ja': 'カタリナ'
-          }
-        }
-      ]
-      const handleSuggestClick = jest.fn()
-      const wrapper = mount(
-        <CommandInputSuggest
-          data={data}
-          handleSuggestClick={handleSuggestClick}
-          language={village.Language.en}
-          left={0}
-          selected={0}
-          suggestable={false}
-          top={0}
-        />
-      )
-
-      expect(wrapper.isEmptyRender()).toBe(true)
-      expect(handleSuggestClick).toHaveBeenCalledTimes(0)
-    })
-  })
-  describe('componentDidUpdate', () => {
-    test('data.length > 0', () => {
-      const data = [
-        {
-          id: 'Alvin',
-          name: {
-            'en': 'Alvin',
-            'ja': 'アルビン'
-          }
-        },
-        {
-          id: 'Catalina',
-          name: {
-            'en': 'Catalina',
-            'ja': 'カタリナ'
-          }
-        }
-      ]
-      const handleSuggestClick = jest.fn()
-      const wrapper = mount(
-        <CommandInputSuggest
-          data={data}
-          handleSuggestClick={handleSuggestClick}
-          language={village.Language.en}
-          left={0}
-          selected={0}
-          suggestable
-          top={0}
-        />
-      )
-      const spy = jest.spyOn(CommandInputSuggest.prototype, 'componentDidUpdate')
-
-      wrapper.setProps({
-        selected: 1
-      })
-      expect(spy).toHaveBeenCalled()
-    })
-    test('data.length <= 0', () => {
-      const handleSuggestClick = jest.fn()
-      const wrapper = mount(
-        <CommandInputSuggest
-          data={[]}
-          handleSuggestClick={handleSuggestClick}
-          language={village.Language.en}
-          left={0}
-          selected={0}
-          suggestable
-          top={0}
-        />
-      )
-      const spy = jest.spyOn(CommandInputSuggest.prototype, 'componentDidUpdate')
-
-      wrapper.setProps({
-        data: []
-      })
-      expect(spy).toHaveBeenCalled()
-    })
-    test('suggestable={false}', () => {
-      const data = [
-        {
-          id: 'Alvin',
-          name: {
-            'en': 'Alvin',
-            'ja': 'アルビン'
-          }
-        },
-        {
-          id: 'Catalina',
-          name: {
-            'en': 'Catalina',
-            'ja': 'カタリナ'
-          }
-        }
-      ]
-      const handleSuggestClick = jest.fn()
-      const wrapper = mount(
-        <CommandInputSuggest
-          data={data}
-          handleSuggestClick={handleSuggestClick}
-          language={village.Language.en}
-          left={0}
-          selected={0}
-          suggestable
-          top={0}
-        />
-      )
-      const spy = jest.spyOn(CommandInputSuggest.prototype, 'componentDidUpdate')
-
-      wrapper.setProps({
-        suggestable: false
-      })
-      expect(spy).toHaveBeenCalled()
-    })
-    test('typeof itemElem === "undefined"', () => {
-      const data = [
-        {
-          id: 'Alvin',
-          name: {
-            'en': 'Alvin',
-            'ja': 'アルビン'
-          }
-        },
-        {
-          id: 'Catalina',
-          name: {
-            'en': 'Catalina',
-            'ja': 'カタリナ'
-          }
-        }
-      ]
-      const handleSuggestClick = jest.fn()
-      const wrapper = mount(
-        <CommandInputSuggest
-          data={data}
-          handleSuggestClick={handleSuggestClick}
-          language={village.Language.en}
-          left={0}
-          selected={2}
-          suggestable
-          top={0}
-        />
-      )
-      const spy = jest.spyOn(CommandInputSuggest.prototype, 'componentDidUpdate')
-
-      wrapper.setProps({
-        suggestable: true
-      })
-      expect(spy).toHaveBeenCalled()
-    })
-  })
-  test('handleSuggestClick', () => {
+describe('render', () => {
+  test('data.length > 0', () => {
     const data = [
       {
         id: 'Alvin',
@@ -249,8 +34,133 @@ describe('<CommandInputSuggest />', () => {
       />
     )
 
-    wrapper.find('.vi--command--input--suggest--item.selected').simulate('click')
-    expect(handleSuggestClick).toHaveBeenCalledTimes(1)
-    expect(handleSuggestClick).toHaveBeenCalledWith('Alvin')
+    expect(wrapper.isEmptyRender()).toBe(false)
+    expect(handleSuggestClick).toHaveBeenCalledTimes(0)
   })
+  test('data.length <= 0', () => {
+    const handleSuggestClick = jest.fn()
+    const wrapper = mount(
+      <CommandInputSuggest
+        data={[]}
+        handleSuggestClick={handleSuggestClick}
+        language={village.Language.en}
+        left={0}
+        selected={0}
+        suggestable
+        top={0}
+      />
+    )
+
+    expect(wrapper.isEmptyRender()).toBe(true)
+    expect(handleSuggestClick).toHaveBeenCalledTimes(0)
+  })
+  test('suggestable={false}', () => {
+    const data = [
+      {
+        id: 'Alvin',
+        name: {
+          'en': 'Alvin',
+          'ja': 'アルビン'
+        }
+      },
+      {
+        id: 'Catalina',
+        name: {
+          'en': 'Catalina',
+          'ja': 'カタリナ'
+        }
+      }
+    ]
+    const handleSuggestClick = jest.fn()
+    const wrapper = mount(
+      <CommandInputSuggest
+        data={data}
+        handleSuggestClick={handleSuggestClick}
+        language={village.Language.en}
+        left={0}
+        selected={0}
+        suggestable={false}
+        top={0}
+      />
+    )
+
+    expect(wrapper.isEmptyRender()).toBe(true)
+    expect(handleSuggestClick).toHaveBeenCalledTimes(0)
+  })
+})
+describe('componentDidUpdate', () => {
+  test('data.length > 0', () => {
+    const data = [
+      {
+        id: 'Alvin',
+        name: {
+          'en': 'Alvin',
+          'ja': 'アルビン'
+        }
+      },
+      {
+        id: 'Catalina',
+        name: {
+          'en': 'Catalina',
+          'ja': 'カタリナ'
+        }
+      }
+    ]
+    const handleSuggestClick = jest.fn()
+    const wrapper = mount(
+      <CommandInputSuggest
+        data={data}
+        handleSuggestClick={handleSuggestClick}
+        language={village.Language.en}
+        left={0}
+        selected={0}
+        suggestable
+        top={0}
+      />
+    )
+    const spy = jest.spyOn(
+      wrapper.find('.vi--command--input--suggest--list').getDOMNode(),
+      'scrollTop',
+      'set'
+    )
+
+    wrapper.setProps({
+      selected: 1
+    })
+    expect(spy).toHaveBeenCalled()
+  })
+})
+test('handleSuggestClick', () => {
+  const data = [
+    {
+      id: 'Alvin',
+      name: {
+        'en': 'Alvin',
+        'ja': 'アルビン'
+      }
+    },
+    {
+      id: 'Catalina',
+      name: {
+        'en': 'Catalina',
+        'ja': 'カタリナ'
+      }
+    }
+  ]
+  const handleSuggestClick = jest.fn()
+  const wrapper = mount(
+    <CommandInputSuggest
+      data={data}
+      handleSuggestClick={handleSuggestClick}
+      language={village.Language.en}
+      left={0}
+      selected={0}
+      suggestable
+      top={0}
+    />
+  )
+
+  wrapper.find('.vi--command--input--suggest--item.selected').simulate('click')
+  expect(handleSuggestClick).toHaveBeenCalledTimes(1)
+  expect(handleSuggestClick).toHaveBeenCalledWith('Alvin')
 })
