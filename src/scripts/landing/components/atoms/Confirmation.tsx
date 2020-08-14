54db624a466ffcc2680bc9a763d8fdcded078673
Yudai Udagawa <nimius.rd@gmail.com>
2020-08-14 10:06:14 +0900

    Update Confirmation

--- src/scripts/landing/components/atoms/Confirmation.tsx
+++ src/scripts/landing/components/atoms/Confirmation.tsx
@@ -1,23 +1,34 @@
 import * as React from 'react'
+import {Checkbox} from '../../../lobby/components/atoms/Checkbox'
 import {FormattedMessage} from 'react-intl'
 import {Link} from './Link'
 
-export const Confirmation: React.FC<{}> = () => (
-  <p className="la--form--confirmation">
-    <FormattedMessage
-      id="signup-confirmation"
-      values={{
-        // eslint-disable-next-line
-        a: (... chunks: React.ReactNode[]) => (
-          <Link
-            className="la--form--link"
-            href="https://werewolfguide.netlify.app/docs/en/privacy"
-          >
-            {chunks}
-          </Link>
-        )
-      }}
-    />
-  </p>
-)
+export const Confirmation: React.FC<{}> = () => {
+  const [checked, setChecked] = React.useState(false)
+
+  return (
+    <p className="la--form--confirmation">
+      <Checkbox
+        checked={checked}
+        className="la--checkbox"
+        handleChange={setChecked}
+        navigatable
+      />
+      <FormattedMessage
+        id="signup-confirmation"
+        values={{
+          // eslint-disable-next-line
+          a: (... chunks: React.ReactNode[]) => (
+            <Link
+              className="la--form--link"
+              href="https://werewolfguide.netlify.app/docs/en/privacy"
+            >
+              {chunks}
+            </Link>
+          )
+        }}
+      />
+    </p>
+  )
+}
 Confirmation.displayName = 'Confirmation'
